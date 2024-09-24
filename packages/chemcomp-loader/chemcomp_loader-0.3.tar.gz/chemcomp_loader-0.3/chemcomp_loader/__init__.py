4e8052eed155df73e0cf7f7489d21e4bf8346f16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 14:03:16 +0000

    Add package chemcomp-loader/0.3/chemcomp_loader-0.3.tar.gz

---
+++ packages/chemcomp-loader/chemcomp_loader-0.3.tar.gz/chemcomp_loader-0.3/chemcomp_loader/__init__.py
@@ -0,0 +1,5 @@
+from chemcomp_loader import Disc_class
+from chemcomp_loader import help
+from chemcomp_loader import Disc_gas
+from chemcomp_loader import Disc_dust
+from chemcomp_loader import Disc_chemistry
