92c8a416af137c3b85581c962154e5b05db83c9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 14:03:41 +0000

    Add package rorf/0.1.2/rorf-0.1.2.tar.gz

---
+++ packages/rorf/rorf-0.1.2.tar.gz/rorf-0.1.2/rorf/config.py
@@ -0,0 +1,48 @@
+ROUTELLM_MODELS = [
+    "gpt-4-1106-preview",
+    "mixtral-8x7b-instruct-v0.1"
+]
+
+GPT_MODELS = [
+    'openai/gpt-4o-2024-05-13',
+    'openai/gpt-4o-mini-2024-07-18',
+]
+
+LLAMA_MODELS = [
+    'fireworksai/llama-3.1-405b-instruct',
+    'fireworksai/llama-3.1-70b-instruct',
+]
+
+SONNET_GPT = [
+    'anthropic/claude-3-5-sonnet-20240620',
+    'openai/gpt-4o-2024-05-13',
+]
+
+LLAMA_SONNET = [
+    'fireworksai/llama-3.1-405b-instruct',
+    'anthropic/claude-3-5-sonnet-20240620',
+]
+
+SONNET_LLAMA = [
+    'anthropic/claude-3-5-sonnet-20240620',
+    'fireworksai/llama-3.1-405b-instruct',
+]
+
+LLAMA_GPT = [
+    'fireworksai/llama-3.1-405b-instruct',
+    'openai/gpt-4o-2024-05-13',
+]
+
+LLAMA_MISTRAL = [
+    'fireworksai/llama-3.1-405b-instruct',
+    'mistral/mistral-large-2407',
+]
+
+RENAMES = {
+    "openai/gpt-4o-2024-05-13": "gpt4o",
+    "openai/gpt-4o-mini-2024-07-18": "gpt4omini",
+    "anthropic/claude-3-5-sonnet-20240620": "claude35sonnet",
+    "fireworksai/llama-3.1-405b-instruct": "llama31405b",
+    "fireworksai/llama-3.1-70b-instruct": "llama3170b",
+    "mistral/mistral-large-2407": "mistrallarge",
+}
