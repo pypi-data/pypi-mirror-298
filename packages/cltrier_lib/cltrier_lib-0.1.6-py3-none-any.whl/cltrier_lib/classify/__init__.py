bb77b410b92c00fd48344fe84cadbe78122406f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 14:03:48 +0000

    Add package cltrier_lib/0.1.6/cltrier_lib-0.1.6-py3-none-any.whl

---
+++ packages/cltrier_lib/cltrier_lib-0.1.6-py3-none-any.whl/cltrier_lib/classify/__init__.py
@@ -0,0 +1,32 @@
+import logging
+import typing
+
+import pydantic
+import requests
+
+
+class Pipeline(pydantic.BaseModel):
+    endpoint: str = "https://metrics.twon.uni-trier.de/"
+
+    def __call__(
+        self, samples: typing.List[str], threshold: float = 0.5
+    ) -> typing.List[typing.Dict[str, str | float]]:
+        response: typing.List[typing.Dict[str, str | float]] = []
+
+        try:
+            response = requests.post(
+                self.endpoint,
+                json={
+                    "samples": samples,
+                    "threshold": threshold,
+                },
+            ).json()["predictions"]
+
+        except Exception as e:
+            logging.warning(e)
+
+        finally:
+            return response
+
+
+__all__ = ["Pipeline"]
