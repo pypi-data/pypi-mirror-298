21bbb85b1d8ad265434b3fbf7a75dfe5535a9bd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 20:04:51 +0000

    Add package parllama/0.3.10/parllama-0.3.10-py3-none-any.whl

---
+++ packages/parllama/parllama-0.3.10-py3-none-any.whl/parllama/models/jobs.py
@@ -0,0 +1,41 @@
+"""Data classes for job queue"""
+
+from __future__ import annotations
+
+from uuid import UUID
+from uuid import uuid4
+
+from pydantic import BaseModel
+from pydantic import Field
+
+
+class QueueJob(BaseModel):
+    """Base job"""
+
+    id: UUID = Field(default_factory=uuid4)
+    modelName: str
+
+
+class PushModelJob(QueueJob):
+    """Push model job"""
+
+
+class PullModelJob(QueueJob):
+    """Pull model job"""
+
+
+class DeleteModelJob(QueueJob):
+    """Delete model job"""
+
+
+class CopyModelJob(QueueJob):
+    """Copy model job"""
+
+    dstModelName: str
+
+
+class CreateModelJob(QueueJob):
+    """Create model job"""
+
+    modelCode: str
+    quantizationLevel: str | None
