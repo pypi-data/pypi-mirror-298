990df818f5ff290f9e68da1ca90f9cb54fd847c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 14:03:33 +0000

    Add package ipecharts/1.0.7/ipecharts-1.0.7-py3-none-any.whl

---
+++ packages/ipecharts/ipecharts-1.0.7-py3-none-any.whl/ipecharts/echarts.py
@@ -0,0 +1,35 @@
+from ipywidgets import DOMWidget, widget_serialization
+from traitlets import Dict, Instance, Unicode
+
+from .option.option import Option
+from ._frontend import module_name, module_version
+
+
+class EChartsWidget(DOMWidget):
+    _model_name = Unicode("EChartsWidgetModel").tag(sync=True)
+    _model_module = Unicode(module_name).tag(sync=True)
+    _model_module_version = Unicode(module_version).tag(sync=True)
+    _view_name = Unicode("EChartsWidgetView").tag(sync=True)
+    _view_module = Unicode(module_name).tag(sync=True)
+    _view_module_version = Unicode(module_version).tag(sync=True)
+
+    option = Instance(Option, kw={}, args=()).tag(sync=True, **widget_serialization)
+    style = Dict(
+        {},
+        help="Style configuration",
+    ).tag(sync=True)
+
+
+class EChartsRawWidget(DOMWidget):
+    _model_name = Unicode("EChartsRawWidgetModel").tag(sync=True)
+    _model_module = Unicode(module_name).tag(sync=True)
+    _model_module_version = Unicode(module_version).tag(sync=True)
+    _view_name = Unicode("EChartsRawWidgetView").tag(sync=True)
+    _view_module = Unicode(module_name).tag(sync=True)
+    _view_module_version = Unicode(module_version).tag(sync=True)
+
+    option = Dict(default_value={}).tag(sync=True, **widget_serialization)
+    style = Dict(
+        {},
+        help="Style configuration",
+    ).tag(sync=True)
