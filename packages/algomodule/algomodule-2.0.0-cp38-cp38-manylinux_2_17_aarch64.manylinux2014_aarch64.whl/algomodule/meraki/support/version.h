ed290b7c1e7d46a95518c2ba1e456c703cd69588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 18:04:28 +0000

    Add package algomodule/2.0.0/algomodule-2.0.0-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/algomodule/algomodule-2.0.0-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/algomodule/meraki/support/version.h
@@ -0,0 +1,18 @@
+/* meraki: C/C++ implementation of Meraki, the Telestai Proof of Work algorithm.
+ * Copyright 2019 Pawel Bylica.
+ * Licensed under the Apache License, Version 2.0.
+ */
+
+#pragma once
+
+/** The meraki library version. */
+#define MERAKI_VERSION "0.5.2"
+
+#ifdef __cplusplus
+namespace meraki
+{
+/// The meraki library version.
+constexpr auto version = MERAKI_VERSION;
+
+}  // namespace meraki
+#endif
