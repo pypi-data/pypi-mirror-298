7f2564a06434ec63c1f6a4b5b96f45c81facbebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 20:04:28 +0000

    Add package polaris-lib/0.8.6/polaris_lib-0.8.6-py3-none-any.whl

---
+++ packages/polaris-lib/polaris_lib-0.8.6-py3-none-any.whl/polaris/utils/misc.py
@@ -0,0 +1,33 @@
+from typing import TYPE_CHECKING, Any
+
+from polaris.utils.types import ChecksumStrategy, SlugCompatibleStringType
+
+if TYPE_CHECKING:
+    from polaris.dataset import DatasetV1
+
+
+def listit(t: Any):
+    """
+    Converts all tuples in a possibly nested object to lists
+    https://stackoverflow.com/questions/1014352/how-do-i-convert-a-nested-tuple-of-tuples-and-lists-to-lists-of-lists-in-python
+    """
+    return list(map(listit, t)) if isinstance(t, (list, tuple)) else t
+
+
+def sluggify(sluggable: SlugCompatibleStringType):
+    """
+    Converts a string to a slug-compatible string.
+    """
+    return sluggable.lower().replace("_", "-")
+
+
+def should_verify_checksum(strategy: ChecksumStrategy, dataset: "DatasetV1") -> bool:
+    """
+    Determines whether a checksum should be verified.
+    """
+    if strategy == "ignore":
+        return False
+    elif strategy == "verify":
+        return True
+    else:
+        return not dataset.uses_zarr
