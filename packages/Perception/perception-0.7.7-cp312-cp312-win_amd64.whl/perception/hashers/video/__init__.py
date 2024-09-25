30f7be2167d8f36aef7f89344532e0ad7f49191e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 14:03:51 +0000

    Add package Perception/0.7.7/perception-0.7.7-cp312-cp312-win_amd64.whl

---
+++ packages/Perception/perception-0.7.7-cp312-cp312-win_amd64.whl/perception/hashers/video/__init__.py
@@ -0,0 +1,5 @@
+from .framewise import FramewiseHasher
+from .scenes import SimpleSceneDetection
+from .tmk import TMKL1, TMKL2
+
+__all__ = ["FramewiseHasher", "TMKL1", "TMKL2", "SimpleSceneDetection"]
