faf90cad5816b10520090d1e111dc8b6a6f1ac57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 17:04:26 +0000

    Add package SigProfilerMatrixGenerator/1.2.29/SigProfilerMatrixGenerator-1.2.29-py3-none-any.whl

---
+++ packages/SigProfilerMatrixGenerator/SigProfilerMatrixGenerator-1.2.29-py3-none-any.whl/SigProfilerMatrixGenerator/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEMATRIXGENERATOR SETUP.PY
+short_version = '1.2.29'
+version = '1.2.29'
+Update = 'v1.2.29: Resolve issue with non-sorted input and BED files.'
+
+	
