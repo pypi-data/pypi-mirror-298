eb442e35227df13f8e5db3e0df8307b74fb17928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 17:03:59 +0000

    Add package axiom-py/0.8.0/axiom_py-0.8.0.tar.gz

---
+++ packages/axiom-py/axiom_py-0.8.0.tar.gz/axiom_py-0.8.0/src/axiom_py/logging.py
@@ -0,0 +1,50 @@
+"""Logging contains the AxiomHandler and related methods to do with logging."""
+
+import time
+import atexit
+
+from logging import Handler, NOTSET, getLogger, WARNING
+from .client import Client
+
+
+class AxiomHandler(Handler):
+    """A logging handler that sends logs to Axiom."""
+
+    client: Client
+    dataset: str
+    buffer: list
+    interval: int
+    last_run: float
+
+    def __init__(self, client: Client, dataset: str, level=NOTSET, interval=1):
+        super().__init__()
+        # set urllib3 logging level to warning, check:
+        # https://github.com/axiomhq/axiom-py/issues/23
+        # This is a temp solution that would stop requests
+        # library from flooding the logs with debug messages
+        getLogger("urllib3").setLevel(WARNING)
+        self.client = client
+        self.dataset = dataset
+        self.buffer = []
+        self.last_run = time.monotonic()
+        self.interval = interval
+
+        # register flush on exit,
+        atexit.register(self.flush)
+
+    def emit(self, record):
+        """emit sends a log to Axiom."""
+        self.buffer.append(record.__dict__)
+        if (
+            len(self.buffer) >= 1000
+            or time.monotonic() - self.last_run > self.interval
+        ):
+            self.flush()
+
+    def flush(self):
+        """flush sends all logs in the logcache to Axiom."""
+        self.last_run = time.monotonic()
+        if len(self.buffer) == 0:
+            return
+        self.client.ingest_events(self.dataset, self.buffer)
+        self.buffer = []
---
+++ packages/axiom-py/axiom_py-0.8.0.tar.gz/axiom_py-0.8.0/tests/test_logger.py
@@ -0,0 +1,44 @@
+"""This module contains test for the logging Handler."""
+
+import os
+import logging
+import unittest
+from .helpers import get_random_name
+from axiom_py import Client
+from axiom_py.logging import AxiomHandler
+
+
+class TestLogger(unittest.TestCase):
+    def test_log(self):
+        """Tests a simple log"""
+        client = Client(
+            os.getenv("AXIOM_TOKEN"),
+            os.getenv("AXIOM_ORG_ID"),
+            os.getenv("AXIOM_URL"),
+        )
+        # create a dataset for that purpose
+        dataset_name = get_random_name()
+        client.datasets.create(
+            dataset_name, "a dataset to test axiom-py logger"
+        )
+
+        axiom_handler = AxiomHandler(client, dataset_name)
+
+        logger = logging.getLogger()
+        logger.addHandler(axiom_handler)
+
+        logger.warning("foo")
+
+        # this log shouldn't be ingested yet
+        res = client.apl_query(dataset_name)
+        self.assertEqual(0, res.status.rowsExamined)
+
+        # flush events
+        axiom_handler.flush()
+
+        # now we should have a log
+        res = client.apl_query(dataset_name)
+        self.assertEqual(1, res.status.rowsExamined)
+
+        # cleanup created dataset
+        client.datasets.delete(dataset_name)
