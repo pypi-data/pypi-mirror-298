e9dac85082b0aa0e80e0b5ca4cecc9b33077cf18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 14:03:10 +0000

    Add package vIQA/1.13.0/viqa-1.13.0-cp312-cp312-win_amd64.whl

---
+++ packages/vIQA/viqa-1.13.0-cp312-cp312-win_amd64.whl/viqa/__init__.py
@@ -0,0 +1,57 @@
+"""
+vIQA is a Python package for the assessment of volumetric image quality.
+
+The package provides a set of metrics to assess the quality of volumetric images.
+It can be used for 2D and 3D images.
+"""
+
+__version__ = "1.13.0"
+__author__ = "Lukas Behammer"
+__all__ = [
+    "FSIM",
+    "GSM",
+    "MAD",
+    "UQI",
+    "MSSSIM",
+    "PSNR",
+    "PSNR",
+    "RMSE",
+    "SSIM",
+    "VIFp",
+    "VSI",
+    "CNR",
+    "SNR",
+    "QMeasure",
+    "load_data",
+    "normalize_data",
+    "crop_image",
+    "export_metadata",
+    "export_results",
+    "export_image",
+    "fuse_metrics_linear_combination",
+    "visualize_2d",
+    "visualize_3d",
+    "BatchMetrics",
+    "MultipleMetrics",
+    "ImageArray",
+]
+
+from .fr_metrics import *
+from .fusion import fuse_metrics_linear_combination
+from .load_utils import ImageArray, crop_image, load_data, normalize_data
+from .multiple import BatchMetrics, MultipleMetrics
+from .nr_metrics import *
+from .utils import export_image, export_metadata, export_results
+from .visualization_utils import visualize_2d, visualize_3d
+
+
+def get_version():
+    """
+    Return the version of the package.
+
+    Returns
+    -------
+    str
+        The version of the package
+    """
+    return __version__
