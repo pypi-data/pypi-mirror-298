de121eb1f7bbfcf188ad2546d0a571a40a50e240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 20:04:04 +0000

    Add package mach-cctt/0.0.52/mach_cctt-0.0.52-py3-none-any.whl

---
+++ packages/mach-cctt/mach_cctt-0.0.52-py3-none-any.whl/mach_cctt/log.py
@@ -0,0 +1,47 @@
+import datetime
+import logging
+import os
+import pathlib
+from pathlib import Path
+import sys
+from typing import Any, MutableMapping, Optional
+
+from . import config
+
+sys.stdout.reconfigure(line_buffering=False)  # type: ignore
+
+Logger = logging.Logger | logging.LoggerAdapter
+
+
+# Simple adapter that supports adding a single string-formattable object as context
+class LogContextAdapter(logging.LoggerAdapter):
+    def __init__(self, logger: Logger, context: Any):
+        super().__init__(logger)
+        self.context = context
+
+    def process(self, msg, kwargs) -> tuple[Any, MutableMapping[str, Any]]:
+        return (f"[{self.context}] {msg}", kwargs)  # type: ignore
+
+
+def make_logger(name: str, stdout: bool, file: Optional[Path] = None) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    if stdout:
+        handler: logging.Handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(logging.INFO)
+        logger.addHandler(handler)
+
+    if file:
+        config.log_dir.mkdir(parents=True, exist_ok=True)
+        handler = logging.FileHandler(file)
+        handler.setLevel(logging.DEBUG)
+        logger.addHandler(handler)
+
+    logger.debug(f"START {name} {datetime.datetime.today()}\n")
+
+    return logger
+
+
+# Default logger for this project
+logger: Logger = make_logger("cctt", True, config.log_files["app"])
