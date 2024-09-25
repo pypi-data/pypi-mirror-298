32246d60b6a52540c7c579bebb0faacd6525f72f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 21:04:38 +0000

    Add package NumNeighbors/0.1.0/NumNeighbors-0.1.0.tar.gz

---
+++ packages/NumNeighbors/NumNeighbors-0.1.0.tar.gz/NumNeighbors-0.1.0/setup.py
@@ -0,0 +1,17 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="NumNeighbors",
+    version="0.1.0",
+    description="A package to display predefined programs",
+    author="AJ",
+    author_email="shredder1142110@gmail.com",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
