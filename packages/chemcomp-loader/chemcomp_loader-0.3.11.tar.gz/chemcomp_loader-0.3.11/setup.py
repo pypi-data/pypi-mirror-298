438f3741e34a776a86cb17d09e03b969c1dbccb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 16:03:56 +0000

    Add package chemcomp-loader/0.3.11/chemcomp_loader-0.3.11.tar.gz

---
+++ packages/chemcomp-loader/chemcomp_loader-0.3.11.tar.gz/chemcomp_loader-0.3.11/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.3.11'
+DESCRIPTION = 'Load disc data from chemcomp simulations.'
+LONG_DESCRIPTION = 'Python package to load disc data from simulations from the semi-analytical viscous disc code "chemcomp" by Schneider & Bitsch 2021.'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="chemcomp_loader", 
+        version=VERSION,
+        author="Joe Williams",
+        author_email="joepw1@hotmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        # download_url='https://github.com/AstroWilliams/chemcomp-plotter/archive/refs/tags/v_0.3.8.tar.gz',
+        packages=find_packages(),
+        install_requires=['tables', 'numpy', 'matplotlib'], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'chemcomp'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Topic :: Scientific/Engineering :: Astronomy",
+            # "Operating System :: Microsoft :: Windows",
+        ]
+)
