6fbe6000708ad80ee8abc05ce3114aa606836dda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 21:04:51 +0000

    Add package patchwork-cli/0.0.62/patchwork_cli-0.0.62-py3-none-any.whl

---
+++ packages/patchwork-cli/patchwork_cli-0.0.62-py3-none-any.whl/patchwork/steps/CallCode2Prompt/typed.py
@@ -0,0 +1,25 @@
+from typing_extensions import Annotated, Iterable, TypedDict
+
+from patchwork.common.utils.step_typing import StepTypeConfig
+
+
+class __CallCode2PromptRequiredInputs(TypedDict):
+    folder_path: Annotated[str, StepTypeConfig(is_config=True, is_path=True)]
+
+
+class CallCode2PromptInputs(__CallCode2PromptRequiredInputs, total=False):
+    filter: Annotated[str, StepTypeConfig(is_config=True)]
+    suppress_comments: Annotated[bool, StepTypeConfig(is_config=True)]
+    markdown_file_name: Annotated[str, StepTypeConfig(is_config=True)]
+    code2prompt_modes: str
+
+
+class FileToPatch(TypedDict):
+    uri: str
+    startLine: int
+    endLine: int
+    fullContent: str
+
+
+class CallCode2PromptOutputs(TypedDict):
+    files_to_patch: Iterable[FileToPatch]
