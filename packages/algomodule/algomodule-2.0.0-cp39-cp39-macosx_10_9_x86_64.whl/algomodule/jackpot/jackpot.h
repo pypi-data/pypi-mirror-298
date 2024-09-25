4d75387880adf1b9b24a5445905a79545190cc8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 18:03:36 +0000

    Add package algomodule/2.0.0/algomodule-2.0.0-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/algomodule/algomodule-2.0.0-cp39-cp39-macosx_10_9_x86_64.whl/algomodule/jackpot/jackpot.h
@@ -0,0 +1,14 @@
+#ifndef JACKPOTCOIN_H
+#define JACKPOTCOIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern void jackpot_hash(const char* input, char* output);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
