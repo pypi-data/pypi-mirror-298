3b92c6644c3335ef9fe97c297c22779052786492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 14:03:43 +0000

    Add package deeporigin-data-sdk/0.1.0a11/deeporigin_data_sdk-0.1.0a11.tar.gz

---
+++ packages/deeporigin-data-sdk/deeporigin_data_sdk-0.1.0a11.tar.gz/deeporigin_data_sdk-0.1.0a11/src/deeporigin_data/types/client_create_file_upload_params.py
@@ -0,0 +1,17 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing_extensions import Required, Annotated, TypedDict
+
+from .._utils import PropertyInfo
+
+__all__ = ["ClientCreateFileUploadParams"]
+
+
+class ClientCreateFileUploadParams(TypedDict, total=False):
+    content_length: Required[Annotated[str, PropertyInfo(alias="contentLength")]]
+
+    name: Required[str]
+
+    content_type: Annotated[str, PropertyInfo(alias="contentType")]
