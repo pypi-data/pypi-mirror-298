b49acc036a85f2f3272f34cd2de17f8f114194eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 17:03:54 +0000

    Add package open-webui/0.3.27.dev3/open_webui-0.3.27.dev3-py3-none-any.whl

---
+++ packages/open-webui/open_webui-0.3.27.dev3-py3-none-any.whl/open_webui/frontend/_app/immutable/chunks/clone.vScmQ5_r.js
@@ -0,0 +1,2 @@
+import{b as r}from"./graph.B8eH0dWH.js";var e=4;function a(o){return r(o,e)}export{a as c};
+//# sourceMappingURL=clone.vScmQ5_r.js.map
---
+++ packages/open-webui/open_webui-0.3.27.dev3-py3-none-any.whl/open_webui/frontend/_app/immutable/entry/start.DIoxuxZd.js
@@ -0,0 +1,2 @@
+import{a as t}from"../chunks/entry.ByS_XuNF.js";export{t as start};
+//# sourceMappingURL=start.DIoxuxZd.js.map
