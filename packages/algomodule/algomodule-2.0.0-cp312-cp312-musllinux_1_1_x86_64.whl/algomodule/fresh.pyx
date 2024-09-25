b9456bd0d9c83dd1bbc0b7fcf48de16633f27af7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 18:04:19 +0000

    Add package algomodule/2.0.0/algomodule-2.0.0-cp312-cp312-musllinux_1_1_x86_64.whl

---
+++ packages/algomodule/algomodule-2.0.0-cp312-cp312-musllinux_1_1_x86_64.whl/algomodule/fresh.pyx
@@ -0,0 +1,12 @@
+from libc.stdint cimport uint32_t, int64_t, uint8_t, uint64_t
+from libc.stdlib cimport malloc, free
+from libc.string cimport memcpy
+
+cdef extern from "fresh/fresh.h":
+	extern void fresh_hash(const char* input, char* output, uint32_t input_len);
+
+def _fresh_hash(hash):
+	cdef char output[32];
+	cdef uint32_t input_len = len(hash);
+	fresh_hash(hash, output, input_len);
+	return output[:32];
