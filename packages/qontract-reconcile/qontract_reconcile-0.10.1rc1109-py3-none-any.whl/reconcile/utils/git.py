57711a586799dcd7d526919175956c4dc161f90d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 14:03:26 +0000

    Add package qontract-reconcile/0.10.1rc1109/qontract_reconcile-0.10.1rc1109-py3-none-any.whl

---
+++ packages/qontract-reconcile/qontract_reconcile-0.10.1rc1109-py3-none-any.whl/reconcile/utils/git.py
@@ -0,0 +1,50 @@
+import os
+import subprocess
+
+
+class GitError(Exception):
+    pass
+
+
+def clone(repo_url, wd, depth=None, verify=True):
+    cmd = ["git"]
+    if not verify:
+        cmd += ["-c", "http.sslVerify=false"]
+    cmd += ["clone"]
+    if depth:
+        cmd += ["--depth", str(depth)]
+    cmd += [repo_url, wd]
+    result = subprocess.run(cmd, cwd=wd, capture_output=True, check=False)
+    if result.returncode != 0:
+        raise GitError(f"git clone failed: {repo_url}")
+
+
+def checkout(commit, wd):
+    cmd = ["git", "checkout", commit]
+    result = subprocess.run(cmd, cwd=wd, capture_output=True, check=False)
+    if result.returncode != 0:
+        raise GitError(f"git checkout failed: {commit}")
+
+
+def is_file_in_git_repo(file_path):
+    real_path = os.path.realpath(file_path)
+    dir_path = os.path.dirname(real_path)
+    cmd = ["git", "rev-parse", "--is-inside-work-tree"]
+    result = subprocess.run(cmd, cwd=dir_path, capture_output=True, check=False)
+    return result.returncode == 0
+
+
+def has_uncommited_changes() -> bool:
+    cmd = ["git", "diff"]
+    result = subprocess.run(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=True
+    )
+    return bool(result.stdout)
+
+
+def show_uncommited_changes() -> str:
+    cmd = ["git", "diff"]
+    result = subprocess.run(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=True
+    )
+    return result.stdout.decode("utf-8")
