7a1f96d36240cfd6a5466f729f41ff678582e936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 16:04:10 +0000

    Add package open-magvit2/1.0/open_magvit2-1.0.tar.gz

---
+++ packages/open-magvit2/open_magvit2-1.0.tar.gz/open_magvit2-1.0/README.md
@@ -0,0 +1,12 @@
+## OPEN-MAGVIT2 package
+
+This is a fork of [OPEN-MAGVIT2: An Open-source Project Toward Democratizing Auto-Regressive Visual Generation](https://github.com/TencentARC/Open-MAGVIT2) in order to make it a package for easy usage.
+
+## Install
+```
+pip install open-magvit2
+```
+
+## Example of usage
+
+[open-MAGVIT2-package-inference-example.ipynb](https://colab.research.google.com/drive/1lpqnekYG__GgSTmW2y7w4FZEZms54Sc5?usp=sharing)
---
+++ packages/open-magvit2/open_magvit2-1.0.tar.gz/open_magvit2-1.0/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup, find_packages
+
+
+def parse_requirements(filename):
+    with open(filename, 'r') as f:
+        return [
+            line.strip() for line in f
+            if line and not line.startswith('#') and '--index-url' not in line
+        ]
+
+def install_special_dependencies():
+    os.system('pip install torch==2.1.1 --index-url https://download.pytorch.org/whl/cu118')
+    os.system('pip install torchvision==0.16.1 --index-url https://download.pytorch.org/whl/cu118')
+
+setup(
+    name="open_magvit2",
+    version="1.0",
+    author="vinyesm",
+    author_email="",
+    description='Packaging of Open-source replication of Google\'s MAGVIT-v2 tokenizer',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',  
+    url="https://github.com/vinyesm/Open-MAGVIT2",
+    download_url="https://github.com/vinyesm/Open-MAGVIT2/archive/refs/tags/v0.1.0.tar.gz",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'open_magvit2': ['configs/gpu/*.yaml', 'configs/npu/*.yaml'],
+    },
+    install_requires=parse_requirements('requirements.txt'),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
