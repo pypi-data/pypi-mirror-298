20627730a32ded7c7adfc3b5c139dcf3922df0ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 16:04:19 +0000

    Add package alquimodelia/0.0.2/alquimodelia-0.0.2-py3-none-any.whl

---
+++ packages/alquimodelia/alquimodelia-0.0.2-py3-none-any.whl/alquimodelia/builders/__init__.py
@@ -0,0 +1,7 @@
+from alquimodelia.builders.cnn import CNN
+from alquimodelia.builders.fcnn import FCNN
+from alquimodelia.builders.lstm import LSTM
+from alquimodelia.builders.transformer import Transformer
+from alquimodelia.builders.unet import UNet
+
+__all__ = ["UNet", "Transformer", "CNN", "FCNN", "LSTM"]
