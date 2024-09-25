b3fbc96adb69451585a9e2130212e464032e2ad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 18:03:39 +0000

    Add package pysell/1.3.2/pysell-1.3.2.tar.gz

---
+++ packages/pysell/pysell-1.3.2.tar.gz/pysell-1.3.2/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# update pySELL itself
+python3 build-pysell.py
+
+# update the python package
+rm dist/*.whl
+rm dist/*.tar.gz
+python3 -m build
+echo "upload to pypi.org via:  twine upload dist/*"
