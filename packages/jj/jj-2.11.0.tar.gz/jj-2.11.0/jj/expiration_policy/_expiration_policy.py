8e6bd16c9c886681e47e194f31391abe0572d2e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 17:03:23 +0000

    Add package jj/2.11.0/jj-2.11.0.tar.gz

---
+++ packages/jj/jj-2.11.0.tar.gz/jj-2.11.0/jj/expiration_policy/_expiration_policy.py
@@ -0,0 +1,11 @@
+from ..requests import Request
+
+__all__ = ("ExpirationPolicy",)
+
+
+class ExpirationPolicy:
+    async def is_expired(self, request: Request) -> bool:
+        raise NotImplementedError()
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__qualname__}()"
