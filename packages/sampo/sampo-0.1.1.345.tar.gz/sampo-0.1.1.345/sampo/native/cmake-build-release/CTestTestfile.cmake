a7af3840355c018238ee9d8200a973c5e05f46de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 17:04:49 +0000

    Add package sampo/0.1.1.345/sampo-0.1.1.345.tar.gz

---
+++ packages/sampo/sampo-0.1.1.345.tar.gz/sampo-0.1.1.345/sampo/native/cmake-build-release/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Quarter/PycharmProjects/sampo/sampo/native
+# Build directory: C:/Users/Quarter/PycharmProjects/sampo/sampo/native/cmake-build-release
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("third_party")
+subdirs("modules")
---
+++ packages/sampo/sampo-0.1.1.345.tar.gz/sampo-0.1.1.345/sampo/native/cmake-build-release/third_party/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Quarter/PycharmProjects/sampo/sampo/native/third_party
+# Build directory: C:/Users/Quarter/PycharmProjects/sampo/sampo/native/cmake-build-release/third_party
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("pybind11")
+subdirs("boost-conan")
+subdirs("gtest-conan")
+subdirs("google-benchmark-conan")
