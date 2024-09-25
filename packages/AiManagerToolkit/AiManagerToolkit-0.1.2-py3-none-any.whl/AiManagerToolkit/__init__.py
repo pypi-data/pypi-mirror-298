e120d7b897b6dfa35b20ddcf831437ac874ad840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 18:04:03 +0000

    Add package AiManagerToolkit/0.1.2/AiManagerToolkit-0.1.2-py3-none-any.whl

---
+++ packages/AiManagerToolkit/AiManagerToolkit-0.1.2-py3-none-any.whl/AiManagerToolkit/__init__.py
@@ -0,0 +1,35 @@
+from .log import Log
+from .sync_azure import AzureAiToolkit
+from .sync_openai import OpenAiToolkit
+from .toolbox import Tool, Toolbox
+from .messages import (
+    SystemMessage,
+    UserMessage,
+    AssistantMessage,
+    PromptTemplate,
+    SystemTemplate,
+    UserTemplate,
+    AssistantTemplate,
+    History,
+    Message
+)
+
+__version__ = "0.1.2"
+
+
+__all__ = [
+    'AzureAiToolkit',
+    'OpenAiToolkit',
+    'Tool',
+    'Toolbox',
+    'Log',
+    'SystemMessage',
+    'UserMessage',
+    'AssistantMessage',
+    'PromptTemplate',
+    'SystemTemplate',
+    'UserTemplate',
+    'AssistantTemplate',
+    'History',
+    'Message'
+]
