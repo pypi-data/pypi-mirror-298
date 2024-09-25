9cb321ed27788161eb7acea2136d6dbc5e0fb13c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 21:04:45 +0000

    Add package kuzu-wasm/0.0.8/kuzu_wasm-0.0.8-py3-none-any.whl

---
+++ packages/kuzu-wasm/kuzu_wasm-0.0.8-py3-none-any.whl/kuzu_wasm/__init__.py
@@ -0,0 +1,52 @@
+__version__ = "0.0.8"
+async def init(wasmUrl=None) -> object:
+    import js
+    if wasmUrl is None:
+        rootUrl = js.location.origin
+        wasmUrl = f"{rootUrl}/package/dist/kuzu.js"
+    js_function = js.Function('obj', f'''
+        async function connectKuzu() {{
+                const wasmUrl = `{wasmUrl}`;
+                const kuzu_wasm = await import(wasmUrl)
+                const kuzu = await kuzu_wasm.default();
+                kuzu.FS.mkdir("data")
+                return kuzu;
+            }}
+        return connectKuzu()
+    ''')
+    js_obj = js.Object()
+    # global kuzu 
+    kuzu = await js_function(js_obj)
+
+    def upload(filepath):
+        import os,hashlib
+        def encode_path_to_filename(file_path):
+            # use SHA-256 encode
+            encoded_path = hashlib.sha256(file_path.encode()).hexdigest()
+            # get extension name
+            file_extension = os.path.splitext(file_path)[1]
+            # generate new filename
+            new_file_name = f"{encoded_path}{file_extension}"
+            return new_file_name
+        target_filepath = "/data/" + encode_path_to_filename(filepath)
+        try:
+            with open(filepath,"r") as f:
+                content = f.read()
+                kuzu.FS.writeFile(target_filepath, content);
+                return target_filepath
+        except FileNotFoundError:
+            print(f"Error: The file '{file_path}' was not found.")
+            return False
+        except IOError:
+            print(f"Error: An I/O error occurred while reading the file '{file_path}'.")
+            return False
+    kuzu.upload = upload
+
+    # def read(filepath):
+    #     data_list = list(kuzu.FS.readFile("/data/user.csv"))
+    #     byte_data = bytes(int_list)
+    #     return byte_data.decode('utf-8')
+    # kuzu.FS.read = read
+    return kuzu
+
+
