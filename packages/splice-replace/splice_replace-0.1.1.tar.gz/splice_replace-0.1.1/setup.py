b1de20d6e79ffe9af08568d124598b06e14d34b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 18:04:02 +0000

    Add package splice-replace/0.1.1/splice_replace-0.1.1.tar.gz

---
+++ packages/splice-replace/splice_replace-0.1.1.tar.gz/splice_replace-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='splice-replace',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'splice-replace=splice_replace.splice_replace:main',
+        ],
+    },
+    description='A utility to extract, process, and replace text in files based on line and column offsets.',
+    author='Ryan Taylor',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/saltlakeryan/splice-replace',  # Update with your repository URL
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
