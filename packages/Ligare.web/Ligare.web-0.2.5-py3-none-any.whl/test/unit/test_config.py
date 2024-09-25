e32448f5a64db5b64c19d82273043eaf487f09e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 19:03:39 +0000

    Add package Ligare.web/0.2.5/Ligare.web-0.2.5-py3-none-any.whl

---
+++ packages/Ligare.web/Ligare.web-0.2.5-py3-none-any.whl/test/unit/test_config.py
@@ -0,0 +1,45 @@
+import pytest
+from Ligare.programming.collections.dict import AnyDict
+from Ligare.programming.config import load_config
+from Ligare.web.config import Config
+from pytest_mock import MockerFixture
+
+
+def test__Config__load_config__reads_toml_file(mocker: MockerFixture):
+    fake_config_dict = {}
+    toml_mock = mocker.patch("toml.load", return_value=fake_config_dict)
+    _ = load_config(Config, "foo.toml")
+    assert toml_mock.called
+
+
+def test__Config__load_config__initializes_logging_config(mocker: MockerFixture):
+    fake_config_dict = {"logging": {"log_level": "DEBUG"}}
+    _ = mocker.patch("io.open")
+    _ = mocker.patch("toml.decoder.loads", return_value=fake_config_dict)
+    config = load_config(Config, "foo.toml")
+    assert config.logging.log_level == "DEBUG"
+
+
+def test__Config__load_config__initializes_flask_config(mocker: MockerFixture):
+    fake_config_dict = {"flask": {"app_name": "test_app"}}
+    _ = mocker.patch("io.open")
+    _ = mocker.patch("toml.decoder.loads", return_value=fake_config_dict)
+    config = load_config(Config, "foo.toml")
+    assert config.flask is not None
+    assert config.flask.app_name == "test_app"
+
+
+def test__Config__prepare_env_for_flask__requires_flask_secret_key_when_sessions_are_used(
+    mocker: MockerFixture,
+):
+    fake_config_dict: AnyDict = {
+        "flask": {"app_name": "test_app", "session": {"cookie": {}}}
+    }
+    _ = mocker.patch("io.open")
+    _ = mocker.patch("toml.decoder.loads", return_value=fake_config_dict)
+    config = load_config(Config, "foo.toml")
+
+    with pytest.raises(
+        Exception, match=r"^`flask.session.cookie.secret_key` must be set in config.$"
+    ):
+        config.prepare_env_for_flask()
