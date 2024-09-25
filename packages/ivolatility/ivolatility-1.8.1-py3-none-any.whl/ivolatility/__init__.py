78e12719778129dce9dc7df4af79c0bf484ad875
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 21:05:12 +0000

    Add package ivolatility/1.8.1/ivolatility-1.8.1-py3-none-any.whl

---
+++ packages/ivolatility/ivolatility-1.8.1-py3-none-any.whl/ivolatility/__init__.py
@@ -0,0 +1,10 @@
+"""
+IVolatility API wrapper python library.
+"""
+
+from ivolatility.ivolatility import *
+
+__version__ = '1.2.2'
+__author__ = 'Vladimir Goldin'
+__credits__ = 'IVolatility.com'
+
