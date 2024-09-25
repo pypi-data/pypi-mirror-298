35213218aa463d96ccedc13fc38461e03a426bdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 21:04:19 +0000

    Add package Hundred/0.2.2/hundred-0.2.2-py3-none-any.whl

---
+++ packages/Hundred/hundred-0.2.2-py3-none-any.whl/hundred/domain/entity.py
@@ -0,0 +1,22 @@
+from abc import ABC
+from typing import ClassVar, Self
+from uuid import UUID
+
+from pydantic import BaseModel, ConfigDict, Field
+
+
+class Entity(BaseModel, ABC):
+    id: UUID = Field(frozen=True)
+
+    model_config: ClassVar[ConfigDict] = ConfigDict(validate_assignment=True)
+
+
+class Aggregate(Entity, ABC):
+    version: int = Field(default=1, gt=0)
+
+    def bump_version(self) -> Self:
+        self.version += 1
+        return self
+
+    def is_outdated(self, existing_version: int) -> bool:
+        return existing_version != self.version - 1
