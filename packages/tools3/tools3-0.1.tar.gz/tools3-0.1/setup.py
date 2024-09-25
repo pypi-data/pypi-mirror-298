4c7e9fd61553fc6676f4bd3468cced272dbab342
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 16:04:35 +0000

    Add package tools3/0.1/tools3-0.1.tar.gz

---
+++ packages/tools3/tools3-0.1.tar.gz/tools3-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tools3",
+    version="0.1",
+    packages=find_packages(),
+    description="A package to create folders and store a .py file",
+    author="jerrry",
+    author_email="jerryjones23@gmail.com",
+)
