6347b4fffd2823cc7264499b5c6f35df9763939d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 20:04:58 +0000

    Add package smtp-emailer/6.0.0/smtp_emailer-6.0.0-py3-none-any.whl

---
+++ packages/smtp-emailer/smtp_emailer-6.0.0-py3-none-any.whl/smtp_emailer/__init__.py
@@ -0,0 +1,35 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import formatdate
+from smtplib import SMTP
+from ssl import create_default_context
+
+#
+# Put an email together in MIMEMultipart and send it off.
+#
+def send(
+    host,
+    port,
+    username,
+    password,
+    sender,
+    recipient,
+    subject,
+    html,
+    attachments=[]
+):
+    msg = MIMEMultipart()
+    msg['From'] = sender
+    msg['To'] = recipient
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = subject
+    msg['X-PM-Message-Stream'] = 'outbound'
+    msg.attach(MIMEText(html, 'html'))
+    for attachment in attachments:
+        msg.attach(attachment)
+    # Send it off via smtp.mail.me.com
+    context = create_default_context()
+    with SMTP(host, port) as smtp:
+        smtp.starttls(context=context)
+        smtp.login(username, password)
+        smtp.sendmail(sender, recipient, msg.as_string())
