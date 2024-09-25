d5816c997c15002025b37a3c8d42d53526058bb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 16:03:43 +0000

    Add package postman-github-sync/0.1.0.3/postman_github_sync-0.1.0.3.tar.gz

---
+++ packages/postman-github-sync/postman_github_sync-0.1.0.3.tar.gz/postman_github_sync-0.1.0.3/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="postman_github_sync",
+    version="0.1.0.3",
+    author="Oluwaseyi Ajadi",
+    author_email="oluwaseyinexus137@gmail.com",
+    description="This script automates the process of setting up GitHub Actions workflows for Postman collections. It supports both GitHub repository integration and local workflow setup, allowing for flexible usage in various scenarios.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/BlazinArtemis/postman_to_github_actions",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        'requests==2.31.0',
+        'urllib3==2.0.7',
+        'chardet==5.1.0',
+        'charset_normalizer==3.1.0',
+        'argparse'
+    ],
+    entry_points={
+        "console_scripts": [
+                                "postman_github_sync=postman_github_sync.main:main",
+        ],
+    },
+)
