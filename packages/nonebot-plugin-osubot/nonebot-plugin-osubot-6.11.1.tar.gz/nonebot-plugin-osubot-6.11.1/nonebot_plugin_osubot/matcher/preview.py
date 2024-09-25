37c97fc50957424f0b66a02b6fb3e558db31239e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 20:04:31 +0000

    Add package nonebot-plugin-osubot/6.11.1/nonebot-plugin-osubot-6.11.1.tar.gz

---
+++ packages/nonebot-plugin-osubot/nonebot-plugin-osubot-6.11.1.tar.gz/nonebot-plugin-osubot-6.11.1/nonebot_plugin_osubot/matcher/preview.py
@@ -0,0 +1,34 @@
+from nonebot import on_command
+from nonebot.typing import T_State
+from nonebot.params import CommandArg
+from nonebot.internal.adapter import Message
+from nonebot_plugin_alconna import UniMessage
+
+from ..api import osu_api
+from ..file import download_tmp_osu
+from ..mania import generate_preview_pic
+
+generate_preview = on_command("预览", aliases={"preview", "完整预览"}, priority=11, block=True)
+
+
+@generate_preview.handle()
+async def _(
+    state: T_State,
+    args: Message = CommandArg(),
+):
+    osu_id = args.extract_plain_text().strip()
+    if not osu_id or not osu_id.isdigit():
+        await UniMessage.text("请输入正确的地图mapID").finish(reply_to=True)
+    data = await osu_api("map", map_id=int(osu_id))
+    if not data:
+        await UniMessage.text("未查询到该地图").finish(reply_to=True)
+    if isinstance(data, str):
+        await UniMessage.text(data).finish(reply_to=True)
+    if data.get("total_length") and data["total_length"] > 1200:
+        await UniMessage.text("谱面长度大于20分钟，预览其他短一点的谱吧！").finish(reply_to=True)
+    osu = await download_tmp_osu(osu_id)
+    if state["_prefix"]["command"][0] == "完整预览":
+        pic = await generate_preview_pic(osu, True)
+    else:
+        pic = await generate_preview_pic(osu)
+    await UniMessage.image(raw=pic).finish(reply_to=True)
