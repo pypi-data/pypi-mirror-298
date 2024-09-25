21688443f7ddea1e823fb6e7603825c595026150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 20:04:43 +0000

    Add package cobo-waas2/1.4.0/cobo_waas2-1.4.0-py3-none-any.whl

---
+++ packages/cobo-waas2/cobo_waas2-1.4.0-py3-none-any.whl/cobo_waas2/api/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+
+# import apis into api package
+from cobo_waas2.api.developers_webhooks_api import DevelopersWebhooksApi
+from cobo_waas2.api.o_auth_api import OAuthApi
+from cobo_waas2.api.stakings_api import StakingsApi
+from cobo_waas2.api.transactions_api import TransactionsApi
+from cobo_waas2.api.wallets_api import WalletsApi
+from cobo_waas2.api.wallets_exchange_wallet_api import WalletsExchangeWalletApi
+from cobo_waas2.api.wallets_mpc_wallets_api import WalletsMPCWalletsApi
+
