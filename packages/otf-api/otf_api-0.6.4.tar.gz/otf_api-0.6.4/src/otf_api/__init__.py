a45df65753d75e7866106cf0c5773ae37434a7fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 19:03:43 +0000

    Add package otf-api/0.6.4/otf_api-0.6.4.tar.gz

---
+++ packages/otf-api/otf_api-0.6.4.tar.gz/otf_api-0.6.4/src/otf_api/__init__.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+from loguru import logger
+
+from .api import Otf
+from .auth import OtfUser
+
+__version__ = "0.6.4"
+
+
+__all__ = ["Otf", "OtfUser"]
+
+logger.remove()
+logger.add(sink=sys.stdout, level=os.getenv("OTF_LOG_LEVEL", "INFO"))
