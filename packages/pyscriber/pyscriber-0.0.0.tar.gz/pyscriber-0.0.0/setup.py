c08d984cb926fd7e578d820478b1d09559500539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 18:03:41 +0000

    Add package pyscriber/0.0.0/pyscriber-0.0.0.tar.gz

---
+++ packages/pyscriber/pyscriber-0.0.0.tar.gz/pyscriber-0.0.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+# Third party modules
+from setuptools import setup
+
+setup(name='pyscriber',
+      version='0.0.0',
+      description='python transcribe video files to text',
+      url='http://github.com/SK/pyscriber',
+      author='stephenking',
+      author_email='stephen.ra.king@gmail.com',
+      license='MIT',
+      packages=['pyscriber'],
+      zip_safe=False)
