738b00725f73e96a51e3059a137472089c25c7b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 21:04:32 +0000

    Add package logaid/1.1.9/logaid-1.1.9.tar.gz

---
+++ packages/logaid/logaid-1.1.9.tar.gz/logaid-1.1.9/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+      name='logaid',  # 包的名称
+      version='1.1.9',  # 包的版本号
+      author='yunxiao',  # 作者姓名
+      description='A log aid for you.',
+      long_description=open('README.md','r',encoding='utf-8').read(),
+      long_description_content_type="text/markdown",
+      license="MIT"
+)
+
