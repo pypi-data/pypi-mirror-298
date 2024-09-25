6d9987119daa2903fd36672675d9aaba6a63a92a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 16:03:30 +0000

    Add package scrapegraphai/1.23.1/scrapegraphai-1.23.1.tar.gz

---
+++ packages/scrapegraphai/scrapegraphai-1.23.1.tar.gz/scrapegraphai-1.23.1/scrapegraphai/utils/__init__.py
@@ -0,0 +1,20 @@
+"""
+    __init__.py file for utils folder
+"""
+from .convert_to_csv import convert_to_csv
+from .convert_to_json import convert_to_json
+from .prettify_exec_info import prettify_exec_info
+from .proxy_rotation import Proxy, parse_or_search_proxy, search_proxy_servers
+from .save_audio_from_bytes import save_audio_from_bytes
+from .sys_dynamic_import import dynamic_import, srcfile_import
+from .cleanup_html import cleanup_html
+from .logging import *
+from .convert_to_md import convert_to_md
+from .screenshot_scraping.screenshot_preparation import (take_screenshot,
+                                                         select_area_with_opencv,
+                                                         select_area_with_ipywidget,
+                                                         crop_image)
+from .screenshot_scraping.text_detection import detect_text
+from .tokenizer import num_tokens_calculus
+from .split_text_into_chunks import split_text_into_chunks
+from .llm_callback_manager import CustomLLMCallbackManager
