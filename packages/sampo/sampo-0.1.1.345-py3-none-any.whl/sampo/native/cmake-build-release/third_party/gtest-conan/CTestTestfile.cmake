e6accd594d4d962dbe679f93bfce0fcdd33c5b46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 17:04:08 +0000

    Add package sampo/0.1.1.345/sampo-0.1.1.345-py3-none-any.whl

---
+++ packages/sampo/sampo-0.1.1.345-py3-none-any.whl/sampo/native/cmake-build-release/third_party/gtest-conan/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Quarter/PycharmProjects/sampo/sampo/native/third_party/gtest-conan
+# Build directory: C:/Users/Quarter/PycharmProjects/sampo/sampo/native/cmake-build-release/third_party/gtest-conan
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
