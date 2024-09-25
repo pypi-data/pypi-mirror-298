d8e56ed0060092a22226f2b299c26aa529f2b3c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 18:03:54 +0000

    Add package pydftracer/1.0.5/pydftracer-1.0.5.tar.gz

---
+++ packages/pydftracer/pydftracer-1.0.5.tar.gz/pydftracer-1.0.5/src/dftracer/df_logger.h
@@ -0,0 +1,246 @@
+//
+// Created by haridev on 3/28/23.
+//
+
+#ifndef DFTRACER_GENERIC_LOGGER_H
+#define DFTRACER_GENERIC_LOGGER_H
+
+#include <dftracer/core/logging.h>
+#include <dftracer/core/singleton.h>
+#include <dftracer/utils/configuration_manager.h>
+#include <dftracer/utils/utils.h>
+#include <dftracer/writer/chrome_writer.h>
+#include <libgen.h>
+#include <sys/time.h>
+#include <time.h>
+#include <unistd.h>
+
+#include <any>
+#include <chrono>
+#include <cstdint>
+#include <cstring>
+#include <dftracer/dftracer_config.hpp>
+#include <unordered_map>
+
+#ifdef DFTRACER_MPI_ENABLE
+#include <mpi.h>
+#endif
+
+typedef std::chrono::high_resolution_clock chrono;
+
+class DFTLogger {
+ private:
+  bool throw_error;
+  bool is_init, dftracer_tid;
+  ProcessID process_id;
+  std::shared_ptr<dftracer::ChromeWriter> writer;
+  uint32_t level;
+  std::vector<int> index_stack;
+  std::atomic_int index;
+  bool has_entry;
+#ifdef DFTRACER_MPI_ENABLE
+  bool mpi_event;
+#endif
+ public:
+  bool include_metadata;
+  DFTLogger(bool init_log = false)
+      : is_init(false),
+        dftracer_tid(false),
+        level(0),
+        index_stack(),
+        index(0),
+        has_entry(false),
+#ifdef DFTRACER_MPI_ENABLE
+        mpi_event(false),
+#endif
+        include_metadata(false) {
+    DFTRACER_LOG_DEBUG("DFTLogger.DFTLogger", "");
+    auto conf =
+        dftracer::Singleton<dftracer::ConfigurationManager>::get_instance();
+    include_metadata = conf->metadata;
+    dftracer_tid = conf->tids;
+    throw_error = conf->throw_error;
+    this->is_init = true;
+  }
+  ~DFTLogger() { index_stack.clear(); }
+  inline void update_log_file(std::string log_file, std::string exec_name,
+                              std::string cmd, ProcessID process_id = -1) {
+    DFTRACER_LOG_DEBUG("DFTLogger.update_log_file %s", log_file.c_str());
+    this->process_id = process_id;
+    this->writer = dftracer::Singleton<dftracer::ChromeWriter>::get_instance();
+    if (this->writer != nullptr) {
+      this->writer->initialize(log_file.data(), this->throw_error);
+      std::unordered_map<std::string, std::any> *meta = nullptr;
+      if (include_metadata) {
+        meta = new std::unordered_map<std::string, std::any>();
+        meta->insert_or_assign("version", DFTRACER_VERSION);
+        meta->insert_or_assign("exec", exec_name);
+        meta->insert_or_assign("cmd", cmd);
+        time_t ltime;       /* calendar time */
+        ltime = time(NULL); /* get current cal time */
+        char timestamp[1024];
+        auto size = sprintf(timestamp, "%s", asctime(localtime(&ltime)));
+        timestamp[size - 1] = '\0';
+        meta->insert_or_assign("date", std::string(timestamp));
+        meta->insert_or_assign("ppid", getppid());
+      }
+      this->enter_event();
+      this->log("start", "dftracer", this->get_time(), 0, meta);
+      if (include_metadata) {
+        delete (meta);
+      }
+      this->exit_event();
+    }
+    this->is_init = true;
+    DFTRACER_LOG_INFO("Writing trace to %s", log_file.c_str());
+  }
+
+  inline void enter_event() {
+    index++;
+    if (include_metadata) {
+      level++;
+      index_stack.push_back(index.load());
+    }
+  }
+
+  inline void exit_event() {
+    if (include_metadata) {
+      level--;
+      index_stack.pop_back();
+    }
+  }
+
+  inline TimeResolution get_time() {
+    DFTRACER_LOG_DEBUG("DFTLogger.get_time", "");
+    struct timeval tv {};
+    gettimeofday(&tv, NULL);
+    TimeResolution t = 1000000 * tv.tv_sec + tv.tv_usec;
+    return t;
+  }
+
+  inline void log(ConstEventNameType event_name, ConstEventNameType category,
+                  TimeResolution start_time, TimeResolution duration,
+                  std::unordered_map<std::string, std::any> *metadata) {
+    DFTRACER_LOG_DEBUG("DFTLogger.log", "");
+    ThreadID tid = 0;
+    if (dftracer_tid) {
+      tid = df_gettid() + this->process_id;
+    }
+    int local_index;
+    if (!include_metadata) {
+      local_index = index.load();
+    }
+    if (metadata != nullptr) {
+      metadata->insert_or_assign("level", level);
+      int parent_index_value = -1;
+      if (level > 1) {
+        parent_index_value = index_stack[level - 2];
+      }
+      metadata->insert_or_assign("p_idx", parent_index_value);
+    }
+#ifdef DFTRACER_MPI_ENABLE
+    if (!mpi_event && include_metadata) {
+      int initialized;
+      int status = MPI_Initialized(&initialized);
+      if (status == MPI_SUCCESS && initialized == true &&
+          this->writer != nullptr) {
+        int rank;
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        auto meta = std::unordered_map<std::string, std::any>();
+        meta.insert_or_assign("rank", rank);
+        auto start = this->get_time();
+        this->enter_event();
+        this->writer->log(index_stack[level - 1], "mpi", "dftracer",
+                          EventType::COMPLETE_EVENT, start, 0, &meta,
+                          this->process_id, tid);
+        this->exit_event();
+        char process_name[1024];
+        auto size = sprintf(process_name, "Rank %d", rank);
+        process_name[size] = '\0';
+        this->enter_event();
+        this->writer->log(index_stack[level - 1], "process_name", process_name,
+                          EventType::METADATA_EVENT, 0, 0, nullptr,
+                          this->process_id, tid);
+        this->exit_event();
+
+        mpi_event = true;
+      }
+    }
+#endif
+
+    if (this->writer != nullptr) {
+      if (include_metadata) {
+        this->writer->log(index_stack[level - 1], event_name, category,
+                          EventType::COMPLETE_EVENT, start_time, duration,
+                          metadata, this->process_id, tid);
+      } else {
+        this->writer->log(local_index, event_name, category,
+                          EventType::COMPLETE_EVENT, start_time, duration,
+                          metadata, this->process_id, tid);
+      }
+
+      has_entry = true;
+    } else {
+      DFTRACER_LOG_ERROR("DFTLogger.log writer not initialized", "");
+    }
+  }
+
+  inline void finalize() {
+    DFTRACER_LOG_DEBUG("DFTLogger.finalize", "");
+    if (this->writer != nullptr) {
+      auto meta = std::unordered_map<std::string, std::any>();
+      meta.insert_or_assign("num_events", index.load());
+      this->enter_event();
+      this->log("end", "dftracer", this->get_time(), 0, &meta);
+      this->exit_event();
+      writer->finalize(has_entry);
+      DFTRACER_LOG_INFO("Released Logger", "");
+      this->writer = nullptr;
+    } else {
+      DFTRACER_LOG_WARN("DFTLogger.finalize writer not initialized", "");
+    }
+  }
+};
+
+#define DFT_LOGGER_INIT() dftracer::Singleton<DFTLogger>::get_instance()
+#define DFT_LOGGER_FINI() \
+  dftracer::Singleton<DFTLogger>::get_instance()->finalize()
+#define DFT_LOGGER_UPDATE(value)               \
+  if (trace && this->logger->include_metadata) \
+    metadata->insert_or_assign(#value, value);
+#define DFT_LOGGER_START(entity)                                  \
+  DFTRACER_LOG_DEBUG("Calling function %s", __FUNCTION__);        \
+  const char *fname = is_traced(entity, __FUNCTION__);            \
+  bool trace = fname != nullptr;                                  \
+  TimeResolution start_time = 0;                                  \
+  std::unordered_map<std::string, std::any> *metadata = nullptr;  \
+  if (trace) {                                                    \
+    if (this->logger->include_metadata) {                         \
+      metadata = new std::unordered_map<std::string, std::any>(); \
+      DFT_LOGGER_UPDATE(fname);                                   \
+    }                                                             \
+    this->logger->enter_event();                                  \
+    start_time = this->logger->get_time();                        \
+  }
+#define DFT_LOGGER_START_ALWAYS()                                 \
+  DFTRACER_LOG_DEBUG("Calling function %s", __FUNCTION__);        \
+  bool trace = true;                                              \
+  TimeResolution start_time = 0;                                  \
+  std::unordered_map<std::string, std::any> *metadata = nullptr;  \
+  if (trace) {                                                    \
+    if (this->logger->include_metadata) {                         \
+      metadata = new std::unordered_map<std::string, std::any>(); \
+    }                                                             \
+    this->logger->enter_event();                                  \
+    start_time = this->logger->get_time();                        \
+  }
+#define DFT_LOGGER_END()                                          \
+  if (trace) {                                                    \
+    TimeResolution end_time = this->logger->get_time();           \
+    this->logger->log((char *)__FUNCTION__, CATEGORY, start_time, \
+                      end_time - start_time, metadata);           \
+    this->logger->exit_event();                                   \
+    if (this->logger->include_metadata) delete (metadata);        \
+  }
+
+#endif  // DFTRACER_GENERIC_LOGGER_H
---
+++ packages/pydftracer/pydftracer-1.0.5.tar.gz/pydftracer-1.0.5/src/dftracer/writer/chrome_writer.h
@@ -0,0 +1,147 @@
+//
+// Created by haridev on 3/28/23.
+//
+
+#ifndef DFTRACER_CHROME_WRITER_H
+#define DFTRACER_CHROME_WRITER_H
+
+#include <dftracer/core/constants.h>
+#include <dftracer/core/typedef.h>
+#include <dftracer/utils/configuration_manager.h>
+#include <dftracer/utils/posix_internal.h>
+#include <dftracer/utils/utils.h>
+#if DISABLE_HWLOC == 1
+#include <hwloc.h>
+#endif
+#include <assert.h>
+#include <unistd.h>
+
+#include <any>
+#include <atomic>
+#include <mutex>
+#include <shared_mutex>
+#include <string>
+#include <thread>
+#include <unordered_map>
+namespace dftracer {
+class ChromeWriter {
+ private:
+  std::unordered_map<char *, std::any> metadata;
+  std::shared_mutex mtx;
+
+ protected:
+  bool throw_error;
+  std::string filename;
+
+ private:
+  bool include_metadata, enable_compression;
+
+  bool enable_core_affinity;
+#if DISABLE_HWLOC == 1
+  hwloc_topology_t topology;
+#endif
+  FILE *fh;
+  char hostname[256];
+  static const int MAX_LINE_SIZE = 4096;
+  static const int MAX_META_LINE_SIZE = 3000;
+  size_t write_buffer_size;
+
+  size_t current_index;
+  std::vector<char> buffer;
+  void convert_json(int index, ConstEventNameType event_name,
+                    ConstEventNameType category, EventType type,
+                    TimeResolution start_time, TimeResolution duration,
+                    std::unordered_map<std::string, std::any> *metadata,
+                    ProcessID process_id, ThreadID thread_id);
+
+  bool is_first_write;
+  inline size_t write_buffer_op(bool force = false) {
+    if (current_index == 0 || (!force && current_index < write_buffer_size))
+      return 0;
+    DFTRACER_LOG_DEBUG("ChromeWriter.write_buffer_op %s",
+                       this->filename.c_str());
+    size_t written_elements = 0;
+    {
+      std::unique_lock<std::shared_mutex> lock(mtx);
+      flockfile(fh);
+      written_elements = fwrite(buffer.data(), current_index, sizeof(char), fh);
+      funlockfile(fh);
+      current_index = 0;
+    }
+
+    if (written_elements != 1) {  // GCOVR_EXCL_START
+      DFTRACER_LOG_ERROR(
+          "unable to log write only %ld of %d with error code "
+          "%d",
+          written_elements, 1, errno);
+    }  // GCOVR_EXCL_STOP
+    return written_elements;
+  }
+  std::vector<unsigned> core_affinity() {
+    DFTRACER_LOG_DEBUG("ChromeWriter.core_affinity", "");
+    auto cores = std::vector<unsigned>();
+#if DISABLE_HWLOC == 1
+    if (enable_core_affinity) {
+      hwloc_cpuset_t set = hwloc_bitmap_alloc();
+      hwloc_get_cpubind(topology, set, HWLOC_CPUBIND_PROCESS);
+      for (unsigned id = hwloc_bitmap_first(set); id != -1;
+           id = hwloc_bitmap_next(set, id)) {
+        cores.push_back(id);
+      }
+      hwloc_bitmap_free(set);
+    }
+#endif
+    return cores;
+  }
+
+  void get_hostname(char *hostname) {
+    DFTRACER_LOG_DEBUG("ChromeWriter.get_hostname", "");
+    gethostname(hostname, 256);
+  }
+
+ public:
+  ChromeWriter()
+      : metadata(),
+        throw_error(false),
+        filename(),
+        include_metadata(false),
+        enable_compression(false),
+        enable_core_affinity(false),
+        fh(nullptr),
+        current_index(0),
+        is_first_write(true) {
+    DFTRACER_LOG_DEBUG("ChromeWriter.ChromeWriter", "");
+    auto conf =
+        dftracer::Singleton<dftracer::ConfigurationManager>::get_instance();
+    get_hostname(hostname);
+    include_metadata = conf->metadata;
+    enable_core_affinity = conf->core_affinity;
+    enable_compression = conf->compression;
+    write_buffer_size = conf->write_buffer_size;
+    {
+      std::unique_lock<std::shared_mutex> lock(mtx);
+      buffer = std::vector<char>(write_buffer_size + 4096);
+      current_index = 0;
+    }
+
+    if (enable_core_affinity) {
+#if DISABLE_HWLOC == 1
+      hwloc_topology_init(&topology);  // initialization
+      hwloc_topology_load(topology);   // actual detection
+#endif
+    }
+  }
+  ~ChromeWriter() { DFTRACER_LOG_DEBUG("Destructing ChromeWriter", ""); }
+  void initialize(char *filename, bool throw_error);
+
+  void log(int index, ConstEventNameType event_name,
+           ConstEventNameType category, EventType type,
+           TimeResolution start_time, TimeResolution duration,
+           std::unordered_map<std::string, std::any> *metadata,
+           ProcessID process_id, ThreadID tid);
+
+  void finalize(bool has_entry);
+};
+}  // namespace dftracer
+
+#endif  // DFTRACER_CHROME_WRITER_H
