31a4f7f4636529056af88dd674bdf3b302a6be2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 16:03:49 +0000

    Add package computer2mqtt/0.1.2/computer2mqtt-0.1.2.tar.gz

---
+++ packages/computer2mqtt/computer2mqtt-0.1.2.tar.gz/computer2mqtt-0.1.2/README.md
@@ -0,0 +1,66 @@
+# Computer2Mqtt
+
+This project is a multi-system and multi-OS adaptation of [mac2mqtt](https://github.com/bessarabov/mac2mqtt). It allows you to execute any command on your system, providing endless possibilities for automation and control.
+
+
+## Requirements
+
+This assumes you have [direnv](https://direnv.net/) and [uv](https://github.com/astral-sh/uv) installed
+
+Simply run `direnv allow` to setup the environment, you can read the contents of [.envrc](.envrc) to see what it does behind the scenes.
+
+
+## Setup
+
+1. Clone this repository to your local machine.
+2. Run `direnv allow` in the project directory. This will set up the Python environment using `pyenv`.
+3. Create a `computer2mqtt.yaml` by copying [computer2mqtt-example.yaml](computer2mqtt-example.yaml) configuration file in the project root.
+There are 2 important sections first the mqtt config, and second the commands you want to run.
+In the example below these are commands for macs to turn on/off the display. This should be changed to commands for your target system. Possibility is endless.
+
+```yaml
+mqtt:
+  ip: 192.168.1.100
+  port: 1883
+  user: username
+  password: password
+
+
+commands:
+  # Example mac command to wake/sleep display
+  displaysleep: "pmset displaysleepnow"
+  displaywake: "caffeinate -u -t 1"
+```
+
+4. Run `computer2mqtt` and note down the `Sanitized hostname: Mac-mini`, Mac-mini in this case.
+5. Set up Home Assistant Scripts to trigger commands. Here is an example from my `scripts.yaml` file in Home Assistant
+
+```yaml
+macmini_displaysleep:
+  alias: Mac Mini Display Sleep
+  icon: mdi:laptop
+  sequence:
+    - service: mqtt.publish
+      data:
+        topic: "mac2mqtt/{device_name}/command/displaysleep"
+        payload: "displaysleep"
+
+macmini_displaywake:
+  alias: Mac Mini Display Wake
+  icon: mdi:laptop
+  sequence:
+    - service: mqtt.publish
+      data:
+        topic: "mac2mqtt/{device_name}/command/displaywake"
+        payload: "displaywake"
+```
+Replace {device_name} with the name of your device from the previous step!
+
+# Running in the background
+
+To run the script in the background, you can use the screen command. screen is a full-screen window manager that multiplexes a physical terminal between several processes.
+
+1. Start a new screen session with name of your choice with the command `screen -S {mysession}`.
+1. Run the script with the command `computer2mqtt`.
+1. Detach from the screen session with the command Ctrl-a d.
+1. You can reattach to the session at any time with the command `screen -r {mysession}`. This will keep the script running in the background, even if you close the terminal.
---
+++ packages/computer2mqtt/computer2mqtt-0.1.2.tar.gz/computer2mqtt-0.1.2/src/computer2mqtt/__init__.py
@@ -0,0 +1,132 @@
+import asyncio
+import os
+import re
+import signal
+import sys
+from typing import TYPE_CHECKING, Any, Dict
+
+import aiomqtt
+import click
+import yaml
+
+if TYPE_CHECKING or (sys.platform.lower() == "win32" or os.name.lower() == "nt"):
+    pass  # type: ignore
+
+
+class Config:
+    """Handles loading and accessing configuration from a YAML file."""
+
+    def __init__(self, config_path: str = "computer2mqtt.yaml") -> None:
+        """Initializes the configuration from the given YAML file."""
+        self.ip: str = ""
+        self.port: int = 1883  # Default MQTT port
+        self.user: str = ""
+        self.password: str = ""
+
+        self.commands: Dict[str, Any] = {}
+        self.load_config(config_path)
+
+    def load_config(self, config_path: str) -> None:
+        """Loads configuration from a YAML file."""
+        try:
+            with open(config_path, "r") as stream:
+                config = yaml.safe_load(stream)
+                mqtt_config = config.get("mqtt", {})
+                self.ip = mqtt_config.get("ip", "")
+                self.port = mqtt_config.get("port", 1883)
+                self.user = mqtt_config.get("user", "")
+                self.password = mqtt_config.get("password", "")
+                # Load command configurations
+                self.commands = config.get("commands", {})
+        except (FileNotFoundError, yaml.YAMLError) as exc:
+            print(f"Error loading config: {exc}")
+
+
+def get_hostname() -> str:
+    """Returns a sanitized hostname suitable for use in topics."""
+    hostname = os.uname()[1]
+    sanitized_hostname = re.sub(r"[^a-zA-Z0-9_-]", "", hostname.split(".")[0])
+    print(f"Sanitized hostname: {sanitized_hostname}")
+    return sanitized_hostname
+
+
+async def run_command(command: str, *args: str) -> None:
+    """Executes the given command with the provided arguments asynchronously."""
+    await asyncio.create_subprocess_exec(command, *args)
+
+
+async def command_executor(config: Config, command_key: str) -> None:
+    """Executes a command based on the key from the configuration."""
+    command = config.commands.get(command_key)
+    if command:
+        command_list = command.split()
+        await run_command(*command_list)
+    else:
+        print(f"Command for '{command_key}' not found in the configuration.")
+
+
+async def mqtt_client_task(config: Config):
+    """Task for running the MQTT client and handling messages with reconnection logic."""
+    while True:
+        try:
+            async with aiomqtt.Client(
+                config.ip,
+                username=config.user or "default_user",
+                password=config.password or "default_password",
+                port=config.port,
+            ) as client:
+                await client.subscribe(f"mac2mqtt/{get_hostname()}/command/#")
+                async for message in client.messages:
+                    print(
+                        f"Received message: {str(message.topic)} => {str(message.payload)}"
+                    )
+                    topic_parts = str(message.topic).split("/")
+                    if isinstance(message.payload, bytes):
+                        payload = message.payload.decode()
+                    else:
+                        payload = str(message.payload)
+
+                    if len(topic_parts) == 4:
+                        command_key = topic_parts[3]
+                        if payload == command_key:
+                            await command_executor(config, command_key)
+        except aiomqtt.exceptions.MqttError as e:
+            print(f"MQTT Error: {e}, attempting to reconnect in 10 seconds...")
+            await asyncio.sleep(10)  # Wait for 10 seconds before reconnecting
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            break  # Exit the loop if an unexpected error occurs
+
+
+async def main(config_path: str) -> None:
+    """Main function to setup and run the application."""
+    config = Config(config_path)
+    main_task = asyncio.create_task(mqtt_client_task(config))
+
+    def signal_handler():
+        print("Caught SIGINT signal, stopping...")
+        main_task.cancel()
+
+    loop = asyncio.get_running_loop()
+    loop.add_signal_handler(signal.SIGINT, signal_handler)
+
+    try:
+        await main_task
+    except asyncio.CancelledError:
+        print("Main task cancelled, application shutting down.")
+    finally:
+        print("Cleaning up...")
+
+
+@click.command()
+@click.option(
+    "--config", default="computer2mqtt.yaml", help="Path to the configuration file."
+)
+def sync_main(config: str):
+    """Synchronously runs the main function with the provided configuration file."""
+    print(f"Using configuration file: {config}")
+    asyncio.run(main(config))
+
+
+if __name__ == "__main__":
+    sync_main()
