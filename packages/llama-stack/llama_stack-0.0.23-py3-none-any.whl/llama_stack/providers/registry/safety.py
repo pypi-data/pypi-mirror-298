3aafb11a42de176ae61a85d6e3a276fd785860a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 17:04:01 +0000

    Add package llama-stack/0.0.23/llama_stack-0.0.23-py3-none-any.whl

---
+++ packages/llama-stack/llama_stack-0.0.23-py3-none-any.whl/llama_stack/providers/registry/safety.py
@@ -0,0 +1,35 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the terms described in the LICENSE file in
+# the root directory of this source tree.
+
+from typing import List
+
+from llama_stack.distribution.datatypes import *  # noqa: F403
+
+
+def available_providers() -> List[ProviderSpec]:
+    return [
+        InlineProviderSpec(
+            api=Api.safety,
+            provider_id="meta-reference",
+            pip_packages=[
+                "accelerate",
+                "codeshield",
+                "torch",
+                "transformers",
+            ],
+            module="llama_stack.providers.impls.meta_reference.safety",
+            config_class="llama_stack.providers.impls.meta_reference.safety.SafetyConfig",
+        ),
+        remote_provider_spec(
+            api=Api.safety,
+            adapter=AdapterSpec(
+                adapter_id="sample",
+                pip_packages=[],
+                module="llama_stack.providers.adapters.safety.sample",
+                config_class="llama_stack.providers.adapters.safety.sample.SampleConfig",
+            ),
+        ),
+    ]
