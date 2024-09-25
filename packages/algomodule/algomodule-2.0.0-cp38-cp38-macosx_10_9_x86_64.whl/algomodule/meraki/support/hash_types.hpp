5378130b10b1c84c6d287fb86acef3e611b00f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 18:04:25 +0000

    Add package algomodule/2.0.0/algomodule-2.0.0-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/algomodule/algomodule-2.0.0-cp38-cp38-macosx_10_9_x86_64.whl/algomodule/meraki/support/hash_types.hpp
@@ -0,0 +1,15 @@
+// meraki: C/C++ implementation of Meraki, the Telestai Proof of Work algorithm.
+// Copyright 2018-2019 Pawel Bylica.
+// Licensed under the Apache License, Version 2.0.
+
+#pragma once
+
+#include "algomodule/meraki/support/hash_types.h"
+
+namespace meraki
+{
+using hash256 = meraki_hash256;
+using hash512 = meraki_hash512;
+using hash1024 = meraki_hash1024;
+using hash2048 = meraki_hash2048;
+}  // namespace meraki
