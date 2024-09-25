30ca69ffdf04cdf9030ded22660fb02271fefab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 19:03:31 +0000

    Add package powermake/1.13.1/powermake-1.13.1.tar.gz

---
+++ packages/powermake/powermake-1.13.1.tar.gz/powermake-1.13.1/powermake/linkers/gnu.py
@@ -0,0 +1,90 @@
+# Copyright 2024 Macéo Tuloup
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import typing as T
+
+from ..tools import translate_flags
+from .common import Linker
+
+_powermake_flags_to_gnu_flags: T.Dict[str, T.List[str]] = {
+    "-ffuzzer": [],
+    "-Weverything": ["-Wall", "-Wextra"],
+}
+
+_powermake_flags_to_clang_flags: T.Dict[str, T.List[str]] = {
+    "-ffuzzer": ["-fsanitize=address,fuzzer"]
+}
+
+_powermake_flags_to_ld_flags: T.Dict[str, T.List[str]] = {
+    "-ffuzzer": [],
+    "-Weverything": [],
+    "-Wall": [],
+    "-Wextra": [],
+    "-m32": [],
+    "-m64": []
+}
+
+
+class LinkerGNU(Linker):
+    type: T.ClassVar = "gnu"
+    exe_extension: T.ClassVar = ""
+    translation_dict: T.ClassVar = _powermake_flags_to_gnu_flags
+
+    def __init__(self, path: str = "cc"):
+        super().__init__(path)
+
+    @classmethod
+    def format_args(self, shared_libs: T.List[str], flags: T.List[str]) -> T.List[str]:
+        return ["-l" + lib for lib in shared_libs] + translate_flags(flags, self.translation_dict)
+
+    def basic_link_command(self, outputfile: str, objectfiles: T.Iterable[str], archives: T.List[str] = [], args: T.List[str] = []) -> T.List[str]:
+        return [self.path, "-o", outputfile, *objectfiles, *archives, *args]
+
+
+class LinkerLD(LinkerGNU):
+    type: T.ClassVar = "ld"
+    translation_dict: T.ClassVar = _powermake_flags_to_ld_flags
+
+    def __init__(self, path: str = "ld"):
+        super().__init__(path)
+
+
+class LinkerGCC(LinkerGNU):
+    type: T.ClassVar = "gcc"
+
+    def __init__(self, path: str = "gcc"):
+        super().__init__(path)
+
+
+class LinkerGPlusPlus(LinkerGNU):
+    type: T.ClassVar = "g++"
+
+    def __init__(self, path: str = "g++"):
+        super().__init__(path)
+
+
+class LinkerClang(LinkerGNU):
+    type: T.ClassVar = "clang"
+    translation_dict: T.ClassVar = _powermake_flags_to_clang_flags
+
+    def __init__(self, path: str = "clang"):
+        super().__init__(path)
+
+
+class LinkerClangPlusPlus(LinkerGNU):
+    type: T.ClassVar = "clang++"
+    translation_dict: T.ClassVar = _powermake_flags_to_clang_flags
+
+    def __init__(self, path: str = "clang++"):
+        super().__init__(path)
