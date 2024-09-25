ef02962f1453bb196d4dc2e7d6bb8a7e94407125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 20:04:13 +0000

    Add package iteach-dhyolo/0.1/iteach_dhyolo-0.1.tar.gz

---
+++ packages/iteach-dhyolo/iteach_dhyolo-0.1.tar.gz/iteach_dhyolo-0.1/dhyolo/model.py
@@ -0,0 +1,15 @@
+from detect import run as run_detection
+
+class Predict:
+    def __init__(self, image_path, model_path):
+        self.image_path = image_path
+        self.model_path = model_path
+
+    def get_inference(self):
+        run_detection(
+            weights = self.model_path,
+            source = self.image_path
+        )
+    
+
+
