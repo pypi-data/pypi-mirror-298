3141ac5928afa797dfe6a3df9b7122643c1d84d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 21:04:27 +0000

    Add package bourguiba/0.1.0/bourguiba-0.1.0.tar.gz

---
+++ packages/bourguiba/bourguiba-0.1.0.tar.gz/bourguiba-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bourguiba',
+    version='0.1.0',
+    description='A Python library to generate commands based on descriptions using T5',
+    author='si mohamed aziz bahloul wo chrikou si mahdi magroun',
+    author_email='azizbahloul3@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'transformers'
+    ],
+    entry_points={
+        'console_scripts': [
+            'bourguiba=bourguiba.bourguiba:main',
+        ],
+    },
+    include_package_data=True,
+)
