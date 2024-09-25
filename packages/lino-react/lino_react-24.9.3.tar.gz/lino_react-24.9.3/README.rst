e371b4cb611399af2ecd445f92d3bb8e5a069e1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 17:04:40 +0000

    Add package lino-react/24.9.3/lino_react-24.9.3.tar.gz

---
+++ packages/lino-react/lino_react-24.9.3.tar.gz/lino_react-24.9.3/README.rst
@@ -0,0 +1,13 @@
+==========================
+The ``lino_react`` package
+==========================
+
+
+
+
+The React front end for Lino
+
+Project homepage is https://gitlab.com/lino-framework/react
+
+
+
---
+++ packages/lino-react/lino_react-24.9.3.tar.gz/lino_react-24.9.3/lino_react/__init__.py
@@ -0,0 +1,15 @@
+"""
+.. autosummary::
+   :toctree:
+
+    react
+
+"""
+
+from .setup_info import SETUP_INFO
+
+__version__ = SETUP_INFO['version']
+
+srcref_url = 'https://gitlab.com/lino-framework/react/blob/master/%s'
+# doc_trees = []
+intersphinx_urls = dict(docs="https://lino-framework.gitlab.io/react/")
