18384c50b5db349d6663f56119038320ba64b17b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 16:04:10 +0000

    Add package ngi-calculations/0.1.5/ngi_calculations-0.1.5-py3-none-any.whl

---
+++ packages/ngi-calculations/ngi_calculations-0.1.5-py3-none-any.whl/ngi_calculations/cpt_correlations/methods/cpt_process/options.py
@@ -0,0 +1,11 @@
+from typing import Literal
+
+from pydantic.main import BaseModel
+
+
+class CptProcessOptions(BaseModel):
+    elevation: float = 0
+    shift_depth: float = 0.0  # Shift CPT depth (in m, negative value shifts upwards)
+    adjust_depth_tilt: bool = False  # Adjust depth due to tilt
+    compensate_atm_pressure: bool = False
+    interpolation_mode: Literal["linear", "padding"] = "linear"
