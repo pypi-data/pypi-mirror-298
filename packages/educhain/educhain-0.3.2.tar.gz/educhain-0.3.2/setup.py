8e3ffc88a8f6a91891531021705e279b5ebacdb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 19:04:16 +0000

    Add package educhain/0.3.2/educhain-0.3.2.tar.gz

---
+++ packages/educhain/educhain-0.3.2.tar.gz/educhain-0.3.2/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="educhain",
+    version="0.3.2",
+    packages=find_packages(),
+    install_requires=[
+        "langchain",
+        "langchain-community",
+        "langchain-openai",
+        "openai",
+        "python-dotenv", 
+        "pandas",
+        "reportlab",
+        "PyPDF2",
+        "beautifulsoup4",
+        "youtube_transcript_api",
+        "pydantic",
+        "requests",
+        "chromadb",
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "black",
+            "flake8",
+        ],
+    },
+    author="Satvik Paramkusham",
+    author_email="satvik@buildfastwithai.com",
+    description="A Python package for generating educational content using Generative AI",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/satvik314/educhain",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires='>=3.7',
+)
