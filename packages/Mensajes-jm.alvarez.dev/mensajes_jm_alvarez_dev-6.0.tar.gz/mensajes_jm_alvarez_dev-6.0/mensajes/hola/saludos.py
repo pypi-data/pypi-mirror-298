adf3e427e777652808ab6f8a02678bac97c39290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 20:04:05 +0000

    Add package Mensajes-jm.alvarez.dev/6.0/mensajes_jm_alvarez_dev-6.0.tar.gz

---
+++ packages/Mensajes-jm.alvarez.dev/mensajes_jm_alvarez_dev-6.0.tar.gz/mensajes_jm_alvarez_dev-6.0/mensajes/hola/saludos.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def saludar():
+    print("Hola, te saludo desde saludos.saludar()")
+
+def prueba():
+    print("Esto es una nueva prueba de la nueva version 6.0.")
+
+def generar_array(numeros):
+      return np.arange(numeros)
+
+class Saludos:
+    def __init__(self):
+        print("Hola, te saludo desde Saludos.__init__()")
+
+if __name__=='__main__':
+   print(generar_array(5))
