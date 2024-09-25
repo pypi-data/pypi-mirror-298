870a307ac00c3291fcd6ac80e73780534043f683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 21:03:58 +0000

    Add package facturacion-electronica/0.19.8/facturacion_electronica-0.19.8.tar.gz

---
+++ packages/facturacion-electronica/facturacion_electronica-0.19.8.tar.gz/facturacion_electronica-0.19.8/facturacion_electronica/signature_cert.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+from facturacion_electronica import clase_util as util
+import datetime
+from OpenSSL.crypto import load_pkcs12
+from OpenSSL.crypto import dump_privatekey
+from OpenSSL.crypto import dump_certificate
+from OpenSSL.crypto import FILETYPE_PEM
+import base64
+import logging
+
+
+_logger = logging.getLogger(__name__)
+type_ = FILETYPE_PEM
+
+
+class SignatureCert(object):
+
+    def __init__(self, signature={}):
+        if not signature:
+            return
+        util.set_from_keys(self, signature, priorizar=['string_firma', 'string_password'])
+
+    @property
+    def errores(self):
+        if not hasattr(self, '_errores'):
+            return []
+        return self._errores
+
+    @errores.setter
+    def errores(self, val):
+        _errores = self.errores
+        _errores.append(val)
+        self._errores = _errores
+
+    @property
+    def cert(self):
+        if not hasattr(self, '_cert'):
+            return False
+        return self._cert
+
+    @cert.setter
+    def cert(self, val):
+        self._cert = val
+
+    @property
+    def init_signature(self):
+        if not hasattr(self, '_init_signature'):
+            return True
+        return self._init_signature
+
+    @init_signature.setter
+    def init_signature(self, val):
+        self._init_signature = val
+        if not val:
+            return
+        try:
+            p12 = load_pkcs12(self.string_firma, self.string_password)
+        except Exception as e:
+            err = str(e)
+            if "'mac verify failure'" in err:
+                self.errores = "Error en clave del Certificado, verificar que esté correcta: %s" % err
+            else:
+                self.errores = "Error en apertura de archivo: %s" % err
+            _logger.warning(str(e), exc_info=True)
+            return
+        try:
+            cert = p12.get_certificate()
+            privky = p12.get_privatekey()
+            cacert = p12.get_ca_certificates()
+            issuer = cert.get_issuer()
+            subject = cert.get_subject()
+            self.not_before = datetime.datetime.strptime(cert.get_notBefore().decode("utf-8"), '%Y%m%d%H%M%SZ')
+            self.not_after = datetime.datetime.strptime(cert.get_notAfter().decode("utf-8"), '%Y%m%d%H%M%SZ')
+            self.subject_c = subject.C
+            self.subject_title = subject.title
+            self.subject_common_name = subject.CN
+            if subject.serialNumber:
+                self.subject_serial_number = subject.serialNumber
+            self.subject_email_address = subject.emailAddress
+            self.issuer_country = issuer.C
+            self.issuer_organization = issuer.O
+            self.issuer_common_name = issuer.CN
+            self.issuer_serial_number = issuer.serialNumber
+            self.issuer_email_address = issuer.emailAddress
+            self.status = 'expired' if cert.has_expired() else 'valid'
+            self.cert_serial_number = cert.get_serial_number()
+            self.cert_signature_algor = cert.get_signature_algorithm()
+            self.cert_version = cert.get_version()
+            self.cert_hash = cert.subject_name_hash()
+            self.private_key_bits = privky.bits()
+            self.private_key_check = privky.check()
+            self.private_key_type = privky.type()
+            self.cacert = cacert
+            certificate = p12.get_certificate()
+            private_key = p12.get_privatekey()
+            self.priv_key = dump_privatekey(type_, private_key)
+            self.cert = dump_certificate(type_, certificate)
+        except Exception as e:
+            msg = "Error en obtención de datos de la firma: %s" % str(e)
+            _logger.warning(msg, exc_info=True)
+            self.errores = msg
+
+    @property
+    def priv_key(self):
+        if not hasattr(self, '_priv_key'):
+            return False
+        return self._priv_key
+
+    @priv_key.setter
+    def priv_key(self, val):
+        if type(val) is str:
+            val = val.encode()
+        self._priv_key = val
+
+    @property
+    def rut_firmante(self):
+        return self.subject_serial_number
+
+    @rut_firmante.setter
+    def rut_firmante(self, val):
+        self.subject_serial_number = val
+
+    @property
+    def string_password(self):
+        if not hasattr(self, '_string_password'):
+            return False
+        return self._string_password
+
+    @string_password.setter
+    def string_password(self, val):
+        if isinstance(val, str):
+            val = val.encode('ISO-8859-1')
+        self._string_password = val
+
+    @property
+    def string_firma(self):
+        if not hasattr(self, '_string_firma'):
+            return False
+        return self._string_firma
+
+    @string_firma.setter
+    def string_firma(self, val):
+        self._string_firma = base64.b64decode(val)
+
+    @property
+    def subject_serial_number(self):
+        if not hasattr(self, '_subject_serial_number'):
+            return False
+        return self._subject_serial_number
+
+    @subject_serial_number.setter
+    def subject_serial_number(self, val):
+        self._subject_serial_number = val
