66dac29fae7f03e8fc367ef369afc1977dddda6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 19:04:11 +0000

    Add package GoktugTest/0.1.0/GoktugTest-0.1.0.tar.gz

---
+++ packages/GoktugTest/GoktugTest-0.1.0.tar.gz/GoktugTest-0.1.0/Packages/my_module.py
@@ -0,0 +1,39 @@
+# my_package/my_module.py
+
+def greet(name):
+    """
+    Returns a greeting message.
+    
+    Parameters:
+    name (str): The name of the person to greet.
+
+    Returns:
+    str: Greeting message.
+    """
+    return f"Hello, {name}!"
+
+def add(a, b):
+    """
+    Returns the sum of two numbers.
+    
+    Parameters:
+    a (int or float): The first number.
+    b (int or float): The second number.
+
+    Returns:
+    int or float: The sum of the two numbers.
+    """
+    return a + b
+
+def subtract(a, b):
+    """
+    Returns the difference between two numbers.
+    
+    Parameters:
+    a (int or float): The first number.
+    b (int or float): The second number.
+
+    Returns:
+    int or float: The difference between the two numbers.
+    """
+    return a - b
