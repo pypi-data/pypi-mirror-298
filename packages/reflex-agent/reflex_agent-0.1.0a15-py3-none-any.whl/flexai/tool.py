2316ea087c5395e290a4dc2ad3177a8039983efa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 18:04:10 +0000

    Add package reflex-agent/0.1.0a15/reflex_agent-0.1.0a15-py3-none-any.whl

---
+++ packages/reflex-agent/reflex_agent-0.1.0a15-py3-none-any.whl/flexai/tool.py
@@ -0,0 +1,106 @@
+"""Base class to define agent tools."""
+
+# from __future__ import annotations
+
+import inspect
+from dataclasses import dataclass
+from typing import Callable
+
+
+@dataclass(frozen=True)
+class Tool:
+    """A tool is a function that can be called by an agent."""
+
+    # The name of the tool.
+    name: str
+
+    # A description of how the tool works - this should be detailed
+    description: str
+
+    # The function parameters and their types.
+    params: tuple[tuple[str, str], ...]
+
+    # The return type of the function.
+    return_type: str
+
+    # The function to call.
+    fn: Callable
+
+    @classmethod
+    def from_function(cls, func: Callable) -> "Tool":
+        """Create a tool from a function.
+
+        Args:
+            func: The function to convert to a tool.
+
+        Returns:
+            A new tool instance.
+        """
+        signature = inspect.signature(func)
+        params = tuple(
+            (
+                (
+                    name,
+                    (
+                        param.annotation.__name__
+                        if hasattr(param.annotation, "__name__")
+                        else "No annotation"
+                    ),
+                )
+            )
+            for name, param in signature.parameters.items()
+        )
+        return_type = (
+            signature.return_annotation.__name__
+            if hasattr(signature.return_annotation, "__name__")
+            else "No annotation"
+        )
+        description = inspect.getdoc(func) or "No description"
+        return cls(
+            name=func.__name__,
+            description=description,
+            params=params,
+            return_type=return_type,
+            fn=func,
+        )
+
+    def to_description(self) -> dict:
+        """Convert the tool to a description.
+
+        Returns:
+            A dictionary with the tool description.
+        """
+        type_map = {
+            "str": "string",
+            "int": "number",
+            "float": "number",
+            "bool": "boolean",
+            "list": "array",
+            "dict": "object",
+        }
+
+        input_schema = {
+            "type": "object",
+            "properties": {},
+        }
+        for param_name, param_type in self.params:
+            param_type = type_map.get(str(param_type), param_type)
+            input_schema["properties"][param_name] = {
+                "type": param_type,
+            }
+
+        description = {
+            "name": self.name,
+            "description": self.description,
+            "input_schema": input_schema,
+        }
+        return description
+
+
+def send_message(message: str) -> None:
+    """Send a final message to the user. This should be done after all internal processing is completed.
+
+    Args:
+        message: The message to send to the user.
+    """
+    pass
