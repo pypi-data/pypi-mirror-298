ab8aa34444bdaeba115b8415f5b6da6716a7557a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 19:04:04 +0000

    Add package pipefunc/0.33.0/pipefunc-0.33.0-py3-none-any.whl

---
+++ packages/pipefunc/pipefunc-0.33.0-py3-none-any.whl/pipefunc/__init__.py
@@ -0,0 +1,20 @@
+"""PipeFunc: A Python library for defining, managing, and executing function pipelines."""
+
+from pipefunc import cache, lazy, map, resources, sweep
+from pipefunc._pipefunc import ErrorSnapshot, NestedPipeFunc, PipeFunc, pipefunc
+from pipefunc._pipeline import Pipeline
+from pipefunc._version import __version__
+
+__all__ = [
+    "__version__",
+    "pipefunc",
+    "PipeFunc",
+    "Pipeline",
+    "NestedPipeFunc",
+    "ErrorSnapshot",
+    "cache",
+    "lazy",
+    "map",
+    "resources",
+    "sweep",
+]
