5e71479827356571e3d009ad868f63e1f332b137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 18:04:01 +0000

    Add package PicImageSearch/3.10.13/picimagesearch-3.10.13-py3-none-any.whl

---
+++ packages/PicImageSearch/picimagesearch-3.10.13-py3-none-any.whl/PicImageSearch/ehentai.py
@@ -0,0 +1,97 @@
+from pathlib import Path
+from typing import Any, Optional, Union
+
+from .model import EHentaiResponse
+from .network import HandOver
+from .utils import read_file
+
+
+class EHentai(HandOver):
+    """API client for the EHentai image search engine.
+
+    Used for performing reverse image searches using EHentai service.
+
+    Attributes:
+        base_url: The base URL for EHentai searches.
+        base_url_ex: The base URL for EXHentai searches.
+        covers: A flag to search only for covers.
+        similar: A flag to enable similarity scanning.
+        exp: A flag to include results from expunged galleries.
+    """
+
+    def __init__(
+        self,
+        base_url: str = "https://upld.e-hentai.org",
+        base_url_ex: str = "https://upld.exhentai.org",
+        covers: bool = False,
+        similar: bool = True,
+        exp: bool = False,
+        **request_kwargs: Any,
+    ):
+        """Initializes an EHentai API client with specified configurations.
+
+        Args:
+            base_url: The base URL for EHentai searches.
+            base_url_ex: The base URL for EXHentai searches.
+            covers: If True, search only for covers; otherwise, False.
+            similar: If True, enable similarity scanning; otherwise, False.
+            exp: If True, include results from expunged galleries; otherwise, False.
+            **request_kwargs: Additional arguments for network requests.
+        """
+        super().__init__(**request_kwargs)
+        self.base_url = base_url
+        self.base_url_ex = base_url_ex
+        self.covers: bool = covers
+        self.similar: bool = similar
+        self.exp: bool = exp
+
+    async def search(
+        self,
+        url: Optional[str] = None,
+        file: Union[str, bytes, Path, None] = None,
+        ex: bool = False,
+    ) -> EHentaiResponse:
+        """Performs a reverse image search on EHentai.
+
+        Supports searching by image URL or by uploading an image file.
+
+        Requires either 'url' or 'file' to be provided.
+
+        Args:
+            url: URL of the image to search.
+            file: Local image file (path or bytes) to search.
+            ex: If True, search on exhentai.org; otherwise, use e-hentai.org.
+
+        Returns:
+            EHentaiResponse: Contains search results and additional information.
+
+        Raises:
+            ValueError: If neither 'url' nor 'file' is provided.
+
+        Note:
+            Searching on exhentai.org requires logged-in status via cookies in `EHentai.request_kwargs`.
+        """
+        if not url and not file:
+            raise ValueError("Either 'url' or 'file' must be provided")
+
+        _url: str = (
+            f"{self.base_url_ex}/upld/image_lookup.php"
+            if ex
+            else f"{self.base_url}/image_lookup.php"
+        )
+        data: dict[str, Any] = {"f_sfile": "File Search"}
+
+        if url:
+            files: dict[str, Any] = {"sfile": await self.download(url)}
+        else:
+            files = {"sfile": read_file(file)}
+
+        if self.covers:
+            data["fs_covers"] = "on"
+        if self.similar:
+            data["fs_similar"] = "on"
+        if self.exp:
+            data["fs_exp"] = "on"
+
+        resp = await self.post(url=_url, data=data, files=files)
+        return EHentaiResponse(resp.text, resp.url)
---
+++ packages/PicImageSearch/picimagesearch-3.10.13-py3-none-any.whl/PicImageSearch/iqdb.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+from typing import Any, Optional, Union
+
+from .model import IqdbResponse
+from .network import HandOver
+from .utils import read_file
+
+
+class Iqdb(HandOver):
+    """API client for the Iqdb image search engine.
+
+    Used for performing reverse image searches using Iqdb service.
+
+    Attributes:
+        base_url: The base URL for Iqdb searches.
+        base_url_3d: The base URL for Iqdb 3D searches.
+    """
+
+    def __init__(
+        self,
+        base_url: str = "https://iqdb.org",
+        base_url_3d: str = "https://3d.iqdb.org",
+        **request_kwargs: Any
+    ):
+        """Initializes an Iqdb API client with request configuration.
+
+        Args:
+            base_url: The base URL for Iqdb searches.
+            base_url_3d: The base URL for Iqdb 3D searches.
+            **request_kwargs: Additional arguments for network requests.
+        """
+        super().__init__(**request_kwargs)
+        self.base_url = base_url
+        self.base_url_3d = base_url_3d
+
+    async def search(
+        self,
+        url: Optional[str] = None,
+        file: Union[str, bytes, Path, None] = None,
+        force_gray: bool = False,
+        is_3d: bool = False,
+    ) -> IqdbResponse:
+        """Performs a reverse image search on Iqdb.
+
+        Supports searching by image URL or by uploading an image file.
+
+        Requires either 'url' or 'file' to be provided.
+
+        Args:
+            url: URL of the image to search.
+            file: Local image file (path or bytes) to search.
+            force_gray: If True, ignores color information in the image.
+            is_3d: If True, searches on 3d.iqdb.org for real-life images; otherwise, iqdb.org for anime images.
+
+        Returns:
+            IqdbResponse: Contains search results and additional information.
+
+        Raises:
+            ValueError: If neither 'url' nor 'file' is provided.
+
+        Note:
+            Search can be tailored for anime or real-life images using `is_3d` parameter.
+        """
+        if not url and not file:
+            raise ValueError("Either 'url' or 'file' must be provided")
+
+        iqdb_url = self.base_url_3d if is_3d else self.base_url
+        data: dict[str, Any] = {}
+
+        if force_gray:
+            data["forcegray"] = "on"
+        if url:
+            data["url"] = url
+            resp = await self.post(iqdb_url, data=data)
+        else:
+            files = {"file": read_file(file)}
+            resp = await self.post(iqdb_url, data=data, files=files)
+
+        return IqdbResponse(resp.text)
