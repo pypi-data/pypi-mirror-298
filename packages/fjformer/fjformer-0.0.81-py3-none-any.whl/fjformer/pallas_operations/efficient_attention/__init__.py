3813bb79c4a2dfd19ed878b388dc82375ebf2777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 17:04:24 +0000

    Add package fjformer/0.0.81/fjformer-0.0.81-py3-none-any.whl

---
+++ packages/fjformer/fjformer-0.0.81-py3-none-any.whl/fjformer/pallas_operations/efficient_attention/__init__.py
@@ -0,0 +1,3 @@
+from fjformer.pallas_operations.efficient_attention.efficient_attention import (
+	efficient_attention as efficient_attention,
+)
---
+++ packages/fjformer/fjformer-0.0.81-py3-none-any.whl/fjformer/pallas_operations/gpu/rms_norm/__init__.py
@@ -0,0 +1,1 @@
+from fjformer.pallas_operations.gpu.rms_norm.rms_norm import rms_norm as rms_norm
