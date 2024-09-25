093fd08518867d82cb4cfa88b5d40a529c26f051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 14:03:33 +0000

    Add package django-invoicing/8.0.0/django_invoicing-8.0.0.tar.gz

---
+++ packages/django-invoicing/django_invoicing-8.0.0.tar.gz/django_invoicing-8.0.0/invoicing/taxation/eu.py
@@ -0,0 +1,150 @@
+from decimal import Decimal
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured, ValidationError
+from django.core.validators import EMPTY_VALUES
+from internationalflavor.vat_number import VATNumberValidator
+
+from invoicing.taxation import TaxationPolicy
+
+
+class EUTaxationPolicy(TaxationPolicy):
+    """
+    This taxation policy should be correct for all EU countries. It uses following rules:
+        * if supplier country is not in EU - assert error,
+        * return 'default tax' in cases:
+            * if supplier country and customer country are the same,
+            * if supplier country and customer country are not the same, but customer is private person from EU,
+            * if supplier country and customer country are not the same, customer is company from EU, but his VAT ID is not valid according VIES system.
+        * return tax not applicable (None) in cases:
+            * if supplier country and customer country are not the same, customer is company from EU and his tax id is valid according VIES system.
+            * if supplier country and customer country are not the same and customer is private person not from EU,
+            * if supplier country and customer country are not the same and customer is company not from EU.
+
+
+    Please note, that term "private person" refers in system to user that did not provide tax ID and
+    ``company`` refers to user that provides it.
+
+    """
+    EU_COUNTRIES_RATES = {
+        'AT': Decimal(20),  # Austria
+        'BE': Decimal(21),  # Belgium
+        'BG': Decimal(20),  # Bulgaria
+        'CY': Decimal(19),  # Cyprus
+        'CZ': Decimal(21),  # Czech Republic
+        'DK': Decimal(25),  # Denmark
+        'EE': Decimal(22),  # Estonia
+        'FI': Decimal(24),  # Finland
+        'FR': Decimal(20),  # France
+        'DE': Decimal(19),  # Germany
+        'GR': Decimal(24),  # Greece
+        'HR': Decimal(25),  # Croatia
+        'HU': Decimal(27),  # Hungary
+        'IE': Decimal(23),  # Ireland
+        'IT': Decimal(22),  # Italy
+        'LV': Decimal(21),  # Latvia
+        'LT': Decimal(21),  # Lithuania
+        'LU': Decimal(17),  # Luxembourg
+        'MT': Decimal(18),  # Malta
+        'NL': Decimal(21),  # Netherlands
+        'PL': Decimal(23),  # Poland
+        'PT': Decimal(23),  # Portugal
+        'RO': Decimal(19),  # Romania
+        'SK': Decimal(20),  # Slovakia
+        'SI': Decimal(22),  # Slovenia
+        'ES': Decimal(21),  # Spain
+        'SE': Decimal(25),  # Sweden
+        # 'GB': Decimal(20),  # United Kingdom (Great Britain)
+    }
+
+    @classmethod
+    def is_in_EU(cls, country_code):
+        return country_code.upper() in cls.EU_COUNTRIES_RATES.keys()
+
+    @classmethod
+    def is_reverse_charge(cls, invoice, delivery_country=None, check_items=True):
+        supplier_country = invoice.supplier_country.code if invoice.supplier_country else None
+
+        if not supplier_country:
+            supplier_country = cls.get_supplier_country_code()
+
+        # Supplier has to be from EU
+        if not cls.is_in_EU(supplier_country):
+            return False
+
+        # Supplier VAT ID has to be set
+        if invoice.supplier_vat_id in EMPTY_VALUES:
+            return False
+
+        # Customer VAT ID has to be set
+        if invoice.customer_vat_id in EMPTY_VALUES:
+            return False
+
+        # supplier and delivery countries have to be different
+        place_of_supply = delivery_country or invoice.customer_country
+
+        # missing place of supply
+        if place_of_supply in EMPTY_VALUES:
+            return False
+
+        # same supplier country as place of supply
+        if invoice.supplier_country == place_of_supply:
+            return False
+
+        # there has to be at least one invoice item with None tax rate
+        if check_items and invoice.item_set.exists() and not invoice.item_set.filter(tax_rate=None).exists():
+            return False
+
+        # customer has to be from EU -> not True (for example Great Britain)
+        # if not invoice.is_EU_customer():
+        #     return False
+
+        return True
+
+    @classmethod
+    def get_default_tax(cls, country_code=None):
+        """
+        Gets default tax rate.``
+
+        :return: Decimal()
+        """
+
+        default_tax_rate = super().get_default_tax(country_code)
+
+        # tax rate by country
+        if country_code and not hasattr(settings, 'INVOICING_TAX_RATE'):
+            return cls.EU_COUNTRIES_RATES.get(country_code, default_tax_rate)
+
+        return default_tax_rate
+
+    @classmethod
+    def get_tax_rate(cls, invoice):
+        if invoice.supplier_vat_id in EMPTY_VALUES:
+            # Supplier is not a VAT payer
+            return None
+
+        # customer_country = invoice.customer_country.code if invoice.customer_country else None
+        supplier_country = invoice.supplier_country.code if invoice.supplier_country else None
+
+        if not supplier_country:
+            supplier_country = cls.get_supplier_country_code()
+
+        if not cls.is_in_EU(supplier_country):
+            raise ImproperlyConfigured("EUTaxationPolicy requires that supplier country is in EU")
+
+        # Reverse charge
+        if cls.is_reverse_charge(invoice, check_items=False):
+            if not getattr(settings, 'INVOICING_USE_VIES_VALIDATOR', True):
+                return None
+
+            try:
+                # Verify VAT ID in VIES
+                VATNumberValidator(eu_only=True, vies_check=True)(invoice.customer_vat_id)
+
+                # Company is registered in VIES
+                # Charge back
+                return None
+            except ValidationError:
+                pass
+
+        # return default tax
+        return cls.get_default_tax(supplier_country)
---
+++ packages/django-invoicing/django_invoicing-8.0.0.tar.gz/django_invoicing-8.0.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+from invoicing import VERSION
+
+setup(
+    name='django-invoicing',
+    version=VERSION,
+    description='Django app for invoicing.',
+    long_description=open('README.rst').read(),
+    author='Pragmatic Mates',
+    author_email='info@pragmaticmates.com',
+    maintainer='Pragmatic Mates',
+    maintainer_email='info@pragmaticmates.com',
+    url='https://github.com/PragmaticMates/django-invoicing',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=(
+        'django>=4.2', 'django-countries', 'django-model-utils', 'django-money',
+        'django-localflavor', 'django-internationalflavor'
+    ),
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: OS Independent',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Development Status :: 3 - Alpha'
+    ],
+    license='GPL License',
+    keywords="django invoice invoicing billing supplier issuer purchaser buyer commerce products taxes vat pdf",
+)
