b1d362c00d570c31e1c85c1fc94e70ec944cdfc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 19:03:55 +0000

    Add package lcmutils/0.1.0/lcmutils-0.1.0-py3-none-any.whl

---
+++ packages/lcmutils/lcmutils-0.1.0-py3-none-any.whl/lcmutils/typing.py
@@ -0,0 +1,35 @@
+from collections.abc import Callable
+from typing import BinaryIO, Protocol, Any
+
+
+class LCMType(Protocol):
+    """
+    LCM type protocol to support static duck typing for LCM type classes generated via lcm-gen.
+    """
+
+    def encode(self) -> bytes: ...
+
+    def _encode_one(self, buf: BinaryIO): ...
+
+    @staticmethod
+    def decode(data: bytes) -> "LCMType": ...
+
+    @staticmethod
+    def _decode_one(buf: BinaryIO) -> "LCMType": ...
+
+    @staticmethod
+    def _get_hash_recursive(parents: list[Any]) -> int: ...
+
+    @staticmethod
+    def _get_packed_fingerprint() -> bytes: ...
+
+    def get_hash(self) -> int: ...
+
+
+class LCMHandler(Callable[[str, bytes], None]):
+    """
+    LCM message handler type.
+    """
+
+
+__all__ = ["LCMType", "LCMHandler"]
