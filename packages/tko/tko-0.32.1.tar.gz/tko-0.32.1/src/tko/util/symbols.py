fa3c4deb867570474388a880678afbdecb394ebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 20:04:32 +0000

    Add package tko/0.32.1/tko-0.32.1.tar.gz

---
+++ packages/tko/tko-0.32.1.tar.gz/tko-0.32.1/src/tko/util/symbols.py
@@ -0,0 +1,59 @@
+from .sentence import Token
+
+class __Symbols:
+
+    def __init__(self):
+        self.downloaded = Token("▼")
+        self.to_download = Token("▽")
+        self.cant_download = Token("◉")
+        self.opening = Token("=> ")
+        self.neutral = Token("»")
+        self.success = Token("✓")
+        self.failure = Token("✗")
+        self.wrong = Token("ω")
+        self.compilation = Token("ϲ")
+        self.execution = Token("ϵ")
+        self.unequal = Token("├")
+        self.equalbar = Token("│")
+        self.hbar = Token("─")
+        self.vbar = Token("│")
+
+        self.whitespace = Token("·")
+        # self.whitespace = Token("␣")
+        
+        # self.newline = Token("¶")
+        self.newline = Token("↲")
+        # self.newline = Token("⏎")
+
+        self.cfill = Token("_")
+        self.tab = Token("    ")
+        self.arrow_up = Token("↑")
+
+        self.check = Token("✓")
+        self.uncheck = Token("✗")
+        # self.opcheck = Token("ⴲ")
+        # self.opuncheck = Token("ⵔ")
+        self.infinity = Token("∞")
+        self.locked_free = Token("⇉")
+        self.locked_locked = Token("⇟")
+        self.left_toggle = Token("━─")
+        self.right_toggle = Token("─━")
+        self.timer = Token("⏰") # ⌛⏰
+        self.diff_down = Token("⇕")
+        self.diff_left = Token("⇔")
+        self.sharpR = Token("")
+        self.sharpL = Token("")
+
+
+    def set_colors(self):
+        self.opening.fmt = "b"
+        self.neutral.fmt = "b"
+        self.success.fmt = "g"
+        self.failure.fmt = "r"
+        self.wrong.fmt = "r"
+        self.compilation.fmt = "y"
+        self.execution.fmt = "y"
+        self.unequal.fmt = "r"
+        self.equalbar.fmt = "g"
+
+symbols = __Symbols()
---
+++ packages/tko/tko-0.32.1.tar.gz/tko-0.32.1/todo.md
@@ -0,0 +1,24 @@
+# Todo
+
+- tempo label do lado to fixar label ao lado dos cases
+- +- label ao lado do tab para habilitar e desabilitar
+- +- no mesmo simbolo se possível
+
+- pasta user para adicionar problemas do usuário
+- paleta embaixo abrindo floating com textos pendurados em functors
+- opção de mostrar quantos testes passaram no principal
+- opção de rodar os testes no computador do aluno
+- for each folder tko test folder --compact
+
+- 3 modos de fazer a tarefa
+  - sozinho
+  - ajuda (grupo, monitor, colega, IA, internet)
+  - guiado (aula, vídeo, tutorial)
+- 3 níveis de compreensão do conteúdo
+  - superficial
+  - intermediário
+  - profundo
+- 3 níveis de dificuldade
+  - fácil
+  - médio
+  - difícil
