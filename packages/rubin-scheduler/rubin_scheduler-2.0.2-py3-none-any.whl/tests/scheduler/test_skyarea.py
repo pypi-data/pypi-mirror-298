3d268ab70e85638a1c6fab077f2410e9a48964a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 19:03:38 +0000

    Add package rubin-scheduler/2.0.2/rubin_scheduler-2.0.2-py3-none-any.whl

---
+++ packages/rubin-scheduler/rubin_scheduler-2.0.2-py3-none-any.whl/tests/scheduler/test_skyarea.py
@@ -0,0 +1,105 @@
+import os
+import unittest
+
+import numpy as np
+
+from rubin_scheduler.data import get_data_dir
+from rubin_scheduler.scheduler.utils import (
+    EuclidOverlapFootprint,
+    SkyAreaGenerator,
+    SkyAreaGeneratorGalplane,
+    make_rolling_footprints,
+)
+
+datadir = os.path.join(get_data_dir(), "scheduler")
+
+
+class TestSkyArea(unittest.TestCase):
+    def setUp(self):
+        self.nside = 32
+
+    def test_make_rolling(self):
+        """Check that we can make rolling footprints"""
+        # Check that we can run with no kwargs
+        footprints = make_rolling_footprints()
+
+        assert footprints is not None
+
+        # check that various kwarg combos work
+        for nslice in [2, 3]:
+            for uniform in [True, False]:
+                for order_roll in [0, 1]:
+                    for n_cycles in [3, 4]:
+                        footprints = make_rolling_footprints(
+                            nslice=nslice,
+                            scale=0.8,
+                            nside=32,
+                            wfd_indx=None,
+                            order_roll=order_roll,
+                            n_cycles=n_cycles,
+                            n_constant_start=2,
+                            n_constant_end=6,
+                            verbose=False,
+                            uniform=uniform,
+                        )
+
+                        assert footprints is not None
+
+    @unittest.skipUnless(os.path.isdir(datadir), "Test data not available.")
+    def test_skyareagenerator(self):
+        # Just test that it sets up and returns maps
+        s = SkyAreaGenerator(nside=self.nside)
+        footprints, labels = s.return_maps()
+        expected_labels = ["", "LMC_SMC", "bulge", "dusty_plane", "lowdust", "nes", "scp", "virgo"]
+        self.assertEqual(set(np.unique(labels)), set(expected_labels))
+        # Check that ratios in the low-dust wfd in r band are 1
+        # This doesn't always have to be the case, but should be with defaults
+        lowdust = np.where(labels == "lowdust")
+        self.assertTrue(np.all(footprints["r"][lowdust] == 1))
+
+    @unittest.skipUnless(os.path.isdir(datadir), "Test data not available.")
+    def test_skyareagenerator_nside(self):
+        # Just check two other likely common nsides
+        for nside in (16, 64):
+            s = SkyAreaGenerator(nside=nside)
+            footprints, labels = s.return_maps()
+            lowdust = np.where(labels == "lowdust")
+            self.assertTrue(np.all(footprints["r"][lowdust] == 1))
+
+    @unittest.skipUnless(os.path.isdir(datadir), "Test data not available.")
+    def test_skyareageneratorgalplane(self):
+        # Just test that it sets up and returns maps
+        s = SkyAreaGeneratorGalplane(nside=self.nside)
+        footprints, labels = s.return_maps()
+        expected_labels = ["", "LMC_SMC", "bulgy", "dusty_plane", "lowdust", "nes", "scp", "virgo"]
+        self.assertEqual(set(np.unique(labels)), set(expected_labels))
+        # Check that ratios in the low-dust wfd in r band are 1
+        # This doesn't always have to be the case, but should be with defaults
+        lowdust = np.where(labels == "lowdust")
+        self.assertTrue(np.all(footprints["r"][lowdust] == 1))
+
+    @unittest.skipUnless(os.path.isdir(datadir), "Test data not available.")
+    def test_euclidoverlapfootprint(self):
+        # Just test that it sets up and returns maps
+        s = EuclidOverlapFootprint(nside=self.nside)
+        footprints, labels = s.return_maps()
+        expected_labels = [
+            "",
+            "LMC_SMC",
+            "bulgy",
+            "dusty_plane",
+            "lowdust",
+            "nes",
+            "scp",
+            "virgo",
+            "euclid_overlap",
+        ]
+        self.assertEqual(set(np.unique(labels)), set(expected_labels))
+        # Check that ratios in the low-dust wfd in r band are 1
+        # This doesn't always have to be the case, but should be with defaults
+        lowdust = np.where(labels == "lowdust")
+        self.assertTrue(np.all(footprints["r"][lowdust] == 1))
+
+
+if __name__ == "__main__":
+    unittest.main()
