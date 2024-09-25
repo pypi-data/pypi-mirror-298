8fe9ce44645a276fd55d3d68f3550845025ba0f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-25 14:03:45 +0000

    Add package bongsang/0.1.0/bongsang-0.1.0.tar.gz

---
+++ packages/bongsang/bongsang-0.1.0.tar.gz/bongsang-0.1.0/README.md
@@ -0,0 +1,8 @@
+# bongsang
+
+A collection of statistics and machine learning libraries developed by Bongsang Kim, including a linear regression class similar to R's `lm` function.
+
+## Installation
+
+```bash
+pip install bongsang
---
+++ packages/bongsang/bongsang-0.1.0.tar.gz/bongsang-0.1.0/setup.py
@@ -0,0 +1,32 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='bongsang',  # Package name is 'bongsang'
+    version='0.1.0',
+    author='Bongsang Kim',
+    author_email='happykbs@gmail.com',
+    description='A collection of statistics and machine learning libraries under the bongsang package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/bongsang/bongsang',  # Update with your repository URL
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.18.0',
+        'pandas>=1.0.0',
+        'matplotlib>=3.0.0',
+        'scikit-learn>=0.22.0',
+        'scipy>=1.4.0',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
