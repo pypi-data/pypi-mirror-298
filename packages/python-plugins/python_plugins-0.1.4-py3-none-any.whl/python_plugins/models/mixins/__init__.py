05b33ef09f78f0549b967461c1511826d831f458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 14:04:59 +0000

    Add package python-plugins/0.1.4/python_plugins-0.1.4-py3-none-any.whl

---
+++ packages/python-plugins/python_plugins-0.1.4-py3-none-any.whl/python_plugins/models/mixins/__init__.py
@@ -0,0 +1,4 @@
+from .primary_key_mixin import PrimaryKeyMixin
+from .timestamp_mixin import TimestampMixin, CreateTimestampMixin, UpdateTimestampMixin
+from .data_mixin import DataMixin
+from .user_minxin import UserMixin
