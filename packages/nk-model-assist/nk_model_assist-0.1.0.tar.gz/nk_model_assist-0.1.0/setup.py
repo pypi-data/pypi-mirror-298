7ba63bd3ec0ab1777183c9525d1e59298a702aef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 15:05:13 +0000

    Add package nk-model-assist/0.1.0/nk_model_assist-0.1.0.tar.gz

---
+++ packages/nk-model-assist/nk_model_assist-0.1.0.tar.gz/nk_model_assist-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='nk-model-assist',
+version='0.1.0',
+author='Nithesh Kumar',
+author_email='nitheshmanimaran@example.com',
+description='This is a package to assist with model training and evaluation. You can use it to train your model and evaluate it. Check out the github repo for more details.',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
