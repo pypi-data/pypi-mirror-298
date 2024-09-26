c7cb219bbcb8fda437fb8cd97d6e7589460ac5ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 16:05:26 +0000

    Add package filesystempro/1.0.4.0/filesystempro-1.0.4.0-py2-none-any.whl

---
+++ packages/filesystempro/filesystempro-1.0.4.0-py2-none-any.whl/filesystem/android/__init__.py
@@ -0,0 +1,31 @@
+## FileSystem - Android
+## This file contains some default directories of your system
+
+## Home location on Android
+home = '/storage/emulated/0'
+## Alarms
+alarms = f'{home}/Alarms/'
+## Android
+android = f'{home}/Android/'
+## DCIM
+dcim = f'{home}/DCIM/'
+## Documents
+documents = f'{home}/Documents/'
+## Download
+downloads = f'{home}/Download/'
+## Movies
+movies = f'{home}/Movies/'
+## Music
+music = f'{home}/Music/'
+## Notifications
+notifications = f'{home}/Notifications/'
+## Pictures
+pictures = f'{home}/Pictures/'
+## Podcasts
+podcasts = f'{home}/Podcasts/'
+## Ringtones
+ringtones = f'{home}/Ringtones/'
+
+def ExternalStorage():
+    print(">> This method is not callable yet")
+    print(">> Future update...")
