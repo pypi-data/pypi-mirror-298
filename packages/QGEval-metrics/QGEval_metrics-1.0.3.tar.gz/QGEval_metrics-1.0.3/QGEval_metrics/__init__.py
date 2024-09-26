c01a10bc29a486a345ccbed6fa2eb7eea8937826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 05:05:22 +0000

    Add package QGEval-metrics/1.0.3/QGEval_metrics-1.0.3.tar.gz

---
+++ packages/QGEval-metrics/QGEval_metrics-1.0.3.tar.gz/QGEval_metrics-1.0.3/QGEval_metrics/__init__.py
@@ -0,0 +1,14 @@
+# QGEval/__init__.py  
+  
+__version__ = '1.0.0'  
+  
+__doc__ = """  
+QGEval 
+============  
+  
+This is a QGEval package. It includes two modules: qg and metrics.  
+  
+"""  
+
+from qg import *
+from metris import *
