2afc7ab13e5e3c288805ae73b9a4afc2e2f9b5d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 05:04:41 +0000

    Add package pytimbr-api/1.0.2/pytimbr_api-1.0.2.tar.gz

---
+++ packages/pytimbr-api/pytimbr_api-1.0.2.tar.gz/pytimbr_api-1.0.2/pytimbr_api/timbr_http_connector.py
@@ -0,0 +1,34 @@
+#
+#             *###              .,              @%             
+#       *%##  `#// %%%*  *@     ``              @%             
+#        #*.    * .%%%`  @@@@*  @@   @@@@,@@@@  @&@@@@   .&@@@*
+#            #%%#   ..   *@     @@  @`  @@` ,@  @%   #@  @@  
+#      ,, .,%(##/./%%#,  *@     @@  @`  @@` ,@  @%   #@  @@   
+#    ,%##%          ``   `/@@*  @@  @`  @@` ,@  (/@@@#/  @@   
+#      ``                                                     
+#  ``````````````````````````````````````````````````````````````
+#  Copyright (C) 2018-2024 timbr.ai
+
+import requests
+
+def executeTimbrQuery(url, ontology, token, query, override_datasource, nested, verify, enableIPv6):
+  datasource_addition = ''
+  if override_datasource:
+    datasource_addition = f'?datasource={override_datasource}'
+  headers = {'Content-Type': 'application/text', 'x-api-key': token, 'nested': nested, 'Connection': 'close'}
+  requests.packages.urllib3.util.connection.HAS_IPV6 = enableIPv6
+  response = requests.post(f'{url}timbr/openapi/ontology/{ontology}/query{datasource_addition}', headers = headers, data = query, verify = verify)
+  return response.json()
+
+def advancedQueryExecute(hostname, port, ontology, token, query, enabled_ssl=True, verify_ssl=True, nested = 'false', enableIPv6 = False, datasource = None):
+  baseUrl = "http"
+  if enabled_ssl == True:
+    baseUrl = baseUrl + "s"
+  baseUrl = f"{baseUrl}://{hostname}:{port}/"
+  return executeTimbrQuery(baseUrl, ontology, token, query, datasource, nested, verify_ssl, enableIPv6)
+
+def simpleQueryExecution(url, ontology, token, query, datasource = None, nested = 'false'):
+  base_url = url
+  if not base_url.endswith('/'):
+    base_url = f'{url}/'
+  return executeTimbrQuery(base_url, ontology, token, query, datasource, nested, True, False)
