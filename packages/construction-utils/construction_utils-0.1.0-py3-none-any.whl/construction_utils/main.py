eadf246846c60f42f24675eec9636809b5060b51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 03:08:30 +0000

    Add package construction-utils/0.1.0/construction_utils-0.1.0-py3-none-any.whl

---
+++ packages/construction-utils/construction_utils-0.1.0-py3-none-any.whl/construction_utils/main.py
@@ -0,0 +1,39 @@
+# Copyright (C) 2024 twyleg
+import argparse
+
+from pathlib import Path
+from simple_python_app.generic_application import GenericApplication
+
+from construction_utils import __version__
+from construction_utils.readme_generator import generate_readmes_for_workspace
+
+
+FILE_DIR = Path(__file__).parent
+
+
+class Application(GenericApplication):
+
+    def __init__(self):
+        # fmt: off
+        super().__init__(
+            application_name="construction_utils",
+            version=__version__,
+            application_config_init_enabled=False,
+            logging_logfile_output_dir=Path.cwd() / ".logs/"
+        )
+        # fmt: on
+
+    def add_arguments(self, argparser: argparse.ArgumentParser):
+        pass
+
+    def run(self, args: argparse.Namespace):
+        generate_readmes_for_workspace(Path.cwd())
+
+
+def main() -> None:
+    application = Application()
+    application.start()
+
+
+if __name__ == "__main__":
+    main()
