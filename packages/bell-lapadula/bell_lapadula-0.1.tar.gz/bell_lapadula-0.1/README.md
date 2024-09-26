97f35582342937414b6dc54cb8e204fc1a8235ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 16:05:46 +0000

    Add package bell-lapadula/0.1/bell_lapadula-0.1.tar.gz

---
+++ packages/bell-lapadula/bell_lapadula-0.1.tar.gz/bell_lapadula-0.1/README.md
@@ -0,0 +1,6 @@
+# Bell-LaPadula Modeli
+
+Bu kutubxona Bell-LaPadula modelini amalga oshirish uchun mo'ljallangan.
+
+## O'rnatish
+
---
+++ packages/bell-lapadula/bell_lapadula-0.1.tar.gz/bell_lapadula-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bell-lapadula",
+    version="0.1",
+    author="Kenjayev",
+    author_email="shahzodkenjayevyangiboyevich@gmail.com",
+    description="Bell-LaPadula modelini amalga oshirish",
+    packages=find_packages(),
+    install_requires=[
+        # Kerakli kutubxonalar ro'yxati
+    ],
+)
