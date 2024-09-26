f9bc76269a64f693f458125aed4e2197d432d1fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 04:04:29 +0000

    Add package MobileInventoryCLI/0.4.532/mobileinventorycli-0.4.532.tar.gz

---
+++ packages/MobileInventoryCLI/mobileinventorycli-0.4.532.tar.gz/mobileinventorycli-0.4.532/MobileInventoryCLI/CodeProcessing/RecordCodesAndBarcodes/DB/testClass.py
@@ -0,0 +1,13 @@
+from MobileInventoryCLI.CodeProcessing.RecordCodesAndBarcodes.DB.PrintLogging import prinL as print
+class EntryExtras:
+    def __add__(self,val):
+        return self.Price+val
+    def __sub__(self,val):
+        return self.Price-val
+    def __truediv__(self,val):
+        return self.Price/val
+    def __mul__(self,val):
+        return self.Price*val
+    def __pow__(self,val):
+        return self.Price**val
+
