5bd7ad94817778c6ed85546932efb6a67b314166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 03:08:52 +0000

    Add package dgtl-pyqldb/1.0.10/dgtl_pyqldb-1.0.10.tar.gz

---
+++ packages/dgtl-pyqldb/dgtl_pyqldb-1.0.10.tar.gz/dgtl_pyqldb-1.0.10/setup.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+from setuptools import setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="dgtl_pyqldb",
+    version="1.0.10",
+    description="AWS DynamoDB python wrapper with backward compatibility",
+    author="Olivier Witteman",
+    license="MIT",
+    packages=["dgtl_pyqldb"],
+    install_requires=["boto3",
+                      "pandas", "uuid"],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+    ]
+)
+
