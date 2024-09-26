df048de84ada24deb65059bc0cb32d0c3167c559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 05:04:37 +0000

    Add package omlish/0.0.0.dev33/omlish-0.0.0.dev33-py3-none-any.whl

---
+++ packages/omlish/omlish-0.0.0.dev33-py3-none-any.whl/omlish/__about__.py
@@ -0,0 +1,147 @@
+__version__ = '0.0.0.dev33'
+__revision__ = 'd7a0b08ef5d46a2402c157972b3b25f71ed628ca-dirty'
+
+
+#
+
+
+class ProjectBase:
+    name: str | None = None
+    authors = [{'name': 'wrmsr'}]
+    urls = {'source': 'https://github.com/wrmsr/omlish'}
+    license = {'text': 'BSD-3-Clause'}
+    requires_python = '~=3.12'
+
+    version = __version__
+
+    classifiers = [
+        'License :: OSI Approved :: BSD License',
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+
+        'Operating System :: OS Independent',
+        'Operating System :: POSIX',
+    ]
+
+
+class Project(ProjectBase):
+    name = 'omlish'
+    description = 'omlish'
+
+    optional_dependencies = {
+        'async': [
+            'anyio ~= 4.6',
+            'sniffio ~= 1.3',
+
+            'greenlet ~= 3.1',
+
+            'trio ~= 0.26',
+            'trio-asyncio ~= 0.15',
+        ],
+
+        'compress': [
+            'lz4 ~= 4.0',
+
+            'python-snappy ~= 0.7; python_version < "3.13"',
+
+            'zstd ~= 1.5',
+        ],
+
+        'diag': [
+            'asttokens ~= 2.4',
+            'executing ~= 2.1',
+
+            'psutil ~= 6.0',
+        ],
+
+        'formats': [
+            'orjson ~= 3.10',
+            'ujson ~= 5.10',
+
+            'json5 ~= 0.9',
+
+            'pyyaml ~= 5.0',
+
+            'cbor2 ~= 5.6',
+
+            'cloudpickle ~= 3.0',
+        ],
+
+        'http': [
+            'httpx[http2] ~= 0.27',
+        ],
+
+        'misc': [
+            'wrapt ~= 1.14',
+        ],
+
+        'secrets': [
+            'cryptography ~= 43.0',
+        ],
+
+        'sqlalchemy': [
+            'sqlalchemy[asyncio] ~= 2.0',
+        ],
+
+        'sqlalchemy-drivers': [
+            'pg8000 ~= 1.31',
+            # 'psycopg2 ~= 2.9',
+            # 'psycopg ~= 3.2',
+
+            'pymysql ~= 1.1',
+            # 'mysql-connector-python ~= 9.0',
+            # 'mysqlclient ~= 2.2',
+
+            'aiomysql ~= 0.2',
+            'aiosqlite ~= 0.20',
+            'asyncpg ~= 0.29; python_version < "3.13"',
+
+            'sqlean.py ~= 3.45; python_version < "3.13"',
+
+            'duckdb ~= 1.1',
+        ],
+
+        'testing': [
+            'pytest ~= 8.0',
+        ],
+    }
+
+    entry_points = {
+        'omlish.manifests': {name: name},
+    }
+
+
+#
+
+
+class SetuptoolsBase:
+    manifest_in = [
+        'global-exclude **/conftest.py',
+    ]
+
+    find_packages = {
+        'exclude': [
+            '*.tests',
+            '*.tests.*',
+        ],
+    }
+
+    package_data = {
+        '*': [
+            '*.c',
+            '*.cc',
+            '*.cu',
+            '*.h',
+
+            '.manifests.json',
+
+            'LICENSE',
+        ],
+    }
+
+
+class Setuptools(SetuptoolsBase):
+    find_packages = {
+        'include': [Project.name, f'{Project.name}.*'],
+        'exclude': [*SetuptoolsBase.find_packages['exclude']],
+    }
