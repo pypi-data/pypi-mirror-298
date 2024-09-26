20051fb9f3b92bb2af686437b85a4481f1dde995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 05:04:57 +0000

    Add package eudplib/0.78.4/eudplib-0.78.4-cp311-cp311-win_amd64.whl

---
+++ packages/eudplib/eudplib-0.78.4-cp311-cp311-win_amd64.whl/eudplib/maprw/loadmap.py
@@ -0,0 +1,48 @@
+# Copyright 2014 by trgk.
+# All rights reserved.
+# This file is part of EUD python library (eudplib),
+# and is released under "MIT License Agreement". Please see the LICENSE
+# file that should have been included as part of this package.
+
+import os
+
+from ..core.mapdata import chktok, mapdata, mpqapi
+from ..localize import _
+from ..utils import EPError, ep_assert, ep_eprint
+from .mpqadd import update_filelist_by_listfile
+
+
+def LoadMap(fname: str) -> None:  # noqa: N802
+    """Load basemap from fname
+
+    :param fname: Path for basemap.
+    """
+
+    print(_("Loading map {}").format(fname))
+    if not os.path.isfile(fname):
+        raise FileNotFoundError(_("input path is not a map file"))
+    try:
+        rawfile = open(fname, "rb").read()
+    except FileNotFoundError:
+        ep_eprint(_("input map does not exist"))
+        raise
+    except PermissionError:
+        ep_eprint(
+            _("You lacks permission to get access rights for input map"),
+            _("Try to turn off antivirus or StarCraft"),
+            sep="\n",
+        )
+        raise
+    mpqr = mpqapi.MPQ()
+    ep_assert(mpqr.Open(fname), _("Fail to open input map"))
+    chkt = chktok.CHK()
+    b = mpqr.Extract("staredit\\scenario.chk")
+    if not b:
+        raise EPError(_("Fail to extract scenario.chk, maybe invalid scx"))
+    chkt.loadchk(b)
+    mapdata.init_map_data(chkt, rawfile)
+    update_filelist_by_listfile(mpqr)
+    for f in mpqr.EnumFiles():
+        if f and f not in ("staredit\\scenario.chk", "(listfile)"):
+            mapdata.AddListFiles(f, mpqr.Extract(f))
+    mpqr.Close()
---
+++ packages/eudplib/eudplib-0.78.4-cp311-cp311-win_amd64.whl/eudplib/maprw/savemap.py
@@ -0,0 +1,117 @@
+# Copyright 2014 by trgk.
+# All rights reserved.
+# This file is part of EUD python library (eudplib),
+# and is released under "MIT License Agreement". Please see the LICENSE
+# file that should have been included as part of this package.
+
+import binascii
+import os
+from collections.abc import Callable
+
+from ..core import RegisterCreatePayloadCallback
+from ..core.eudfunc.trace.tracetool import _get_trace_map, _reset_trace_map
+from ..core.mapdata import fixmapdata, mapdata, mpqapi
+from ..localize import _
+from ..utils import EPError, ep_assert, ep_eprint
+from .injector.apply_injector import apply_injector
+from .injector.mainloop import main_starter
+from .inlinecode.ilcprocesstrig import _preprocess_inline_code
+from .mpqadd import _update_mpq
+
+trace_header = None
+trace_map = []
+
+
+def get_trace_map() -> None:
+    global trace_map, trace_header
+    new_trace_header, new_trace_map = _get_trace_map()
+    if not new_trace_header[1]:
+        raise EPError(_("Empty trace header"))
+    if new_trace_map:
+        trace_header = new_trace_header
+        trace_map = list(new_trace_map)
+    _reset_trace_map()
+
+
+RegisterCreatePayloadCallback(get_trace_map)
+
+
+def SaveMap(fname: str, rootf: Callable, *, sector_size=None) -> None:  # noqa: N802
+    """Save output map with root function.
+
+    :param fname: Path for output map.
+    :param rootf: Main entry function.
+    """
+    from ctypes import GetLastError
+
+    print(_("Saving to {}...").format(fname))
+    chkt = mapdata.GetChkTokenized()
+
+    _reset_trace_map()
+
+    # Add payload
+    root = main_starter(rootf)
+    _preprocess_inline_code(chkt)
+    mapdata.update_map_data()
+
+    fixmapdata._fix_map_data(chkt)
+    apply_injector(chkt, root)
+
+    chkt.optimize()
+    rawchk = chkt.savechk()
+    print(f"\nOutput scenario.chk : {len(rawchk) / 1000000:.3f}MB")
+    mw = mpqapi.MPQ()
+
+    if sector_size and isinstance(sector_size, int):
+        if os.path.isfile(fname):
+            e = _("You lacks permission to get access rights for output map")
+            try:
+                os.remove(fname)
+            except PermissionError:
+                ep_eprint(
+                    e,
+                    _("Try to turn off antivirus or StarCraft"),
+                    sep="\n",
+                )
+                raise
+        if not mw.Create(fname, sector_size=sector_size):
+            raise EPError(_("Fail to access output map ({})").format(GetLastError()))
+        for n, f in mapdata.IterListFiles():
+            if f:
+                ep_assert(
+                    mw.PutFile(n, f),
+                    _("Fail to export input map data to output map"),
+                )
+    else:
+        # Process by modifying existing mpqfile
+        try:
+            file = open(fname, "wb")
+        except PermissionError:
+            ep_eprint(
+                _("You lacks permission to get access rights for output map"),
+                _("Try to turn off antivirus or StarCraft"),
+                sep="\n",
+            )
+            raise
+        else:
+            file.write(mapdata.GetRawFile())
+            file.close()
+        if not mw.Open(fname):
+            raise EPError(_("Fail to access output map ({})").format(GetLastError()))
+
+    if not mw.PutFile("staredit\\scenario.chk", rawchk):
+        raise EPError(_("Fail to add scenario.chk"))
+    _update_mpq(mw)
+    ep_assert(mw.Compact(), _("Fail to compact MPQ"))
+    mw.Close()
+
+    if trace_map:
+        trace_fname = fname + ".epmap"
+        print(_("Writing trace file to {}").format(trace_fname))
+        if trace_header is None:
+            raise EPError(_("Unreachable callback error"))
+        with open(trace_fname, "w", encoding="utf-8") as wf:
+            wf.write(f"H0: {binascii.hexlify(trace_header[0]).decode('ascii')}\n")
+            wf.write(f"H1: {binascii.hexlify(trace_header[1]).decode('ascii')}\n")
+            for k, v in trace_map:
+                wf.write(f" - {k:08}%08X : {v}\n")
