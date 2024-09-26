83b0cf223a0d9a5227b5bca56821b15c32ace5af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 03:09:05 +0000

    Add package tvb-ext-unicore/3.0.0/tvb_ext_unicore-3.0.0.tar.gz

---
+++ packages/tvb-ext-unicore/tvb_ext_unicore-3.0.0.tar.gz/tvb_ext_unicore-3.0.0/tvb_ext_unicore/utils.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#
+# "TheVirtualBrain - Widgets" package
+#
+# (c) 2022-2024, TVB Widgets Team
+#
+
+import json
+import enum
+from tvb_ext_unicore.logger.builder import get_logger
+
+LOGGER = get_logger(__name__)
+
+
+class DownloadStatus(str, enum.Enum):  # inherit str for json serialization
+    """
+    Describes the status of a file download attempt
+    """
+    WARNING = 'warning'
+    ERROR = 'error'
+    SUCCESS = 'success'
+
+
+def build_response(status, message):
+    # type: (DownloadStatus, str) -> str
+    """
+    function to build a response as json string
+    """
+    return json.dumps({'status': status, 'message': message})
