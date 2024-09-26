e24a27fc4e3ec163f752d721e09d2631fa667f21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 07:04:31 +0000

    Add package pyinfra/3.1.1/pyinfra-3.1.1.tar.gz

---
+++ packages/pyinfra/pyinfra-3.1.1.tar.gz/pyinfra-3.1.1/pyinfra/facts/apt.py
@@ -0,0 +1,96 @@
+from __future__ import annotations
+
+import re
+
+from pyinfra.api import FactBase
+
+from .gpg import GpgFactBase
+from .util import make_cat_files_command
+
+
+def parse_apt_repo(name):
+    regex = r"^(deb(?:-src)?)(?:\s+\[([^\]]+)\])?\s+([^\s]+)\s+([^\s]+)\s+([a-z-\s\d]*)$"
+
+    matches = re.match(regex, name)
+
+    if not matches:
+        return
+
+    # Parse any options
+    options = {}
+    options_string = matches.group(2)
+    if options_string:
+        for option in options_string.split():
+            key, value = option.split("=", 1)
+            if "," in value:
+                value = value.split(",")
+
+            options[key] = value
+
+    return {
+        "options": options,
+        "type": matches.group(1),
+        "url": matches.group(3),
+        "distribution": matches.group(4),
+        "components": list(matches.group(5).split()),
+    }
+
+
+class AptSources(FactBase):
+    """
+    Returns a list of installed apt sources:
+
+    .. code:: python
+
+        [
+            {
+                "type": "deb",
+                "url": "http://archive.ubuntu.org",
+                "distribution": "trusty",
+                "components", ["main", "multiverse"],
+            },
+        ]
+    """
+
+    def command(self) -> str:
+        return make_cat_files_command(
+            "/etc/apt/sources.list",
+            "/etc/apt/sources.list.d/*.list",
+        )
+
+    def requires_command(self) -> str:
+        return "apt"  # if apt installed, above should exist
+
+    default = list
+
+    def process(self, output):
+        repos = []
+
+        for line in output:
+            repo = parse_apt_repo(line)
+            if repo:
+                repos.append(repo)
+
+        return repos
+
+
+class AptKeys(GpgFactBase):
+    """
+    Returns information on GPG keys apt has in its keychain:
+
+    .. code:: python
+
+        {
+            "KEY-ID": {
+                "length": 4096,
+                "uid": "Oxygem <hello@oxygem.com>"
+            },
+        }
+    """
+
+    # This requires both apt-key *and* apt-key itself requires gpg
+    def command(self) -> str:
+        return "! command -v gpg || apt-key list --with-colons"
+
+    def requires_command(self) -> str:
+        return "apt-key"
