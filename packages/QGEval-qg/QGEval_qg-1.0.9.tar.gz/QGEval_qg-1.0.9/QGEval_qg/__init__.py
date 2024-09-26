9e62f7bd5c3851c690840ea81d2f3b10779d3935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 05:04:36 +0000

    Add package QGEval-qg/1.0.9/QGEval_qg-1.0.9.tar.gz

---
+++ packages/QGEval-qg/QGEval_qg-1.0.9.tar.gz/QGEval_qg-1.0.9/QGEval_qg/__init__.py
@@ -0,0 +1,7 @@
+# QGEval/__init__.py  
+
+from . import apply_lmqg
+from . import bart
+from . import Flan_T5_lora
+from . import Flan_T5
+from . import T5
---
+++ packages/QGEval-qg/QGEval_qg-1.0.9.tar.gz/QGEval_qg-1.0.9/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages  
+  
+setup(  
+    name='QGEval_qg',  
+    version='1.0.9',  
+    author='WeipingFu',  
+    author_email='fuweiping1993@foxmail.com',  
+    description='A tool for question generation based on LMs',  
+    long_description=open('README.md').read(),  
+    long_description_content_type='text/markdown',  
+    url='https://github.com/WeipingFu/QGEval/tree/main',  
+    packages=find_packages(),  
+    classifiers=[  
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',  
+        'Programming Language :: Python',  
+        'Programming Language :: Python :: 3',  
+        'Programming Language :: Python :: 3.7',  
+        'Programming Language :: Python :: 3.8',  
+        'Programming Language :: Python :: 3.9',  
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Database',  
+    ],  
+    python_requires='>=3.7',  
+    install_requires=open('requirements.txt').read().splitlines(),  
+)
