d52090eb8cac237073bead76549d42cc061c6315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 05:04:58 +0000

    Add package undidPyjl/0.0.0/undidPyjl-0.0.0-py3-none-any.whl

---
+++ packages/undidPyjl/undidPyjl-0.0.0-py3-none-any.whl/undidPyjl/__init__.py
@@ -0,0 +1,7 @@
+# undidPyjl/__init__.py
+
+from .julia_management import checkundidversion
+# from .wrappers import some_function
+# from .plotting import plot_parallel_trends
+
+__all__ = ['checkundidversion']
