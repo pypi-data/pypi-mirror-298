b4c716539d2c4dcd15f04f3e106562a49a113150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 04:04:28 +0000

    Add package switch-audio-device/0.1.5/switch_audio_device-0.1.5.tar.gz

---
+++ packages/switch-audio-device/switch_audio_device-0.1.5.tar.gz/switch_audio_device-0.1.5/README.md
@@ -0,0 +1,45 @@
+# Switch Audio Device
+
+Creates a [rofi](https://github.com/davatorium/rofi) menu to select primary audio device on machines running Pulse Audio for managing their audio. You need to have rofi installed on the system for this to work.
+
+## Install from PyPI
+```
+pip install switch_audio_device
+```
+
+## How to use
+The installation step should have automatically created an executable script called switch_audio_device.
+
+```
+switch_audio_device
+```
+
+
+## How to build
+
+```
+mamba create env -yn switch_audio_device
+conda activate switch_audio_device
+pip install build twine
+python3 -m build
+```
+
+## Install from local tree
+
+```
+pip install .
+```
+
+##  Upload to PyPI
+```
+twine upload dist/*
+```
+## Upload to test PyPI
+```
+twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+```
+
+## Install from test PyPI
+```
+pip install --index-url https://test.pypi.org/simple/ your-package-name
+```
---
+++ packages/switch-audio-device/switch_audio_device-0.1.5.tar.gz/switch_audio_device-0.1.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name='switch_audio_device',
+    version='0.1.5',
+    packages=find_packages(),
+    install_requires=[
+        'pulsectl',
+    ],
+    entry_points={
+        'console_scripts': [
+            'switch_audio_device=switch_audio_device.main:main',
+        ],
+    },
+    long_description="Creates a [rofi](https://github.com/davatorium/rofi) menu to select primary audio device on machines running Pulse Audio for managing their audio. You need to have rofi installed on the system for this to work.",
+    long_description_content_type='text/markdown',
+    project_urls={
+        'Source': 'https://gitlab.com/sandeep-datta/switch_audio_device',
+        'Tracker': 'https://gitlab.com/sandeep-datta/switch_audio_device/issues',
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+    ],
+    python_requires='>=3.6',
+)
