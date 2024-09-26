562a9b96641a6bbd64956db9e1504415677b48de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 05:04:47 +0000

    Add package weave/0.51.9/weave-0.51.9-py3-none-any.whl

---
+++ packages/weave/weave-0.51.9-py3-none-any.whl/weave/frontend/assets/Component-B7hFDgDZ.js
@@ -0,0 +1,2 @@
+import{ah as i,u as o,R as e}from"./index-DH3zn7Tz.js";const n=l=>{const a=l.input,r=i({file:a}),t=o(r);return e.createElement("div",null,t.loading?e.createElement("div",null):e.createElement("img",{style:{maxWidth:"100%"},alt:"cool-alt",src:t.result}))};export{n as default};
+//# sourceMappingURL=Component-B7hFDgDZ.js.map
