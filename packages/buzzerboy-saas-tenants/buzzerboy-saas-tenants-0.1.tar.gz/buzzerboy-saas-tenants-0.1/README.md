5d184e751ab246fd816cde1637a13c74dc00a162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 03:08:16 +0000

    Add package buzzerboy-saas-tenants/0.1/buzzerboy-saas-tenants-0.1.tar.gz

---
+++ packages/buzzerboy-saas-tenants/buzzerboy-saas-tenants-0.1.tar.gz/buzzerboy-saas-tenants-0.1/README.md
@@ -0,0 +1,11 @@
+# Introduction 
+This is a standard starter Kit for a Django application with UI already built-in
+
+# Getting Started
+Installation process
+Recommend to use Linux or WSL, and ensure Python3 and Pip are installed. Run `pip3 install -r requirements.txt` from root directory, then `python3 manage.py runserver`
+
+# Build and Test
+TO DO: Create layout and base
+TO DO: Create templatetags for left menu and other components that occur on all pages
+
---
+++ packages/buzzerboy-saas-tenants/buzzerboy-saas-tenants-0.1.tar.gz/buzzerboy-saas-tenants-0.1/core/email_service.py
@@ -0,0 +1,25 @@
+# core/email_service.py
+from django.core.mail import send_mail
+from core.jinja2_env import jinja_env  # Import the Jinja2 environment
+from django.utils.html import strip_tags
+
+class EmailService:
+    @staticmethod
+    def send_template_email(template_content, context, subject, from_email, recipient_list):
+        # Load the template from the string
+        template = jinja_env.from_string(template_content)
+
+        # Render the template with the context data
+        rendered_content = template.render(context)
+
+        plain_message = strip_tags(rendered_content)
+
+        # Send the email using the rendered content
+        send_mail(
+            subject=subject,
+            message=plain_message,  # Fallback message if HTML is not supported
+            html_message=rendered_content,
+            from_email=from_email,
+            recipient_list=recipient_list,
+            fail_silently=False,
+        )
