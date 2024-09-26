118293e5354a2a5e4a4543691929fa745da85076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 15:04:47 +0000

    Add package llmcompressor-nightly/0.2.0.20240926/llmcompressor_nightly-0.2.0.20240926-py3-none-any.whl

---
+++ packages/llmcompressor-nightly/llmcompressor_nightly-0.2.0.20240926-py3-none-any.whl/llmcompressor/transformers/__init__.py
@@ -0,0 +1,11 @@
+"""
+Tools for integrating LLM Compressor with transformers training flows
+"""
+
+# flake8: noqa
+
+# isort: skip_file
+# (import order matters for circular import avoidance)
+from .utils import *
+from .sparsification import SparseAutoModel, SparseAutoModelForCausalLM
+from .finetune import *
