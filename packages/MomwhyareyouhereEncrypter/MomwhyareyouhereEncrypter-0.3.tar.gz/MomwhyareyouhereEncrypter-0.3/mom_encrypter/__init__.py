392068b703809d153aa18b5d8466ab54a52fd512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 15:05:37 +0000

    Add package MomwhyareyouhereEncrypter/0.3/MomwhyareyouhereEncrypter-0.3.tar.gz

---
+++ packages/MomwhyareyouhereEncrypter/MomwhyareyouhereEncrypter-0.3.tar.gz/MomwhyareyouhereEncrypter-0.3/mom_encrypter/__init__.py
@@ -0,0 +1,39 @@
+# mom_encrypter/__init__.py
+
+from .encrypter import encrypt, unencrypt, run
+
+class MomwhyareyouhereEncrypter:
+    @staticmethod
+    def encrypt(filename):
+        with open(filename, 'r') as f:
+            content = f.read()
+        
+        # Simple example of "encryption" by appending 'MOM ENCRYPTED' repeatedly
+        encrypted_content = 'MOM ENCRYPTED ' * 10 + ''.join([chr(ord(c) + 1) for c in content])
+        
+        # Write to .mom file
+        new_filename = filename.replace(".py", ".mom")
+        with open(new_filename, 'w') as f:
+            f.write(encrypted_content)
+        print(f"Encrypted content written to {new_filename}")
+    
+    @staticmethod
+    def unencrypt(filename):
+        with open(filename, 'r') as f:
+            content = f.read()
+        
+        # Simple "decryption" logic: removing the 'MOM ENCRYPTED' text and reversing the shift
+        decrypted_content = ''.join([chr(ord(c) - 1) for c in content.replace('MOM ENCRYPTED ' * 10, '')])
+        
+        # Write to a new .py file
+        new_filename = filename.replace(".mom", ".py")
+        with open(new_filename, 'w') as f:
+            f.write(decrypted_content)
+        print(f"Decrypted content written to {new_filename}")
+    
+    @staticmethod
+    def run(filename):
+        with open(filename, 'r') as f:
+            content = f.read()
+        print(f"Running encrypted file: {filename}")
+        # Here you would add logic to run the file if needed
