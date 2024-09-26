718ca5d453898dfa8ff801021b23bdbbb8a460e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 03:09:07 +0000

    Add package kition-djangodefaults/0.2.0rc3/kition_djangodefaults-0.2.0rc3-py3-none-any.whl

---
+++ packages/kition-djangodefaults/kition_djangodefaults-0.2.0rc3-py3-none-any.whl/kition_djangodefaults/settings/default_basedir.py
@@ -0,0 +1,10 @@
+import importlib
+import os
+from pathlib import Path
+
+from django.conf import ENVIRONMENT_VARIABLE as DJANGO_SETTINGS_ENVIRONMENT_VARIABLE
+
+# Setting the BASE_DIR to be the grandparent of the DJANGO_SETTINGS_MODULE
+SETTINGS_MODULE = os.environ.get(DJANGO_SETTINGS_ENVIRONMENT_VARIABLE)
+django_settings_module = importlib.import_module(SETTINGS_MODULE)
+BASE_DIR = Path(django_settings_module.__file__).resolve().parent.parent
