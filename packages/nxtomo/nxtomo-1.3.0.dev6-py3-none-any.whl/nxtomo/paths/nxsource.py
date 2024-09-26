d139a118ab01c7d8e2ae230438ca0a09b6d6b6cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 04:05:00 +0000

    Add package nxtomo/1.3.0.dev6/nxtomo-1.3.0.dev6-py3-none-any.whl

---
+++ packages/nxtomo/nxtomo-1.3.0.dev6-py3-none-any.whl/nxtomo/paths/nxsource.py
@@ -0,0 +1,23 @@
+"""nexus path used to define a `NXsource <https://manual.nexusformat.org/classes/base_classes/NXsource.html>`_"""
+
+
+class NEXUS_SOURCE_PATH:
+    NAME = "name"
+    TYPE = "type"
+    PROBE = "probe"
+
+
+class NEXUS_SOURCE_PATH_V_1_0(NEXUS_SOURCE_PATH):
+    pass
+
+
+class NEXUS_SOURCE_PATH_V_1_1(NEXUS_SOURCE_PATH_V_1_0):
+    pass
+
+
+class NEXUS_SOURCE_PATH_V_1_2(NEXUS_SOURCE_PATH_V_1_1):
+    pass
+
+
+class NEXUS_SOURCE_PATH_V_1_3(NEXUS_SOURCE_PATH_V_1_2):
+    pass
