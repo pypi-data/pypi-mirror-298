f99298646fbd2617f2192423483a8a9799aa6870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 04:04:30 +0000

    Add package mars-similarity-tools/0.3.1/mars_similarity_tools-0.3.1.tar.gz

---
+++ packages/mars-similarity-tools/mars_similarity_tools-0.3.1.tar.gz/mars_similarity_tools-0.3.1/mars_similarity_tools/models/__init__.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass, asdict
+from hashlib import sha256
+from dill import dumps
+
+@dataclass(frozen=True)
+class SimilarityObject:
+
+    def sha256(self) -> str:
+        return sha256(dumps(self)).hexdigest()
+    
+    def to_dict(self) -> dict:
+        return asdict(self)
+
+    @classmethod
+    def from_dict(cls, d: dict) -> "SimilarityObject":
+        return cls(**d)
+    
+@dataclass
+class SimilarityResult:
+    
+    score: float
+    obj: SimilarityObject
