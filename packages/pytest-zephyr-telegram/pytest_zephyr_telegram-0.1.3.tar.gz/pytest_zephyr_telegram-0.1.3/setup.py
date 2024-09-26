6a19f884aaeecf47cd754dfbab53c8a908672cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 15:05:45 +0000

    Add package pytest-zephyr-telegram/0.1.3/pytest_zephyr_telegram-0.1.3.tar.gz

---
+++ packages/pytest-zephyr-telegram/pytest_zephyr_telegram-0.1.3.tar.gz/pytest_zephyr_telegram-0.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='pytest-zephyr-telegram',
+    version='0.1.3',
+    description='Плагин для отправки данных автотестов в Телеграм и Зефир',
+    author='slug',
+    author_email='',
+    py_modules=['plugin'],
+    install_requires=[
+        'pytest == 8.3.2',
+        'pytelegrambotapi == 4.22.1',
+        'emoji == 2.7.0',
+        'pytz == 2024.1',
+        'curlify == 2.2.1',
+        'allure-pytest == 2.13.5',
+    ],
+    entry_points={'pytest11': ['zephyr_telegram = plugin']},
+)
