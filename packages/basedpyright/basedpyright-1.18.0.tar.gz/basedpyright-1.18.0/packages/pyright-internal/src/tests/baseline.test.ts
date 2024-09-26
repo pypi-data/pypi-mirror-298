5672dd89ed33b0a8d685b08f5539e2181a08546c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 04:04:38 +0000

    Add package basedpyright/1.18.0/basedpyright-1.18.0.tar.gz

---
+++ packages/basedpyright/basedpyright-1.18.0.tar.gz/basedpyright-1.18.0/packages/pyright-internal/src/tests/baseline.test.ts
@@ -0,0 +1,27 @@
+import path from 'path';
+import { ConfigOptions } from '../common/configOptions';
+import { DiagnosticRule } from '../common/diagnosticRules';
+import { Uri } from '../common/uri/uri';
+import { resolveSampleFilePath, typeAnalyzeSampleFiles, validateResultsButBased } from './testUtils';
+
+const typeAnalyzeFilesWithBaseline = (sampleFolderName: string, files: string[]) => {
+    const sampleFolder = path.join('baseline', sampleFolderName);
+    return typeAnalyzeSampleFiles(
+        files.map((file) => path.join(sampleFolder, file)),
+        (serviceProvider) => new ConfigOptions(Uri.file(resolveSampleFilePath(sampleFolder), serviceProvider))
+    );
+};
+
+test('baselined error not reported', () => {
+    const analysisResults = typeAnalyzeFilesWithBaseline('baselined_error_not_reported', ['foo.py']);
+
+    validateResultsButBased(analysisResults, {
+        errors: [
+            { line: 0, code: DiagnosticRule.reportAssignmentType, baselineStatus: 'baselined' },
+            { line: 1, code: DiagnosticRule.reportUndefinedVariable },
+        ],
+        warnings: [{ line: 1, code: DiagnosticRule.reportUnusedExpression }],
+    });
+});
+
+// TODO: more tests
