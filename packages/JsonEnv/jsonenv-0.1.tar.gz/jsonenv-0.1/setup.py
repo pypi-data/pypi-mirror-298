458dfaab59a589e2b5acf8fe539e5339999984a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 06:04:02 +0000

    Add package JsonEnv/0.1/jsonenv-0.1.tar.gz

---
+++ packages/JsonEnv/jsonenv-0.1.tar.gz/jsonenv-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="JsonEnv",
+    version="0.1",
+    packages=["JsonEnv"],
+    description="JsonEnv allows you to make environment variables with Json, allowing you to write numbers and other types of variables",
+    long_description=open("README.md", encoding='utf-8'),
+    long_description_content_type='text/markdown',
+    author="Greem3",
+    author_email="ianpichardo575@gmail.com",
+    url="https://github.com/Greem3/JsonEnv"
+)
