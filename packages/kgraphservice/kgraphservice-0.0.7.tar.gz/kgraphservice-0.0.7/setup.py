8da45f7871feb4e501674a6a7831d415815c33a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 14:04:58 +0000

    Add package kgraphservice/0.0.7/kgraphservice-0.0.7.tar.gz

---
+++ packages/kgraphservice/kgraphservice-0.0.7.tar.gz/kgraphservice-0.0.7/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kgraphservice',
+    version='0.0.7',
+    author='Marc Hadfield',
+    author_email='marc@vital.ai',
+    description='KGraph Service',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/vital-ai/kgraphservice',
+    packages=find_packages(exclude=["test"]),
+    license='Apache License 2.0',
+    scripts=[
+        'bin/kgraphservice'
+    ],
+    install_requires=[
+        'vital-ai-vitalsigns>=0.1.21',
+        'vital-ai-domain>=0.1.7',
+        'six',
+        'pyyaml',
+        'vital-ai-haley-kg>=0.1.18',
+        'rdflib==7.0.0',
+        'SPARQLWrapper==2.0.0',
+        'networkx',
+        'matplotlib',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10'
+)
