4219081aa4baa9b559aba967b6f82316935679f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 05:05:00 +0000

    Add package screwdrivercd/2.1.962045/screwdrivercd-2.1.962045.tar.gz

---
+++ packages/screwdrivercd/screwdrivercd-2.1.962045.tar.gz/screwdrivercd-2.1.962045/src/screwdrivercd/version/version_types.py
@@ -0,0 +1,370 @@
+# Copyright 2019, Oath Inc.
+# Licensed under the terms of the Apache 2.0 license.  See the LICENSE file in the project root for terms
+"""Versioning plugin classes"""
+import configparser
+import logging
+import os
+import subprocess  # nosec
+from datetime import datetime
+from typing import List, Union
+from .exceptions import VersionError
+
+
+LOG = logging.getLogger(__name__)
+
+
+class Version:
+    """
+    Base Screwdriver Versioning class
+    """
+    name: Union[str, None] = None
+    default_version: List[str] = ['0', '0', '0']
+    setup_cfg_filename: str = 'setup.cfg'
+    _meta_version: str = ''
+
+    def __init__(self, setup_cfg_filename=None, ignore_meta_version: bool = False, update_sdv4_meta: bool = True, meta_command: str = 'meta', link_to_project: bool = False):
+        if setup_cfg_filename:  # pragma: no cover
+            self.setup_cfg_filename = setup_cfg_filename
+        self.meta_command = meta_command
+        self.ignore_meta_version = ignore_meta_version
+        self.update_sdv4_meta = update_sdv4_meta
+        self.link_to_project = link_to_project
+
+    def __repr__(self):
+        return repr(self.version)
+
+    def __str__(self):
+        return self.version
+
+    @staticmethod
+    def get_env(env_vars, default_value=None):
+        """
+        Return the value from a list of environment variables searched in order
+        returning the default value if a value is not found.
+
+        Parameters
+        ----------
+        env_vars: str or list of str
+            List of environment variables to search
+        default_value: str
+            Value to return if no environment variables are found
+
+        Returns
+        -------
+        str:
+            Returns the value of the first matching environemnt variable
+            or the value of default_value if not found.
+        """
+        if isinstance(env_vars, str):
+            env_vars = [env_vars]
+        for env_var in env_vars:
+            value = os.environ.get(env_var)
+            if value is not None:
+                LOG.debug(f"Found env variable {env_var}={value!r}")
+                return value
+        return default_value
+
+    def commit_changed_setup_cfg(self):  # pragma: no cover
+        """
+        Git commit the setup.cfg
+        """
+        try:
+            output = subprocess.check_output(['git', 'commit', '-m', 'Updated version', self.setup_cfg_filename])  # nosec
+            LOG.debug(f'Git commit output {output}')
+        except (FileNotFoundError, subprocess.CalledProcessError):
+            pass
+
+    def read_setup_version(self):
+        """
+        Read the package version from the setup.cfg file
+
+        Returns
+        -------
+        str:
+            The version number from the setup.cfg [metadata] section or the default version if the version is not
+            present.
+        """
+        config = configparser.ConfigParser()
+        config.read(self.setup_cfg_filename)
+        if 'metadata' in config.sections():
+            return config['metadata'].get('version', '').split('.')
+        return self.default_version
+
+    def generate(self):
+        """
+        Generate the version
+        """
+        return self.read_setup_version()
+
+    def get_link_to_project_using_hash(self):
+        """
+        Generate and return link to build-triggering commit using its SHA hash
+        """
+        scm_url = os.environ.get('SCM_URL', '')
+        sha = self.get_env(['SD_BUILD_SHA', 'GITHUB_SHA'], '')
+        if self.link_to_project and scm_url and sha:
+            if scm_url.startswith('git@'):
+                hostname = scm_url.split(':')[0].split('@')[1]
+                path = ':'.join(scm_url.split(':')[1:])
+                return f'https://{hostname}/{path}/tree/{sha}'
+            else:
+                return f'{scm_url}/tree/{sha}'
+        return ''
+
+    def update_setup_cfg_metadata(self):
+        """
+        Update the version value in the setup.cfg file
+        """
+        if not self.version:  # pragma: no cover
+            return
+        link_to_project = self.get_link_to_project_using_hash()
+        config = configparser.ConfigParser()
+        config.read(self.setup_cfg_filename)
+        if 'metadata' not in config.sections():
+            config['metadata'] = {}
+
+        config['metadata']['version'] = self.version
+        if link_to_project:
+            project_urls_str = config['metadata'].get('project_urls', '')
+            project_urls_dict = {}
+            if project_urls_str:
+                for entry in project_urls_str.split(os.linesep):
+                    entry = entry.strip()
+                    if '=' in entry:
+                        key, value = entry.split('=')
+                        key = key.strip()
+                        value = value.strip()
+                        project_urls_dict[key] = value
+
+            project_urls_dict['Source'] = link_to_project
+
+            project_urls_str = '\n'
+            for key, value in project_urls_dict.items():
+                project_urls_str += f'{key} = {value}\n'
+
+            config['metadata']['project_urls'] = project_urls_str.rstrip('\n')
+
+        with open(self.setup_cfg_filename, 'w') as config_file_handle:
+            config.write(config_file_handle)
+
+        self.commit_changed_setup_cfg()
+
+    def update_meta_version(self):  # noqa
+        """
+        Update the meta_version value based on the generated version and pull_request_number
+        """
+        if not self.meta_version:  # pragma: no cover
+            if not self.pull_request_number:
+                new_version = self.generated_version
+            else:  # pragma: no cover
+                new_version = f'{self.generated_version}a{self.pull_request_number}'
+            print(f'Updating the screwdriver metadata: package.version={new_version}', flush=True)
+            self.meta_version = new_version
+
+    @property
+    def pull_request_number(self):
+        """
+        Return the Pull request number from the Screwdriver SD_PULL_REQUEST env variable if present, returns 0
+        if it is not present.
+        
+        Returns
+        -------
+        int:
+            Pull request number or 0 if not running from a pull request
+        """
+        try:
+            prnum = int(self.get_env(['SD_PULL_REQUEST', 'GITHUB_REF'], None))
+        except (TypeError, ValueError):
+            prnum = 0
+        return prnum
+
+    @property
+    def generated_version(self):
+        """
+        The generated version
+        """
+        return '.'.join(self.generate())
+
+    @property
+    def meta_version(self) -> str:
+        """
+        The version from the screwdriver metadata package.version value or None if not present.
+        """
+        if self.ignore_meta_version:
+            return ''
+        if self._meta_version:  # pragma: no cover
+            return self._meta_version
+        try:  # pragma: no cover
+            self._meta_version = subprocess.check_output([self.meta_command, 'get', 'package.version']).decode(errors='ignore').strip()  # nosec
+        except (FileNotFoundError, subprocess.CalledProcessError):  # pragma: no cover
+            pass
+        if self._meta_version == 'null':
+            self._meta_version = ''
+        return self._meta_version  # pragma: no cover
+
+    @meta_version.setter
+    def meta_version(self, new_version):
+        if not self.update_sdv4_meta:  # pragma: no cover
+            return
+        try:
+            subprocess.check_call([self.meta_command, 'set', 'package.version', new_version])  # nosec
+        except FileNotFoundError:  # pragma: no cover
+            LOG.warning('The screwdriver meta command is missing, unable to set version in screwdriver metadata')
+        self._meta_version = new_version
+
+    @property
+    def version(self):
+        """
+        The version from the versioner, will be the version from the screwdriver package.version if it is set, otherwise
+        it will be the generated version number from the versioner.
+        """
+        if self.meta_version:  # pragma: no cover
+            return self.meta_version
+
+        ver = self.generated_version
+        if not self.pull_request_number:
+            return ver
+        return f'{ver}a{self.pull_request_number}'
+
+
+class VersionManualUpdate(Version):
+    """
+    Version updater that just reads the setup.cfg and takes the `version`
+    number from `metadata` section. Be aware when using this updater, as this
+    updater does not add any unique identifier to the version number.
+
+    It is thus the caller's responsibility to ensure uniqueness is maintained
+    during version update.
+    """
+    name: str = 'manual_update'
+
+
+class VersionUpdateRevision(Version):
+    """
+    Version updater that updates the revision (last component) of a semetic version.
+    """
+    log_errors: bool = True
+    default_revision_value: str = '0'
+    
+    def __init__(self, *args, **kwargs):
+        self.log_errors = kwargs.pop('log_errors', self.log_errors)
+        super().__init__(*args, **kwargs)
+
+    def revision_value(self):  # pragma: no cover
+        """Method to return a newly generated revision value"""
+        return self.default_revision_value
+
+    def generate(self):  # pragma: no cover
+        version = super().generate()
+        if version:
+            try:
+                version[-1] = self.revision_value()
+            except VersionError as error:
+                if self.log_errors:
+                    LOG.exception(f'Unable to get revision value for versioner {self.name!r}')
+                else:
+                    raise error
+        return version
+
+
+class VersionGitRevisionCount(VersionUpdateRevision):
+    """
+    Version Revision updater that sets the revision number to be equal to the number of git commits of the current
+    repository.
+    
+    Each new git commit will increment the revision value.
+    
+    Notes
+    -----
+    This Versioner may not work correctly if the current git repository is a shallow git clone.
+    """
+    name: str = 'git_revision_count'
+
+    def revision_value(self):
+        """
+        Get a count of the number of git commits and return the count
+        Returns
+        -------
+        int
+            Revision count or None if git was not found
+        """
+        try:
+            result = subprocess.check_output(['git', 'rev-list', '--count', 'HEAD']).decode(errors='ignore')  # nosec
+        except subprocess.CalledProcessError:  # pragma: no cover
+            try:  # pragma: no cover
+                result = len(subprocess.check_output(['git', 'rev-list', 'HEAD']).decode(errors='ignore').split('\n'))  # nosec
+            except subprocess.CalledProcessError:
+                raise VersionError('Unable to generate a version from the git revision count')
+        try:
+            result = int(result)
+        except ValueError:  # pragma: no cover
+            raise VersionError('Got invalid response from the git rev-list command')
+        return str(result)
+
+
+class VersionSDV4Build(VersionUpdateRevision):
+    """
+    Version Revision updater that sets the revision number to be equal to the value of the screwdriver SD_BUILD number.
+    
+    Each new screwdriver job run will increment the revision number.
+    """
+    name = 'sdv4_SD_BUILD'
+
+    def revision_value(self):
+        revision = self.get_env(['SD_BUILD', 'SD_BUILD_ID', 'GITHUB_RUN_ID'])
+        if not revision:
+            raise VersionError('Unable to generate version, no SD_BUILD or SD_BUILD_ID value set in the environment variables')
+        return revision
+
+
+class VersionUTCDate(Version):
+    """
+    Version updater that generates a version based on the current UTC date/time.
+    
+    Each new screwdriver job will get a version based on the current date and time.
+    """
+    name = 'utc_date'
+    now = None
+
+    def __init__(self, *args, **kwargs):
+        self.now = kwargs.pop('now', None)
+        super().__init__(*args, **kwargs)
+
+    def generate(self):
+        now = datetime.utcnow()
+        if self.now:  # pragma: no cover
+            now = self.now
+        return [f'{now.year}', f'{now.month}{now.day:02}', f'{now.hour:02}{now.minute:02}{now.second:02}']
+
+
+class VersionDateSDV4Build(Version):
+    """
+    Version updater that generates a version based on the current year and month and the screwdriver build ID
+    """
+    name = 'sdv4_date'
+
+    def __init__(self, *args, **kwargs):
+        self.now = kwargs.pop('now', None)
+        super().__init__(*args, **kwargs)
+
+    def generate(self):
+        now = self.now if self.now else datetime.utcnow()
+        revision = self.get_env(['SD_BUILD', 'SD_BUILD_ID', 'GITHUB_RUN_ID'], '')
+        if not revision:
+            raise VersionError('Unable to generate version, no SD_BUILD or SD_BUILD_ID value set in the environment variables')
+        return [f'{str(now.year)[-2:]}', f'{now.month}', revision]
+
+
+versioners = {
+    'default': VersionGitRevisionCount,
+    VersionGitRevisionCount.name: VersionGitRevisionCount,
+    VersionUTCDate.name: VersionUTCDate,
+    VersionSDV4Build.name: VersionSDV4Build,
+    VersionDateSDV4Build.name: VersionDateSDV4Build,
+    VersionManualUpdate.name: VersionManualUpdate
+}
+
+# Make sure the versioners are listed all lowercase to make identifying them easier
+for key, value in list(versioners.items()):
+    if key.lower() not in versioners.keys():
+        versioners[key.lower()] = value
---
+++ packages/screwdrivercd/screwdrivercd-2.1.962045.tar.gz/screwdrivercd-2.1.962045/tests/test_versioners.py
@@ -0,0 +1,231 @@
+# Copyright 2019, Oath Inc.
+# Licensed under the terms of the Apache 2.0 license.  See the LICENSE file in the project root for termsimport copy
+import datetime
+import os
+import subprocess  # nosec
+import unittest
+from . import ScrewdriverTestCase
+from tempfile import NamedTemporaryFile
+
+from screwdrivercd.version.exceptions import VersionError
+from screwdrivercd.version.version_types import versioners, Version, VersionGitRevisionCount, VersionSDV4Build, VersionDateSDV4Build, VersionUTCDate, VersionManualUpdate
+
+
+existing_project_url_config = {
+    'setup.cfg': b"""
+[metadata]
+author = Yahoo Python Platform Team
+author_email = python@verizonmedia.com
+name=mypyvalidator
+project_urls = 
+    Documentation = https://yahoo.github.io/mypyvalidator/
+    Change Log = https://yahoo.github.io/python-screwdrivercd/changelog/
+    CI Pipeline = https://cd.screwdriver.cd/pipelines/1
+    Source = https://github.com/yahoo/mypyvalidator/tree/a5c3785ed8d6a35868bc169f07e40e889087fd2e
+version=0.0.0
+
+[options]
+packages = find_namespace:
+package_dir =
+    =src
+
+[options.packages.find]
+where=src
+"""
+}
+
+
+class TestVersioners(ScrewdriverTestCase):
+    environ_keys = {
+        'BASE_PYTHON', 'PACKAGE_DIR', 'PACKAGE_DIRECTORY', 'SD_ARTIFACTS_DIR', 'SD_BUILD', 'SD_BUILD_ID',
+        'SD_PULL_REQUEST', 'SCM_URL', 'SD_BUILD_SHA',
+    }
+
+    def test__version__read_setup_version__no_version(self):
+        version = Version(ignore_meta_version=True).read_setup_version()
+        self.assertEqual(version, Version.default_version)
+
+    def test__version__get_link_to_project_using_hash__unset_env_variables(self):
+        link = Version(ignore_meta_version=True, link_to_project=True).get_link_to_project_using_hash()
+        self.assertEqual(link, '')
+
+    def test__version__get_link_to_project_using_hash__set_and_unset_env_variables(self):
+        os.environ['SD_BUILD_SHA'] = 'a5c3785ed8d6a35868bc169f07e40e889087fd2e'
+        link = Version(ignore_meta_version=True, link_to_project=True).get_link_to_project_using_hash()
+        self.assertEqual(link, '')
+
+    def test__version__get_link_to_project_using_hash__set_env_variables__https_git(self):
+        os.environ['SCM_URL'] = 'https://github.com/org/project'
+        os.environ['SD_BUILD_SHA'] = 'a5c3785ed8d6a35868bc169f07e40e889087fd2e'
+        ver = Version(ignore_meta_version=True, link_to_project=True)
+        link = ver.get_link_to_project_using_hash()
+        self.assertEqual(link, 'https://github.com/org/project/tree/a5c3785ed8d6a35868bc169f07e40e889087fd2e')
+
+        ver.update_setup_cfg_metadata()
+        self.assertTrue(os.path.exists('setup.cfg'))
+        with open('setup.cfg') as fh:
+            setup_cfg = fh.read()
+        self.assertIn(link, setup_cfg)
+
+    def test__version__get_link_to_project_using_hash__set_env_variables__https_git__existing_project_urls(self):
+        os.environ['SCM_URL'] = 'https://github.com/org/project'
+        os.environ['SD_BUILD_SHA'] = 'a5c3785ed8d6a35868bc169f07e40e889087fd2e'
+        self.write_config_files(existing_project_url_config)
+
+        ver = Version(ignore_meta_version=True, link_to_project=True)
+        link = ver.get_link_to_project_using_hash()
+        self.assertEqual(link, 'https://github.com/org/project/tree/a5c3785ed8d6a35868bc169f07e40e889087fd2e')
+
+        ver.update_setup_cfg_metadata()
+        self.assertTrue(os.path.exists('setup.cfg'))
+        with open('setup.cfg') as fh:
+            setup_cfg = fh.read()
+        self.assertIn(link, setup_cfg)
+        self.assertIn('Documentation = https://yahoo.github.io/mypyvalidator/', setup_cfg)
+
+    def test__version__get_link_to_project_using_hash__set_env_variables__ssh_git(self):
+        os.environ['SCM_URL'] = 'git@github.com:org/project'
+        os.environ['SD_BUILD_SHA'] = 'a5c3785ed8d6a35868bc169f07e40e889087fd2e'
+        ver = Version(ignore_meta_version=True, link_to_project=True)
+        link = ver.get_link_to_project_using_hash()
+        self.assertEqual(link, 'https://github.com/org/project/tree/a5c3785ed8d6a35868bc169f07e40e889087fd2e')
+
+        ver.update_setup_cfg_metadata()
+        self.assertTrue(os.path.exists('setup.cfg'))
+        with open('setup.cfg') as fh:
+            setup_cfg = fh.read()
+        self.assertIn(link, setup_cfg)
+
+    def test__manual_version_update(self):
+        with NamedTemporaryFile('w') as file:
+            setup_cfg_content = '[metadata]\nversion = 1.0.5'
+            file.write(setup_cfg_content)
+
+            file.seek(0)
+            version = VersionManualUpdate(setup_cfg_filename=file.name, ignore_meta_version=True)
+            self.assertEqual(version.generate(), ['1', '0', '5'])
+
+    def test__git_revision_count__no_git(self):
+        with self.assertRaises(VersionError):
+            version = str(VersionGitRevisionCount(ignore_meta_version=True, log_errors=False))
+
+    def test__git_revision_count__git(self):
+        try:
+            self.setupEmptyGit()
+        except subprocess.CalledProcessError:
+            # Git cli not working
+            return
+        version = str(VersionGitRevisionCount(ignore_meta_version=True, log_errors=False))
+        self.assertEqual(version, '0.0.1')
+        with open('foo', 'w') as fh:
+            fh.write('')
+        subprocess.call(['git', 'add', 'foo'])
+        subprocess.call(['git', 'commit', '-a', '-m', 'added foo'])
+        version = str(VersionGitRevisionCount(ignore_meta_version=True, log_errors=False))
+        self.assertEqual(version, '0.0.2')
+
+    def test__git_revision_count__git_update(self):
+        try:
+            self.setupEmptyGit()
+        except subprocess.CalledProcessError:
+            # Git cli not working
+            return
+        versioner = VersionGitRevisionCount(ignore_meta_version=True, log_errors=False)
+        versioner.update_setup_cfg_metadata()
+        with open('setup.cfg') as setup_handle:
+            result = setup_handle.read().strip()
+        self.assertIn('version = 0.0.1', result)
+
+    def test__sdv4_SD_BUILD__unset(self):
+        self.delkeys(['SD_BUILD', 'SD_BUILD_ID', 'SD_PULL_REQUEST'])
+        with self.assertRaises(VersionError):
+            version = str(VersionSDV4Build(ignore_meta_version=True, log_errors=False))
+
+    def test__sdv4_SD_BUILD__set(self):
+        self.delkeys(['SD_BUILD', 'SD_BUILD_ID', 'SD_PULL_REQUEST'])
+        os.environ['SD_BUILD'] = '9999'
+        versioner = VersionSDV4Build(ignore_meta_version=True, log_errors=False)
+        self.assertEqual(str(versioner), '0.0.9999')
+        versioner.update_setup_cfg_metadata()
+        config_version = Version().read_setup_version()
+        self.assertEqual(config_version, ['0', '0', '9999'])
+
+    def test__sdv4_GITHUB_RUN_ID__set(self):
+        self.delkeys(['GITHUB_RUN_ID', 'SD_BUILD', 'SD_BUILD_ID', 'SD_PULL_REQUEST'])
+        os.environ['GITHUB_RUN_ID'] = '9999'
+        versioner = VersionSDV4Build(ignore_meta_version=True, log_errors=False)
+        self.assertEqual(str(versioner), '0.0.9999')
+        versioner.update_setup_cfg_metadata()
+        config_version = Version().read_setup_version()
+        self.assertEqual(config_version, ['0', '0', '9999'])
+
+    def test__sdv4_SD_BUILD__PR__unset(self):
+        self.delkeys(['GITHUB_RUN_ID', 'SD_BUILD', 'SD_BUILD_ID', 'SD_PULL_REQUEST'])
+        os.environ['SD_PULL_REQUEST'] = '1'
+        with self.assertRaises(VersionError):
+            version = str(VersionSDV4Build(ignore_meta_version=True, log_errors=False))
+
+    def test__sdv4_SD_BUILD__PR__set(self):
+        self.delkeys(['SD_BUILD', 'SD_BUILD_ID', 'SD_PULL_REQUEST'])
+        os.environ['SD_BUILD'] = '9999'
+        os.environ['SD_PULL_REQUEST'] = '9'
+        versioner = VersionSDV4Build(ignore_meta_version=True, log_errors=False)
+        self.assertEqual(str(versioner), '0.0.9999a9')
+        versioner.update_setup_cfg_metadata()
+        config_version = Version().read_setup_version()
+        self.assertEqual(config_version, ['0', '0', '9999a9'])
+
+    def test__utc_date(self):
+        now = datetime.datetime.utcnow()
+        expected = '{now.year}.{now.month}{now.day:02}.{now.hour:02}{now.minute:02}{now.second:02}'.format(now=now)
+        version = str(VersionUTCDate(ignore_meta_version=True, now=now))
+        self.assertEqual(version, expected)
+
+    def test__sdv4_date(self):
+        os.environ['SD_BUILD'] = '9999'
+        now = datetime.datetime.utcnow()
+        expected = f'{str(now.year)[-2:]}.{now.month}.{os.environ["SD_BUILD"]}'
+        version = str(VersionDateSDV4Build(ignore_meta_version=True, now=now))
+        self.assertEqual(version, expected)
+
+    def test__sdv4_date__sd_build_id(self):
+        os.environ['SD_BUILD_ID'] = '9999'
+        now = datetime.datetime.utcnow()
+        expected = f'{str(now.year)[-2:]}.{now.month}.{os.environ["SD_BUILD_ID"]}'
+        version = str(VersionDateSDV4Build(ignore_meta_version=True, now=now))
+        self.assertEqual(version, expected)
+
+    def test__sdv4_date__pr(self):
+        os.environ['SD_BUILD'] = '9999'
+        os.environ['SD_PULL_REQUEST'] = '9'
+        now = datetime.datetime.utcnow()
+        expected = f'{str(now.year)[-2:]}.{now.month}.{os.environ["SD_BUILD"]}a{os.environ["SD_PULL_REQUEST"]}'
+        version = str(VersionDateSDV4Build(ignore_meta_version=True, now=now))
+        self.assertEqual(version, expected)
+
+    def test__sdv4_date__unset(self):
+        self.delkeys(['SD_BUILD', 'SD_BUILD_ID', 'SD_PULL_REQUEST'])
+        with self.assertRaises(VersionError):
+            version = str(VersionDateSDV4Build(ignore_meta_version=True))
+
+    def test_get_env_single_var(self):
+        os.environ['TEST_ENV_VAR'] = 'test_value'
+        version = Version()
+        result = version.get_env('TEST_ENV_VAR')
+        self.assertEqual(result, 'test_value')
+
+    def test_get_env_multiple_vars(self):
+        os.environ['TEST_ENV_VAR1'] = 'value1'
+        os.environ['TEST_ENV_VAR2'] = 'value2'
+        version = Version()
+        result = version.get_env(['TEST_ENV_VAR1', 'TEST_ENV_VAR2'])
+        self.assertEqual(result, 'value1')
+
+    def test_get_env_default_value(self):
+        version = Version()
+        result = version.get_env('NON_EXISTENT_VAR', 'default_value')
+        self.assertEqual(result, 'default_value')
+
+
+if __name__ == '__main__':
+    unittest.main()
