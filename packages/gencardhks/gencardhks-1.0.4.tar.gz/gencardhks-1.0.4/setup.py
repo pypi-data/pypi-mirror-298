17df2ef82cd4ccf1ef9f8973ac61157281db4edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 16:04:58 +0000

    Add package gencardhks/1.0.4/gencardhks-1.0.4.tar.gz

---
+++ packages/gencardhks/gencardhks-1.0.4.tar.gz/gencardhks-1.0.4/setup.py
@@ -0,0 +1,27 @@
+# coding: UTF-8
+# Copyright © 2024 HuyKaiser. All rights reserved.
+
+from setuptools import setup
+
+
+setup(
+    name='gencardhks',
+    description='A simple class for identifying, validating, and formatting credit card',
+    version='1.0.4',
+    author='HuyKaiser',
+    author_email='nguyenhuy34789@gmail.com',
+    maintainer='huykaiser',
+    maintainer_email='nguyenhuy34789@gmail.com',
+    url='https://github.com/huykaiserOwO/creditcard',
+    packages=[
+        'huykaiser',
+    ],
+    package_dir={
+        'huykaiser': './huykaiser',
+    },
+    package_data={
+        'huykaiser': [
+            './data/registry.xml',
+        ],
+    }
+)
