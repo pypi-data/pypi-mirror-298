d6cf66eab262d053d5fb8ed2d8237ec3786da628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 03:08:57 +0000

    Add package lino-react/24.9.4/lino_react-24.9.4-py3-none-any.whl

---
+++ packages/lino-react/lino_react-24.9.4-py3-none-any.whl/lino_react/__init__.py
@@ -0,0 +1,14 @@
+"""
+.. autosummary::
+   :toctree:
+
+    react
+
+"""
+
+
+__version__ = '24.9.4'
+
+srcref_url = 'https://gitlab.com/lino-framework/react/blob/master/%s'
+# doc_trees = []
+intersphinx_urls = dict(docs="https://lino-framework.gitlab.io/react/")
