38eb4898de355f943e69e44c417a30a56d711457
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 04:04:29 +0000

    Add package eocanvas/0.1.0/eocanvas-0.1.0.tar.gz

---
+++ packages/eocanvas/eocanvas-0.1.0.tar.gz/eocanvas-0.1.0/eocanvas/exceptions.py
@@ -0,0 +1,42 @@
+class EOCanvasError(Exception):
+    """Base class for EO Canvas exceptions."""
+
+
+class CredentialsError(EOCanvasError):
+    """Generic error regarding the credentials."""
+
+
+class MalformedCredentialsError(CredentialsError):
+    """Malformed credentials error."""
+
+
+class CorruptedCredentialsError(CredentialsError):
+    """Credentials file is bad YAML."""
+
+
+class CredentialsNotFoundError(CredentialsError):
+    """Missing credentials file."""
+
+
+class HTTPError(EOCanvasError):
+    """Exception on low level HTTP errors."""
+
+
+class QuotaExceededError(EOCanvasError):
+    """Exception on user quota exceeded."""
+
+
+class MalformedSnapError(EOCanvasError):
+    """Exception on malformed SNAP operators file."""
+
+
+class SnapOperatorNotFound(EOCanvasError):
+    """Exception on non-existent SNAP operator."""
+
+
+class JobFailed(EOCanvasError):
+    """Exception on a job that returns failed status."""
+
+
+class InvalidChainError(EOCanvasError):
+    """Exception on invalid YAML file for a Data Tailor Chain."""
