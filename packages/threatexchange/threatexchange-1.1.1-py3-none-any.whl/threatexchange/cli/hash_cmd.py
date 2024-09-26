834f65280c8045d1e6776552f3d0c825a26e625e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 05:05:19 +0000

    Add package threatexchange/1.1.1/threatexchange-1.1.1-py3-none-any.whl

---
+++ packages/threatexchange/threatexchange-1.1.1-py3-none-any.whl/threatexchange/cli/hash_cmd.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+
+"""
+Hash command to convert content into signatures.
+"""
+
+import argparse
+import pathlib
+import typing as t
+from threatexchange import common
+from threatexchange.cli.cli_config import CLISettings
+from threatexchange.cli.exceptions import CommandError
+from threatexchange.content_type.content_base import ContentType
+
+from threatexchange.signal_type.signal_base import FileHasher, SignalType
+from threatexchange.cli import command_base
+from threatexchange.cli.helpers import FlexFilesInputAction
+
+
+class HashCommand(command_base.Command):
+    """
+    Take content and convert it into signatures (aka hashes)
+
+    # Input
+    You can pass in data to the command in a few different ways:
+    (Note - you may not be able to hash text without an extension)
+    ```
+    # As an input file that contains one signal
+    $ threatexchange hash photo my_photo.jpg
+
+    # As stdin
+    $ echo This is my cool text | threatexchange hash text -
+
+    # Inline
+    $ threatexchange hash text -- This is my cool text
+    ```
+
+    # Output
+    <SignalType> <hash string>
+    """
+
+    @classmethod
+    def init_argparse(cls, settings: CLISettings, ap: argparse.ArgumentParser) -> None:
+        signal_types = [
+            s for s in settings.get_all_signal_types() if issubclass(s, FileHasher)
+        ]
+
+        content_choices = sorted(s.get_name() for s in settings.get_all_content_types())
+        signal_choices = sorted(
+            s.get_name() for s in signal_types if issubclass(s, FileHasher)
+        )
+        ap.add_argument(
+            "content_type",
+            **common.argparse_choices_pre_type_kwargs(
+                choices=content_choices,
+                type=settings.get_content_type,
+            ),
+            help="what kind of content to hash",
+        )
+
+        ap.add_argument(
+            "files",
+            nargs=argparse.REMAINDER,
+            action=FlexFilesInputAction,
+            help="list of files, URLs, - for stdin, or -- to interpret remainder as a string",
+        )
+
+        ap.add_argument(
+            "--signal-type",
+            "-S",
+            **common.argparse_choices_pre_type_kwargs(
+                choices=signal_choices,
+                type=settings.get_signal_type,
+            ),
+            help="only generate these signal types",
+        )
+
+    def __init__(
+        self,
+        content_type: t.Type[ContentType],
+        signal_type: t.Optional[t.Type[SignalType]],
+        files: t.List[pathlib.Path],
+    ) -> None:
+        self.content_type = content_type
+        self.signal_type = signal_type
+
+        self.files = files
+
+    def execute(self, settings: CLISettings) -> None:
+        hashers = [
+            s
+            for s in settings.get_signal_types_for_content(self.content_type)
+            if issubclass(s, FileHasher)
+        ]
+        if self.signal_type is not None:
+            if self.signal_type not in hashers:
+                raise CommandError.user(
+                    f"{self.signal_type.get_name()} "
+                    f"does not apply to {self.content_type.get_name()}"
+                )
+            hashers = [self.signal_type]  # type: ignore  # can't detect intersection types
+
+        for file in self.files:
+            for hasher in hashers:
+                hash_str = hasher.hash_from_file(file)
+                if hash_str:
+                    print(hasher.get_name(), hash_str)
---
+++ packages/threatexchange/threatexchange-1.1.1-py3-none-any.whl/threatexchange/exchanges/clients/ncmec/tests/test_hash_api.py
@@ -0,0 +1,325 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+
+from unittest.mock import Mock
+import urllib.parse
+import typing as t
+import pytest
+import requests
+from threatexchange.exchanges.clients.ncmec.hash_api import (
+    NCMECEntryType,
+    NCMECEntryUpdate,
+    NCMECHashAPI,
+    NCMECEnvironment,
+)
+
+STATUS_XML = """
+<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
+<status xmlns="https://hashsharing.ncmec.org/hashsharing/v2">
+    <ipAddress>127.0.0.1</ipAddress>
+    <username>testington</username>
+    <member id="1">Sir Testington</member>
+</status>
+""".strip()
+
+NEXT_UNESCAPED = (
+    "/v2/entries?from=2017-10-20T00%3A00%3A00.000Z"
+    "&to=2017-10-30T00%3A00%3A00.000Z&start=2001&size=1000&max=3000"
+)
+
+NEXT_UNESCAPED2 = (
+    "/v2/entries?from=2017-10-20T00%3A00%3A00.000Z"
+    "&to=2017-10-30T00%3A00%3A00.000Z&start=3001&size=1000&max=4000"
+)
+NEXT_UNESCAPED3 = (
+    "/v2/entries?from=2017-10-20T00%3A00%3A00.000Z"
+    "&to=2017-10-30T00%3A00%3A00.000Z&start=4001&size=1000&max=5000"
+)
+
+ENTRIES_XML = """
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<queryResult xmlns="https://hashsharing.ncmec.org/hashsharing/v2">
+    <images count="2" maxTimestamp="2017-10-24T15:10:00Z">
+        <image>
+            <member id="42">Example Member</member>
+            <timestamp>2017-10-24T15:00:00Z</timestamp>
+            <id>image1</id>
+            <classification>A1</classification>
+            <fingerprints>
+                <md5>a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1</md5>
+                <sha1>a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1</sha1>
+                <pdna>a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1...</pdna>
+            </fingerprints>
+        </image>
+        <deletedImage>
+            <member id="43">Example Member2</member>
+            <id>image4</id>
+            <timestamp>2017-10-24T15:10:00Z</timestamp>
+        </deletedImage>
+    </images>
+    <videos count="2" maxTimestamp="2017-10-24T15:20:00Z">
+        <video>
+            <member id="42">Example Member</member>
+            <timestamp>2017-10-24T15:00:00Z</timestamp>
+            <id>video1</id>
+            <fingerprints>
+                <md5>b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1</md5>
+                <sha1>b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1</sha1>
+            </fingerprints>
+        </video>
+        <deletedVideo>
+            <member id="42">Example Member</member>
+            <id>video4</id>
+            <timestamp>2017-10-24T15:20:00Z</timestamp>
+        </deletedVideo>
+    </videos>
+    <paging>
+        <next>/v2/entries?from=2017-10-20T00%3A00%3A00.000Z&amp;to=2017-10-30T00%3A00%3A00.000Z&amp;start=2001&amp;size=1000&amp;max=3000</next>
+    </paging>
+</queryResult>
+""".strip()
+
+
+ENTRIES_XML2 = """
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<queryResult xmlns="https://hashsharing.ncmec.org/hashsharing/v2">
+    <images count="1" maxTimestamp="2019-10-24T15:10:00Z">
+        <image>
+            <member id="42">Example Member</member>
+            <timestamp>2019-10-24T15:00:00Z</timestamp>
+            <id>image10</id>
+            <classification>A1</classification>
+            <fingerprints>
+                <md5>b1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1</md5>
+                <sha1>b1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1</sha1>
+                <pdna>b1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1...</pdna>
+            </fingerprints>
+        </image>
+    </images>
+    <paging>
+        <next>/v2/entries?from=2017-10-20T00%3A00%3A00.000Z&amp;to=2017-10-30T00%3A00%3A00.000Z&amp;start=3001&amp;size=1000&amp;max=4000</next>
+    </paging>
+</queryResult>
+""".strip()
+
+# This example isn't in the documentation, but shows how updates work
+ENTRIES_XML3 = """
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<queryResult xmlns="https://hashsharing.ncmec.org/hashsharing/v2">
+    <videos count="2" maxTimestamp="2019-11-25T15:10:00Z">
+        <video>
+            <member id="101">TX Example</member>
+            <timestamp>2019-11-25T15:10:00Z</timestamp>
+            <id>willupdate</id>
+            <classification>A1</classification>
+            <fingerprints>
+                <md5>facefacefacefacefacefacefaceface</md5>
+            </fingerprints>
+        </video>
+        <video>
+            <member id="101">TX Example</member>
+            <timestamp>2019-11-24T15:10:00Z</timestamp>
+            <id>willdelete</id>
+            <classification>A1</classification>
+            <fingerprints>
+                <md5>bacebacebacebacebacebacebacebace</md5>
+            </fingerprints>
+        </video>
+    </videos>
+    <paging>
+        <next>/v2/entries?from=2017-10-20T00%3A00%3A00.000Z&amp;to=2017-10-30T00%3A00%3A00.000Z&amp;start=4001&amp;size=1000&amp;max=5000</next>
+    </paging>
+</queryResult>
+""".strip()
+
+ENTRIES_XML4 = """
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<queryResult xmlns="https://hashsharing.ncmec.org/hashsharing/v2">
+    <videos count="2" maxTimestamp="2019-11-24T15:10:00Z">
+        <video>
+            <member id="101">TX Example</member>
+            <timestamp>2019-11-24T15:10:00Z</timestamp>
+            <id>willupdate</id>
+            <classification>A2</classification>
+            <fingerprints>
+                <md5>facefacefacefacefacefacefaceface</md5>
+            </fingerprints>
+        </video>
+        <deletedVideo>
+            <member id="101">TX Example</member>
+            <timestamp>2019-11-25T15:10:00Z</timestamp>
+            <id>willdelete</id>
+        </deletedVideo>
+    </videos>
+</queryResult>
+""".strip()
+
+ENTRIES_LARGE_FINGERPRINTS = """
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<queryResult xmlns="https://hashsharing.ncmec.org/hashsharing/v2">
+    <videos count="1" maxTimestamp="2019-11-24T15:10:00Z">
+        <video>
+            <member id="101">TX Example</member>
+            <timestamp>2019-11-24T15:10:00Z</timestamp>
+            <id>largetags</id>
+            <classification>A2</classification>
+            <fingerprints>
+                <md5>facefacefacefacefacefacefaceface</md5>
+                <tmk-pdqf rel="self" href="/v2/entries/1/fingerprints/TMK_PDQF"/>
+				<videntifier rel="self" href="/v2/entries/1/fingerprints/VIDENTIFIER"/>
+            </fingerprints>
+        </video>
+    </videos>
+</queryResult>
+""".strip()
+
+
+def mock_get_impl(url: str, **params):
+    content = ENTRIES_XML
+    if url.endswith(NEXT_UNESCAPED):
+        content = ENTRIES_XML2
+    if url.endswith(NEXT_UNESCAPED2):
+        content = ENTRIES_XML3
+    if url.endswith(NEXT_UNESCAPED3):
+        content = ENTRIES_XML4
+    # Void your warantee by messing with requests state
+    resp = requests.Response()
+    resp._content = content.encode()
+    resp.status_code = 200
+    resp.content  # Set the rest of Request's internal state
+    return resp
+
+
+def set_api_return(content: str):
+    # Some day support next
+    # def next_str(i: int) -> str:
+    #     return (
+    #         "/v2/entries?from=2017-10-20T00%3A00%3A00.000Z"
+    #         f"&to=2017-10-30T00%3A00%3A00.000Z&start={i * 1000 + 1}&size=1000&max={(i + 1) * 1000}"
+    #     )
+    #
+    # content = xmls[(int(qs.get("start", "1")) - 1) // 1000]
+
+    def _mock_get_impl(url: str, **params):
+        resp = requests.Response()
+        resp._content = content.encode()
+        resp.status_code = 200
+        resp.content  # Set the rest of Request's internal state
+        return resp
+
+    return _mock_get_impl
+
+
+@pytest.fixture
+def api(monkeypatch: pytest.MonkeyPatch):
+    api = NCMECHashAPI("fake_user", "fake_pass", NCMECEnvironment.test_Industry)
+    session = None
+    session = Mock(
+        strict_spec=["get", "__enter__", "__exit__"],
+        get=mock_get_impl,
+        __enter__=lambda _: session,
+        __exit__=lambda *args: None,
+    )
+    monkeypatch.setattr(api, "_get_session", lambda: session)
+    return api
+
+
+def assert_first_entry(entry: NCMECEntryUpdate) -> None:
+    assert entry.id == "image1"
+    assert entry.member_id == 42
+    assert entry.entry_type == NCMECEntryType.image
+    assert entry.deleted is False
+    assert entry.fingerprints == {
+        "md5": "a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1",
+        "sha1": "a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1",
+        "pdna": "a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1...",
+    }
+
+
+def assert_second_entry(entry: NCMECEntryUpdate) -> None:
+    assert entry.id == "image4"
+    assert entry.member_id == 43
+    assert entry.entry_type == NCMECEntryType.image
+    assert entry.deleted is True
+    assert entry.fingerprints == {}
+
+
+def assert_third_entry(entry: NCMECEntryUpdate) -> None:
+    assert entry.id == "video1"
+    assert entry.member_id == 42
+    assert entry.entry_type == NCMECEntryType.video
+    assert entry.deleted is False
+    assert entry.fingerprints == {
+        "md5": "b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1",
+        "sha1": "b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1",
+    }
+
+
+def assert_fourth_entry(entry: NCMECEntryUpdate) -> None:
+    assert entry.id == "video4"
+    assert entry.member_id == 42
+    assert entry.entry_type == NCMECEntryType.video
+    assert entry.deleted is True
+    assert entry.fingerprints == {}
+
+
+def assert_fifth_entry(entry: NCMECEntryUpdate) -> None:
+    assert entry.id == "image10"
+    assert entry.member_id == 42
+    assert entry.entry_type == NCMECEntryType.image
+    assert entry.deleted is False
+    assert entry.fingerprints == {
+        "md5": "b1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1",
+        "sha1": "b1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1",
+        "pdna": "b1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1...",
+    }
+
+
+def test_mocked_get_hashes(api: NCMECHashAPI):
+    result = api.get_entries()
+
+    assert len(result.updates) == 4
+    assert result.max_timestamp == 1508858400
+    assert result.next != ""
+    one, two, three, four = result.updates
+    assert_first_entry(one)
+    assert_second_entry(two)
+    assert_third_entry(three)
+    assert_fourth_entry(four)
+
+    second_result = api.get_entries(next_=result.next)
+
+    assert len(second_result.updates) == 1
+    assert second_result.max_timestamp == 1571929800
+    assert second_result.next != ""
+    five = second_result.updates[0]
+    assert_fifth_entry(five)
+
+    # These later results don't need to be tested for this test, matters
+    # for the SignalExchange API
+    third_result = api.get_entries(next_=second_result.next)
+    assert third_result.next != ""
+    assert third_result not in (result, second_result)
+    forth_result = api.get_entries(next_=third_result.next)
+    assert forth_result not in (result, second_result, third_result)
+    assert forth_result.next == ""
+
+    # The other updates don't need to be tested here
+
+
+def test_large_fingerprint_entries(monkeypatch):
+    api = NCMECHashAPI("fake_user", "fake_pass", NCMECEnvironment.test_Industry)
+    session = Mock(
+        strict_spec=["get", "__enter__", "__exit__"],
+        get=set_api_return(ENTRIES_LARGE_FINGERPRINTS),
+        __enter__=lambda _: session,
+        __exit__=lambda *args: None,
+    )
+    monkeypatch.setattr(api, "_get_session", lambda: session)
+
+    result = api.get_entries()
+
+    assert len(result.updates) == 1
+    update = result.updates[0]
+    assert len(update.fingerprints) == 1
+    assert update.fingerprints == {"md5": "facefacefacefacefacefacefaceface"}
+    assert result.next == ""
