bad7ba9852b45d1da62a2ea5b7fcd0492255d54e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 05:04:50 +0000

    Add package django-oscar-bundles/2.2.2/django_oscar_bundles-2.2.2-py3-none-any.whl

---
+++ packages/django-oscar-bundles/django_oscar_bundles-2.2.2-py3-none-any.whl/oscarbundles/dashboard/apps.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from django.utils.translation import gettext_lazy as _
+from oscar.core.application import OscarDashboardConfig
+
+
+class OscarBundlesDashboardConfig(OscarDashboardConfig):
+    label = "oscarbundles_dashboard"
+    name = "oscarbundles.dashboard"
+    verbose_name = _("Oscar Bundles Dashboard")
+    default = True
+
+    namespace = "oscarbundles_dashboard"
+    default_permissions = [
+        "is_staff",
+    ]
+
+    def get_urls(self):
+        from .views import BundleGroupSPAView
+
+        urlpatterns = [
+            path(
+                "bundle-groups/",
+                BundleGroupSPAView.as_view(),
+                name="dashboard-bundle-group-list",
+            ),
+        ]
+        return self.post_process_urls(urlpatterns)
