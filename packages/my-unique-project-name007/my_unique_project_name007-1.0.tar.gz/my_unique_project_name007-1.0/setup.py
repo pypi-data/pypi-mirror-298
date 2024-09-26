0a2a80e4af057ea297b833afb6d47ca3bf6d85a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 15:04:57 +0000

    Add package my-unique-project-name007/1.0/my_unique_project_name007-1.0.tar.gz

---
+++ packages/my-unique-project-name007/my_unique_project_name007-1.0.tar.gz/my_unique_project_name007-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_unique_project_name007',
+    version='1.0',
+    packages=find_packages(),
+    description='A simple example library',
+    author='Your Name',
+    author_email='your.email@asdasexample.com',
+)
