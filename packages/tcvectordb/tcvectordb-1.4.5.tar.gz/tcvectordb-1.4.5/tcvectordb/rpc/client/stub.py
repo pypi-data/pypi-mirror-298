5c16aa95ada223682d1e3759b6b3f3914ce903d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 03:09:01 +0000

    Add package tcvectordb/1.4.5/tcvectordb-1.4.5.tar.gz

---
+++ packages/tcvectordb/tcvectordb-1.4.5.tar.gz/tcvectordb-1.4.5/tcvectordb/rpc/client/stub.py
@@ -0,0 +1,224 @@
+from typing import Optional, List, Union, Dict
+
+from cachetools import cached, TTLCache
+from requests.adapters import HTTPAdapter
+
+from tcvectordb import VectorDBClient
+from tcvectordb.model.ai_database import AIDatabase
+from tcvectordb.model.database import Database
+from tcvectordb.model.document import Document, Filter, AnnSearch, KeywordSearch, Rerank
+from tcvectordb.model.enum import ReadConsistency
+from tcvectordb.rpc.client.rpcclient import RPCClient
+from tcvectordb.rpc.client.vdbclient import VdbClient
+from tcvectordb.rpc.model.database import RPCDatabase, db_convert
+
+
+class RPCVectorDBClient(VectorDBClient):
+    """
+    RPCVectorDBClient create a grpc client for database operate.
+    """
+
+    def __init__(self,
+                 url: str,
+                 username='',
+                 key='',
+                 read_consistency: ReadConsistency = ReadConsistency.EVENTUAL_CONSISTENCY,
+                 timeout=10,
+                 adapter: HTTPAdapter = None,
+                 pool_size: int = 10,
+                 proxies: Optional[dict] = None,
+                 **kwargs):
+        super().__init__(url, username, key, read_consistency, timeout, adapter,
+                         pool_size=pool_size, proxies=proxies)
+        rpc_client = RPCClient(url=url,
+                               username=username,
+                               key=key,
+                               timeout=timeout,
+                               **kwargs)
+        self.vdb_client = VdbClient(client=rpc_client, read_consistency=read_consistency)
+
+    def create_database(self, database_name: str, timeout: Optional[float] = None) -> RPCDatabase:
+        sdb = super().create_database(database_name=database_name, timeout=timeout)
+        return db_convert(sdb, self.vdb_client)
+
+    def list_databases(self, timeout: Optional[float] = None) -> List[Database]:
+        sdbs = super().list_databases(timeout=timeout)
+        dbs = []
+        for sdb in sdbs:
+            if isinstance(sdb, AIDatabase):
+                dbs.append(sdb)
+            else:
+                dbs.append(db_convert(sdb, self.vdb_client))
+        return sdbs
+
+    @cached(cache=TTLCache(maxsize=1024, ttl=3))
+    def database(self, database: str) -> Union[RPCDatabase, AIDatabase]:
+        sdb = super().database(database)
+        return db_convert(sdb, self.vdb_client)
+
+    def close(self):
+        super().close()
+        self.vdb_client.close()
+
+    def upsert(self,
+               database_name: str,
+               collection_name: str,
+               documents: List[Union[Document, Dict]],
+               timeout: Optional[float] = None,
+               build_index: bool = True,
+               **kwargs):
+        return self.vdb_client.upsert(
+            database_name=database_name,
+            collection_name=collection_name,
+            documents=documents,
+            timeout=timeout,
+            build_index=build_index,
+            **kwargs
+        )
+
+    def delete(self,
+               database_name: str,
+               collection_name: str,
+               document_ids: List[str] = None,
+               filter: Filter = None,
+               timeout: Optional[float] = None):
+        return self.vdb_client.delete(
+            database_name=database_name,
+            collection_name=collection_name,
+            document_ids=document_ids,
+            filter=filter,
+            timeout=timeout,
+        )
+
+    def update(self,
+               database_name: str,
+               collection_name: str,
+               data: Union[Document, Dict],
+               filter: Optional[Filter] = None,
+               document_ids: Optional[List[str]] = None,
+               timeout: Optional[float] = None):
+        return self.vdb_client.update(
+            database_name=database_name,
+            collection_name=collection_name,
+            data=data,
+            filter=filter,
+            document_ids=document_ids,
+            timeout=timeout,
+        )
+
+    def query(self,
+              database_name: str,
+              collection_name: str,
+              document_ids: Optional[List] = None,
+              retrieve_vector: bool = False,
+              limit: Optional[int] = None,
+              offset: Optional[int] = None,
+              filter: Optional[Filter] = None,
+              output_fields: Optional[List[str]] = None,
+              timeout: Optional[float] = None,
+              ) -> List[Dict]:
+        return self.vdb_client.query(
+            database_name=database_name,
+            collection_name=collection_name,
+            document_ids=document_ids,
+            retrieve_vector=retrieve_vector,
+            limit=limit,
+            offset=offset,
+            filter=filter,
+            output_fields=output_fields,
+            timeout=timeout,
+        )
+
+    def search(self,
+               database_name: str,
+               collection_name: str,
+               vectors: List[List[float]],
+               filter: Filter = None,
+               params=None,
+               retrieve_vector: bool = False,
+               limit: int = 10,
+               output_fields: Optional[List[str]] = None,
+               timeout: Optional[float] = None,
+               ) -> List[List[Dict]]:
+        return self.vdb_client.search(
+            database_name=database_name,
+            collection_name=collection_name,
+            vectors=vectors,
+            filter=filter,
+            params=params,
+            retrieve_vector=retrieve_vector,
+            limit=limit,
+            output_fields=output_fields,
+            timeout=timeout,
+        )
+
+    def search_by_id(self,
+                     database_name: str,
+                     collection_name: str,
+                     document_ids: List[str],
+                     filter: Filter = None,
+                     params=None,
+                     retrieve_vector: bool = False,
+                     limit: int = 10,
+                     output_fields: Optional[List[str]] = None,
+                     timeout: Optional[float] = None,
+                     ) -> List[List[Dict]]:
+        return self.vdb_client.search(
+            database_name=database_name,
+            collection_name=collection_name,
+            document_ids=document_ids,
+            filter=filter,
+            params=params,
+            retrieve_vector=retrieve_vector,
+            limit=limit,
+            output_fields=output_fields,
+            timeout=timeout,
+        )
+
+    def search_by_text(self,
+                       database_name: str,
+                       collection_name: str,
+                       embedding_items: List[str],
+                       filter: Filter = None,
+                       params=None,
+                       retrieve_vector: bool = False,
+                       limit: int = 10,
+                       output_fields: Optional[List[str]] = None,
+                       timeout: Optional[float] = None,
+                       ) -> List[List[Dict]]:
+        return self.vdb_client.search(
+            database_name=database_name,
+            collection_name=collection_name,
+            embedding_items=embedding_items,
+            filter=filter,
+            params=params,
+            retrieve_vector=retrieve_vector,
+            limit=limit,
+            output_fields=output_fields,
+            timeout=timeout,
+        )
+
+    def hybrid_search(self,
+                      database_name: str,
+                      collection_name: str,
+                      ann: Optional[Union[List[AnnSearch], AnnSearch]] = None,
+                      match: Optional[Union[List[KeywordSearch], KeywordSearch]] = None,
+                      filter: Optional[Filter] = None,
+                      rerank: Optional[Rerank] = None,
+                      retrieve_vector: Optional[bool] = None,
+                      output_fields: Optional[List[str]] = None,
+                      limit: Optional[int] = None,
+                      timeout: Optional[float] = None,
+                      **kwargs) -> List[List[Dict]]:
+        return self.vdb_client.hybrid_search(
+            database_name=database_name,
+            collection_name=collection_name,
+            ann=ann,
+            match=match,
+            filter=filter,
+            rerank=rerank,
+            retrieve_vector=retrieve_vector,
+            output_fields=output_fields,
+            limit=limit,
+            timeout=timeout,
+            **kwargs)
---
+++ packages/tcvectordb/tcvectordb-1.4.5.tar.gz/tcvectordb-1.4.5/tcvectordb/rpc/client/vdbclient.py
@@ -0,0 +1,441 @@
+from typing import List, Union, Dict, Optional, Any
+
+from tcvectordb.exceptions import ServerInternalError
+from tcvectordb.model.document import Document, Filter, AnnSearch, KeywordSearch, Rerank, WeightedRerank, RRFRerank
+from tcvectordb.model.enum import ReadConsistency
+from tcvectordb.rpc.client.rpcclient import RPCClient
+from tcvectordb.rpc.proto import olama_pb2
+
+
+class VdbClient:
+    def __init__(self,
+                 client: RPCClient,
+                 read_consistency: ReadConsistency,
+                 ):
+        self.rpc_client = client
+        self.read_consistency = read_consistency
+
+    def close(self):
+        self.rpc_client.close()
+
+    def upsert(self,
+               database_name: str,
+               collection_name: str,
+               documents: List[Union[Document, Dict]],
+               timeout: Optional[float] = None,
+               build_index: bool = True,
+               **kwargs):
+        buildIndex = bool(kwargs.get("buildIndex", True))
+        res_build_index = buildIndex and build_index
+        doc_list = []
+        ai = False
+        if len(documents) > 0:
+            if isinstance(documents[0], dict):
+                ai = isinstance(documents[0].get('vector'), str)
+            else:
+                ai = isinstance(vars(documents[0]).get('vector'), str)
+        for doc in documents:
+            doc_list.append(self._doc2pb(doc))
+        request = olama_pb2.UpsertRequest(
+            database=database_name,
+            collection=collection_name,
+            documents=doc_list,
+            buildIndex=res_build_index,
+        )
+        result: olama_pb2.UpsertResponse = self.rpc_client.upsert(request, timeout=timeout, ai=ai)
+        res = {
+            'code': result.code,
+            'msg': result.msg,
+            'affectedCount': result.affectedCount
+        }
+        if result.embedding_extra_info and result.embedding_extra_info.token_used:
+            res['embeddingExtraInfo'] = {'tokenUsed': result.embedding_extra_info.token_used}
+        return res
+
+    def delete(self,
+               database_name: str,
+               collection_name: str,
+               document_ids: List[str] = None,
+               filter: Filter = None,
+               timeout: float = None):
+        query = olama_pb2.QueryCond()
+        if document_ids is not None:
+            query.documentIds.extend(document_ids)
+        if filter is not None:
+            query.filter = filter.cond
+        request = olama_pb2.DeleteRequest(
+            database=database_name,
+            collection=collection_name,
+            query=query,
+        )
+        result: olama_pb2.DeleteResponse = self.rpc_client.delete(request, timeout=timeout)
+        return {
+            'code': result.code,
+            'msg': result.msg,
+            'affectedCount': result.affectedCount
+        }
+
+    def update(self,
+               database_name: str,
+               collection_name: str,
+               data: Union[Document, Dict],
+               filter: Optional[Filter] = None,
+               document_ids: Optional[List[str]] = None,
+               timeout: Optional[float] = None):
+        query = olama_pb2.QueryCond()
+        if document_ids is not None:
+            query.documentIds.extend(document_ids)
+        if filter is not None:
+            query.filter = filter.cond
+        ai = False
+        if isinstance(data, dict):
+            ai = isinstance(data.get('vector'), str)
+        else:
+            ai = isinstance(vars(data).get('vector'), str)
+        request = olama_pb2.UpdateRequest(
+            database=database_name,
+            collection=collection_name,
+            query=query,
+            update=self._doc2pb(data),
+        )
+        result: olama_pb2.UpdateResponse = self.rpc_client.update(request, timeout=timeout, ai=ai)
+        res = {
+            'warning': result.warning,
+            'affectedCount': result.affectedCount
+        }
+        if result.embedding_extra_info and result.embedding_extra_info.token_used:
+            res['embeddingExtraInfo'] = {'tokenUsed': result.embedding_extra_info.token_used}
+        return res
+
+    def query(self,
+              database_name: str,
+              collection_name: str,
+              document_ids: Optional[List] = None,
+              retrieve_vector: bool = False,
+              limit: Optional[int] = None,
+              offset: Optional[int] = None,
+              filter: Optional[Filter] = None,
+              output_fields: Optional[List[str]] = None,
+              timeout: Optional[float] = None) -> List[Dict]:
+        query = olama_pb2.QueryCond(
+            retrieveVector=retrieve_vector,
+        )
+        if document_ids is not None:
+            query.documentIds.extend(document_ids)
+        if filter is not None:
+            query.filter = filter.cond
+        if retrieve_vector is not None:
+            query.retrieveVector = retrieve_vector
+        if limit is not None:
+            query.limit = limit
+        if offset is not None:
+            query.offset = offset
+        if output_fields is not None:
+            query.outputFields.extend(output_fields)
+        request = olama_pb2.QueryRequest(
+            database=database_name,
+            collection=collection_name,
+            query=query,
+            readConsistency=self.read_consistency.value,
+        )
+        result: olama_pb2.QueryResponse = self.rpc_client.query(request, timeout=timeout)
+        res = []
+        for d in result.documents:
+            res.append(self._pb2doc(d))
+        return res
+
+    def search(self,
+               database_name: str,
+               collection_name: str,
+               document_ids: Optional[List[str]] = None,
+               vectors: Optional[List[List[float]]] = None,
+               embedding_items: List[str] = None,
+               filter: Filter = None,
+               params=None,
+               retrieve_vector: bool = False,
+               limit: int = 10,
+               output_fields: Optional[List[str]] = None,
+               timeout: Optional[float] = None,
+               ) -> List[List[Dict]]:
+        return self.search_with_warning(
+            database_name=database_name,
+            collection_name=collection_name,
+            document_ids=document_ids,
+            vectors=vectors,
+            embedding_items=embedding_items,
+            filter=filter,
+            params=params,
+            retrieve_vector=retrieve_vector,
+            limit=limit,
+            output_fields=output_fields,
+            timeout=timeout,
+        ).get('documents')
+
+    def search_with_warning(self,
+                            database_name: str,
+                            collection_name: str,
+                            document_ids: Optional[List[str]] = None,
+                            vectors: Optional[List[List[float]]] = None,
+                            embedding_items: List[str] = None,
+                            filter: Filter = None,
+                            params=None,
+                            retrieve_vector: bool = False,
+                            limit: int = 10,
+                            output_fields: Optional[List[str]] = None,
+                            timeout: Optional[float] = None,
+                            ) -> Dict[str, Any]:
+        search = olama_pb2.SearchCond()
+        if vectors is not None:
+            for v in vectors:
+                search.vectors.append(olama_pb2.VectorArray(vector=v))
+        if document_ids is not None:
+            search.documentIds.extend(document_ids)
+        if embedding_items is not None:
+            search.embeddingItems.extend(embedding_items)
+        if params is not None:
+            if not isinstance(params, dict):
+                params = vars(params)
+            if params.get('ef') is not None:
+                search.params.ef = params.get('ef')
+            if params.get('nprobe') is not None:
+                search.params.nprobe = params.get('nprobe')
+            if params.get('radius') is not None:
+                search.params.radius = params.get('radius')
+        if filter is not None:
+            search.filter = filter.cond
+        if output_fields is not None:
+            search.outputfields.extend(output_fields)
+        if retrieve_vector is not None:
+            search.retrieveVector = retrieve_vector
+        if limit is not None:
+            search.limit = limit
+        request = olama_pb2.SearchRequest(
+            database=database_name,
+            collection=collection_name,
+            readConsistency=self.read_consistency.value,
+            search=search,
+        )
+        res: olama_pb2.SearchResponse = self.rpc_client.search(request, timeout=timeout)
+        rtl = []
+        for r in res.results:
+            docs = []
+            for d in r.documents:
+                docs.append(self._pb2doc(d))
+            rtl.append(docs)
+        return {
+            'warning': res.warning,
+            'documents': rtl
+        }
+
+    def _search_cond(self,
+                     ann: Optional[List[AnnSearch]] = None,
+                     match: Optional[List[KeywordSearch]] = None,
+                     filter: Optional[Filter] = None,
+                     rerank: Optional[Rerank] = None,
+                     retrieve_vector: Optional[bool] = None,
+                     output_fields: Optional[List[str]] = None,
+                     limit: Optional[int] = None,
+                     embedding_items: List[str] = None,
+                     **kwargs
+                     ):
+        search = olama_pb2.SearchCond()
+        ai = False
+        if rerank is not None:
+            search.rerank_params.method = rerank.method
+            if isinstance(rerank, WeightedRerank):
+                if rerank.field_list is not None and rerank.weight is not None:
+                    for i in range(len(rerank.field_list)):
+                        search.rerank_params.weights[rerank.field_list[i]] = rerank.weight[i]
+            elif isinstance(rerank, RRFRerank):
+                if rerank.k is not None:
+                    search.rerank_params.rrf_k = rerank.k
+        if match is not None:
+            for m in match:
+                md = olama_pb2.SparseData()
+                if m.field_name is not None:
+                    md.fieldName = m.field_name
+                if m.limit is not None:
+                    md.limit = m.limit
+                data = m.data
+                # hybrid_search sdk暂时不提供batch，但接口是batch
+                if isinstance(data, list) and len(data) > 0 and isinstance(data[0], list) \
+                        and len(data[0]) > 0 and type(data[0][0]) == int:
+                    data = [data]
+                for item in data:
+                    sva = olama_pb2.SparseVectorArray()
+                    for pair in item:
+                        svi = olama_pb2.SparseVecItem()
+                        svi.term_id = pair[0]
+                        svi.score = pair[1]
+                        sva.sp_vector.append(svi)
+                    md.data.append(sva)
+                search.sparse.append(md)
+        if filter is not None:
+            search.filter = filter.cond
+        if output_fields is not None:
+            search.outputfields.extend(output_fields)
+        if retrieve_vector is not None:
+            search.retrieveVector = retrieve_vector
+        if limit is not None:
+            search.limit = limit
+        if embedding_items is not None:
+            search.embeddingItems.extend(embedding_items)
+        if ann:
+            d_type_str = False
+            if len(ann) > 0:
+                if isinstance(ann[0].data, str):
+                    d_type_str = True
+                    ai = True
+                elif isinstance(ann[0].data, list) and len(ann[0].data) > 0:
+                    if isinstance(ann[0].data[0], str):
+                        d_type_str = True
+                        ai = True
+            for a in ann:
+                ann_data = olama_pb2.AnnData()
+                if a.field_name is not None:
+                    ann_data.fieldName = a.field_name
+                if a.document_ids is not None:
+                    ann_data.documentIds.extend(a.document_ids)
+                if a.data is not None:
+                    data = a.data
+                    if isinstance(data, str):
+                        data = [data]
+                    elif isinstance(data, list) and len(data) > 0 and type(data[0]) in (int, float, complex):
+                        data = [data]
+                    for v in data:
+                        if isinstance(v, str):
+                            if d_type_str:
+                                ann_data.data_expr.append(v)
+                            else:
+                                raise ServerInternalError(
+                                    code=14100,
+                                    message="vector datatype must be same as the first vector's datatype vector")
+                        else:
+                            if d_type_str:
+                                raise ServerInternalError(
+                                    code=14100,
+                                    message="vector datatype must be same as the first vector's datatype expression")
+                            else:
+                                ann_data.data.append(olama_pb2.VectorArray(vector=v))
+                if a.params:
+                    params = a.params
+                    if not isinstance(params, dict):
+                        params = vars(params)
+                    if params.get('ef') is not None:
+                        ann_data.params.ef = params.get("ef")
+                    if params.get('nprobe') is not None:
+                        ann_data.params.nprobe = params.get("nprobe")
+                    if params.get('radius') is not None:
+                        ann_data.params.radius = params.get("radius")
+                if a.limit is not None:
+                    ann_data.limit = a.limit
+                search.ann.append(ann_data)
+        return search, ai
+
+    def hybrid_search(self,
+                      database_name: str,
+                      collection_name: str,
+                      ann: Optional[Union[List[AnnSearch], AnnSearch]] = None,
+                      match: Optional[Union[List[KeywordSearch], KeywordSearch]] = None,
+                      filter: Optional[Filter] = None,
+                      rerank: Optional[Rerank] = None,
+                      retrieve_vector: Optional[bool] = None,
+                      output_fields: Optional[List[str]] = None,
+                      limit: Optional[int] = None,
+                      timeout: Optional[float] = None,
+                      embedding_items: List[str] = None,
+                      **kwargs) -> List[List[Dict]]:
+        single = True
+        if isinstance(ann, List):
+            single = False
+        else:
+            ann = [ann]
+        if isinstance(match, List):
+            single = False
+        else:
+            match = [match]
+        search, ai = self._search_cond(
+            ann=ann,
+            match=match,
+            filter=filter,
+            rerank=rerank,
+            retrieve_vector=retrieve_vector,
+            output_fields=output_fields,
+            limit=limit,
+            embedding_items=embedding_items,
+            **kwargs
+        )
+        request = olama_pb2.SearchRequest(
+            database=database_name,
+            collection=collection_name,
+            readConsistency=self.read_consistency.value,
+            search=search,
+        )
+        res: olama_pb2.SearchResponse = self.rpc_client.hybrid_search(request, timeout=timeout, ai=ai)
+        if 'warning' in res.warning:
+            Warning(res.warning)
+        rtl = []
+        for r in res.results:
+            docs = []
+            for d in r.documents:
+                docs.append(self._pb2doc(d))
+            rtl.append(docs)
+        if single:
+            rtl = rtl[0]
+        return rtl
+
+    def _pb2doc(self, d: olama_pb2.Document) -> dict:
+        doc = {
+            'id': d.id,
+        }
+        if d.score is not None:
+            doc['score'] = d.score
+        if d.vector:
+            vecs = []
+            for v in d.vector:
+                vecs.append(v.real)
+            doc['vector'] = vecs
+        if d.sparse_vector:
+            sp_vector = []
+            for item in d.sparse_vector:
+                sp_vector.append([item.term_id, item.score])
+            doc['sparse_vector'] = sp_vector
+        for k, v in d.fields.items():
+            if len(v.val_str) > 0:
+                doc[k] = str(v.val_str, encoding='utf-8')
+            elif len(v.val_str_arr.str_arr) > 0:
+                arr = []
+                for a in v.val_str_arr.str_arr:
+                    arr.append(str(a, encoding='utf-8'))
+                doc[k] = arr
+            else:
+                doc[k] = v.val_u64
+        return doc
+
+    def _doc2pb(self, doc: Union[Document, Dict]) -> olama_pb2.Document:
+        doc_dict = doc if isinstance(doc, dict) else vars(doc)
+        d = olama_pb2.Document()
+        for k, v in doc_dict.items():
+            if 'id' == k:
+                d.id = v
+            elif 'vector' == k:
+                if isinstance(v, str):
+                    d.data_expr = v
+                else:
+                    d.vector.extend(v)
+            elif 'sparse_vector' == k:
+                for sp in v:
+                    d.sparse_vector.append(olama_pb2.SparseVecItem(
+                        term_id=sp[0],
+                        score=sp[1],
+                    ))
+            elif isinstance(v, int):
+                d.fields[k].val_u64 = v
+            elif isinstance(v, str):
+                d.fields[k].val_str = bytes(v, encoding='utf-8')
+            elif isinstance(v, list):
+                al = []
+                for arr in v:
+                    al.append(bytes(arr, encoding='utf-8'))
+                d.fields[k].val_str_arr.str_arr.extend(al)
+        return d
