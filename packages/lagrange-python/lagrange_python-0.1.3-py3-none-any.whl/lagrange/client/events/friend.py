25b87a0c785fcd5874e6ad6946ce4be1446d2f3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 05:05:25 +0000

    Add package lagrange-python/0.1.3/lagrange_python-0.1.3-py3-none-any.whl

---
+++ packages/lagrange-python/lagrange_python-0.1.3-py3-none-any.whl/lagrange/client/events/friend.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
+from . import BaseEvent
+
+if TYPE_CHECKING:
+    from lagrange.client.message.types import Element
+
+
+@dataclass
+class FriendEvent(BaseEvent):
+    from_uin: int
+    from_uid: str
+    to_uin: int
+    to_uid: str
+
+
+@dataclass
+class FriendMessage(FriendEvent):
+    seq: int
+    msg_id: int
+    timestamp: int
+    msg: str
+    msg_chain: list[Element]
+
+
+@dataclass
+class FriendRecall(FriendEvent):
+    seq: int
+    msg_id: int
+    timestamp: int
