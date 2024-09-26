852f692b4751491b888791f00902ec2c46683382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 15:05:17 +0000

    Add package pepe-toolbox/0.3.6/pepe_toolbox-0.3.6-py3-none-any.whl

---
+++ packages/pepe-toolbox/pepe_toolbox-0.3.6-py3-none-any.whl/pepe_toolbox/File/__init__.py
@@ -0,0 +1,4 @@
+from .image_converter import ImageConverter
+from .zipfile import ZipFile
+from .document_preprocessor import DocumentPreprocessor
+__all__ = ["ImageConverter", "ZipFile", "DocumentPreprocessor"]
