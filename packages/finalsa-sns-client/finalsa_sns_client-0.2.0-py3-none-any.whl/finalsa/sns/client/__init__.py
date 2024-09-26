3f963dac9e538f59682c40abcf4560d7f3bc43a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 14:05:18 +0000

    Add package finalsa-sns-client/0.2.0/finalsa_sns_client-0.2.0-py3-none-any.whl

---
+++ packages/finalsa-sns-client/finalsa_sns_client-0.2.0-py3-none-any.whl/finalsa/sns/client/__init__.py
@@ -0,0 +1,11 @@
+from finalsa.sns.client.interface import SnsClient
+from finalsa.sns.client.client import SnsClientImpl
+from finalsa.sns.client.tests import SnsClientTest
+
+__version__ = "0.2.0"
+
+__all__ = [
+    "SnsClient",
+    "SnsClientImpl",
+    "SnsClientTest"
+]
