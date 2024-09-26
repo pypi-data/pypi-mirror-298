efe5c9f26d4c20bcc43b9857a2fc1be062b37e62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 04:04:57 +0000

    Add package levutils/0.1/levutils-0.1.tar.gz

---
+++ packages/levutils/levutils-0.1.tar.gz/levutils-0.1/README.md
@@ -0,0 +1,21 @@
+# eais
+
+The "levutils" package contains several common utilities modules
+
+   ## Installation
+
+``` 
+     pip install levutils
+```
+
+   ## Usage
+
+``` python
+     import levutils.mybag
+     import levutils.myutils
+     import levutils.util_jupyter
+```
+
+   ## License
+
+   This project is licensed under the MIT License - see the LICENSE file for details.
---
+++ packages/levutils/levutils-0.1.tar.gz/levutils-0.1/setup.py
@@ -0,0 +1,22 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='levutils',
+    version='0.1',
+    packages=find_packages(exclude=['tests*', 'doc*']),
+    author='Lev Selector',
+    author_email='lev.selector@gmail.com',
+    description='A package containing modules mybag and myutils ',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/eais',
+    install_requires=[
+        "pandas", "numpy", "unidecode", "ipython"
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
