50e638ea540cec8d4867b78a4961a6eaeda1403a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 06:04:36 +0000

    Add package udotcolors/0.1/udotcolors-0.1.tar.gz

---
+++ packages/udotcolors/udotcolors-0.1.tar.gz/udotcolors-0.1/udotcolors/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ["colors", "palettes"]
+
+from . import colors, palettes
+from .colors import get_color, get_color_list, list_colors
+from .palettes import palette, palette_seq, palette_div, list_palettes
