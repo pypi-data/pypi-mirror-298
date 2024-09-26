7793c785c40c2f6957f3db029335b9fa0fe5b60f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 06:03:59 +0000

    Add package sklearmmm/0.2.5/sklearmmm-0.2.5.tar.gz

---
+++ packages/sklearmmm/sklearmmm-0.2.5.tar.gz/sklearmmm-0.2.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sklearmmm",
+    version="0.2.5",
+    packages=find_packages(),
+    include_package_data=True,  # Включаем дополнительные файлы (например, изображения)
+    package_data={
+        "sklearmmm": ["images/*.png"],  # Указываем, какие файлы включить (все изображения PNG)
+    },
+    description="Library with get_task function",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author="John Doe",
+    author_email="johndoe@gmail.com",
+    url="https://github.com/johndoe/sklearmmm",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
