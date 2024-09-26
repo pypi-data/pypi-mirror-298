a4406c5bfd239199b8b665d3e22ea54e826479fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 15:05:12 +0000

    Add package ldjango/2.3/ldjango-2.3.tar.gz

---
+++ packages/ldjango/ldjango-2.3.tar.gz/ldjango-2.3/setup.py
@@ -0,0 +1,31 @@
+import pkg_resources
+from setuptools import find_packages, setup
+
+
+def get_latest_version(package):
+    try:
+        return pkg_resources.get_distribution(package).version
+    except pkg_resources.DistributionNotFound:
+        return None
+
+required_packages = ['Click', 'Django','whitenoise','tqdm']
+install_requires = [f"{pkg}>={get_latest_version(pkg) or '0'}" for pkg in required_packages]
+
+setup(
+    name='ldjango',
+    version='2.3',
+    packages=find_packages(),
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+            'ldjango = ldjango.cli:cli',
+        ],
+    },
+    description='CLI tool for creating Django projects with a predefined structure.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Liaranda',
+    author_email='hafiztamvan15@gmail.com',
+    license='MIT',
+)
+
