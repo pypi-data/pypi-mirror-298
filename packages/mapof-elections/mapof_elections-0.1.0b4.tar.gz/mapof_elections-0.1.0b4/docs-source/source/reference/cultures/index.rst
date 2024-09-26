861bc4edb3da5c9fb8de568cf6152cf13ecfddfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 03:08:56 +0000

    Add package mapof-elections/0.1.0b4/mapof_elections-0.1.0b4.tar.gz

---
+++ packages/mapof-elections/mapof_elections-0.1.0b4.tar.gz/mapof_elections-0.1.0b4/docs-source/source/reference/cultures/index.rst
@@ -0,0 +1,9 @@
+Cultures
+========
+
+.. toctree::
+    :maxdepth: 2
+
+    main
+    euclidean
+    guardians
---
+++ packages/mapof-elections/mapof_elections-0.1.0b4.tar.gz/mapof_elections-0.1.0b4/tests/func/mapof/elections/objects/test_OnlineApprovalExperiment.py
@@ -0,0 +1,13 @@
+
+import mapof.elections as mapof
+
+
+class TestOnlineApprovalExperiment:
+
+    def test_experiment_creation(self):
+
+        experiment = mapof.prepare_online_approval_experiment()
+
+
+
+
