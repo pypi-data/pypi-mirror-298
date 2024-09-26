9d83c8404fc95a7024e1f907be04e08828445669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 04:04:30 +0000

    Add package mytoolsID/0.0.0.1.dev33/mytoolsID-0.0.0.1.dev33-py3-none-any.whl

---
+++ packages/mytoolsID/mytoolsID-0.0.0.1.dev33-py3-none-any.whl/mytools/__init__.py
@@ -0,0 +1,20 @@
+from .button import Button
+from .chatbot import Api, ImageGen
+from .database import LocalDataBase, MongoDataBase
+from .encrypt import BinaryEncryptor, CryptoEncryptor
+from .getuser import Extract
+from .logger import LoggerHandler
+from .misc import Handler
+from .trans import Translate
+
+__version__ = "0.0.0.1.dev33"
+
+mytoolsID = """
+ __    __     __  __     ______   ______     ______     __         ______     __     _____    
+/\ "-./  \   /\ \_\ \   /\__  _\ /\  __ \   /\  __ \   /\ \       /\  ___\   /\ \   /\  __-.  
+\ \ \-./\ \  \ \____ \  \/_/\ \/ \ \ \/\ \  \ \ \/\ \  \ \ \____  \ \___  \  \ \ \  \ \ \/\ \ 
+ \ \_\ \ \_\  \/\_____\    \ \_\  \ \_____\  \ \_____\  \ \_____\  \/\_____\  \ \_\  \ \____- 
+  \/_/  \/_/   \/_____/     \/_/   \/_____/   \/_____/   \/_____/   \/_____/   \/_/   \/____/ 
+"""
+
+print(f"\033[1;37;41m{mytoolsID}\033[0m")
