f8406925419333926daec2ebe449987bb5b6e8a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 14:05:19 +0000

    Add package etu-df-mapp/1.0.2/etu_df_mapp-1.0.2-py3-none-any.whl

---
+++ packages/etu-df-mapp/etu_df_mapp-1.0.2-py3-none-any.whl/etu_df_mapp/django/4.2.16/app/models/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/1/20 4:26 下午
+# @Author  : Jieay
+# @File    : __init__.py.py
+
+from app.models.common.fake_model import FakeModel
+from app.models.user.members import *
+from app.models.common.settings_model import SettingConfigsModel
