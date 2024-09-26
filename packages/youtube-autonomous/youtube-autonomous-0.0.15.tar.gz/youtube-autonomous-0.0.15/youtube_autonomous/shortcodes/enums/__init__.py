1c357294a1d04b0cdd305bbb29e794a605bf9324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 03:08:55 +0000

    Add package youtube-autonomous/0.0.15/youtube-autonomous-0.0.15.tar.gz

---
+++ packages/youtube-autonomous/youtube-autonomous-0.0.15.tar.gz/youtube-autonomous-0.0.15/youtube_autonomous/shortcodes/enums/__init__.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class ShortcodeType(Enum):
+    """
+    This Enum represents the different type of shortcodes that
+    we can handle according to their scopes. It could be simple
+    [tag] shortcode or a block-scoped one [tag] ... [/tag].
+    """
+    BLOCK = 'block'
+    SIMPLE = 'simple'
