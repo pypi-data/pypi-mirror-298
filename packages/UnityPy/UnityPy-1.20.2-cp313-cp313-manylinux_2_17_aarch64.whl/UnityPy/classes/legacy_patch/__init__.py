37eb4005f8f14bb06c0a5c58ad9663249ef595b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 03:08:04 +0000

    Add package UnityPy/1.20.2/UnityPy-1.20.2-cp313-cp313-manylinux_2_17_aarch64.whl

---
+++ packages/UnityPy/UnityPy-1.20.2-cp313-cp313-manylinux_2_17_aarch64.whl/UnityPy/classes/legacy_patch/__init__.py
@@ -0,0 +1,7 @@
+from .AudioClip import AudioClip
+from .Mesh import Mesh
+from .Renderer import Renderer
+from .Shader import Shader
+from .Sprite import Sprite
+from .Texture2D import Texture2D
+from .Texture2DArray import Texture2DArray
