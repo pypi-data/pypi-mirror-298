2939a72a80e24a59e15f12adb853dbe0a7f0860e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 15:04:50 +0000

    Add package my-unique-project-name/0.356/my_unique_project_name-0.356.tar.gz

---
+++ packages/my-unique-project-name/my_unique_project_name-0.356.tar.gz/my_unique_project_name-0.356/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_unique_project_name',
+    version='0.356',
+    packages=find_packages(),
+    description='A simple example library',
+    author='Your Name',
+    author_email='your.email@asdasexample.com',
+)
