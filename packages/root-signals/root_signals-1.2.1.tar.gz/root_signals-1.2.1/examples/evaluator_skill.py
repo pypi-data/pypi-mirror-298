dfa653cfc50dd9cd84045dc01764e9587b7b7a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 04:05:00 +0000

    Add package root-signals/1.2.1/root_signals-1.2.1.tar.gz

---
+++ packages/root-signals/root_signals-1.2.1.tar.gz/root_signals-1.2.1/examples/evaluator_skill.py
@@ -0,0 +1,32 @@
+from root import RootSignals
+from root.validators import Validator
+
+client = RootSignals()
+
+evaluator_skill = client.skills.create(
+    name="Cooking recipe",
+    intent="This skill will evaluate if the answer is a cooking recipe.",
+    prompt="Is the following a cooking recipe: {{output}}",
+    model="gpt-4o",
+    is_evaluator=True,
+)
+
+cooking_skill = client.skills.create(
+    name="Cooking skill with a custom evaluator",
+    prompt="Find me a good recipe for Italian food.",
+    validators=[
+        Validator(evaluator_id=evaluator_skill.id, threshold=0.1),
+        Validator(
+            evaluator_name="Truthfulness",
+            threshold=0.5,
+        ),
+    ],
+)
+response = cooking_skill.run()
+
+# Check if the recipe was about cooking
+print(response.validation)
+
+# {'validator_results': [{'evaluator_name': 'Cooking recipe
+# evaluator', 'evaluator_id': '...', 'threshold': 0.1, 'is_valid':
+# True, 'result': 0.9, 'status': 'finished'}], 'is_valid': True}
---
+++ packages/root-signals/root_signals-1.2.1.tar.gz/root_signals-1.2.1/examples/minimal.py
@@ -0,0 +1,29 @@
+from root import RootSignals
+
+# Connect to the Root Signals API
+client = RootSignals()
+
+# Create a skill
+skill = client.skills.create(
+    """
+    Classify this text into one of the following: {{categories}}
+    Text: {{text}}
+    """
+)
+
+# Execute it
+response = skill.run(
+    {
+        "text": "The expectation for rate cuts has been steadily declining.",
+        "categories": "Finance, Sports, Politics",
+    }
+)
+
+print(response)
+
+# "llm_output": "Finance",
+# "validation": Validation(is_valid=True, validator_results=[]),
+# "model": "gpt-4o",
+# "execution_log_id": "9b3c713d-7bdc-4f7d-a85c-ed7d92ff4a56",
+# "rendered_prompt": "Classify this text into ...",
+# "cost": 5.6e-05,
