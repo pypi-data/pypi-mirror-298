d71dca2050ddfff445bf7d0da8215644d58b32ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 05:05:19 +0000

    Add package ldjango/0.5/ldjango-0.5.tar.gz

---
+++ packages/ldjango/ldjango-0.5.tar.gz/ldjango-0.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ldjango',
+    version='0.5',
+    packages=find_packages(),
+    install_requires=[
+        'Click',
+        'Django',
+    ],
+    entry_points={
+        'console_scripts': [
+            'ldjango = ldjango.cli:cli',
+        ],
+    },
+)
