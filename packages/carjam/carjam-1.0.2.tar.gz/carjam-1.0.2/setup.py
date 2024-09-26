1e32243f986e0b28c285c5f8e311d2d4376fe4ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 03:08:34 +0000

    Add package carjam/1.0.2/carjam-1.0.2.tar.gz

---
+++ packages/carjam/carjam-1.0.2.tar.gz/carjam-1.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='carjam',
+    version='1.0.2',
+    description='Unoffical Python API for CarJam NZ',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/kianz20/CarJam-API',
+    author='Kian Jazayeri',
+    author_email='kianja02@gmail.com',
+    packages=find_packages(exclude=["tests"]),
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License", 
+        "Operating System :: OS Independent",
+    ],
+)
