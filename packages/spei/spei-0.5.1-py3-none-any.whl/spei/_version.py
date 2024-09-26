3da0a491cf8171079ac0e83da8a61746cb6f44d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 16:05:17 +0000

    Add package spei/0.5.1/spei-0.5.1-py3-none-any.whl

---
+++ packages/spei/spei-0.5.1-py3-none-any.whl/spei/_version.py
@@ -0,0 +1,16 @@
+from importlib import metadata
+from platform import python_version
+
+__version__ = "0.5.1"
+
+
+def show_versions() -> str:
+    msg = f"Versions\npython: {python_version()}\nspei: {__version__}\n"
+
+    requirements = metadata.requires("spei")
+    if requirements:
+        deps = [x for x in requirements if "extra" not in x]
+        for dep in deps:
+            msg += f"{dep}: {metadata.version(dep)}\n"
+
+    return msg
