f95108b955a72881f4836bbd4b1f7ea35161affc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 05:05:20 +0000

    Add package ecom-data-helpers-lib/0.0.13/ecom_data_helpers_lib-0.0.13.tar.gz

---
+++ packages/ecom-data-helpers-lib/ecom_data_helpers_lib-0.0.13.tar.gz/ecom_data_helpers_lib-0.0.13/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ecom_data_helpers_lib',
+    version='0.0.13',
+    description='A library of reusable utilities for AWS Lambda functions in ECOM Data Projects',
+    author='Augusto Lorencatto',
+    author_email='augusto.lorencatto@ecomenergia.com.br',
+    packages=find_packages(),
+    install_requires=[
+        'boto3',
+        'python-docx==1.1.2',
+        'PyPDF2==3.0.1',
+        'pdf2image==1.17.0'
+    ],
+)
