b24a4e59e8e5bb3a0717042b2e5e51da680f3e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 07:04:37 +0000

    Add package frostconnect/0.0.7/frostconnect-0.0.7.tar.gz

---
+++ packages/frostconnect/frostconnect-0.0.7.tar.gz/frostconnect-0.0.7/setup.py
@@ -0,0 +1,21 @@
+import pathlib
+import setuptools
+
+
+setuptools.setup (
+    name="frostconnect",
+    version="0.0.7",
+    description="Small package that has db connection functionality.",
+    long_description=pathlib.Path("Readme.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://www.github.com/mk732",
+    author="MK732",
+    license="The Unlicense",
+    python_requires=">=3.9",
+    install_requires=["asyncpg", "asyncio", "python-dotenv"],
+    packages=setuptools.find_packages(),
+    extras_require={
+        "dev" : ["pytest>=7.0", "twine>=4.0.2"]
+    },
+    include_package_data=True,
+)
