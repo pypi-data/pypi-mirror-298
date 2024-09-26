14632f50116857fea5b399aca3208e4eca2b4b17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 15:05:38 +0000

    Add package MomwhyareyouhereEncrypter/0.8/MomwhyareyouhereEncrypter-0.8.tar.gz

---
+++ packages/MomwhyareyouhereEncrypter/MomwhyareyouhereEncrypter-0.8.tar.gz/MomwhyareyouhereEncrypter-0.8/setup.py
@@ -0,0 +1,13 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='MomwhyareyouhereEncrypter',
+    version='0.8',
+    packages=find_packages(),
+    description='A Encrypter',
+    author='Momwhyareyouhere',
+    author_email='momwhyareyouherebusiness@gmail.com',
+    url='https://github.com/Momwhyareyouhere/MomwhyareyouhereEncrypter',  # Update with your URL
+)
