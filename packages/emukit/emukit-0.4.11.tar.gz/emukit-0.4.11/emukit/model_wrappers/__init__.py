c2fc73b48982ecba042d03240dc5e25ec0c303cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 14:04:42 +0000

    Add package emukit/0.4.11/emukit-0.4.11.tar.gz

---
+++ packages/emukit/emukit-0.4.11.tar.gz/emukit-0.4.11/emukit/model_wrappers/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2020-2024 The Emukit Authors. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+# Copyright 2018-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+
+from .gpy_model_wrappers import GPyModelWrapper, GPyMultiOutputWrapper  # noqa: F401
+from .simple_gp_model import SimpleGaussianProcessModel  # noqa: F401
