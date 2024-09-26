1579039430ebbe1baf556d0e770666ec90ec628f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 14:04:15 +0000

    Add package task_5_syrkin/0.0.7/task_5_syrkin-0.0.7.tar.gz

---
+++ packages/task_5_syrkin/task_5_syrkin-0.0.7.tar.gz/task_5_syrkin-0.0.7/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='task_5_syrkin',
+    version='0.0.7',
+    packages=find_packages(where='src'),  # Указываем, что пакеты находятся в папке src
+    package_dir={'': 'src'},  # Корневой каталог кода
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'task_5_syrkin=task_5_syrkin.collection_app:main',  # Путь к функции main в твоем коде
+        ],
+    },
+)
