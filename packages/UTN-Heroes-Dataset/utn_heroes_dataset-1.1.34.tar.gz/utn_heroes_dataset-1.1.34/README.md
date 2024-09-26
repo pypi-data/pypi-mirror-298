fde8f1f2a1d51270b5f6feb3559df4a87f18d85c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 07:04:35 +0000

    Add package UTN-Heroes-Dataset/1.1.34/utn_heroes_dataset-1.1.34.tar.gz

---
+++ packages/UTN-Heroes-Dataset/utn_heroes_dataset-1.1.34.tar.gz/utn_heroes_dataset-1.1.34/README.md
@@ -0,0 +1,42 @@
+<table>
+    <tr>
+        <td align='center'>
+            <img alt="Logo UTN Large" src="https://github.com/caidevOficial/Logos/blob/master/Personales/Logo_Developer.png?raw=true" href="https://www.utnfravirtual.org.ar/" width="750px"/>
+        </td>
+    </tr>
+</table></br>
+
+### Set de datos con informaci&oacute;n de h&eacute;roes y villanos para los desaf&iacute;os:
+
+- Desafio 01: Listas
+- Desafio 02: Matr&iacute;z
+
+## Para el Desaf&iacute;o 01:
+
+Te proveemos un gran set de datos de h&eacute;roes, dividido en listas paralelas
+para que puedas realizar tus ejercicios.
+
+Para poder usarlos importa en tus modulos lo siguiente
+
+```py
+from UTN_Heroes_Dataset.utn_listas import (
+    lista_alturas_heroes, lista_poder_heroes,
+    lista_identidades_heroes, lista_nombres_heroes,
+    lista_generos_heroes, lista_apodos_heroes
+)
+```
+
+A partir de este punto, estas en condiciones de utilizar las listas, imprimirlas, recorrerlas o hacer lo que se te plazca
+
+
+## Para el Desaf&iacute;o 02:
+
+Te proveemos un gran set de datos de h&eacute;roes, dividido en una matr&iacute;z
+para que puedas realizar tus ejercicios.
+
+Para poder usarlos importa en tus modulos lo siguiente
+
+```py
+from UTN_Heroes_Dataset.utn_matrices import matriz_data_heroes
+```
+A partir de este punto, estas en condiciones de utilizar las listas, imprimirlas, recorrerlas o hacer lo que se te plazca
---
+++ packages/UTN-Heroes-Dataset/utn_heroes_dataset-1.1.34.tar.gz/utn_heroes_dataset-1.1.34/UTN_Heroes_Dataset/utn_funciones/auxiliares.py
@@ -0,0 +1,80 @@
+# Copyright (C) 2024 <UTN FRA>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import pygame.mixer as mixer
+import time
+from ..utn_assets import sound_path
+from ..common_variables import VERSION
+
+def colour_text(text: str, message_type: str = '') -> str:
+    """
+    The function `colour_text` takes a text input and a message type, and returns the text formatted
+    with color based on the message type.
+    
+    :param text: The `text` parameter in the `colour_text` function is the string that you want to
+    colorize based on the `message_type`. It is the main content that will be displayed with the
+    specified color and message type
+    :type text: str
+    :param message_type: The `message_type` parameter in the `colour_text` function is used to specify
+    the type of message being displayed. It has a default value of an empty string, which means if no
+    message type is provided when calling the function, it will default to a general system message
+    :type message_type: str
+    """
+    _b_red: str = '\033[41m'
+    _b_green: str = '\033[42m'
+    _b_blue: str = '\033[44m'
+    _f_white: str = '\033[37m'
+    _no_color: str = '\033[0m'
+    message_type = message_type.strip().capitalize()
+    match message_type:
+        case 'Error':
+            text =  f'{_b_red}{_f_white}> Error: {text}{_no_color}'
+        case 'Success':
+            text = f'{_b_green}{_f_white}> Success: {text}{_no_color}'
+        case 'Info':
+            text = f'{_b_blue}{_f_white}> Information: {text}{_no_color}'
+        case _:
+            text =  f'{_b_red}{_f_white}> System: {text}{_no_color}'
+
+    return text
+
+def clear_console():
+    """
+    The function `clear_console` clears the console screen and prompts the user to press Enter to
+    continue.
+    """
+    import os
+    _ = input(colour_text("\nPresiona Enter para continuar..."))
+    os.system('cls' if os.name in ['nt', 'dos'] else 'clear')
+
+def play_sound():
+    """
+    The `play_sound` function initializes the mixer, loads a sound file, sets the volume to 0.4, and
+    plays the sound.
+    """
+    mixer.init()
+    mixer.music.load(sound_path)
+    mixer.music.set_volume(0.4)
+    mixer.music.play()
+    time.sleep(0.4)
+
+def saludo():
+    """
+    The function `saludo()` prints a greeting message with information about the UTN community and
+    dataset version.
+    """
+    message = f'Hello from the UTN community. You are using the UTN_Heroes_Dataset version: {VERSION}'
+    print(colour_text(message, 'Info'))
+    
