76728b37385e12e740d950dbd303ea39b19c03b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 14:04:15 +0000

    Add package omdev/0.0.0.dev38/omdev-0.0.0.dev38.tar.gz

---
+++ packages/omdev/omdev-0.0.0.dev38.tar.gz/omdev-0.0.0.dev38/omdev/scripts/execrss.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# @omlish-script
+import resource
+import sys
+
+
+def _get_rss() -> int:
+    return resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+
+
+def _main() -> None:
+    [src] = sys.argv[1:]
+    start = _get_rss()
+    exec(src)
+    end = _get_rss()
+    print(end - start)
+
+
+if __name__ == '__main__':
+    _main()
