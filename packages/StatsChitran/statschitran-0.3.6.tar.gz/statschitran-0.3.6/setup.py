c79a92469677fed1f1ba18e49d63de3bb06d797f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 14:04:59 +0000

    Add package StatsChitran/0.3.6/statschitran-0.3.6.tar.gz

---
+++ packages/StatsChitran/statschitran-0.3.6.tar.gz/statschitran-0.3.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='StatsChitran',  # Name of your package
+    version='0.3.6',  # Package version
+    packages=find_packages(),  # Automatically find sub-packages
+    install_requires=[],  # List your dependencies here
+    author='Chitran Ghosal',
+    author_email='ghosal.chitran@gmail.com',
+    description='A collection of few statistical functions in a package for direct use',
+    url='https://github.com/yourusername/my_package',  # Your package's URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
