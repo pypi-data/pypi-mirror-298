2a6627c1000913322cba40618ba51caa72fb82a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 03:09:10 +0000

    Add package finsolvepy/1.1.0/finsolvepy-1.1.0.tar.gz

---
+++ packages/finsolvepy/finsolvepy-1.1.0.tar.gz/finsolvepy-1.1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='finsolvepy',
+    version='1.1.0',
+    description='finsolvepy is a Python package for financial analysis, allowing users to fetch stock details, validate symbols, and perform calculations like P/E ratio effortlessly',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Vengatesh K',
+    author_email='vengatesh00014@example.com',
+    url='https://github.com/vengateshk18/finsolvepy',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',  # Supports Python 3.11
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7,<4.0',  # Specifies Python version requirements
+    install_requires=[
+        'pandas>=1.0.0,<3.0.0',         # Specify compatible pandas version
+        'requests>=2.0.0,<3.0.0',       # Specify compatible requests version
+        'python-dotenv>=0.10.2',  # Specify compatible python-dotenv version
+    ],
+    include_package_data=True,
+    package_data={
+        'finsolvepy': ['database/*.csv']  # Include CSV files in the package
+    },
+)
