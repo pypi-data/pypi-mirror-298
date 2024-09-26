98daa827b53f20092d7c46c47cc9f3c90f4ae650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 03:08:49 +0000

    Add package DLMSAdapter/0.4.0/dlmsadapter-0.4.0.tar.gz

---
+++ packages/DLMSAdapter/dlmsadapter-0.4.0.tar.gz/dlmsadapter-0.4.0/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+
+setuptools.setup(
+    long_description_content_type="text/markdown",
+    url="https://github.com/youserj/DlmsSPODES",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License"
+    ],
+)
