c647de086e1a341249d9977487066cd288aa3028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 03:09:12 +0000

    Add package yta-general-utils/0.0.50/yta-general-utils-0.0.50.tar.gz

---
+++ packages/yta-general-utils/yta-general-utils-0.0.50.tar.gz/yta-general-utils-0.0.50/yta_general_utils/programming/enum.py
@@ -0,0 +1,73 @@
+from enum import Enum
+
+
+class YTAEnum(Enum):
+    @classmethod
+    def is_valid(cls, value):
+        """
+        This method returns True if the provided 'value' is a valid
+        value for the also provided Enum class 'cls', or False if
+        not.
+        """
+        return is_valid(value, cls)
+    
+    @classmethod
+    def get_all(cls):
+        """
+        This method returns all the existing items in this 'cls' Enum
+        class.
+        """
+        return get_all(cls)
+    
+    @classmethod
+    def get_all_values(cls):
+        """
+        This method returns all the values of the existing items in 
+        this 'cls' Enum class.
+        """
+        return get_all_values(cls)
+
+def is_enum(cls: Enum):
+    """
+    This method returns True if the provided 'cls' parameter is
+    an enum class or subclass.
+    """
+    return isinstance(cls, Enum) or issubclass(cls, (Enum, YTAEnum))
+
+def is_valid(value: any, enum: Enum):
+    """
+    This method returns True if the provided 'value' is a valid value for
+    the also provided 'enum' Enum object, or False if not.
+    """
+    if not value:
+        raise Exception('No "value" provided.')
+    
+    if not issubclass(enum, Enum):
+        raise Exception('The "enum" parameter is not an Enum.')
+
+    try:
+        enum(type)
+        return True
+    except Exception:
+        return False
+    
+def get_all(enum: Enum):
+    """
+    This method returns all the items defined in a Enum subtype that
+    is provided as the 'enum' parameter.
+    """
+    if not enum:
+        raise Exception('No "enum" provided.')
+    
+    if not issubclass(enum, Enum):
+        raise Exception('The "enum" parameter provided is not an Enum.')
+    
+    return [item for item in enum]
+
+@classmethod
+def get_all_values(cls):
+    """
+    Returns a list containing all the registered EnhancementElementMode
+    enum values.
+    """
+    return [item.value for item in get_all(cls)]
