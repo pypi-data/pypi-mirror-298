d6ba312a4289b159c5966159c635aa7d1a0eb1f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 16:05:31 +0000

    Add package elastic-opentelemetry/0.3.0/elastic_opentelemetry-0.3.0-py3-none-any.whl

---
+++ packages/elastic-opentelemetry/elastic_opentelemetry-0.3.0-py3-none-any.whl/elasticotel/distro/__init__.py
@@ -0,0 +1,76 @@
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from logging import getLogger
+
+from opentelemetry.environment_variables import (
+    OTEL_METRICS_EXPORTER,
+    OTEL_TRACES_EXPORTER,
+)
+from opentelemetry.instrumentation.distro import BaseDistro
+from opentelemetry.instrumentation.instrumentor import BaseInstrumentor
+from opentelemetry.instrumentation.system_metrics import (
+    _DEFAULT_CONFIG as SYSTEM_METRICS_DEFAULT_CONFIG,
+    SystemMetricsInstrumentor,
+)
+from opentelemetry.sdk._configuration import _OTelSDKConfigurator
+from opentelemetry.sdk.environment_variables import (
+    OTEL_EXPERIMENTAL_RESOURCE_DETECTORS,
+    OTEL_EXPORTER_OTLP_PROTOCOL,
+)
+from pkg_resources import EntryPoint
+
+from elasticotel.distro.environment_variables import ELASTIC_OTEL_SYSTEM_METRICS_ENABLED
+
+
+logger = getLogger(__name__)
+
+
+class ElasticOpenTelemetryConfigurator(_OTelSDKConfigurator):
+    pass
+
+
+class ElasticOpenTelemetryDistro(BaseDistro):
+    def load_instrumentor(self, entry_point: EntryPoint, **kwargs):
+        # When running in the k8s operator loading of an instrumentor may fail because the environment
+        # in which python extensions are built does not match the one from the running container.
+        # There are at least two cases:
+        # - different python version
+        # - different kind of wheels, e.g. manylinux vs musllinux
+        # To avoid the distro loading to fail catch ImportError here, that is the kind of exception we see
+        # when loading shared objects or cython extensions fails.
+        try:
+            instrumentor_class: BaseInstrumentor = entry_point.load()
+        except ImportError:
+            logger.exception("Instrumenting of %s failed", entry_point.name)
+            return
+
+        instrumentor_kwargs = {}
+        if instrumentor_class == SystemMetricsInstrumentor:
+            system_metrics_configuration = os.environ.get(ELASTIC_OTEL_SYSTEM_METRICS_ENABLED, "false")
+            system_metrics_enabled = system_metrics_configuration.lower() == "true"
+            if not system_metrics_enabled:
+                instrumentor_kwargs["config"] = {
+                    k: v for k, v in SYSTEM_METRICS_DEFAULT_CONFIG.items() if k.startswith("process.runtime")
+                }
+        instrumentor_class(**instrumentor_kwargs).instrument(**kwargs)
+
+    def _configure(self, **kwargs):
+        os.environ.setdefault(OTEL_TRACES_EXPORTER, "otlp")
+        os.environ.setdefault(OTEL_METRICS_EXPORTER, "otlp")
+        os.environ.setdefault(OTEL_EXPORTER_OTLP_PROTOCOL, "grpc")
+        os.environ.setdefault(OTEL_EXPERIMENTAL_RESOURCE_DETECTORS, "process_runtime,os,otel,telemetry_distro")
