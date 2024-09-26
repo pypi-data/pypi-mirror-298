dc864ee0b144b26395a80f8d09a851995cb6916e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 04:04:30 +0000

    Add package chemex/2024.9.25/chemex-2024.9.25.tar.gz

---
+++ packages/chemex/chemex-2024.9.25.tar.gz/chemex-2024.9.25/chemex/__main__.py
@@ -0,0 +1,7 @@
+"""chemex.__main__: executed when the chemex directory is called as script."""
+from __future__ import annotations
+
+from chemex.chemex import main
+
+if __name__ == "__main__":
+    main()
