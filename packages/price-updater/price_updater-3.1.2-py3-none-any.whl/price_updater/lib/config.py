528b1aa8ed553e11c5cc6464739a5243d5c2c5fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-26 05:04:46 +0000

    Add package price-updater/3.1.2/price_updater-3.1.2-py3-none-any.whl

---
+++ packages/price-updater/price_updater-3.1.2-py3-none-any.whl/price_updater/lib/config.py
@@ -0,0 +1,25 @@
+import importlib.resources
+from kivy.utils import get_color_from_hex
+from kivy.core.text import LabelBase
+
+
+VERSION: str = "Price Updater© 3.1.2"
+
+with importlib.resources.path("price_updater", "Gabarito-SemiBold.ttf") as font_path:
+    LabelBase.register(name="standard", fn_regular=str(font_path))
+
+font_config: str = "standard"
+
+color_error: list[float] = get_color_from_hex("#FF0000")
+color_button: list[float] = get_color_from_hex("#444444FF")
+color_background_input: list[float] = get_color_from_hex("#5AC4FF0F")
+color_top_bar: list[float] = get_color_from_hex("#403C3CFF")
+color_top_bar_button: list[float] = get_color_from_hex("#403C3C00")
+color_orange_theme: list[float] = get_color_from_hex("#E06600")
+color_window: list[float] = get_color_from_hex("#FFFDF5FF")
+color_behind_window: list[float] = get_color_from_hex("#000000CC")
+color_behind_info: list[float] = get_color_from_hex("#00000000")
+color_success_info: list[float] = get_color_from_hex("#00FF00DD")
+color_error_info: list[float] = get_color_from_hex("#FF0000DD")
+color_checkbox: list[float] = [80, 80, 80, 1]
+color_asset_button: list[float] = get_color_from_hex("#EBE6D5CC")
