dc0cccce82c7479723c1ab473c0dd5284547b3ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 05:05:18 +0000

    Add package checkout-sdk/3.1.2/checkout_sdk-3.1.2-py3-none-any.whl

---
+++ packages/checkout-sdk/checkout_sdk-3.1.2-py3-none-any.whl/checkout_sdk/payments/links/payments_links.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+from checkout_sdk.common.common import CustomerRequest
+from checkout_sdk.common.enums import Currency
+from checkout_sdk.payments.payments import BillingDescriptor, PaymentType, ShippingDetails, ThreeDsRequest, \
+    RiskRequest, PaymentRecipient, ProcessingSettings
+from checkout_sdk.payments.payments_previous import BillingInformation
+
+
+class PaymentLinkRequest:
+    payment_type: PaymentType.REGULAR
+    amount: int
+    currency: Currency
+    payment_ip: str
+    billing_descriptor: BillingDescriptor
+    reference: str
+    description: str
+    expires_in: int
+    customer: CustomerRequest
+    shipping: ShippingDetails
+    billing: BillingInformation
+    recipient: PaymentRecipient
+    processing: ProcessingSettings
+    allow_payment_methods: list  # PaymentSourceType
+    products: list  # common.Product
+    metadata: dict
+    three_ds: ThreeDsRequest
+    risk: RiskRequest
+    return_url: str
+    locale: str
+    capture: bool
+    capture_on: datetime
+    processing_channel_id: str
+    amount_allocations: list  # values of AmountAllocations
