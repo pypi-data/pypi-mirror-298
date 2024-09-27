94d23ac4254e6145e75692771a84b7254bee55f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 14:05:54 +0000

    Add package h3/4.1.0b1/h3-4.1.0b1.tar.gz

---
+++ packages/h3/h3-4.1.0b1.tar.gz/h3-4.1.0b1/tests/test_apis/test_basic_str.py
@@ -0,0 +1,24 @@
+import h3.api.basic_str as h3
+
+
+from .. import util as u
+
+
+def test1():
+    lat, lng = 37.7752702151959, -122.418307270836
+    assert h3.latlng_to_cell(lat, lng, 9) == '8928308280fffff'
+
+
+def test5():
+    expected = [
+        '89283082873ffff',
+        '89283082877ffff',
+        '8928308283bffff',
+        '89283082807ffff',
+        '8928308280bffff',
+        '8928308280fffff',
+        '89283082803ffff'
+    ]
+
+    out = h3.grid_disk('8928308280fffff', 1)
+    assert u.same_set(out, expected)
