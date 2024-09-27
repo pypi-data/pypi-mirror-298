b851bd7ca368b48c90b74c1c47d5ed12f898dc31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 03:09:56 +0000

    Add package pdh/0.4.4/pdh-0.4.4-py3-none-any.whl

---
+++ packages/pdh/pdh-0.4.4-py3-none-any.whl/pdh/filters.py
@@ -0,0 +1,157 @@
+#
+# This file is part of the pdh (https://github.com/mbovo/pdh).
+# Copyright (c) 2020-2024 Manuel Bovo.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+import re
+from typing import List
+
+
+class Filter(object):
+    """
+    Filter is a collection of methods to functions to filter out items from an iterator,
+    useful when used in conjunction with filter()
+    """
+
+    def le(field: str, value: int):
+        def f(item: dict) -> bool:
+            if item[field] <= value:
+                return True
+            return False
+
+        return f
+
+    def ge(field: str, value: int):
+        def f(item: dict) -> bool:
+            if item[field] >= value:
+                return True
+            return False
+
+        return f
+
+    def lt(field: str, value: int):
+        def f(item: dict) -> bool:
+            if item[field] < value:
+                return True
+            return False
+
+        return f
+
+    def gt(field: str, value: int):
+        def f(item: dict) -> bool:
+            if item[field] > value:
+                return True
+            return False
+
+        return f
+
+    def inList(field: str, listOfValues: List[str]):
+        def f(item: dict) -> bool:
+            if item[field] in listOfValues:
+                return True
+            return False
+
+        return f
+
+    def inStr(field: str, value: str):
+        def f(item: dict) -> bool:
+            if value.lower() in item[field].lower():
+                return True
+            return False
+
+        return f
+
+    def ieq(field: str, value: str):
+        def f(item: dict) -> bool:
+            if item[field].lower() == value.lower():
+                return True
+            return False
+
+        return f
+
+    def eq(field: str, value: str):
+        def f(item: dict) -> bool:
+            if item[field] == value:
+                return True
+            return False
+
+        return f
+
+    def regexp(field: str, regexp):
+        """
+        This is a filters operating on regular expression
+
+            Parameters:
+                field (str): Any dictionary field on which operate
+                regexp (str or regexp): The regular expression you would validate
+
+            Returns:
+                True if when regexp is found, false otherwise
+
+        """
+        if type(regexp) is str:
+            regexp = re.compile(regexp)
+
+        def f(item: dict) -> bool:
+            if regexp.search(item[field]):
+                return True
+            return False
+
+        return f
+
+    def not_regexp(field: str, regexp):
+        """
+        This is a filters operating on regular expression
+
+            Parameters:
+                field (str): Any dictionary field on which operate
+                regexp (str or regexp): The regular expression you would validate
+
+            Returns:
+                False if when regexp is found, True otherwise
+
+        """
+        if type(regexp) is str:
+            regexp = re.compile(regexp)
+
+        def f(item: dict) -> bool:
+            if regexp.search(item[field]):
+                return False
+            return True
+
+        return f
+
+    def do(objects: list, transformations: dict|None = None, filters: list = [], preserve : bool = False) -> list:
+        """Given a list of objects, apply every transformations and filters on it, return the new filtered list
+        Transformations is a dict of "key": func(item) where key is the destination key and func(item) the
+                        function to used to extract values from the original list (see Transformation class)
+        Filters is a list of functions in the form f(item:Any)->bool the item will be in the returned list
+                        if the function returns True
+        """
+        ret = list()
+        for obj in objects:
+            if transformations is not None:
+                if preserve:
+                    item = obj
+                else:
+                    item = {}
+                for path, func in transformations.items():
+                    item[path] = func(obj)
+                ret.append(item)
+            else:
+                ret.append(obj)
+        for filter_func in filters:
+            ret = [o for o in filter(filter_func, ret)]
+
+        return ret
---
+++ packages/pdh/pdh-0.4.4-py3-none-any.whl/pdh/pd.py
@@ -0,0 +1,196 @@
+#
+# This file is part of the pdh (https://github.com/mbovo/pdh).
+# Copyright (c) 2020-2024 Manuel Bovo.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+import subprocess
+from typing import Dict, List
+from rich import print
+from pdpyras import APISession, PDClientError
+import json
+
+
+class UnauthorizedException(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+
+URGENCY_HIGH = "high"
+URGENCY_LOW = "low"
+STATUS_TRIGGERED = "triggered"
+STATUS_ACK = "acknowledged"
+STATUS_RESOLVED = "resolved"
+
+DEFAULT_STATUSES = [STATUS_TRIGGERED, STATUS_ACK]
+DEFAULT_URGENCIES = [URGENCY_HIGH, URGENCY_LOW]
+
+
+class PD(object):
+
+    session = None
+    cfg = None
+    users = list()
+    incs = list()
+
+    def __init__(self, cfg: dict()) -> None:
+        super().__init__()
+        if not self.session:
+            self.cfg = cfg
+            self.session = APISession(cfg["apikey"], default_from=cfg["email"])
+            self.session.max_network_attempts = 5
+            try:
+                self.session.get("/users/me")
+            except PDClientError as e:
+                raise UnauthorizedException(str(e))
+
+
+class Incidents(PD):
+    def list(self, userid: list = None, statuses: list = DEFAULT_STATUSES, urgencies: list = DEFAULT_URGENCIES) -> List:
+        """List all incidents"""
+        params = {"statuses[]": statuses, "urgencies[]": urgencies}
+        if userid:
+            params["user_ids[]"] = userid
+        return self.session.list_all("incidents", params=params)
+
+    def mine(self, statuses: list = DEFAULT_STATUSES, urgencies: list = DEFAULT_URGENCIES) -> List:
+        """List all incidents assigned to the configured UserID"""
+        return self.list([self.cfg["uid"]], statuses, urgencies)
+
+    def alerts(self, id: str) -> dict:
+        r = self.session.rget(f"/incidents/{id}/alerts")
+        return r
+
+    def get(self, id: str) -> dict:
+        """Retrieve a single incident by ID"""
+        r = self.session.rget(f"/incidents/{id}")
+        return r
+
+    def ack(self, incs: List):
+        self.change_status(incs, STATUS_ACK)
+
+    def resolve(self, incs: List):
+        self.change_status(incs, STATUS_RESOLVED)
+
+    def change_status(self, incs: List, status: str = STATUS_ACK):
+        for i in incs:
+            if "status" in i:
+                i["status"] = status
+
+        self.bulk_update(incs)
+
+    def snooze(self, incs: List, duration=14400):
+        for i in incs:
+            try:
+                self.session.post(f"/incidents/{i['id']}/snooze", json={"duration": duration})
+            except Exception as e:
+                print(e)
+
+    def bulk_update(self, incs: List):
+        ret = None
+        try:
+            ret = self.session.rput("incidents", json=incs)
+        except PDClientError as e:
+            print(e)
+        return ret
+
+    def update(self, inc):
+        ret = None
+        try:
+            ret = self.session.rput(f"/incidents/{inc['id']}", json=inc)
+        except PDClientError as e:
+            print(e)
+        return ret
+
+    def reassign(self, incs: List, uids: List[str]):
+        for i in incs:
+            assignments = [{"assignee": {"id": u, "type": "user_reference"}} for u in uids]
+            new_inc = {
+                "id": i["id"],
+                "type": "incident_reference",
+                "assignments": assignments,
+            }
+            try:
+                self.session.rput(f"/incidents/{i['id']}", json=new_inc)
+            except Exception as e:
+                print(str(e))
+
+    def apply(self, incs: List, paths: List[str]) -> list:
+        rets = []
+        for script in paths:
+            output = self.apply_single(incs, script)
+            rets.append({"script": script} | output)
+        return rets
+
+    def apply_single(self, incs: List, script: str) -> Dict:
+        process = subprocess.Popen(script, text=True, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        stdout, stderr = process.communicate(json.dumps(incs))
+        process.wait()
+
+        if process.returncode == 0:
+            output = json.loads(stdout)
+            if type(output) not in [dict, list, tuple]:
+                output = {"output": str(output)}
+            else:
+                output = {"output": output}
+        else:
+            output = {"error": stderr}
+
+        return output
+
+
+class Users(PD):
+    def list(self) -> list(dict()):
+        """List all users in PagerDuty account"""
+        users = self.session.iter_all("users")
+
+        return users
+
+    def get(self, id: str) -> Dict:
+        """Get a single user by ID"""
+        return self.session.rget(f"/users/{id}")
+
+    def search(self, query: str, key: str = "name") -> List[dict]:
+        """Retrieve all users matching query on the attribute name"""
+
+        def equiv(s):
+            return query.lower() in s[key].lower()
+
+        users = [u for u in filter(equiv, self.session.iter_all("users"))]
+        return users
+
+    # def filter(self, query: str, key: str = "name", attributes: List[str] = ["id", "name", "email", "time_zone"]) -> List[dict]:
+    #     users = self.search(query, key)
+
+    #     filtered = list()
+    #     for u in users:
+    #         f = dict()
+    #         for attr in attributes:
+    #             f[attr] = u[attr]
+    #         filtered.append(f)
+
+    #     return filtered
+
+    def userIDs(self, query: str, key: str = "name") -> List[str]:
+        """Retrieve all userIDs matching query on the attribute name"""
+        users = self.search(query, key)
+        userIDs = [u["id"] for u in users]
+        return userIDs
+
+    def userID_by_mail(self, query):
+        """Retrieve all usersIDs matching the given (partial) email"""
+        return self.userIDs(query, "email")
+
+    def userID_by_name(self, query):
+        """Retrieve all usersIDs matching the given (partial) name"""
+        return self.userIDs(query, "name")
