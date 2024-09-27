99650eac73aff86fa5db205552415b0dacb141fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 04:05:25 +0000

    Add package jsonschema-gentypes/2.8.0/jsonschema_gentypes-2.8.0-py3-none-any.whl

---
+++ packages/jsonschema-gentypes/jsonschema_gentypes-2.8.0-py3-none-any.whl/jsonschema_gentypes/jsonschema_draft_2019_09_format.py
@@ -0,0 +1,12 @@
+"""
+Automatically generated file from a JSON schema.
+"""
+
+from typing import TypedDict, Union
+
+FormatVocabularyMetaSchema = Union["_FormatVocabularyMetaSchemaObject", bool]
+""" Format vocabulary meta-schema. """
+
+
+class _FormatVocabularyMetaSchemaObject(TypedDict, total=False):
+    format: str
