8b068b4fcd45d590f09ec6ee8a0dd80d11af3699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 06:06:01 +0000

    Add package pyCellPhenoX/0.1.2/pycellphenox-0.1.2-py3-none-any.whl

---
+++ packages/pyCellPhenoX/pycellphenox-0.1.2-py3-none-any.whl/pyCellPhenoX/utils/__init__.py
@@ -0,0 +1,6 @@
+__project__ = "pyCellPhenoX"
+# These version placeholders are updated during build by poetry-dynamic-versioning
+__version__ = "0.1.2"
+__version_tuple__ = (0, 1, 2)
+__license__ = "MIT License"
+__author__ = "pyCellPhenoX Contributors"
