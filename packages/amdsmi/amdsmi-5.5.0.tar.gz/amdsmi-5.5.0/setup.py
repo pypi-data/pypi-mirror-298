e182113378d6b6c5ae24f713e0c27809d3228a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 15:05:14 +0000

    Add package amdsmi/5.5.0/amdsmi-5.5.0.tar.gz

---
+++ packages/amdsmi/amdsmi-5.5.0.tar.gz/amdsmi-5.5.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='amdsmi',
+    version='5.5.0',
+    description="SMI LIB - AMD GPU Monitoring Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['amdsmi'],
+    package_data={'': ['LICENSE']},
+    include_package_data=True,
+    python_requires=">=3.6",
+)
