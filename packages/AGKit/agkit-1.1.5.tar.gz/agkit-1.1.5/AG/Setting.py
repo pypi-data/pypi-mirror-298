70923378cb07e4fc05f8c19ea14b5c8411792205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 15:05:22 +0000

    Add package AGKit/1.1.5/agkit-1.1.5.tar.gz

---
+++ packages/AGKit/agkit-1.1.5.tar.gz/agkit-1.1.5/AG/Setting.py
@@ -0,0 +1,24 @@
+import configparser
+
+from AG.Logger import Logger
+from AG.Singleton import Singleton
+from AG.FileUtil import FileUtil
+
+
+class Setting(Singleton):
+    data = None
+
+    def __init__(self):
+        self.file_name = ""
+
+    def Get(self, section, key):
+        result = self.data.get(section, key)
+        return result
+
+    def Init(self, file_name):
+        self.file_name = file_name
+        if not FileUtil.Exists(self.file_name):
+            Logger.Instance.Info(f"没有找到配置文件：{self.file_name}")
+            return
+        self.data = configparser.ConfigParser()
+        self.data.read(self.file_name)
