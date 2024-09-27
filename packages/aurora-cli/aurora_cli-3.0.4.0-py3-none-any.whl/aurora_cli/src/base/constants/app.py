446a1b4efbde8be59da5a500d25cb9a3e7df9b7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 04:04:55 +0000

    Add package aurora-cli/3.0.4.0/aurora_cli-3.0.4.0-py3-none-any.whl

---
+++ packages/aurora-cli/aurora_cli-3.0.4.0-py3-none-any.whl/aurora_cli/src/base/constants/app.py
@@ -0,0 +1,31 @@
+"""
+Copyright 2024 Vitaliy Zarubin
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+APP_NAME = 'aurora-cli'
+
+APP_VERSION = '3.0.4'
+
+PATH_TEMP = '~/.aurora-cli/temp'
+
+PATH_CLANG_FORMAT_CONF_FLUTTER = '~/.aurora-cli/.clang-format-flutter'
+
+PATH_CLANG_FORMAT_CONF = '~/.aurora-cli/.clang-format'
+
+PATH_REGULAR_KEY = '~/.aurora-cli/regular_key.pem'
+
+PATH_REGULAR_CERT = '~/.aurora-cli/regular_cert.pem'
+
+TIMEOUT = 600
