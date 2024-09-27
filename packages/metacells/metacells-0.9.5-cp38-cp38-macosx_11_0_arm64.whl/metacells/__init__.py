8f7eca6ec609b8f2f7ecc47a864fd24423f8ce26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 15:05:35 +0000

    Add package metacells/0.9.5/metacells-0.9.5-cp38-cp38-macosx_11_0_arm64.whl

---
+++ packages/metacells/metacells-0.9.5-cp38-cp38-macosx_11_0_arm64.whl/metacells/__init__.py
@@ -0,0 +1,21 @@
+"""
+Metacells single-cell RNA sequencing.
+"""
+
+import sys
+
+__author__ = "Oren Ben-Kiki"
+__email__ = "oren@ben-kiki.org"
+__version__ = "0.9.5"
+
+if "sphinx" not in sys.argv[0]:
+    from .should_check_avx2 import SHOULD_CHECK_AVX2
+
+    if SHOULD_CHECK_AVX2:
+        from . import check_avx2
+
+# pylint: disable=wrong-import-position
+
+from . import pipeline as pl
+from . import tools as tl
+from . import utilities as ut
