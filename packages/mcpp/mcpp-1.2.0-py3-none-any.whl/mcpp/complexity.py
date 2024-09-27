dc0bc53e9e5ea7061eb665dc3fc1793b4d904433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 16:04:57 +0000

    Add package mcpp/1.2.0/mcpp-1.2.0-py3-none-any.whl

---
+++ packages/mcpp/mcpp-1.2.0-py3-none-any.whl/mcpp/complexity.py
@@ -0,0 +1,89 @@
+from mcpp.parse import Sitter
+from mcpp.queries import Q_FOR_STMT, Q_DO_STMT, Q_WHILE_STMT, \
+    Q_BINARY_EXPR, Q_CONDITION
+
+
+def c1(root, sitter, lang, calls=None):
+    """Cyclomatic complexity (McCabe): 
+        number conditional predicates + number of loop statements + 1
+    """
+    sitter.add_queries({
+        "Q_BINARY_EXPR": Q_BINARY_EXPR,
+        "Q_CONDITION": Q_CONDITION,
+        "Q_FOR_STMT": Q_FOR_STMT,
+        "Q_DO_STMT": Q_DO_STMT,
+        "Q_WHILE_STMT": Q_WHILE_STMT
+    })
+    logical_ops = [
+        "&", "&&",
+        "|", "||"
+    ]
+
+    complexity = c2(root, sitter, lang, calls)["C2"]
+    for condition in sitter.captures("Q_CONDITION", root, lang).get("condition", []):
+        for expr in sitter.captures("Q_BINARY_EXPR", condition, lang).get("expr", []):
+            if len(expr.children) != 3:
+                continue
+            left, op, right = expr.children
+            if op.text.decode() in logical_ops:
+                complexity += 1
+    complexity += 1
+    return {
+        "C1": complexity
+    }
+
+
+def c2(root, sitter, lang, calls=None):
+    """number of for, while and do-while loops"""
+    sitter.add_queries({
+        "Q_FOR_STMT": Q_FOR_STMT,
+        "Q_WHILE_STMT": Q_WHILE_STMT
+    })
+    complexity = 0
+    for query in ("Q_FOR_STMT", "Q_WHILE_STMT"):
+        complexity += len(sitter.captures(query, root, lang).get("stmt", []))
+    return {
+        "C2": complexity
+    }
+
+
+def c3_c4(root, sitter, lang, calls=None):
+    """
+    C3: number of nested for, while and do-while loops
+    C4: maximum nesting depth
+
+    - count all loops that have some loop ancestor
+    - count ancestors that are also loops
+    """
+    sitter.add_queries({
+        "Q_FOR_STMT": Q_FOR_STMT,
+        "Q_DO_STMT": Q_DO_STMT,
+        "Q_WHILE_STMT": Q_WHILE_STMT
+    })
+    c3_val = 0
+    c4_val = 0
+    for query in ("Q_FOR_STMT", "Q_DO_STMT", "Q_WHILE_STMT"):
+        for loop_node in sitter.captures(query, root, lang).get("stmt", []):
+            nesting_level = _loop_nesting_level(loop_node)
+            if nesting_level > 0:
+                c3_val += 1
+            c4_val = max(c4_val, nesting_level)
+    return {
+        "C3": c3_val,
+        "C4": c4_val
+    }
+
+
+def _loop_nesting_level(node):
+    loop_types = [
+        "do_statement",
+        "while_statement",
+        "for_statement"
+    ]
+    parent = node.parent
+    num_loop_ancestors = 0
+    while parent is not None:
+        if parent.type in loop_types:
+            num_loop_ancestors += 1
+        parent = parent.parent
+    return num_loop_ancestors
---
+++ packages/mcpp/mcpp-1.2.0-py3-none-any.whl/mcpp/queries.py
@@ -0,0 +1,70 @@
+Q_ERROR_NODE = """
+(ERROR) @error_node
+"""
+
+Q_FOR_STMT = """
+(for_statement) @stmt
+"""
+
+Q_DO_STMT = """
+(do_statement) @stmt
+"""
+
+Q_WHILE_STMT = """
+(while_statement) @stmt
+"""
+
+Q_IF_STMT = """
+(if_statement) @if_stmt
+"""
+
+Q_SWITCH_STMT = """
+(switch_statement) @stmt
+"""
+
+
+Q_CONDITION = """
+(_
+    condition: ((_) @condition)
+) @control_stmnt
+"""
+
+Q_BINARY_EXPR = """
+(binary_expression) @expr
+"""
+
+Q_CALL_NAME = """
+(call_expression
+    function: ((identifier) @name)
+) @call
+"""
+
+Q_ARGLIST = """
+(call_expression
+    arguments: ((argument_list) @args)
+) @call
+"""
+
+Q_IDENTIFIER = """
+(identifier) @variable
+"""
+
+Q_FUNCTION_PARAMETER = """
+(parameter_declaration) @param
+"""
+
+Q_POINTER_EXPR = """
+(pointer_expression) @pointer
+"""
+
+Q_ASSIGNMENT_EXPR = """
+(assignment_expression) @expr
+"""
+
+Q_IF_WITHOUT_ELSE = """
+(if_statement
+    condition: ((_) @if)
+    consequence: ((_) @then)
+    !alternative
+) @stmt
+"""
