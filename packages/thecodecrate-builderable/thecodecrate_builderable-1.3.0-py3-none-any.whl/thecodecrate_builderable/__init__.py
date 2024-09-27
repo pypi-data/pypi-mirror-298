127320e027a4c33a97542d6f5fa16e2efa7459dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 14:06:31 +0000

    Add package thecodecrate-builderable/1.3.0/thecodecrate_builderable-1.3.0-py3-none-any.whl

---
+++ packages/thecodecrate-builderable/thecodecrate_builderable-1.3.0-py3-none-any.whl/thecodecrate_builderable/__init__.py
@@ -0,0 +1,24 @@
+from .core.builderable.builderable import Builderable
+from .core.builderable.types import TBuilderOutput, TBuilderPart
+from .traits.with_parts_builder.with_parts_builder import WithPartsBuilder
+from .traits.with_immutability.with_immutability import WithImmutability
+from .traits.with_parts.with_parts import WithParts
+
+# Version of the package
+# DO NOT MODIFY MANUALLY
+# This will be updated by `bumpver` command.
+# - Make sure to commit all changes first before running `bumpver`.
+# - Run `bumpver update --[minor|major|patch]`
+__version__ = "1.3.0"
+
+# Expose the public API
+__all__ = [
+    # Core
+    "Builderable",
+    "TBuilderOutput",
+    "TBuilderPart",
+    # Traits
+    "WithImmutability",
+    "WithPartsBuilder",
+    "WithParts",
+]
