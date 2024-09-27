386fafd27534e02c554a01ea6aac90016489f93b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 15:05:14 +0000

    Add package YsFinance/2.0.9/ysfinance-2.0.9.tar.gz

---
+++ packages/YsFinance/ysfinance-2.0.9.tar.gz/ysfinance-2.0.9/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='YsFinance',
+    version='2.0.9',
+    author='YunSheng',
+    author_email='278211638@qq.com',
+    description='About finance',
+    # url='https://github.com/YunSheng0129/YsFinance.git',
+    packages=find_packages(),
+    package_data={
+        'YsFinance': ['resources/info.derive.finance'],
+    },
+    install_requires=['pandas<=1.6',
+                      'numpy<=1.26',
+                      'scipy',
+                      'tushare',
+                      'scipy',
+                      'statsmodels',
+                      'joblib',
+                      'ipykernel',
+                      'empyrical'],  # 依赖列表
+)
+
+## python setup.py develop
+## python setup.py sdist
+## twine upload dist/*
