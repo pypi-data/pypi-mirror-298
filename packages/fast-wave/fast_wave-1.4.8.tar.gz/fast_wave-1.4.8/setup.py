90ea6b08156f77c85e182582e94b9b033287ba77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 04:04:58 +0000

    Add package fast-wave/1.4.8/fast_wave-1.4.8.tar.gz

---
+++ packages/fast-wave/fast_wave-1.4.8.tar.gz/fast_wave-1.4.8/setup.py
@@ -0,0 +1,62 @@
+import setuptools
+from setuptools import find_packages, Extension
+from Cython.Build import cythonize
+import numpy as np
+import os
+
+current_dir = os.path.abspath(os.path.dirname(__file__))
+
+pyx_file = os.path.join(current_dir, "src", "fast_wave", "wavefunction_cython.pyx")
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+name = "fast_wave"
+version = "1.4.8"
+description = "Package for the calculation of the time-independent wavefunction." 
+author_email = "matheusgomescord@gmail.com"
+url = "https://github.com/pikachu123deimos/fast-wave" 
+
+install_requires = [
+    "llvmlite==0.42.0",
+    "mpmath==1.3.0",
+    "numba==0.59.1",
+    "numpy==1.26.4",
+    "sympy==1.12",
+    "cython==3.0.10",
+]
+
+test_requires = [
+    "pytest= ^7.1.2",
+]
+
+packages = find_packages(where='src')
+
+classifiers = [
+    'Programming Language :: Python :: 3.11',  
+    'Topic :: Scientific/Engineering :: Mathematics',  
+    'License :: OSI Approved :: BSD License',  
+    'Development Status :: 5 - Production/Stable', 
+]
+
+extensions = [
+    Extension("fast_wave.wavefunction_cython", ["fast_wave/wavefunction_cython.pyx"]),
+]
+
+setuptools.setup(
+    name=name,
+    version=version,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    description=description,
+    author=name,
+    author_email=author_email,
+    url=url,
+    install_requires=install_requires,
+    test_requires=test_requires,
+    packages=packages,
+    package_dir={'': 'src'},
+    classifiers=classifiers,
+    ext_modules=cythonize(pyx_file),
+    include_dirs=[np.get_include()]
+)
