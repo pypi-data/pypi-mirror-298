d62b36a5f00030281aef095456e782fc4b7420ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 04:05:41 +0000

    Add package autopep695/1.1.2/autopep695-1.1.2.tar.gz

---
+++ packages/autopep695/autopep695-1.1.2.tar.gz/autopep695-1.1.2/autopep695/analyzer.py
@@ -0,0 +1,274 @@
+# Copyright (c) 2024-present yowoda
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from __future__ import annotations
+
+import functools
+import multiprocessing
+import os
+import platform
+import logging
+import traceback
+import typing as t
+from urllib.parse import urlencode
+
+import libcst as cst
+from libcst.metadata import PositionProvider
+
+from autopep695.check import CheckPEP695Visitor
+from autopep695.format import PEP695Formatter
+from autopep695.ux import (
+    init_logging,
+    format_special,
+    create_hyperlink,
+    get_system_info,
+)
+from autopep695.errors import ParsingError
+from autopep695.base import RemoveAssignments
+
+if t.TYPE_CHECKING:
+    from pathlib import Path
+
+
+def _show_debug_traceback_note() -> str:
+    if logging.getLogger().isEnabledFor(logging.DEBUG):
+        return ""
+
+    return f"View the full traceback by passing the {format_special('--debug', '`')} flag\n"
+
+
+def _show_internal_error_report_note(
+    *, title: str, command: str, settings: dict[str, t.Any]
+) -> str:
+    settings_repr = "\n".join(f"{name}={value}" for name, value in settings.items())
+    params = urlencode(
+        {
+            "title": title,
+            "reproduction-steps": f"Run `{command}` with:\n```\n{settings_repr}\n```",
+            "expected-result": "The command works or an informative error is shown.",
+            "actual-result": f"Internal error:\n```\n{traceback.format_exc()}```",
+            "system-info": get_system_info(),
+        }
+    )
+    link = f"https://github.com/yowoda/autopep695/issues/new?assignees=&labels=bug&projects=&template=bug_report.yml&{params}"
+    return f"Please report this issue on {create_hyperlink(link, 'Github')}."
+
+
+def _file_aware_parse_code(code: str, path: Path) -> cst.Module:
+    try:
+        tree = cst.parse_module(code)
+
+    except Exception as e:
+        logging.error(
+            f"Failed parsing code from {format_special(path)}\n{e}\n{_show_debug_traceback_note()}"
+        )
+        logging.debug("Full Traceback for the error above:", exc_info=e)
+        raise ParsingError
+
+    return tree
+
+
+def format_code(
+    code: str,
+    *,
+    file_path: Path,
+    unsafe: bool,
+    remove_variance: bool,
+    remove_private: bool,
+    keep_assignments: bool,
+) -> str:
+    tree = _file_aware_parse_code(code, file_path)
+    transformer = PEP695Formatter(
+        file_path,
+        unsafe=unsafe,
+        remove_variance=remove_variance,
+        remove_private=remove_private,
+    )
+    new_tree = tree.visit(transformer)
+    if not keep_assignments:
+        new_tree = new_tree.visit(
+            RemoveAssignments(set(transformer.unused_assignments.values()))
+        )
+
+    return new_tree.code
+
+
+def _format_file(
+    path: Path,
+    *,
+    unsafe: bool,
+    remove_variance: bool,
+    remove_private: bool,
+    keep_assignments: bool,
+) -> None:
+    logging.debug("Analyzing file %s", format_special(path))
+    with path.open("r+", encoding="utf-8") as f:
+        try:
+            code = format_code(
+                f.read(),
+                file_path=path,
+                unsafe=unsafe,
+                remove_variance=remove_variance,
+                remove_private=remove_private,
+                keep_assignments=keep_assignments,
+            )
+
+        except ParsingError:  # catch the exception so the file can be skipped and the whole process isn't terminated
+            return
+
+        except Exception as e:
+            github_report_note = _show_internal_error_report_note(
+                title="Internal error while formatting code",
+                command="autopep695 format",
+                settings={
+                    "unsafe": unsafe,
+                    "remove_variance": remove_variance,
+                    "remove_private": remove_private,
+                    "keep_assignments": keep_assignments,
+                },
+            )
+            logging.error(
+                f"Internal error while formatting code in {format_special(path)}\n{github_report_note}\n{_show_debug_traceback_note()}"
+            )
+            logging.debug("Full traceback for the error above:", exc_info=e)
+            return
+
+        f.seek(0)
+        f.write(code)
+        f.truncate()
+
+
+def _format_file_wrapper(
+    unsafe: bool,
+    remove_variance: bool,
+    remove_private: bool,
+    keep_assignments: bool,
+    path: Path,
+) -> None:
+    """
+    A `_format_file` wrapper that modifes the order of arguments.
+    This is neccessary because pool.map can only pass one argument to the mapped function
+    which in this case is the path to the file, so other arguments need to be passed
+    using functools.partial
+    """
+    return _format_file(
+        path,
+        unsafe=unsafe,
+        remove_variance=remove_variance,
+        remove_private=remove_private,
+        keep_assignments=keep_assignments,
+    )
+
+
+def _parallel_format_paths(
+    paths: t.Iterable[Path],
+    *,
+    processes: t.Optional[int],
+    unsafe: bool,
+    remove_variance: bool,
+    remove_private: bool,
+    keep_assignments: bool,
+) -> None:
+    if processes is None:
+        processes = os.cpu_count() or 1
+
+    _has_debug = logging.getLogger().isEnabledFor(logging.DEBUG)
+
+    # on Windows and macOS,subprocesses are created using `spawn`, starting a new python interpreter
+    # for each worker. Unlike `fork`, the interpreter process doesn't inherit the logging config from
+    # the main process, which is why need to pass a `init_logging` wrapper to the initializer arg of the pool,
+    # so the logging config is set for each worker
+
+    if platform.system() in ("Windows", "Darwin"):
+        initializer = init_logging
+        initargs = (_has_debug,)
+
+    else:
+        initializer = None
+        initargs = ()
+
+    with multiprocessing.Pool(
+        processes, initializer=initializer, initargs=initargs
+    ) as pool:
+        logging.debug("Run with --parallel, Starting %s processes...", processes)
+        pool.map(
+            functools.partial(
+                _format_file_wrapper,
+                unsafe,
+                remove_variance,
+                remove_private,
+                keep_assignments,
+            ),
+            paths,
+        )
+
+
+def format_paths(
+    paths: t.Iterable[Path],
+    *,
+    parallel: t.Union[bool, int],
+    unsafe: bool,
+    remove_variance: bool,
+    remove_private: bool,
+    keep_assignments: bool,
+) -> None:
+    if parallel is not False:
+        _parallel_format_paths(
+            paths=paths,
+            processes=None if parallel is True else parallel,
+            unsafe=unsafe,
+            remove_variance=remove_variance,
+            remove_private=remove_private,
+            keep_assignments=keep_assignments,
+        )
+
+    else:
+        for path in paths:
+            _format_file(
+                path,
+                unsafe=unsafe,
+                remove_variance=remove_variance,
+                remove_private=remove_private,
+                keep_assignments=keep_assignments,
+            )
+
+
+def _check_code(code: str, *, file_path: Path, silent: bool) -> int:
+    tree = _file_aware_parse_code(code, file_path)
+    if not silent:
+        setattr(CheckPEP695Visitor, "METADATA_DEPENDENCIES", (PositionProvider,))
+        tree = cst.MetadataWrapper(tree)  # type: ignore
+
+    transformer = CheckPEP695Visitor(file_path, silent=silent)
+    tree.visit(transformer)
+
+    return transformer.errors
+
+
+def _check_file(path: Path, *, silent: bool) -> int:
+    logging.debug("Analyzing file %s", format_special(path))
+    try:
+        return _check_code(
+            path.read_text(encoding="utf-8"), file_path=path, silent=silent
+        )
+
+    except ParsingError:
+        return 0
+
+    except Exception as e:
+        github_report_note = _show_internal_error_report_note(
+            title="Internal error while checking code",
+            command="autopep695 check",
+            settings={"silent": silent},
+        )
+        logging.error(
+            f"Internal error while checking code in {format_special(path)}\n{github_report_note}\n{_show_debug_traceback_note()}"
+        )
+        logging.debug("Full traceback for the error above:", exc_info=e)
+        return 0
+
+
+def check_paths(paths: t.Iterable[Path], *, silent: bool) -> int:
+    return sum(_check_file(p, silent=silent) for p in paths)
---
+++ packages/autopep695/autopep695-1.1.2.tar.gz/autopep695-1.1.2/autopep695/cli.py
@@ -0,0 +1,271 @@
+# Copyright (c) 2024-present yowoda
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import argparse
+import logging
+import sys
+import typing as t
+from pathlib import Path
+
+from colorama import just_fix_windows_console
+
+from autopep695 import analyzer
+from autopep695.ux import (
+    init_logging,
+    RED,
+    RESET,
+    BOLD,
+    format_special,
+    get_system_info,
+)
+from autopep695.errors import InvalidPath
+
+INCLUDE_PATTERNS: t.Final[t.Sequence[str]] = ("*.py", "*.pyi")
+EXCLUDE_PATTERNS: t.Final[t.Sequence[str]] = (
+    ".bzr",
+    ".direnv",
+    ".eggs",
+    ".git",
+    ".git-rewrite",
+    ".hg",
+    ".mypy_cache",
+    ".nox",
+    ".pants.d",
+    ".pytype",
+    ".ruff_cache",
+    ".svn",
+    ".tox",
+    ".venv",
+    "__pypackages__",
+    "_build",
+    "buck-out",
+    "dist",
+    "node_modules",
+    "venv",
+    "__pycache__",
+)
+
+
+def filter_paths(
+    paths: t.Iterable[Path], include: t.Iterable[str], exclude: t.Iterable[str]
+) -> t.Iterable[Path]:
+    for path in paths:
+        if not path.exists():
+            raise InvalidPath(str(path))
+
+        if any((path.match(pattern) for pattern in exclude)):
+            continue
+
+        if path.is_file() and any((path.match(pattern) for pattern in include)):
+            yield path
+
+        elif path.is_dir():
+            yield from filter_paths(path.iterdir(), include=include, exclude=exclude)
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        "PEP695 upgrade tool",
+        description="Automatically upgrade to the new type parameter syntax introduced in python 3.12",
+    )
+    subparsers = parser.add_subparsers(dest="subparser")
+    check_parser = subparsers.add_parser(
+        "check",
+        help="Check code for pep695 compliance",
+        description="Find all places where the code still uses old type annotation syntax",
+    )
+    check_parser.add_argument(
+        "paths",
+        nargs="*",
+        help="Paths to a directory or a file that contains code using the old type annotation syntax",
+        type=Path,
+        default=[Path.cwd()],
+    )
+    check_parser.add_argument(
+        "-s",
+        "--silent",
+        required=False,
+        action="store_true",
+        help="Whether to silent the error reports.",
+    )
+    check_parser.add_argument(
+        "--exclude",
+        nargs="+",
+        help="Paths or patterns to exclude from being checked",
+        required=False,
+        default=EXCLUDE_PATTERNS,
+    )
+    check_parser.add_argument(
+        "--extend-exclude",
+        nargs="+",
+        help="Paths or patterns to exclude from being checked in addition to patterns excluded by default",
+        required=False,
+        default=(),
+    )
+    check_parser.add_argument(
+        "--include",
+        nargs="+",
+        help="Paths or patterns to include in being checked",
+        required=False,
+        default=INCLUDE_PATTERNS,
+    )
+    check_parser.add_argument(
+        "--extend-include",
+        nargs="+",
+        help="Paths or patterns to include in being checked in addition to patterns included by default",
+        required=False,
+        default=(),
+    )
+    check_parser.add_argument(
+        "-d",
+        "--debug",
+        help="Show debug information such as files analyzed",
+        action="store_true",
+        required=False,
+    )
+    format_parser = subparsers.add_parser(
+        "format",
+        help="Format the code to comply with pep695",
+        description="Implement suggestions made by `autopep695 check`",
+    )
+    format_parser.add_argument(
+        "paths",
+        nargs="*",
+        help="Paths to a directory or a file that contains code using the old type annotation syntax",
+        type=Path,
+        default=[Path.cwd()],
+    )
+    format_parser.add_argument(
+        "-u",
+        "--unsafe",
+        required=False,
+        action="store_true",
+        help="Whether to apply unsafe fixes such as type assignments",
+    )
+    format_parser.add_argument(
+        "-p",
+        "--parallel",
+        required=False,
+        nargs="?",
+        default=False,
+        const=True,
+        type=int,
+        help="Whether to process the files in parallel. Specify an integer to set the number of processes used.",
+    )
+    format_parser.add_argument(
+        "--exclude",
+        nargs="+",
+        help="Paths or patterns to exclude from being formatted",
+        required=False,
+        default=EXCLUDE_PATTERNS,
+    )
+    format_parser.add_argument(
+        "--extend-exclude",
+        nargs="+",
+        help="Paths or patterns to exclude from being formatted in addition to patterns excluded by default",
+        required=False,
+        default=(),
+    )
+    format_parser.add_argument(
+        "--include",
+        nargs="+",
+        help="Paths or patterns to include in being formatted",
+        required=False,
+        default=INCLUDE_PATTERNS,
+    )
+    format_parser.add_argument(
+        "--extend-include",
+        nargs="+",
+        help="Paths or patterns to include in being formatted in addition to patterns included by default",
+        required=False,
+        default=(),
+    )
+    format_parser.add_argument(
+        "--remove-variance",
+        help="If given, removes the variance suffixes in the type parameter names",
+        action="store_true",
+        required=False,
+    )
+    format_parser.add_argument(
+        "--remove-private",
+        help="If given, removes the underscore prefixes in the type parameter names",
+        action="store_true",
+        required=False,
+    )
+    format_parser.add_argument(
+        "--keep-assignments",
+        help="Whether to keep unused type parameter assignments",
+        action="store_true",
+        required=False,
+    )
+    format_parser.add_argument(
+        "-d",
+        "--debug",
+        help="Show debug information such as files analyzed",
+        action="store_true",
+        required=False,
+    )
+    subparsers.add_parser("info", help="Show information related to the tool")
+
+    args = parser.parse_args()
+
+    just_fix_windows_console()
+
+    if args.subparser is None:
+        parser.print_help()
+
+    elif args.subparser == "check":
+        init_logging(debug=args.debug, silent=args.silent)
+        paths = filter_paths(
+            args.paths,
+            include=set((*args.include, *args.extend_include)),
+            exclude=set((*args.exclude, *args.extend_exclude)),
+        )
+
+        try:
+            errors = analyzer.check_paths(paths, silent=args.silent)
+
+        except InvalidPath as e:
+            logging.error(
+                f"The specified path {format_special(e.path)} does not exist."
+            )
+            sys.exit(1)
+
+        if errors == 0:
+            print("All checks passed!")
+
+        else:
+            suffix = "s" if errors > 1 else ""
+            pronoun = "them" if errors > 1 else "it"
+            print(
+                f"Found {BOLD}{RED}{errors}{RESET} error{suffix}. Fix {pronoun} using {format_special('autopep695 format', '`')}."
+            )
+            sys.exit(1)
+
+    elif args.subparser == "format":
+        init_logging(debug=args.debug)
+        paths = filter_paths(
+            args.paths,
+            include=set((*args.include, *args.extend_include)),
+            exclude=set((*args.exclude, *args.extend_exclude)),
+        )
+        try:
+            analyzer.format_paths(
+                paths,
+                parallel=args.parallel,
+                unsafe=args.unsafe,
+                remove_variance=args.remove_variance,
+                remove_private=args.remove_private,
+                keep_assignments=args.keep_assignments,
+            )
+
+        except InvalidPath as e:
+            logging.error(
+                f"The specified path {format_special(e.path)} does not exist."
+            )
+            sys.exit(1)
+
+    elif args.subparser == "info":
+        print(get_system_info())
