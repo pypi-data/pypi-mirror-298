86680525a3bf72df65182e0c042d46663bf7e768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 15:05:11 +0000

    Add package fortepyan/0.4.0/fortepyan-0.4.0-py3-none-any.whl

---
+++ packages/fortepyan/fortepyan-0.4.0-py3-none-any.whl/fortepyan/__init__.py
@@ -0,0 +1,12 @@
+import pretty_midi
+
+from fortepyan import view
+from fortepyan.midi.structures import MidiFile, MidiPiece
+
+__all__ = ["view", "MidiFile", "MidiPiece"]
+__version__ = "0.4.0"
+
+# Pretty MIDI will throw an unwanted error for large files with high PPQ
+# This is a workaround
+# https://github.com/craffel/pretty-midi/issues/112
+pretty_midi.MAX_TICK = 1e10
