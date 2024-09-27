bfdc4b324c1727fcf6094645d8dcada279fc0080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 15:05:01 +0000

    Add package literate-python/0.0.3/literate_python-0.0.3.tar.gz

---
+++ packages/literate-python/literate_python-0.0.3.tar.gz/literate_python-0.0.3/literate_python/json.py
@@ -0,0 +1,14 @@
+import calendar, datetime
+
+def default_json_encoder(obj):
+    """Default JSON serializer."""
+    if isinstance(obj, datetime.datetime):
+        if obj.utcoffset() is not None:
+            obj = obj - obj.utcoffset()
+        millis = int(
+            calendar.timegm(obj.timetuple()) * 1000 +
+            obj.microsecond / 1000
+        )
+        return millis
+    else:
+        return str(obj)
