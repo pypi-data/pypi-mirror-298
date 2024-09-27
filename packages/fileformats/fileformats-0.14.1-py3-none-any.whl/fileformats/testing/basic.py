28d8883717761821a5fae5608dff3f3fbfb83bb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 14:05:46 +0000

    Add package fileformats/0.14.1/fileformats-0.14.1-py3-none-any.whl

---
+++ packages/fileformats/fileformats-0.14.1-py3-none-any.whl/fileformats/testing/basic.py
@@ -0,0 +1,21 @@
+from fileformats.generic import UnicodeFile
+
+
+class Foo(UnicodeFile):
+
+    ext = ".foo"
+
+
+class Bar(UnicodeFile):
+
+    ext = ".bar"
+
+
+class Baz(UnicodeFile):
+
+    ext = ".baz"
+
+
+class Qux(UnicodeFile):
+
+    ext = ".qux"
