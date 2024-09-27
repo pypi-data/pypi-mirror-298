b89177b8ae42bbf852e28a5ba1fb5adf3d431fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 04:05:32 +0000

    Add package huaweicloudstorepluginecs/0.0.3/huaweicloudstorepluginecs-0.0.3.tar.gz

---
+++ packages/huaweicloudstorepluginecs/huaweicloudstorepluginecs-0.0.3.tar.gz/huaweicloudstorepluginecs-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='huaweicloudstorepluginecs',
+    version='0.0.3',
+    description='Plugin that provides Huawei Cloud OBS Artifact Store functionality for MLflow',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='AKG103',
+    author_email='',
+    url="https://github.com/adheeshagamage/huaweicloudstorepluginecs.git",
+    packages=find_packages(),
+    install_requires=[
+        'mlflow',
+        'esdk-obs-python>=3.23.9.1'
+    ],
+    entry_points={
+        "mlflow.artifact_repository": [
+            "obs=huaweicloudstorepluginecs.store.artifact.huaweicloud_obs_ecs_artifact_repo:HuaweiCloudObsEcsArtifactRepository"
+        ]
+    },
+    license="Apache License 2.0",
+)
