8edb8e28bf60440f18b8f979fce485d21ecc232a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 15:05:39 +0000

    Add package thecodecrate-builderable/1.5.0/thecodecrate_builderable-1.5.0.tar.gz

---
+++ packages/thecodecrate-builderable/thecodecrate_builderable-1.5.0.tar.gz/thecodecrate_builderable-1.5.0/src/thecodecrate_builderable/traits/with_immutable_parts/with_immutable_parts.py
@@ -0,0 +1,17 @@
+import copy
+from typing import Generic, Protocol, Self
+from ...traits.with_parts.with_parts import WithParts
+from ...core.builderable.types import TBuilderPart, TBuilderOutput
+
+
+class WithImmutableParts(
+    WithParts[TBuilderOutput, TBuilderPart],
+    Generic[TBuilderOutput, TBuilderPart],
+    Protocol,
+):
+    def add_part(self, part: TBuilderPart) -> Self:
+        cloned = copy.deepcopy(self)
+
+        cloned.get_parts().append(part)
+
+        return cloned
