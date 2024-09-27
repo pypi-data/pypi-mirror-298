1b3988987f347df3ebd5cf58f6cb759ebec5c5c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 06:05:17 +0000

    Add package django-useraudit-4x/1.7.2/django-useraudit-4x-1.7.2.tar.gz

---
+++ packages/django-useraudit-4x/django-useraudit-4x-1.7.2.tar.gz/django-useraudit-4x-1.7.2/useraudit/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import re_path
+from .views import reactivate_user
+
+app_name = "useraudit"
+
+urlpatterns = [
+    re_path(
+        r"reactivate/(?P<user_id>\d+)[/]?$", reactivate_user, name="reactivate_user"
+    ),
+]
