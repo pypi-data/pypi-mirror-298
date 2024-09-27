ff8e8c1149ee2fd56ddcbba5907884d252414e67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 14:06:04 +0000

    Add package get-cosmopower-emus/0.0.2/get_cosmopower_emus-0.0.2.tar.gz

---
+++ packages/get-cosmopower-emus/get_cosmopower_emus-0.0.2.tar.gz/get_cosmopower_emus-0.0.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import os
+import subprocess
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        # Run the standard install first
+        install.run(self)
+
+        # Now run the custom post-install command
+        try:
+            # Use subprocess to ensure it runs correctly
+            subprocess.check_call([os.sys.executable, "-m", "get_cosmopower_emus.get_cosmopower_emus"])
+        except subprocess.CalledProcessError as e:
+            print(f"Error during post-installation: {e}")
+        else:
+            print("Successfully ran get_cosmopower_emus.get_cosmopower_emus")
+
+
+# Read the README file for the long description
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="get_cosmopower_emus",  # This is the name of your package
+    version="0.0.2",  # Package version
+
+    packages=find_packages(),
+    cmdclass={
+        'install': PostInstallCommand,
+    },
+
+    include_package_data=True,
+)
