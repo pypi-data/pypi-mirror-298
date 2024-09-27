1f15ebae6db0437e005c8dde03bf13134a6e5dfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 03:09:15 +0000

    Add package rustfluent/0.1.0a5/rustfluent-0.1.0a5.tar.gz

---
+++ packages/rustfluent/rustfluent-0.1.0a5.tar.gz/rustfluent-0.1.0a5/src/rustfluent.pyi
@@ -0,0 +1,10 @@
+Variable = str | int
+
+class Bundle:
+    def __init__(self, language: str, ftl_filenames: list[str], strict: bool = False) -> None: ...
+    def get_translation(
+        self,
+        identifier: str,
+        variables: dict[str, Variable] | None = None,
+        use_isolating: bool = True,
+    ) -> str: ...
