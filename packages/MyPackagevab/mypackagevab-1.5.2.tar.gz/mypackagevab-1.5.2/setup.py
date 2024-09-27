a5d2f7aa12f6c2e3ac3ac1f4cd78052f852b9f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 03:09:56 +0000

    Add package MyPackagevab/1.5.2/mypackagevab-1.5.2.tar.gz

---
+++ packages/MyPackagevab/mypackagevab-1.5.2.tar.gz/mypackagevab-1.5.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MyPackagevab',
+    version='1.5.2',
+    packages=find_packages(),
+    long_description=open('README.md').read(),
+    description='A package for generators, iterators, decorators, and descriptors',
+    author='BychVA',
+)
