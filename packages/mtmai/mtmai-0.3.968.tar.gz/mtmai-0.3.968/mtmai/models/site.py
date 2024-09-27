85fca0731aefca1dc77a159180c6e81b3bf18a61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 16:04:53 +0000

    Add package mtmai/0.3.968/mtmai-0.3.968.tar.gz

---
+++ packages/mtmai/mtmai-0.3.968.tar.gz/mtmai-0.3.968/mtmai/models/site.py
@@ -0,0 +1,73 @@
+from datetime import datetime
+import uuid
+
+from sqlmodel import Field, SQLModel
+
+from mtmai.mtlibs import mtutils
+
+
+class SiteHostBase(SQLModel):
+    domain: str = Field(min_length=3, max_length=255)
+    is_default: bool = Field(default=False)
+    is_https: bool = Field(default=False)
+
+class SiteHost(SQLModel, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+
+
+
+class SiteBase(SQLModel):
+    """
+    站点基础配置
+        1: 站点标题
+        2: 站点域名
+        3: 站点描述: 决定了 agent 如何自动采集文章和生成文章
+        4: 站点关键词: 决定了 agent 如何自动采集文章和生成文章
+        5: 站点作者
+        6: 站点版权
+        7: 站点创建时间
+        8: 站点更新时间
+    """
+
+    title: str | None = Field(default=None, max_length=255)
+    description: str | None = Field(default=None, max_length=255)
+    keywords: str | None = Field(default=None, max_length=255)
+    author: str | None = Field(default=None, max_length=255)
+    copyright: str | None = Field(default=None, max_length=255)
+    created_at: datetime = Field(default=datetime.now())
+    owner_id: str = Field(foreign_key="user.id", nullable=False, ondelete="CASCADE")
+
+
+
+# Database model, database table inferred from class name
+class Site(SiteBase, table=True):
+    # id: str = Field(default_factory=mtutils.gen_orm_id_key, primary_key=True)
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    updated_at: datetime = Field(default=datetime.now())
+
+
+class SiteCreateRequest(SiteBase):
+    pass
+
+
+class SiteUpdateRequest(SiteBase):
+    pass
+
+
+class SiteItemPublic(SiteBase):
+    # id: str
+    id: uuid.UUID
+    # owner_id: str
+
+
+class ListSiteResponse(SQLModel):
+    data: list[SiteItemPublic]
+    count: int
+
+
+class SiteHostItemPublic(SiteHostBase):
+    id: uuid.UUID
+
+class ListSiteHostsResponse(SQLModel):
+    data: list[SiteHostItemPublic]
+    count: int
