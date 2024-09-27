a87684de253e8e15090ca67a403525d21d9d1d25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 03:09:37 +0000

    Add package eds-ds-tools/0.7/eds_ds_tools-0.7.tar.gz

---
+++ packages/eds-ds-tools/eds_ds_tools-0.7.tar.gz/eds_ds_tools-0.7/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='eds_ds_tools',
+      version='0.7',
+      description='tools for data science',
+      packages=['eds_ds_tools'],
+      author_email='wenleicao@gmail.com',
+      zip_safe=False,      
+      )
