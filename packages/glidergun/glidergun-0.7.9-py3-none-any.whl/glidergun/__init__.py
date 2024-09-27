e6e63cae43277819df8c616d3e25158795e1c2e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 15:05:13 +0000

    Add package glidergun/0.7.9/glidergun-0.7.9-py3-none-any.whl

---
+++ packages/glidergun/glidergun-0.7.9-py3-none-any.whl/glidergun/__init__.py
@@ -0,0 +1,26 @@
+# flake8: noqa
+import sys
+import warnings
+
+if not sys.warnoptions:
+    warnings.simplefilter("ignore")
+
+import glidergun._ipython
+from glidergun._estimation import load_model
+from glidergun._functions import (
+    create,
+    distance,
+    interp_linear,
+    interp_nearest,
+    interp_rbf,
+    maximum,
+    mean,
+    minimum,
+    pca,
+    std,
+)
+from glidergun._grid import Grid, con, grid, standardize
+from glidergun._literals import BaseMap, ColorMap, DataType, ResamplingMethod
+from glidergun._mosaic import Mosaic, mosaic
+from glidergun._stack import Stack, stack
+from glidergun._types import CellSize, Extent, Point
