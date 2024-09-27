b50c14f889b87f914b88636906bf9f139c334b82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 05:05:01 +0000

    Add package shaped/0.14.1/shaped-0.14.1-py3-none-any.whl

---
+++ packages/shaped/shaped-0.14.1-py3-none-any.whl/shaped/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "1.0.0"
+from .client import Client
+from .autogen.models.inference_config import InferenceConfig
+from .autogen.models.retriever_top_k_override import RetrieverTopKOverride
