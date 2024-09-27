6b76664497afbba729771a774c108c0f02ca05c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 04:05:07 +0000

    Add package sunholo/0.100.2/sunholo-0.100.2-py3-none-any.whl

---
+++ packages/sunholo/sunholo-0.100.2-py3-none-any.whl/sunholo/langfuse/prompts.py
@@ -0,0 +1,44 @@
+from ..custom_logging import log
+from ..utils import ConfigManager
+
+# Load the YAML file
+def load_prompt_from_yaml(key, prefix="sunholo", load_from_file=False):
+    """
+    Returns a string you can use with Langfuse PromptTemplate.from_template() 
+
+    Will first try to load from the Langfuse prompt library, if unavailable will look in promptConfig type file.
+
+    Langfuse prompts have {{ two braces }}, Langchain prompts have { one brace }.
+
+    Example:
+
+    ```python
+    from sunholo.langfuse.prompts import load_prompt_from_yaml
+    from langchain_core.prompts import PromptTemplate
+    
+    """
+    config = ConfigManager(prefix)
+    if load_from_file:
+        
+        return config.promptConfig(key)
+
+
+    from langfuse import Langfuse
+
+    # Initialize Langfuse client
+    langfuse = Langfuse()
+
+    try:
+        if prefix is None:
+            langfuse_template = key
+        else:
+            langfuse_template = f"{prefix}-{key}"
+            
+        langfuse_prompt = langfuse.get_prompt(langfuse_template, cache_ttl_seconds=300)
+
+        return langfuse_prompt.get_langchain_prompt()
+    
+    except Exception as err:
+        log.warning(f"Could not find langfuse template: {langfuse_template} - {str(err)} - attempting to load from promptConfig")
+
+    return config.promptConfig(key)
