e8118a83aa55c77a6af46c7b5d6744c2d662cabd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 03:10:04 +0000

    Add package anura-kumara-dissanayake/0.0.1/anura_kumara_dissanayake-0.0.1-py3-none-any.whl

---
+++ packages/anura-kumara-dissanayake/anura_kumara_dissanayake-0.0.1-py3-none-any.whl/anura_kumara_dissanayake/udfs.py
@@ -0,0 +1,44 @@
+class ExpectedStringArgument(ValueError):
+    pass
+
+
+def get_percentage_of_nonalphabetical_characters(search_term: str) -> float:
+    """
+    Takes a string containing an arbitrary combination of numeric, alphabetic
+    and other characters, and returns the percentage thereof over the total
+    length of the input string.
+
+    Parameters
+    ----------
+    search_term: str
+        An input string containing an arbitrary combination of numeric,
+        alphabetic and other characters.
+
+    Return
+    ------
+    float: The percentage of non-alphabetical characters in the input string.
+
+    Examples
+    --------
+    >>> f = get_percentage_of_nonalphabetical_characters
+    >>> assert f("letters") == 0.0
+    >>> assert f("0414") == 1.0
+    >>> try:
+    ...   f(3)
+    ... except ValueError:
+    ...   assert True
+    >>> assert f("a different kind of string") == 0.1538
+    """
+    if not isinstance(search_term, str):
+        raise ExpectedStringArgument(
+            f"Expected a string as input, got {type(search_term)}."
+        )
+    if not search_term:
+        return 0.0
+    n_chars = len(search_term)
+    n_unk = len([ch for ch in list(search_term) if not ch.isalpha()])
+    return round(n_unk / n_chars, 4)
+
+if __name__ == "__main__":
+    from doctest import testmod
+    testmod()
