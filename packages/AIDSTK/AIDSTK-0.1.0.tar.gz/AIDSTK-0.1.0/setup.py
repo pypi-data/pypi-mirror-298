6ab734132784edd1c47c874dfaf274fc7d364894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 14:05:31 +0000

    Add package AIDSTK/0.1.0/AIDSTK-0.1.0.tar.gz

---
+++ packages/AIDSTK/AIDSTK-0.1.0.tar.gz/AIDSTK-0.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AIDSTK',
+    version='0.1.0',
+    description='A modular and flexible AI library for processing text data',
+    author='Alex Mendoza',
+    author_email='alexander.mendoza.am@gmail.com',
+    url='https://github.com/amendoxa/AIDSTK',
+    packages=find_packages(),
+    install_requires=[
+        'langchain_community',
+        'pandas',
+        'tqdm',
+    ],
+    include_package_data=True,
+    package_data={
+        'AIDSTK': ['prompts/*.txt', 'configs/*.json'],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
