5d77efcad8572bfcde49626acb86cd08b5b44374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 05:05:54 +0000

    Add package pyzeebe/4.0.0rc5/pyzeebe-4.0.0rc5.tar.gz

---
+++ packages/pyzeebe/pyzeebe-4.0.0rc5.tar.gz/pyzeebe-4.0.0rc5/pyzeebe/errors/zeebe_errors.py
@@ -0,0 +1,21 @@
+import grpc
+
+from pyzeebe.errors.pyzeebe_errors import PyZeebeError
+
+
+class ZeebeBackPressureError(PyZeebeError):
+    pass
+
+
+class ZeebeGatewayUnavailableError(PyZeebeError):
+    pass
+
+
+class ZeebeInternalError(PyZeebeError):
+    pass
+
+
+class UnknownGrpcStatusCodeError(PyZeebeError):
+    def __init__(self, grpc_error: grpc.aio.AioRpcError):
+        super().__init__()
+        self.grpc_error = grpc_error
