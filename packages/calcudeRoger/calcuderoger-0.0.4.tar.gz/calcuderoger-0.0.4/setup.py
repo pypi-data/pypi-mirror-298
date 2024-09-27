6d825f8157b82e868747eee26ac5f85029e947a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 03:09:23 +0000

    Add package calcudeRoger/0.0.4/calcuderoger-0.0.4.tar.gz

---
+++ packages/calcudeRoger/calcuderoger-0.0.4.tar.gz/calcuderoger-0.0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup( 
+    name='calcudeRoger', 
+    version='0.0.4',
+    packages=['calcudeRoger.otrom'],
+    author='Roger', 
+    description= 'Simple test package', 
+    long_description=open("README.rst","r", encoding="utf-8").read(),
+    install_requires=['CoolProp'],
+)
