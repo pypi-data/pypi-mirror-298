82ccc7dac3ce2ef8152d9c8ba5981f7a11d5fd29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 03:09:56 +0000

    Add package lamin_cli/0.17.5/lamin_cli-0.17.5.tar.gz

---
+++ packages/lamin_cli/lamin_cli-0.17.5.tar.gz/lamin_cli-0.17.5/tests/test_get.py
@@ -0,0 +1,32 @@
+from lamin_cli._get import decompose_url
+import subprocess
+
+
+def test_decompose_url():
+    url = "https://lamin.ai/laminlabs/arrayloader-benchmarks/transform/1GCKs8zLtkc85zKv"  # noqa
+    result = decompose_url(url)
+    instance_slug, entity, uid = result
+    assert instance_slug == "laminlabs/arrayloader-benchmarks"
+    assert entity == "transform"
+    assert uid == "1GCKs8zLtkc85zKv"
+
+
+def test_get_transform():
+    result = subprocess.run(
+        "lamin get"
+        " 'https://lamin.ai/laminlabs/arrayloader-benchmarks/transform/1GCKs8zLtkc85zKv'"
+        " --with-env",  # noqa
+        shell=True,
+        capture_output=True,
+    )
+    assert result.returncode == 0
+
+
+def test_get_artifact():
+    result = subprocess.run(
+        "lamin get"
+        " 'https://lamin.ai/laminlabs/lamin-site-assets/artifact/e2G7k9EVul4JbfsEYAy5'",  # noqa
+        shell=True,
+        capture_output=True,
+    )
+    assert result.returncode == 0
---
+++ packages/lamin_cli/lamin_cli-0.17.5.tar.gz/lamin_cli-0.17.5/tests/test_save_scripts.py
@@ -0,0 +1,133 @@
+from pathlib import Path
+import subprocess
+import os
+import lamindb as ln
+from lamindb_setup import settings
+
+
+scripts_dir = Path(__file__).parent.resolve() / "scripts"
+
+
+def test_run_save_cache():
+    env = os.environ
+    env["LAMIN_TESTING"] = "true"
+    filepath = scripts_dir / "run-track-and-finish-sync-git.py"
+
+    # attempt to save the script without it yet being run
+    result = subprocess.run(
+        f"lamin save {filepath}",
+        shell=True,
+        capture_output=True,
+    )
+    # print(result.stdout.decode())
+    assert result.returncode == 1
+    assert "Did you run ln.context.track()?" in result.stdout.decode()
+
+    # run the script
+    result = subprocess.run(
+        f"python {filepath}",
+        shell=True,
+        capture_output=True,
+    )
+    # print(result.stdout.decode())
+    # print(result.stderr.decode())
+    assert result.returncode == 0
+    assert "created Transform" in result.stdout.decode()
+    assert "m5uCHTTpJnjQ0000" in result.stdout.decode()
+    assert "created Run" in result.stdout.decode()
+
+    transform = ln.Transform.get("m5uCHTTpJnjQ")
+    assert transform.hash == "Cwk0OPOyUH5nzTiU2ISlDQ"
+    assert transform.latest_run.environment.path.exists()
+    assert transform._source_code_artifact is None
+
+    # you can rerun the same script
+    result = subprocess.run(
+        f"python {filepath}",
+        shell=True,
+        capture_output=True,
+        env=env,
+    )
+    # print(result.stdout.decode())
+    # print(result.stderr.decode())
+    assert result.returncode == 0
+    assert "loaded Transform" in result.stdout.decode()
+    assert "m5uCHTTpJnjQ0000" in result.stdout.decode()
+    assert "loaded Run" in result.stdout.decode()
+    assert "source code is already saved" in result.stdout.decode()
+
+    # you can re-save the script
+    result = subprocess.run(
+        f"lamin save {filepath}",
+        shell=True,
+        capture_output=True,
+        env=env,
+    )
+    # print(result.stdout.decode())
+    # print(result.stderr.decode())
+    assert result.returncode == 0
+    assert "source code is already saved" in result.stdout.decode()
+    assert "run.environment is already saved" in result.stdout.decode()
+
+    # edit the script
+    content = filepath.read_text() + "\n # edited"
+    filepath.write_text(content)
+
+    # re-run the script without committing
+    result = subprocess.run(
+        f"python {filepath}",
+        shell=True,
+        capture_output=True,
+        env=env,
+    )
+    # print(result.stdout.decode())
+    # print(result.stderr.decode())
+    assert result.returncode == 1
+    assert "Did not find blob hash" in result.stderr.decode()
+
+    # edit the script to remove the git integration
+    content = filepath.read_text()
+    content_lines = content.split("\n")
+    content_lines.remove(
+        'ln.settings.sync_git_repo = "https://github.com/laminlabs/lamin-cli"'
+    )
+    content = "\n".join(content_lines)
+    filepath.write_text(content)
+
+    # re-run the script
+    result = subprocess.run(
+        f"python {filepath}",
+        shell=True,
+        capture_output=True,
+        env=env,
+    )
+    print(result.stdout.decode())
+    print(result.stderr.decode())
+    assert result.returncode == 1
+    assert "Source code changed, bump revision by setting" in result.stderr.decode()
+
+    # try to get the the source code via command line
+    result = subprocess.run(
+        "lamin get"
+        f" https://lamin.ai/{settings.user.handle}/laminci-unit-tests/transform/m5uCHTTpJnjQ0000",  # noqa
+        shell=True,
+        capture_output=True,
+    )
+    print(result.stderr.decode())
+    assert result.returncode == 0
+
+    result = subprocess.run(
+        f"lamin get transform --key {filepath.name}",  # noqa
+        shell=True,
+        capture_output=True,
+    )
+    print(result.stderr.decode())
+    assert result.returncode == 0
+
+    result = subprocess.run(
+        f"lamin get transform --uid m5uCHTTpJnjQ0000",  # noqa
+        shell=True,
+        capture_output=True,
+    )
+    print(result.stderr.decode())
+    assert result.returncode == 0
