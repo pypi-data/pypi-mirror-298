93a5561bf18fda4cdbe470c137586883539c520e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 15:04:57 +0000

    Add package kkutils/1.7.5/kkutils-1.7.5.tar.gz

---
+++ packages/kkutils/kkutils-1.7.5.tar.gz/kkutils-1.7.5/tornado_utils/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Author: zhangkai
+Email: kai.zhang1@nio.com
+Last modified: 2018-08-10 18:13:39
+'''
+from .application import Application, Blueprint
+from .basehandler import BaseHandler
+from .userhandler import bp as bp_user
+from .utils import PageModule, authorized, cache
+
+__all__ = ['BaseHandler', 'Application', 'Blueprint', 'bp_user', 'authorized', 'cache', 'PageModule']
