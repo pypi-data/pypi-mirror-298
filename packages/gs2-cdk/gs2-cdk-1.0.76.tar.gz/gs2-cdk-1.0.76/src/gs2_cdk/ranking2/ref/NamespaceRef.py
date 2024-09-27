a994844b159fc865d328ec6abfa40306e1a535cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 14:06:25 +0000

    Add package gs2-cdk/1.0.76/gs2-cdk-1.0.76.tar.gz

---
+++ packages/gs2-cdk/gs2-cdk-1.0.76.tar.gz/gs2-cdk-1.0.76/src/gs2_cdk/ranking2/ref/NamespaceRef.py
@@ -0,0 +1,107 @@
+# Copyright 2016- Game Server Services, Inc. or its affiliates. All Rights
+# Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+from __future__ import annotations
+from typing import *
+
+from ...core.func import GetAttr, Join
+from .GlobalRankingModelRef import GlobalRankingModelRef
+from .SubscribeRankingModelRef import SubscribeRankingModelRef
+from .ClusterRankingModelRef import ClusterRankingModelRef
+from ..stamp_sheet.CreateGlobalRankingReceivedRewardByUserId import CreateGlobalRankingReceivedRewardByUserId
+from ..stamp_sheet.CreateClusterRankingReceivedRewardByUserId import CreateClusterRankingReceivedRewardByUserId
+
+
+class NamespaceRef:
+    namespace_name: str
+
+    def __init__(
+        self,
+        namespace_name: str,
+    ):
+        self.namespace_name = namespace_name
+
+    def global_ranking_model(
+        self,
+        ranking_name: str,
+    ) -> GlobalRankingModelRef:
+        return GlobalRankingModelRef(
+            self.namespace_name,
+            ranking_name,
+        )
+
+    def subscribe_ranking_model(
+        self,
+        ranking_name: str,
+    ) -> SubscribeRankingModelRef:
+        return SubscribeRankingModelRef(
+            self.namespace_name,
+            ranking_name,
+        )
+
+    def cluster_ranking_model(
+        self,
+        ranking_name: str,
+    ) -> ClusterRankingModelRef:
+        return ClusterRankingModelRef(
+            self.namespace_name,
+            ranking_name,
+        )
+
+    def create_global_ranking_received_reward(
+        self,
+        ranking_name: str,
+        season: Optional[int] = None,
+        user_id: Optional[str] = "#{userId}",
+    ) -> CreateGlobalRankingReceivedRewardByUserId:
+        return CreateGlobalRankingReceivedRewardByUserId(
+            self.namespace_name,
+            ranking_name,
+            season,
+            user_id,
+        )
+
+    def create_cluster_ranking_received_reward(
+        self,
+        ranking_name: str,
+        cluster_name: str,
+        season: Optional[int] = None,
+        user_id: Optional[str] = "#{userId}",
+    ) -> CreateClusterRankingReceivedRewardByUserId:
+        return CreateClusterRankingReceivedRewardByUserId(
+            self.namespace_name,
+            ranking_name,
+            cluster_name,
+            season,
+            user_id,
+        )
+
+    def grn(
+        self,
+    ) -> str:
+        return Join(
+            ":",
+            [
+                "grn",
+                "gs2",
+                GetAttr.region(
+                ).str(
+                ),
+                GetAttr.owner_id(
+                ).str(
+                ),
+                "ranking2",
+                self.namespace_name,
+            ],
+        ).str(
+        )
