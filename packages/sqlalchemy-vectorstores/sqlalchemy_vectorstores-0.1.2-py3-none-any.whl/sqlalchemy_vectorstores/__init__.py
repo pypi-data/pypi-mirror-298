60df265e01b0012304c6ec2aa137cda3b3f10071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 15:05:01 +0000

    Add package sqlalchemy-vectorstores/0.1.2/sqlalchemy_vectorstores-0.1.2-py3-none-any.whl

---
+++ packages/sqlalchemy-vectorstores/sqlalchemy_vectorstores-0.1.2-py3-none-any.whl/sqlalchemy_vectorstores/__init__.py
@@ -0,0 +1,5 @@
+from .databases import SqliteDatabase, PostgresDatabase, AsyncSqliteDatabase, AsyncPostgresDatabase
+from .vectorstores import SqliteVectorStore, PostgresVectorStore, AsyncSqliteVectorStore, AsyncPostgresVectorStore
+
+
+__version__ = "0.1.2"
---
+++ packages/sqlalchemy-vectorstores/sqlalchemy_vectorstores-0.1.2-py3-none-any.whl/sqlalchemy_vectorstores/vectorstores/utils.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+import typing as t
+
+import sqlalchemy as sa
+
+
+def _select_first_to_dict(result: sa.ResultProxy) -> dict | None:
+    '''
+    helper function to convert select query to dict
+    '''
+    if data := result.first():
+        return {k:v for k,v in zip(result.keys(), data)}
+
+
+class Document(t.TypedDict):
+    src_id: str
+    content: str
+    metadata: dict
+    type: str | None
+    target_id: str | None
