b59a1fc87c98f5e1e7e37fd4d2f752c122c54f77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 03:10:06 +0000

    Add package MomwhyareyouhereWebsite/1.2/MomwhyareyouhereWebsite-1.2.tar.gz

---
+++ packages/MomwhyareyouhereWebsite/MomwhyareyouhereWebsite-1.2.tar.gz/MomwhyareyouhereWebsite-1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MomwhyareyouhereWebsite",
+    version="1.2",
+    description="A simple Python package to host a website without using Flask or external frameworks",
+    author="Momwhyareyouhere",
+    author_email="youremail@example.com",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],  # No external dependencies
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
