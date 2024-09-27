67274d330b7c4f55236243020730ea9bcb1fee4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 16:05:03 +0000

    Add package charz/0.0.20/charz-0.0.20.tar.gz

---
+++ packages/charz/charz-0.0.20.tar.gz/charz-0.0.20/charz/_components/_transform.py
@@ -0,0 +1,157 @@
+from __future__ import annotations as _annotations
+
+from copy import deepcopy as _deepcopy
+from typing import (
+    Generator as _Generator,
+    Any as _Any,
+    ClassVar as _ClassVar,
+)
+
+from linflex import Vec2 as _Vec2
+
+from .._annotations import (
+    NodeType as _NodeType,
+    TransformNode as _TransformNode,
+)
+
+
+class Transform:
+    transform_instances: _ClassVar[dict[int, _TransformNode]] = {}
+
+    def __new__(cls: type[_NodeType], *args: _Any, **kwargs: _Any) -> _NodeType:
+        instance = super().__new__(cls, *args, **kwargs)  # type: _TransformNode  # type: ignore[reportAssignmentType]
+        Transform.transform_instances[instance.uid] = instance
+        if (class_position := getattr(instance, "position", None)) is not None:
+            instance.position = _deepcopy(class_position)
+        else:
+            instance.position = _Vec2.ZERO
+        return instance  # type: ignore
+
+    position: _Vec2
+    rotation: float = 0
+    z_index: int = 0
+    is_top_level: bool = False
+
+    # TODO: would be nice to figure out @overload with this function
+    def with_position(
+        self,
+        position: _Vec2 | None = None,
+        /,
+        x: float | None = None,
+        y: float | None = None,
+    ):
+        if position is None and x is None and y is None:
+            raise TypeError(f"not all arguments can be {None} at the same time")
+        if position is not None and (x is not None or y is not None):
+            raise TypeError(
+                "chose either positional argument 'position' "
+                "or keyword arguments 'x' and/or 'y', not all three"
+            )
+        if position is not None:
+            self.position = position
+        if x is not None or y is not None:
+            self.position = _Vec2(x or 0, y or 0)
+        return self
+
+    # TODO: would be nice to figure out @overload with this function
+    def with_global_position(
+        self,
+        global_position: _Vec2 | None = None,
+        /,
+        x: float | None = None,
+        y: float | None = None,
+    ):
+        if global_position is None and x is None and y is None:
+            raise TypeError(f"not all arguments can be {None} at the same time")
+        if global_position is not None and (x is not None or y is not None):
+            raise TypeError(
+                "chose either positional argument 'global_position' "
+                "or keyword arguments 'x' and/or 'y', not all three"
+            )
+        if global_position is not None:
+            self.global_position = global_position
+        if x is not None or y is not None:
+            self.global_position = _Vec2(x or 0, y or 0)
+        return self
+
+    def with_rotation(self, rotation: float, /):
+        self.rotation = rotation
+        return self
+
+    def with_global_rotation(self, global_rotation: float, /):
+        self.global_rotation = global_rotation
+        return self
+
+    def with_z_index(self, z_index: int, /):
+        self.z_index = z_index
+        return self
+
+    def as_top_level(self, state: bool = True, /):
+        self.is_top_level = state
+        return self
+    
+    def set_global_x(self, x: float, /) -> None:
+        diff_x = x - self.global_position.x
+        self.position.x += diff_x
+    
+    def set_global_y(self, y: float, /) -> None:
+        diff_y = y - self.global_position.y
+        self.position.y += diff_y
+
+    @property
+    def global_position(self) -> _Vec2:
+        """Returns a copy of the node's global position (in world space)
+
+        `NOTE`: cannot do `self.global_position.x += 5`,
+        use `self.position += 5` instead, as it only adds a relative value
+
+        `NOTE`: cannot do `self.global_position.x = 42`,
+        use `self.set_global_x(42)`
+        
+        Returns:
+            Vec2: copy of global position
+        """
+        if self.is_top_level:
+            return self.position.copy()
+        global_position = self.position.copy()
+        parent = self.parent  # type: ignore
+        while parent is not None and isinstance(parent, Transform):
+            global_position = parent.position + global_position.rotated(parent.rotation)
+            if parent.is_top_level:
+                return global_position
+            parent = parent.parent  # type: ignore
+        return global_position
+
+    @global_position.setter
+    def global_position(self, position: _Vec2) -> None:
+        """Sets the node's global position (world space)"""
+        diff = position - self.global_position
+        self.position += diff
+
+    @property
+    def global_rotation(self) -> float:
+        """Computes the node's global rotation (world space)
+
+        Returns:
+            float: global rotation in radians
+        """
+        if self.is_top_level:
+            return self.rotation
+        global_rotation = self.rotation
+        parent = self.parent  # type: ignore
+        while parent is not None and isinstance(parent, Transform):
+            global_rotation += parent.rotation
+            if parent.is_top_level:
+                return global_rotation
+            parent = parent.parent  # type: ignore
+        return global_rotation
+
+    @global_rotation.setter
+    def global_rotation(self, rotation: float) -> None:
+        """Sets the node's global rotation (world space)"""
+        diff = rotation - self.global_rotation
+        self.rotation += diff
+
+    def free(self: _TransformNode) -> None:
+        del Transform.transform_instances[self.uid]
+        super().free()
---
+++ packages/charz/charz-0.0.20.tar.gz/charz-0.0.20/charz/_prefabs/_animated_sprite.py
@@ -0,0 +1,7 @@
+from __future__ import annotations as _annotations
+
+from .._components._animation import Animated as _Animated
+from ._sprite import Sprite as _Sprite
+
+
+class AnimatedSprite(_Animated, _Sprite): ...
