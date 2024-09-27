c258712602f465509b83a129860d4edbb6a83c2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 03:09:22 +0000

    Add package distancia/0.0.49/distancia-0.0.49.tar.gz

---
+++ packages/distancia/distancia-0.0.49.tar.gz/distancia-0.0.49/setup.py
@@ -0,0 +1,53 @@
+import setuptools
+#from setuptools import setup, Extension,find_packages
+
+from distutils.core import setup, Extension
+
+from Cython.Build import cythonize
+from readme_renderer import markdown
+#ext_modules = cythonize([Extension("distancia.distance", ["distancia/distance.py"])])
+
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+    
+ 
+    
+setuptools.setup(
+#setup(
+    
+    #ext_modules=ext_modules,
+    #ext_modules = cythonize(ext_modules),
+
+
+    name="distancia", # Replace with your username
+
+    version="0.0.49",
+
+    author="Yves Mercadier",
+
+    author_email="",
+
+    description="distance metrics,data-science deep-learning machine-learning neural-network",
+
+    long_description=markdown.render(long_description),
+
+    long_description_content_type="text/markdown",
+
+    url="https://pypi.org/project/distancia/",
+
+    packages=setuptools.find_packages(),
+
+    classifiers=[
+
+        "Programming Language :: Python :: 3",
+
+        "License :: OSI Approved :: MIT License",
+
+        "Operating System :: OS Independent",
+
+    ],
+
+    python_requires='>=3.0',
+
+)
