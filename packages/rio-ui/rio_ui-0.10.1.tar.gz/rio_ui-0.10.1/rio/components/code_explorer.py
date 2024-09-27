d2dda6fadbce5d253cfb8e4321754443926f50a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 16:05:13 +0000

    Add package rio-ui/0.10.1/rio_ui-0.10.1.tar.gz

---
+++ packages/rio-ui/rio_ui-0.10.1.tar.gz/rio_ui-0.10.1/rio/components/code_explorer.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+from typing import Literal, Sequence
+
+from uniserde import JsonDoc
+
+import rio
+
+from .fundamental_component import FundamentalComponent
+
+__all__ = [
+    "CodeExplorer",
+]
+
+
+class CodeExplorer(FundamentalComponent):
+    """
+    ## Metadata
+
+    `public`: False
+    """
+
+    source_code: str
+    build_result: rio.Component
+    line_indices_to_component_keys: Sequence[str | int | None]
+
+    style: Literal["horizontal", "vertical"] = "horizontal"
+
+    def _custom_serialize_(self) -> JsonDoc:
+        return {
+            "line_indices_to_component_keys": self.line_indices_to_component_keys,  # type: ignore
+        }
+
+
+CodeExplorer._unique_id_ = "CodeExplorer-builtin"
