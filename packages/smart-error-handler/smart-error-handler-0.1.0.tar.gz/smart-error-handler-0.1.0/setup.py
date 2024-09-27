a32824af38da2a93f2171c3ee280328929e9e04b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 15:05:19 +0000

    Add package smart-error-handler/0.1.0/smart-error-handler-0.1.0.tar.gz

---
+++ packages/smart-error-handler/smart-error-handler-0.1.0.tar.gz/smart-error-handler-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="smart-error-handler",
+    version="0.1.0",
+    packages=find_packages(),
+    description="A smart error handler",
+    author="Momwhyareyouhere"
+)
---
+++ packages/smart-error-handler/smart-error-handler-0.1.0.tar.gz/smart-error-handler-0.1.0/smart_error_handler/__init__.py
@@ -0,0 +1,3 @@
+from .handler import SmartErrorHandler
+
+__all__ = ["SmartErrorHandler"]
