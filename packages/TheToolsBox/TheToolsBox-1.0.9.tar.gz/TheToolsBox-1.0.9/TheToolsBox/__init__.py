2482bccbe48876167a51fa550ef2c566f34f63be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 14:06:27 +0000

    Add package TheToolsBox/1.0.9/TheToolsBox-1.0.9.tar.gz

---
+++ packages/TheToolsBox/TheToolsBox-1.0.9.tar.gz/TheToolsBox-1.0.9/TheToolsBox/__init__.py
@@ -0,0 +1,8 @@
+from .baseLib import (create_word,
+                      create_excel,
+                      clean_duplicates,
+                      testOpen,
+                      disk,
+                      typeFile,
+                      SearchIn,
+                      ListingDosFile)
