97e63eacfde2b21fb0d2fc28ac795343abca158b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 15:05:16 +0000

    Add package tortoise-database-url/0.1.2/tortoise_database_url-0.1.2.tar.gz

---
+++ packages/tortoise-database-url/tortoise_database_url-0.1.2.tar.gz/tortoise_database_url-0.1.2/database_url/__init__.py
@@ -0,0 +1,31 @@
+import importlib.metadata as importlib_metadata
+
+from ._main import (
+    DatabaseUrlError,
+    DbDefaultEnum,
+    InvalidEngine,
+    from_django_item,
+    generate,
+)
+
+try:
+    __version__ = importlib_metadata.version(__name__)
+except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+    __version__ = "0.1.0"
+
+__all__ = (
+    "__version__",
+    "generate",
+    "from_django_item",
+    "InvalidEngine",
+    "DatabaseUrlError",
+    "DbDefaultEnum",
+)
+
+
+# Re-export imports so they look like they live directly in this package
+for __value in list(locals().values()):
+    if getattr(__value, "__module__", "").startswith("database_url."):
+        __value.__module__ = __name__
+
+del __value
