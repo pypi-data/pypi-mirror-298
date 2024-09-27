0427147e65d5dd793a64ef245dd8d5cee76b1eeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 14:05:55 +0000

    Add package boto3-stubs-full/1.35.28/boto3_stubs_full-1.35.28-py3-none-any.whl

---
+++ packages/boto3-stubs-full/boto3_stubs_full-1.35.28-py3-none-any.whl/mypy_boto3_resource_groups/paginator.py
@@ -0,0 +1,102 @@
+"""
+Type annotations for resource-groups service client paginators.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_resource_groups/paginators/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+
+    from mypy_boto3_resource_groups.client import ResourceGroupsClient
+    from mypy_boto3_resource_groups.paginator import (
+        ListGroupResourcesPaginator,
+        ListGroupsPaginator,
+        SearchResourcesPaginator,
+    )
+
+    session = Session()
+    client: ResourceGroupsClient = session.client("resource-groups")
+
+    list_group_resources_paginator: ListGroupResourcesPaginator = client.get_paginator("list_group_resources")
+    list_groups_paginator: ListGroupsPaginator = client.get_paginator("list_groups")
+    search_resources_paginator: SearchResourcesPaginator = client.get_paginator("search_resources")
+    ```
+"""
+
+import sys
+from typing import Generic, Iterator, TypeVar
+
+from botocore.paginate import PageIterator, Paginator
+
+from .type_defs import (
+    ListGroupResourcesInputListGroupResourcesPaginateTypeDef,
+    ListGroupResourcesOutputTypeDef,
+    ListGroupsInputListGroupsPaginateTypeDef,
+    ListGroupsOutputTypeDef,
+    SearchResourcesInputSearchResourcesPaginateTypeDef,
+    SearchResourcesOutputTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+
+__all__ = ("ListGroupResourcesPaginator", "ListGroupsPaginator", "SearchResourcesPaginator")
+
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class ListGroupResourcesPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/resource-groups.html#ResourceGroups.Paginator.ListGroupResources)
+    [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_resource_groups/paginators/#listgroupresourcespaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListGroupResourcesInputListGroupResourcesPaginateTypeDef]
+    ) -> _PageIterator[ListGroupResourcesOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/resource-groups.html#ResourceGroups.Paginator.ListGroupResources.paginate)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_resource_groups/paginators/#listgroupresourcespaginator)
+        """
+
+
+class ListGroupsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/resource-groups.html#ResourceGroups.Paginator.ListGroups)
+    [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_resource_groups/paginators/#listgroupspaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListGroupsInputListGroupsPaginateTypeDef]
+    ) -> _PageIterator[ListGroupsOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/resource-groups.html#ResourceGroups.Paginator.ListGroups.paginate)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_resource_groups/paginators/#listgroupspaginator)
+        """
+
+
+class SearchResourcesPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/resource-groups.html#ResourceGroups.Paginator.SearchResources)
+    [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_resource_groups/paginators/#searchresourcespaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[SearchResourcesInputSearchResourcesPaginateTypeDef]
+    ) -> _PageIterator[SearchResourcesOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/resource-groups.html#ResourceGroups.Paginator.SearchResources.paginate)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_resource_groups/paginators/#searchresourcespaginator)
+        """
