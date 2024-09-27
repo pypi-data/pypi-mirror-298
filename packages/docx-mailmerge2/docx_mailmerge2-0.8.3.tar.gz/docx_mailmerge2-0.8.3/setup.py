acfc686e7dc51910e190031e2a9cfeddb2dfa1be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 16:04:49 +0000

    Add package docx-mailmerge2/0.8.3/docx_mailmerge2-0.8.3.tar.gz

---
+++ packages/docx-mailmerge2/docx_mailmerge2-0.8.3.tar.gz/docx_mailmerge2-0.8.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name="docx-mailmerge2",
+    version="0.8.3",
+    description="Performs a Mail Merge on docx (Microsoft Office Word) files",
+    long_description=open("README.rst").read(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Text Processing",
+    ],
+    author="Iulian Ciorăscu",
+    author_email="ciulian@gmail.com",
+    url="http://github.com/iulica/docx-mailmerge",
+    license="MIT",
+    py_modules=["mailmerge"],
+    zip_safe=False,
+    install_requires=["lxml"],
+)
