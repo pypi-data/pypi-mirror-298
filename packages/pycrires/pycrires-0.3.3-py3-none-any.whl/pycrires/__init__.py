19fc6ed628b0db032549a00c10ee300fb80a2963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 05:05:17 +0000

    Add package pycrires/0.3.3/pycrires-0.3.3-py3-none-any.whl

---
+++ packages/pycrires/pycrires-0.3.3-py3-none-any.whl/pycrires/__init__.py
@@ -0,0 +1,9 @@
+from pycrires.pipeline import Pipeline
+from pycrires.util import load_bt_settl_template
+
+__author__ = 'Tomas Stolker'
+__license__ = 'MIT'
+__version__ = '0.3.3'
+__maintainer__ = 'Tomas Stolker'
+__email__ = 'stolker@strw.leidenuniv.nl'
+__status__ = 'Development'
