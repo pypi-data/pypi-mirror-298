f44740a8a00a3f1d2a47beead3eb5167f90a1160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 15:04:53 +0000

    Add package inSis/1.6.2.0/insis-1.6.2.0.tar.gz

---
+++ packages/inSis/insis-1.6.2.0.tar.gz/insis-1.6.2.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='inSis',
+version='1.6.2.0',
+description='installation of inSis Package',
+url='https://www.jaajitech.com',
+author='Sivarambrahmam',
+author_email='sivarambrahmam@jaajitech.com',
+license='MIT',
+packages=['inSis'],
+zip_safe=False)
