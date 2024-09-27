ad40700492917bc23d455bb8ab4ac9c118b2bf2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 03:09:37 +0000

    Add package pywebscrapr/0.1.2/pywebscrapr-0.1.2.tar.gz

---
+++ packages/pywebscrapr/pywebscrapr-0.1.2.tar.gz/pywebscrapr-0.1.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pywebscrapr',
+    version='0.1.2',
+    author='Infinitode Pty Ltd',
+    author_email='infinitode.ltd@gmail.com',
+    description='An open-source Python library for web scraping tasks. Includes support for both image scraping and text scraping.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/infinitode/pywebscrapr',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'bs4',
+        'pillow'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+    python_requires='>=3.6',
+)
