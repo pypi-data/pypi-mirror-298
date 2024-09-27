316a39c0678e104a20182d0350650cb228cd6024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 03:09:57 +0000

    Add package py-calendar-entry-management-functions/0.1.1/py-calendar-entry-management-functions-0.1.1.tar.gz

---
+++ packages/py-calendar-entry-management-functions/py-calendar-entry-management-functions-0.1.1.tar.gz/py-calendar-entry-management-functions-0.1.1/Calendar/calendar_functions.py
@@ -0,0 +1,162 @@
+import calendar
+import datetime
+year = datetime.datetime.now().year
+month = datetime.datetime.now().month
+import json
+
+import os
+
+def load_entries():
+    '''Loads entries from the json file or creates a new structure if it doesn't exist.'''
+    filename = 'filename.json'
+    
+    # Check if json file exists, if not create it with default structure
+    if not os.path.exists(filename):
+        default_structure = {
+            "entries": {
+                "birthdays": {},
+                "events": {},
+                "meetings": {}
+            }
+        }
+        with open(filename, 'w') as f:
+            json.dump(default_structure, f, indent=4)
+        return default_structure['entries']
+    
+    # If file exists, load the data
+    with open(filename) as f:
+        data = json.load(f)
+    return data['entries']
+
+
+
+def save_entry(entries):
+    '''Saves entries and stores them in the json file.'''
+    filename = 'filename.json'
+    
+    # Ensure the file is initialized before saving
+    if not os.path.exists(filename):
+        load_entries()  # This will create the file with the default structure if it doesn't exist
+    
+    # Save the new entries to the file
+    with open(filename, 'w') as file:
+        json.dump({"entries": entries}, file, indent=4)
+
+def display_current_month():
+    '''Calculates and displays the current month. '''                                      
+    print()
+    print(calendar.month(year,month))
+
+def display_current_year():
+    '''Calculates and displays the current year. '''                               
+    print()
+    print(calendar.calendar(year))
+
+def add_entry(data,type,name_or_description,date):
+    '''Assigns the data of new entries and stores them in a specific dictionary (json file).'''
+    if type == 'b':                                    
+        data["birthdays"].update({name_or_description : date})               
+    elif type == 'm':
+        data["meetings"].update({name_or_description : date})
+    elif type == 'e':
+        data["events"].update({name_or_description : date})   
+    save_entry(data)
+    load_entries()
+    print("Entry saved!")  
+                                                             
+def get_entry(data,type,name_or_description):
+    '''Searches a specific entry in the dictionary's (within the json file).'''
+    if type == 'b': 
+        dict_birth = data["birthdays"]
+        for key,value in dict_birth.items():
+            if key == name_or_description:
+                return key,value
+        else:
+            return 'No entry found.'
+    if type == 'm':
+        dict_meet = data["meetings"] 
+        for key,value in dict_meet.items():
+            if key == name_or_description:
+                return key,value
+        else:
+            return 'No entry found.'
+    if type == 'e': 
+        dict_event = data["events"]
+        for key,value in dict_event.items():
+            if key == name_or_description:
+                return key,value
+        else:
+            return 'No entry found.'
+        
+def display_entry(data,type,name_or_description):  
+    '''Displays specific entry (which the user is searching for).'''
+    entry = get_entry(data,type,name_or_description)
+    if type == 'b': 
+        dict_birth = data["birthdays"]
+        if name_or_description in dict_birth:
+            print(f'{name_or_description} : {entry[1]}') 
+        else:
+            print("No entry found.")    
+    if type == 'm': 
+        dict_meet = data["meetings"]
+        if name_or_description in dict_meet:
+            print(f'{name_or_description} : {entry[1]}') 
+        else:
+            print("No entry found.")           
+    if type == 'e': 
+        dict_event = data["events"]
+        if name_or_description in dict_event:
+            print(f'{name_or_description} : {entry[1]}') 
+        else:
+            print("No entry found.")                   
+    
+def delete_entry(data,type,name_or_description):
+    '''Deletes a selected entry from the dictionary (within the json file).'''
+    entry = get_entry(data,type,name_or_description)
+    if entry[0] in data["birthdays"]:
+        del data["birthdays"][entry[0]] 
+        print("Entry deleted!")
+    elif entry[0] in data["meetings"]:
+        del data["meetings"][entry[0]]
+        print("Entry deleted!")
+    elif entry[0] in data["events"]:
+        del data["events"][entry[0]]
+        print("Entry deleted!")
+    elif entry[0] not in data:
+        print('No entry found.')    
+    save_entry(data) 
+    load_entries()       
+    
+def change_entry(data,type,name_or_description,new_date):
+    '''Allows the user to change the date/time (value) of an entry.'''
+    entry = get_entry(data,type,name_or_description)  
+    if entry[0] in data["birthdays"]:
+        data["birthdays"].update({entry[0] : new_date})  
+        print("Entry successfully changed!") 
+    elif entry[0] in data["meetings"]:
+        data["meetings"].update({entry[0] : new_date})
+        print("Entry successfully changed!") 
+    elif entry[0] in data["events"]:
+        data["events"].update({entry[0] : new_date}) 
+        print("Entry successfully changed!") 
+    elif entry[0] not in data:
+        print('No entry found.')       
+    save_entry(data)      
+    load_entries()
+    
+def show_all(data,type):
+    '''Lists all entries of a type.'''
+    if type == 'b': 
+        dict_birth = data["birthdays"]
+        for key,value in dict_birth.items():
+            print(key,value)
+    elif type == 'm':
+        dict_meet = data["meetings"]
+        for key,value in dict_meet.items():
+            print(key,value)
+    elif type == 'e':
+        dict_event = data["events"]
+        for key,value in dict_event.items():
+            print(key,value)    
+    
+    
---
+++ packages/py-calendar-entry-management-functions/py-calendar-entry-management-functions-0.1.1.tar.gz/py-calendar-entry-management-functions-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='py-calendar-entry-management-functions',
+    version='0.1.1',
+    author='Leon',
+    author_email='leonz99@web.de',
+    description='A package to store and access calendar entries in a json file .',
+    packages=find_packages(),
+    install_requires = [
+        #'calendar',        #not existing in pip
+        'datetime',
+        #'json',            #not existing in pip
+        #'os'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
