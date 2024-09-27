c5b1291470fec2982d20a3b1fe2f1cb3447743b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 15:05:06 +0000

    Add package pretix/2024.9.0/pretix-2024.9.0.tar.gz

---
+++ packages/pretix/pretix-2024.9.0.tar.gz/pretix-2024.9.0/src/pretix/plugins/ticketoutputpdf/api.py
@@ -0,0 +1,158 @@
+#
+# This file is part of pretix (Community Edition).
+#
+# Copyright (C) 2014-2020 Raphael Michel and contributors
+# Copyright (C) 2020-2021 rami.io GmbH and contributors
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+# Public License as published by the Free Software Foundation in version 3 of the License.
+#
+# ADDITIONAL TERMS APPLY: Pursuant to Section 7 of the GNU Affero General Public License, additional terms are
+# applicable granting you additional permissions and placing additional restrictions on your usage of this software.
+# Please refer to the pretix LICENSE file to obtain the full terms applicable to this work. If you did not receive
+# this file, see <https://pretix.eu/about/en/license>.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along with this program.  If not, see
+# <https://www.gnu.org/licenses/>.
+#
+from django.conf import settings
+from django.db import transaction
+from django.db.models import QuerySet
+from django.utils.functional import lazy
+from rest_framework import serializers, viewsets
+from rest_framework.exceptions import ValidationError
+
+from pretix.api.serializers.i18n import I18nAwareModelSerializer
+from pretix.api.serializers.order import CompatibleJSONField
+
+from ...api.serializers.fields import UploadedFileField
+from ...base.models import SalesChannel
+from ...base.pdf import PdfLayoutValidator
+from ...multidomain.utils import static_absolute
+from .models import TicketLayout, TicketLayoutItem
+
+
+class ItemAssignmentSerializer(I18nAwareModelSerializer):
+    sales_channel = serializers.SlugRelatedField(
+        slug_field='identifier',
+        queryset=SalesChannel.objects.none(),
+    )
+
+    class Meta:
+        model = TicketLayoutItem
+        fields = ('id', 'layout', 'item', 'sales_channel')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["sales_channel"].queryset = self.context["event"].organizer.sales_channels.all()
+
+
+class NestedItemAssignmentSerializer(I18nAwareModelSerializer):
+    sales_channel = serializers.SlugRelatedField(
+        slug_field='identifier',
+        queryset=SalesChannel.objects.none(),
+    )
+
+    class Meta:
+        model = TicketLayoutItem
+        fields = ('item', 'sales_channel')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["sales_channel"].queryset = lazy(lambda: self.context["event"].organizer.sales_channels.all(), QuerySet)
+
+
+class TicketLayoutSerializer(I18nAwareModelSerializer):
+    layout = CompatibleJSONField(
+        validators=[PdfLayoutValidator()]
+    )
+    item_assignments = NestedItemAssignmentSerializer(many=True, read_only=True)
+    background = UploadedFileField(required=False, allow_null=True, allowed_types=(
+        'application/pdf',
+    ), max_size=settings.FILE_UPLOAD_MAX_SIZE_IMAGE)
+
+    class Meta:
+        model = TicketLayout
+        fields = ('id', 'name', 'default', 'layout', 'background', 'item_assignments')
+
+    def to_representation(self, instance):
+        d = super().to_representation(instance)
+        if not d['background']:
+            d['background'] = static_absolute(instance.event, "pretixpresale/pdf/ticket_default_a4.pdf")
+        return d
+
+    def validate(self, attrs):
+        if attrs.get('default') and self.context['event'].ticket_layouts.filter(default=True).exists:
+            raise ValidationError('You cannot have two layouts with default = True')
+        return attrs
+
+    def create(self, validated_data):
+        validated_data["event"] = self.context["event"]
+        return super().create(validated_data)
+
+
+class TicketLayoutViewSet(viewsets.ModelViewSet):
+    serializer_class = TicketLayoutSerializer
+    queryset = TicketLayout.objects.none()
+    lookup_field = 'id'
+
+    def get_queryset(self):
+        return self.request.event.ticket_layouts.all()
+
+    def get_serializer_context(self):
+        return {
+            **super().get_serializer_context(),
+            'event': self.request.event,
+        }
+
+    @transaction.atomic()
+    def perform_destroy(self, instance):
+        instance.log_action(
+            action='pretix.plugins.ticketoutputpdf.layout.deleted',
+            user=self.request.user, auth=self.request.auth
+        )
+        super().perform_destroy(instance)
+        if not self.request.event.ticket_layouts.filter(default=True).exists():
+            f = self.request.event.ticket_layouts.first()
+            if f:
+                f.default = True
+                f.save(update_fields=['default'])
+
+    @transaction.atomic()
+    def perform_create(self, serializer):
+        super().perform_create(serializer)
+        serializer.instance.log_action(
+            action='pretix.plugins.ticketoutputpdf.layout.added',
+            user=self.request.user,
+            auth=self.request.auth,
+            data=self.request.data,
+        )
+
+    @transaction.atomic()
+    def perform_update(self, serializer):
+        super().perform_update(serializer)
+        serializer.instance.log_action(
+            action='pretix.plugins.ticketoutputpdf.layout.changed',
+            user=self.request.user,
+            auth=self.request.auth,
+            data=self.request.data,
+        )
+
+
+class TicketLayoutItemViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = ItemAssignmentSerializer
+    queryset = TicketLayoutItem.objects.none()
+    lookup_field = 'id'
+
+    def get_queryset(self):
+        return TicketLayoutItem.objects.filter(item__event=self.request.event)
+
+    def get_serializer_context(self):
+        return {
+            **super().get_serializer_context(),
+            'event': self.request.event,
+        }
---
+++ packages/pretix/pretix-2024.9.0.tar.gz/pretix-2024.9.0/src/tests/plugins/ticketoutputpdf/test_api.py
@@ -0,0 +1,215 @@
+#
+# This file is part of pretix (Community Edition).
+#
+# Copyright (C) 2014-2020 Raphael Michel and contributors
+# Copyright (C) 2020-2021 rami.io GmbH and contributors
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+# Public License as published by the Free Software Foundation in version 3 of the License.
+#
+# ADDITIONAL TERMS APPLY: Pursuant to Section 7 of the GNU Affero General Public License, additional terms are
+# applicable granting you additional permissions and placing additional restrictions on your usage of this software.
+# Please refer to the pretix LICENSE file to obtain the full terms applicable to this work. If you did not receive
+# this file, see <https://pretix.eu/about/en/license>.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along with this program.  If not, see
+# <https://www.gnu.org/licenses/>.
+#
+import copy
+import json
+
+import pytest
+from django.core.files.base import ContentFile
+from django.utils.timezone import now
+from django_scopes import scopes_disabled
+from rest_framework.test import APIClient
+
+from pretix.base.models import Event, Item, Organizer, Team
+from pretix.plugins.ticketoutputpdf.models import TicketLayoutItem
+
+
+@pytest.fixture
+def env():
+    o = Organizer.objects.create(name='Dummy', slug='dummy')
+    event = Event.objects.create(
+        organizer=o, name='Dummy', slug='dummy',
+        date_from=now(), plugins='pretix.plugins.banktransfer'
+    )
+    t = Team.objects.create(organizer=event.organizer)
+    t.limit_events.add(event)
+    item1 = Item.objects.create(event=event, name="Ticket", default_price=23)
+    tl = event.ticket_layouts.create(name="Foo", default=True, layout='[{"a": 2}]')
+    TicketLayoutItem.objects.create(layout=tl, item=item1, sales_channel=o.sales_channels.get(identifier="web"))
+    return event, tl, item1
+
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+
+@pytest.fixture
+@scopes_disabled()
+def token_client(client, env):
+    t = env[0].organizer.teams.get().tokens.create(name="Foo")
+    client.credentials(HTTP_AUTHORIZATION="Token " + t.token)
+    return client
+
+
+RES_LAYOUT = {
+    'id': 1,
+    'name': 'Foo',
+    'default': True,
+    'item_assignments': [{'item': 1, 'sales_channel': 'web'}],
+    'layout': [{'a': 2}],
+    'background': 'http://example.com/static/pretixpresale/pdf/ticket_default_a4.pdf'
+}
+
+
+@pytest.mark.django_db
+def test_api_list(env, token_client):
+    res = copy.copy(RES_LAYOUT)
+    res['id'] = env[1].pk
+    res['item_assignments'][0]['item'] = env[2].pk
+    r = token_client.get('/api/v1/organizers/{}/events/{}/ticketlayouts/'.format(
+        env[0].organizer.slug, env[0].slug)).data
+
+    assert r['results'] == [res]
+    r = token_client.get('/api/v1/organizers/{}/events/{}/ticketlayoutitems/'.format(
+        env[0].organizer.slug, env[0].slug)).data
+    assert r['results'] == [{'item': env[2].pk, 'layout': env[1].pk, 'id': env[1].item_assignments.first().pk,
+                             'sales_channel': 'web'}]
+
+
+@pytest.mark.django_db
+def test_api_detail(env, token_client):
+    res = copy.copy(RES_LAYOUT)
+    res['id'] = env[1].pk
+    res['item_assignments'][0]['item'] = env[2].pk
+    r = token_client.get('/api/v1/organizers/{}/events/{}/ticketlayouts/{}/'.format(
+        env[0].organizer.slug, env[0].slug, env[1].pk)).data
+    assert r == res
+
+
+@pytest.mark.django_db
+def test_api_create(env, token_client):
+    r = token_client.post(
+        '/api/v1/upload',
+        data={
+            'media_type': 'application/pdf',
+            'file': ContentFile('file.pdf', 'invalid pdf content')
+        },
+        format='upload',
+        HTTP_CONTENT_DISPOSITION='attachment; filename="file.pdf"',
+    )
+    assert r.status_code == 201
+    file_id_png = r.data['id']
+
+    resp = token_client.post(
+        '/api/v1/organizers/{}/events/{}/ticketlayouts/'.format(env[0].slug, env[0].slug),
+        {
+            'name': 'Foo',
+            'default': False,
+            "background": file_id_png,
+            'layout': [],
+        },
+        format='json'
+    )
+    assert resp.status_code == 201
+    tl = env[0].ticket_layouts.get(pk=resp.data["id"])
+    assert tl.background
+
+
+@pytest.mark.django_db
+def test_api_create_validate_default(env, token_client):
+    resp = token_client.post(
+        '/api/v1/organizers/{}/events/{}/ticketlayouts/'.format(env[0].slug, env[0].slug),
+        {
+            'name': 'Foo',
+            'default': True,
+            'layout': [],
+        },
+        format='json'
+    )
+    assert resp.status_code == 400
+    assert resp.data == {"non_field_errors": ["You cannot have two layouts with default = True"]}
+
+
+@pytest.mark.django_db
+def test_api_create_validate_layout(env, token_client):
+    resp = token_client.post(
+        '/api/v1/organizers/{}/events/{}/ticketlayouts/'.format(env[0].slug, env[0].slug),
+        {
+            'name': 'Foo',
+            'default': True,
+            'layout': [
+                {
+                    "foo": "bar"
+                }
+            ],
+        },
+        format='json'
+    )
+    assert resp.status_code == 400
+    assert resp.data["layout"][0].startswith("Your layout file is not a valid layout. Error message:")
+
+
+@pytest.mark.django_db
+def test_api_update(env, token_client):
+    r = token_client.post(
+        '/api/v1/upload',
+        data={
+            'media_type': 'application/pdf',
+            'file': ContentFile('file.pdf', 'invalid pdf content')
+        },
+        format='upload',
+        HTTP_CONTENT_DISPOSITION='attachment; filename="file.pdf"',
+    )
+    assert r.status_code == 201
+    file_id_png = r.data['id']
+
+    resp = token_client.patch(
+        '/api/v1/organizers/{}/events/{}/ticketlayouts/{}/'.format(env[0].slug, env[0].slug, env[1].pk),
+        {
+            "name": "Bar",
+            "background": file_id_png,
+            "layout": [
+                {"type": "barcodearea", "left": "7.00", "bottom": "11.15", "size": "45.00", "content": "secret"}
+            ]
+        },
+        format='json'
+    )
+    assert resp.status_code == 200
+    env[1].refresh_from_db()
+    assert env[1].name == "Bar"
+    assert env[1].background
+    assert json.loads(env[1].layout) == [
+        {"type": "barcodearea", "left": "7.00", "bottom": "11.15", "size": "45.00", "content": "secret"}
+    ]
+
+
+@pytest.mark.django_db
+def test_api_update_validate_default(env, token_client):
+    tl2 = env[0].ticket_layouts.create(name="Foo", default=False, layout='[{"a": 2}]')
+    resp = token_client.patch(
+        '/api/v1/organizers/{}/events/{}/ticketlayouts/{}/'.format(env[0].slug, env[0].slug, tl2.pk),
+        {
+            "default": True,
+        },
+        format='json'
+    )
+    assert resp.status_code == 400
+    assert resp.data == {"non_field_errors": ["You cannot have two layouts with default = True"]}
+
+
+@pytest.mark.django_db
+def test_api_delete(env, token_client):
+    resp = token_client.delete(
+        '/api/v1/organizers/{}/events/{}/ticketlayouts/{}/'.format(env[0].slug, env[0].slug, env[1].pk),
+    )
+    assert resp.status_code == 204
+    assert not env[0].ticket_layouts.exists()
