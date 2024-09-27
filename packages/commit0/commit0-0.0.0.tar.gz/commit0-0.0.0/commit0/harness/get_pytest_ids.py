58b355cc058e85564bf8323db1765f59d87c54d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 05:05:29 +0000

    Add package commit0/0.0.0/commit0-0.0.0.tar.gz

---
+++ packages/commit0/commit0-0.0.0.tar.gz/commit0-0.0.0/commit0/harness/get_pytest_ids.py
@@ -0,0 +1,16 @@
+import bz2
+from typing import List
+
+
+def main(repo: str, verbose: int) -> List[str]:
+    repo = repo.lower()
+    repo = repo.replace(".", "-")
+    with bz2.open(f"commit0/data/test_ids/{repo}.bz2", "rt") as f:
+        out = f.read()
+    if verbose:
+        print(out)
+    out = out.split("\n")
+    return out
+
+
+__all__ = []
