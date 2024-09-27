6ddfdc89441854544b1a20792c2fb405f1e76334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 15:05:28 +0000

    Add package diffs/0.2.0/diffs-0.2.0-py3-none-any.whl

---
+++ packages/diffs/diffs-0.2.0-py3-none-any.whl/diffs/main.py
@@ -0,0 +1,14 @@
+from nuclear import CliBuilder, arguments, parameter, flag
+
+from .diffs import show_diff
+from .version import __version__
+
+
+def main():
+    CliBuilder('diffs', run=show_diff, help_on_empty=True, version=__version__,
+               help='Command output changes monitor').has(
+        parameter('interval', type=float, default=1, help='interval in seconds between consecutive executions'),
+        flag('clear', help='clear terminal scrollback'),
+        flag('continuous', 'watch', help='compare in continuous mode by watching only for changes'),
+        arguments('cmd', joined_with=' ', help='commmand to be invoked and compare its results'),
+    ).run()
