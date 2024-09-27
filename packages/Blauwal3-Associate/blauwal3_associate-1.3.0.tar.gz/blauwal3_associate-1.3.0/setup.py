443253e1577163085445625902d84aa2d9934f5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 15:05:23 +0000

    Add package Blauwal3-Associate/1.3.0/blauwal3_associate-1.3.0.tar.gz

---
+++ packages/Blauwal3-Associate/blauwal3_associate-1.3.0.tar.gz/blauwal3_associate-1.3.0/setup.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from distutils.extension import Extension
+from os import path
+
+VERSION = '1.3.0'
+
+ENTRY_POINTS = {
+    'orange3.addon': (
+        'associate = orangecontrib.associate',
+    ),
+    # Entry point used to specify packages containing tutorials accessible
+    # from welcome screen. Tutorials are saved Orange Workflows (.ows files).
+    'orange.widgets.tutorials': (
+        # Syntax: any_text = path.to.package.containing.tutorials
+    ),
+
+    # Entry point used to specify packages containing widgets.
+    'orange.widgets': (
+        # Syntax: category name = path.to.package.containing.widgets
+        # Widget category specification can be seen in
+        #    orangecontrib/datafusion/widgets/__init__.py
+        'Associate = orangecontrib.associate.widgets',
+    ),
+
+    # Register widget help
+    "orange.canvas.help": (
+        'html-index = orangecontrib.associate.widgets:WIDGET_HELP_PATH',),
+}
+
+
+def do_setup(ext_modules):
+    setup(
+        name="Blauwal3-Associate",
+        description="用于挖掘频繁项集和关联规则的蓝鲸插件",
+        long_description=open(path.join(path.dirname(__file__), 'README.pypi')).read(),
+        long_description_content_type='text/markdown',
+        version=VERSION,
+        author='大圣实验楼',
+        author_email='dashenglab@163.com',
+        url='https://github.com/biolab/orange3-associate',
+        keywords=(
+            'frequent itemset mining',
+            'frequent pattern mining',
+            'association rules',
+            'apriori',
+            'fp-growth',
+            'frequent patterns',
+            'FIM', 'FPM',
+            'orange3 add-on',
+        ),
+        packages=find_packages(),
+        package_data={
+            "orangecontrib.associate.widgets": ["icons/*.svg"],
+            "orangecontrib.associate": ["*.pyx", "locale/*.yml"],
+        },
+        entry_points=ENTRY_POINTS,
+        install_requires=[
+            'numpy',
+            'scipy',
+            'Blauwal3>=3.33.0'
+        ],
+        extras_require={
+            'test': ['pytest', 'coverage'],
+            'doc': ['sphinx', 'recommonmark', 'sphinx_rtd_theme'],
+        },
+        namespace_packages=['orangecontrib'],
+        ext_modules=ext_modules,
+        classifiers=[
+            'Programming Language :: Python',
+            'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+            'Operating System :: OS Independent',
+            'Topic :: Scientific/Engineering :: Artificial Intelligence',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Intended Audience :: Education',
+            'Intended Audience :: Science/Research',
+            'Intended Audience :: Developers',
+        ],
+        zip_safe=False,
+        include_package_data=True,
+        python_requires='>=3.9'
+    )
+
+
+if __name__ == '__main__':
+    ext_modules = [
+        Extension("orangecontrib.associate._fpgrowth",
+                  sources=[path.sep.join(("orangecontrib", "associate", "_fpgrowth.cpp"))],
+                  extra_compile_args=["-std=c++11", "-O3"],
+                  language="c++", )
+    ]
+    try:
+        do_setup(ext_modules)
+    except:  # fails if no compiler present, e.g. on WinDOS
+        import sys
+
+        print('WARNING: Falling back to NOT compiling extension modules. '
+              'Performance should suffer. Enjoy.',
+              file=sys.stderr)
+        do_setup([])
