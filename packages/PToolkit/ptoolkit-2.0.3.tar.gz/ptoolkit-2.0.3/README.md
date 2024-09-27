50de1ef0064ab6e041d50270b6b610c6dd28f76c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 04:05:26 +0000

    Add package PToolkit/2.0.3/ptoolkit-2.0.3.tar.gz

---
+++ packages/PToolkit/ptoolkit-2.0.3.tar.gz/ptoolkit-2.0.3/README.md
@@ -0,0 +1,15 @@
+# PToolkit
+ 
+# Description
+Python module for creating control software for setups. 
+
+# Install
+```
+pip install PToolkit
+```
+
+# Documentation
+https://ptoolkit.readthedocs.io/en/latest/index.html
+
+
+
---
+++ packages/PToolkit/ptoolkit-2.0.3.tar.gz/ptoolkit-2.0.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+version = "2.0.3"
+import pathlib
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="PToolkit",
+    version=version,
+    description="A set of tools to make working in a lab easier.",
+    long_description_content_type='text/markdown',
+    long_description="README",
+    url="https://github.com/JDVHA/PToolkit",
+    author="H.A.J de Vries",
+    author_email="",
+    license="MIT",
+    include_package_data = True,
+    package_data={'assets' :['PToolkit/LabControl/assets/*.png']},
+    download_url="https://github.com/JDVHA/PToolkit/archive/refs/tags/1.0.tar.gz",
+    install_requires=[
+          'numpy',
+          "matplotlib",
+          "sympy",
+          "scipy",
+          "pyserial"
+      ],
+    entry_points = {
+        'console_scripts': [
+            'PToolkit = PToolkit.cmd:main'
+        ]
+    },
+)
+
