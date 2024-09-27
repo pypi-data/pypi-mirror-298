e993bebcd84cb037f02b52787a9e4f45f2ad618f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 04:05:27 +0000

    Add package gabriel-server/3.0.1/gabriel_server-3.0.1-py3-none-any.whl

---
+++ packages/gabriel-server/gabriel_server-3.0.1-py3-none-any.whl/gabriel_server/local_engine.py
@@ -0,0 +1,73 @@
+import asyncio
+import logging
+import multiprocessing
+import os
+from gabriel_protocol import gabriel_pb2
+from gabriel_server.websocket_server import WebsocketServer
+from gabriel_server.zeromq_server import ZeroMQServer
+
+
+logger = logging.getLogger(__name__)
+
+
+def run(engine_factory, source_name, input_queue_maxsize, port, num_tokens,
+        message_max_size=None, use_zeromq=False):
+    engine_conn, server_conn = multiprocessing.Pipe()
+
+    local_server = _LocalServer(num_tokens, input_queue_maxsize, server_conn, source_name, use_zeromq)
+
+    engine_process = multiprocessing.Process(
+        target=_run_engine, args=(engine_factory, engine_conn))
+    engine_process.start()
+    local_server.launch(port, message_max_size)
+
+    raise Exception('Server stopped')
+
+class _LocalServer:
+    def __init__(self, num_tokens_per_source, input_queue_maxsize, conn, source_name, use_zeromq):
+        self._input_queue = asyncio.Queue(input_queue_maxsize)
+        self._conn = conn
+        self._result_ready = asyncio.Event()
+        self._server = (
+            ZeroMQServer if use_zeromq else WebsocketServer)(num_tokens_per_source, self._send_to_engine)
+        self._server.add_source_consumed(source_name)
+
+    async def _send_to_engine(self, from_client, address):
+        if self._input_queue.full():
+            return False
+
+        self._input_queue.put_nowait((from_client, address))
+        return True
+
+    def launch(self, port, message_max_size):
+        asyncio.get_event_loop().add_reader(
+            self._conn.fileno(), self._result_ready.set)
+        asyncio.ensure_future(self._engine_comm())
+        self._server.launch(port, message_max_size)
+
+    async def _engine_comm(self):
+        await self._server.wait_for_start()
+        while self._server.is_running():
+            from_client, address = await self._input_queue.get()
+            self._conn.send_bytes(from_client.input_frame.SerializeToString())
+            result_wrapper = gabriel_pb2.ResultWrapper()
+
+            while not self._conn.poll():
+                await self._result_ready.wait()
+                self._result_ready.clear()
+
+            result_wrapper.ParseFromString(self._conn.recv_bytes())
+            await self._server.send_result_wrapper(
+                address, from_client.source_name, from_client.frame_id,
+                result_wrapper, return_token=True)
+
+
+def _run_engine(engine_factory, conn):
+    engine = engine_factory()
+    logger.info('Cognitive engine started')
+    while True:
+        input_frame = gabriel_pb2.InputFrame()
+        input_frame.ParseFromString(conn.recv_bytes())
+
+        result_wrapper = engine.handle(input_frame)
+        conn.send_bytes(result_wrapper.SerializeToString())
---
+++ packages/gabriel-server/gabriel_server-3.0.1-py3-none-any.whl/gabriel_server/websocket_server.py
@@ -0,0 +1,111 @@
+import asyncio
+import logging
+import socket
+from gabriel_protocol import gabriel_pb2
+from gabriel_protocol.gabriel_pb2 import ResultWrapper
+from gabriel_server.gabriel_server import GabrielServer
+import websockets
+import websockets.server
+
+logger = logging.getLogger(__name__)
+
+# It isn't necessary to do this as of Python 3.6 because
+# "The socket option TCP_NODELAY is set by default for all TCP connections"
+# per https://docs.python.org/3/library/asyncio-eventloop.html
+# However, this seems worth keeping in case the default behavior changes.
+class NoDelayProtocol(websockets.server.WebSocketServerProtocol):
+    def connection_made(self, transport: asyncio.BaseTransport) -> None:
+        super().connection_made(transport)
+        sock = transport.get_extra_info('socket')
+        sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+
+
+class WebsocketServer(GabrielServer):
+    def __init__(self, num_tokens_per_source, engine_cb):
+        super().__init__(num_tokens_per_source, engine_cb)
+        self._server = None
+
+    def launch(self, port, message_max_size):
+        event_loop = asyncio.get_event_loop()
+        start_server = websockets.serve(
+            self._handler, port=port, max_size=message_max_size,
+            create_protocol=NoDelayProtocol)
+        self._server = event_loop.run_until_complete(start_server)
+
+        # It isn't necessary to set TCP_NODELAY on self._server.sockets because
+        # these are used for listening and not writing.
+
+        self._start_event.set()
+        event_loop.run_forever()
+
+    async def _send_via_transport(self, address, payload):
+        logger.debug('Sending to address: %s', address)
+        try:
+            await self._clients.get(address).websocket.send(payload)
+        except websockets.exceptions.ConnectionClosed:
+            logger.info('No connection to address: %s', address)
+            return False
+
+        return True
+
+    def is_running(self):
+        if self._server is None:
+            return False
+
+        return self._server.is_serving()
+
+    async def _handler(self, websocket, _):
+
+        # We don't waste time checking TCP_NODELAY in production.
+        # Note that websocket.transport is an undocumented property.
+        # sock = websocket.transport.get_extra_info('socket')
+        # assert(sock.getsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY) == 1)
+
+        address = websocket.remote_address
+        logger.info('New Client connected: %s', address)
+
+        client = self._Client(
+            tokens_for_source={source_name: self._num_tokens_per_source
+                               for source_name in self._sources_consumed},
+            inputs=None,
+            task=None,
+            websocket=websocket)
+        self._clients[address] = client
+
+        # Send client welcome message
+        to_client = gabriel_pb2.ToClient()
+        for source_name in self._sources_consumed:
+            to_client.welcome.sources_consumed.append(source_name)
+        to_client.welcome.num_tokens_per_source = self._num_tokens_per_source
+        await websocket.send(to_client.SerializeToString())
+
+        try:
+            await self._consumer(websocket, client)
+        except websockets.exceptions.ConnectionClosed:
+            pass
+        finally:
+            del self._clients[address]
+            logger.info('Client disconnected: %s', address)
+
+    async def _consumer(self, websocket, client):
+        address = websocket.remote_address
+        async for raw_input in websocket:
+            logger.debug('Received input from %s', address)
+
+            from_client = gabriel_pb2.FromClient()
+            from_client.ParseFromString(raw_input)
+
+            status = await self._consumer_helper(client, address, from_client)
+            if status == ResultWrapper.Status.SUCCESS:
+                # Deduct a token when you get a new input from the client
+                client.tokens_for_source[from_client.source_name] -= 1
+                continue
+
+            # Send error message
+            to_client = gabriel_pb2.ToClient()
+            to_client.response.source_name = from_client.source_name
+            to_client.response.frame_id = from_client.frame_id
+            to_client.response.return_token = True
+            to_client.response.result_wrapper.status = status
+            await websocket.send(to_client.SerializeToString())
+
