6bb185127a2ef1fb16b3314032d6af2d252eca21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 06:05:49 +0000

    Add package weave/0.51.10/weave-0.51.10.tar.gz

---
+++ packages/weave/weave-0.51.10.tar.gz/weave-0.51.10/weave/frontend/assets/Component-CNnEahgh.js
@@ -0,0 +1,2 @@
+import{ah as i,u as o,R as e}from"./index-BU8Bet07.js";const n=l=>{const a=l.input,r=i({file:a}),t=o(r);return e.createElement("div",null,t.loading?e.createElement("div",null):e.createElement("img",{style:{maxWidth:"100%"},alt:"cool-alt",src:t.result}))};export{n as default};
+//# sourceMappingURL=Component-CNnEahgh.js.map
