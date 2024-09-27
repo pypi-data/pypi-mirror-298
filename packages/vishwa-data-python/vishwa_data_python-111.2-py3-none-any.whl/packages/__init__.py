bc5acf95b3880d25a3ae7dbc050da1e07f3e8946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 15:05:14 +0000

    Add package vishwa-data-python/111.2/vishwa_data_python-111.2-py3-none-any.whl

---
+++ packages/vishwa-data-python/vishwa_data_python-111.2-py3-none-any.whl/packages/__init__.py
@@ -0,0 +1,3 @@
+from packages.vishwa import add as a , sub as s, mul as m
+from packages.vishwa1 import mod as mm , div as d , exp as e
+from packages.vishwa3 import mass_prince as ms
---
+++ packages/vishwa-data-python/vishwa_data_python-111.2-py3-none-any.whl/packages/vishwa3.py
@@ -0,0 +1,2 @@
+def mass_prince():
+    print("Hi Weclome to the pushing and pulling the python packages")
