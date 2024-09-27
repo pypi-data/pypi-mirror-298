3b6259dcee6510d5a2c4f971b30300b9fa946488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 05:05:22 +0000

    Add package telegrinder/0.3.3/telegrinder-0.3.3-py3-none-any.whl

---
+++ packages/telegrinder/telegrinder-0.3.3-py3-none-any.whl/telegrinder/bot/rules/adapter/__init__.py
@@ -0,0 +1,14 @@
+from telegrinder.bot.rules.adapter.abc import ABCAdapter, Event
+from telegrinder.bot.rules.adapter.errors import AdapterError
+from telegrinder.bot.rules.adapter.event import EventAdapter
+from telegrinder.bot.rules.adapter.node import NodeAdapter
+from telegrinder.bot.rules.adapter.raw_update import RawUpdateAdapter
+
+__all__ = (
+    "ABCAdapter",
+    "AdapterError",
+    "Event",
+    "EventAdapter",
+    "NodeAdapter",
+    "RawUpdateAdapter",
+)
