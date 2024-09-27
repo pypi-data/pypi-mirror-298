c5286d16bb3af4c7b6b49fb19aa9acd49df194a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 16:05:04 +0000

    Add package multipers/2.1.0/multipers-2.1.0-cp311-cp311-macosx_13_0_x86_64.whl

---
+++ packages/multipers/multipers-2.1.0-cp311-cp311-macosx_13_0_x86_64.whl/multipers/_slicer_meta.py
@@ -0,0 +1,185 @@
+from copy import deepcopy
+from typing import Literal, Optional
+
+import numpy as np
+
+import multipers.io as mio
+import multipers.slicer as mps
+from multipers.simplex_tree_multi import is_simplextree_multi
+from multipers.slicer import _column_type, _valid_dtype
+
+
+## TODO : maybe optimize this with cython
+def _blocks2boundary_dimension_grades(
+    blocks,
+    filtration_type=np.float64,
+    num_parameters: int = -1,
+    inplace: bool = False,
+    is_kcritical: bool = False,
+):
+    """
+    Turns blocks, aka scc, into the input of non-simplicial slicers.
+    """
+    if num_parameters < 0:
+        for b in blocks:
+            if len(b[0]) > 0:
+                if is_kcritical:
+                    num_parameters = np.asarray(b[0][0]).shape[1]
+                else:
+                    num_parameters = np.asarray(b[0]).shape[1]
+                break
+        if num_parameters < 0:
+            raise ValueError("Empty Filtration")
+    rblocks = blocks if inplace else deepcopy(blocks)
+    rblocks.reverse()
+    block_sizes = [len(b[0]) for b in rblocks]
+    S = np.cumsum([0, 0] + block_sizes)
+    if is_kcritical:
+        multifiltration = tuple(
+            stuff
+            for b in rblocks
+            for stuff in (b[0] if len(b[0]) > 0 else [np.empty((0, num_parameters))])
+        )
+
+    else:
+        multifiltration = np.concatenate(
+            tuple(
+                b[0] if len(b[0]) > 0 else np.empty((0, num_parameters))
+                for b in rblocks
+            ),
+            dtype=filtration_type,
+        )
+    boundary = tuple(x + S[i] for i, b in enumerate(rblocks) for x in b[1])
+    dimensions = np.fromiter(
+        (i for i, b in enumerate(rblocks) for _ in range(len(b[0]))), dtype=int
+    )
+    return boundary, dimensions, multifiltration
+
+
+def _slicer_from_simplextree(st, backend, vineyard):
+    if vineyard:
+        if backend == "matrix":
+            slicer = mps._SlicerVineSimplicial(st)
+        elif backend == "clement":
+            raise ValueError("This one takes a minpres")
+        elif backend == "graph":
+            slicer = mps._SlicerVineGraph(st)
+        else:
+            raise ValueError(f"Inimplemented backend {backend}.")
+    else:
+        if backend == "matrix":
+            slicer = mps._SlicerNoVineSimplicial(st)
+        elif backend == "clement":
+            raise ValueError("Clement is Vineyard")
+        elif backend == "graph":
+            raise ValueError("Graph is Vineyard")
+        else:
+            raise ValueError(f"Inimplemented backend {backend}.")
+    return slicer
+
+
+def _slicer_from_blocks(
+    blocks,
+    backend,
+    vineyard: bool,
+    is_kcritical: bool,
+    dtype: type,
+    col: _column_type,
+):
+    boundary, dimensions, multifiltrations = _blocks2boundary_dimension_grades(
+        blocks,
+        inplace=False,
+        is_kcritical=is_kcritical,
+    )
+    if backend == "matrix":
+        slicer = mps.get_matrix_slicer(vineyard, is_kcritical, dtype, col)(
+            boundary, dimensions, multifiltrations
+        )
+    elif backend == "clement":
+        assert dtype == np.float32 and not is_kcritical and vineyard
+        slicer = mps._SlicerClement(boundary, dimensions, multifiltrations)
+    else:
+        raise ValueError(f"Inimplemented backend {backend}.")
+    return slicer
+
+
+def Slicer(
+    st,
+    backend: Literal["matrix", "clement", "graph"] = "matrix",
+    vineyard: bool = True,
+    reduce: bool = False,
+    reduce_backend: Optional[str] = None,
+    dtype: _valid_dtype = np.float64,
+    is_kcritical: bool = False,
+    column_type: _column_type = "INTRUSIVE_SET",
+    max_dim: Optional[int] = None,
+) -> mps.Slicer_type:
+    """
+    Given a simplextree or blocks (a.k.a scc for python),
+    returns a structure that can compute persistence on line (or more)
+    slices, eventually vineyard update, etc.
+
+    This can be used to compute interval-decomposable module approximations
+    or signed measures, using, e.g.
+     - `multipers.module_approximation(this, *args)`
+     - `multipers.signed_measure(this, *args)`
+
+    Note : it is recommended and sometime required to apply
+        a minimal presentation before computing these functions !
+    `mp.slicer.minimal_presentation(slicer, *args, **kwargs)`
+
+    Input
+    -----
+     - st : SimplexTreeMulti or scc-like blocks or path to scc file
+     - backend: slicer backend, e.g, "matrix", "clement", "graph"
+     - vineyard: vineyard capable (may slow down computations if true)
+    Output
+    ------
+    The corresponding slicer.
+    """
+    if mps.is_slicer(st):
+        max_dim_idx = (
+            None
+            if max_dim is None
+            else np.searchsorted(st.get_dimensions(), max_dim + 1)
+        )
+        slicer = mps.get_matrix_slicer(vineyard, is_kcritical, dtype, column_type)(
+            st.get_boundaries()[slice(None, max_dim_idx)],
+            st.get_dimensions()[slice(None, max_dim_idx)],
+            st.get_filtrations()[slice(None, max_dim_idx)],
+        )
+        if st.is_squeezed:
+            slicer.filtration_grid = st.filtration_grid
+        slicer.minpres_degree = st.minpres_degree
+    elif is_simplextree_multi(st) and backend == "graph":
+        slicer = _slicer_from_simplextree(st, backend, vineyard)
+        if st.is_squeezed:
+            slicer.filtration_grid = st.filtration_grid
+    elif backend == "graph":
+        raise ValueError(
+            """
+Graph is simplicial, incompatible with minpres.
+You can try using `multipers.slicer.to_simplextree`."""
+        )
+    else:
+        filtration_grid = None
+        if max_dim is not None:
+            st.prune_above_dimension(max_dim)
+        if is_simplextree_multi(st):
+            blocks = st._to_scc()
+            if st.is_squeezed:
+                filtration_grid = st.filtration_grid
+        elif isinstance(st, str):
+            blocks = mio.scc_parser(st)
+        else:
+            blocks = st
+        slicer = _slicer_from_blocks(
+            blocks, backend, vineyard, is_kcritical, dtype, column_type
+        )
+        if filtration_grid is not None:
+            slicer.filtration_grid = filtration_grid
+    if reduce:
+        slicer = mps.minimal_presentation(
+            slicer, backend=reduce_backend, slicer_backend=backend, vineyard=vineyard
+        )
+    return slicer
