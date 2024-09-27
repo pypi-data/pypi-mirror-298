82e98490648f437a5817e97f173f3f0df34f736e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 15:04:54 +0000

    Add package multifunctional/0.8/multifunctional-0.8.tar.gz

---
+++ packages/multifunctional/multifunctional-0.8.tar.gz/multifunctional-0.8/multifunctional/node_dispatch.py
@@ -0,0 +1,15 @@
+from bw2data.backends.proxies import Activity
+from bw2data.backends.schema import ActivityDataset
+
+from .node_classes import (
+    MaybeMultifunctionalProcess,
+    ReadOnlyProcessWithReferenceProduct,
+)
+
+
+def multifunctional_node_dispatcher(node_obj: ActivityDataset) -> Activity:
+    """Dispatch the correct node class depending on node_obj attributes."""
+    if node_obj.type == "readonly_process":
+        return ReadOnlyProcessWithReferenceProduct(document=node_obj)
+    else:
+        return MaybeMultifunctionalProcess(document=node_obj)
