190703d0af2ac8d6a3e9c0fd3230e740703fd215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 04:05:27 +0000

    Add package tsc-web-api/0.3/tsc_web_api-0.3.tar.gz

---
+++ packages/tsc-web-api/tsc_web_api-0.3.tar.gz/tsc_web_api-0.3/tsc_web_api/__init__.py
@@ -0,0 +1,12 @@
+from .utils import (
+    model_to_dict,
+    setup_logger,
+    get_model_info,
+    async_request,
+    sync_request,
+    get_log_config,
+)
+
+from .fastapi_utils import (
+    get_bearer_validator,
+)
