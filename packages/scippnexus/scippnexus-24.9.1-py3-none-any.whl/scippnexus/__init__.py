c18551bfc811d7eadfb25f2cf5c54aeeebbf92b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 03:09:51 +0000

    Add package scippnexus/24.9.1/scippnexus-24.9.1-py3-none-any.whl

---
+++ packages/scippnexus/scippnexus-24.9.1-py3-none-any.whl/scippnexus/__init__.py
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2024 Scipp contributors (https://github.com/scipp)
+# ruff: noqa: E402, F401, F403
+
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version(__package__ or __name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
+
+from . import typing
+from .base import (
+    Group,
+    NexusStructureError,
+    NXobject,
+    base_definitions,
+    create_class,
+    create_field,
+)
+from .field import Attrs, Field
+from .file import File
+from ._load import load
+from .nexus_classes import *
+from .nxtransformations import compute_positions, zip_pixel_offsets
