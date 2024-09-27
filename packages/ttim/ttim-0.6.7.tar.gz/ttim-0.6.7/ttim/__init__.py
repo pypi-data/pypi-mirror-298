56b649d045a5183cc4e11c7b5f0016c328e811c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 15:05:00 +0000

    Add package ttim/0.6.7/ttim-0.6.7.tar.gz

---
+++ packages/ttim/ttim-0.6.7.tar.gz/ttim-0.6.7/ttim/__init__.py
@@ -0,0 +1,27 @@
+"""Copyright (C), 2017, Mark Bakker.
+
+TTim is a computer program for the simulation of transient multi-layer flow with
+analytic elements and consists of a library of Python scripts and FORTRAN extensions.
+
+Mark Bakker, Delft University of Technology mark dot bakker at tudelft dot nl.
+"""
+
+# ruff : noqa: F401
+__name__ = "ttim"
+__author__ = "Mark Bakker"
+from .circareasink import CircAreaSink
+from .fit import Calibrate
+from .linedoublet import LeakyLineDoublet, LeakyLineDoubletString
+from .linesink import (
+    HeadLineSink,
+    HeadLineSinkHo,
+    HeadLineSinkString,
+    LineSink,
+    LineSinkDitchString,
+)
+
+# Import all classes and functions
+from .model import Model3D, ModelMaq
+from .trace import timtrace, timtraceline
+from .version import __version__
+from .well import DischargeWell, HeadWell, Well, WellTest
