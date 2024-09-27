87b81aa971bb18c21cdfa1039014e4f4d447240e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 14:06:08 +0000

    Add package kskimtestmodule3/0.0.2/kskimtestmodule3-0.0.2-py3-none-any.whl

---
+++ packages/kskimtestmodule3/kskimtestmodule3-0.0.2-py3-none-any.whl/kskimtestmodule/testfunc.py
@@ -0,0 +1,9 @@
+from pyspark.sql import SparkSession
+
+def test_func2():
+    print("test_func2 start")
+
+    spark = SparkSession.builder.appName("Testing PySpark Example").getOrCreate()
+    spark.stop()
+
+    print("test_func2 end")
