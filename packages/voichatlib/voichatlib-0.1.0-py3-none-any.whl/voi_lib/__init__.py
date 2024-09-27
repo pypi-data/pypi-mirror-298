d9364f1bf2a7f68a7adc4836562fd397e44cc12e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 14:05:50 +0000

    Add package voichatlib/0.1.0/voichatlib-0.1.0-py3-none-any.whl

---
+++ packages/voichatlib/voichatlib-0.1.0-py3-none-any.whl/voi_lib/__init__.py
@@ -0,0 +1,5 @@
+# Import the main function and classes from chatbot.py
+from .chatbot import create_voi_chatbot
+
+# Expose the create_voi_chatbot function at the top level of the package
+__all__ = ['create_voi_chatbot']
