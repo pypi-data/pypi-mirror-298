c0258a87d2677959e64b828bfb7d22182b3618e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 04:05:09 +0000

    Add package pyprettifier/0.1.5/pyprettifier-0.1.5.tar.gz

---
+++ packages/pyprettifier/pyprettifier-0.1.5.tar.gz/pyprettifier-0.1.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from setuptools.command.install import install
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+setup(
+    name="pyprettifier",
+    version="0.1.5",
+    packages=["pyprettifier"],
+    description="A simple Python utility to improve python output from simple string.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',  
+    author="Sandra Gutierrez",
+    author_email="help@pyprettifier.com",
+    install_requires=[
+        "requests"
+    ],
+    python_requires='>=3.6'
+)
