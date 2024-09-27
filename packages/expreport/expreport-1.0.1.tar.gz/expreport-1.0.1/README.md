8e86a695f7a70ade54f88595aa14d740a0f1d633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 15:05:22 +0000

    Add package expreport/1.0.1/expreport-1.0.1.tar.gz

---
+++ packages/expreport/expreport-1.0.1.tar.gz/expreport-1.0.1/README.md
@@ -0,0 +1,7 @@
+#EXPREPORT
+
+## cli example
+
+expreport --data=/pathofdata/data/2.csv --out=/pathofgenerateddata/data/generate
+
+
---
+++ packages/expreport/expreport-1.0.1.tar.gz/expreport-1.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages,setup
+
+with open('requirements.txt','r',encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="expreport",
+    version="1.0.1",
+    author="Aymen Jemi",
+    author_email="jemiaymen@gmail.com",
+    description="export data from csv to documents word",
+    licence="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    python_requires=">=3.9",
+    entry_points={
+        "console_scripts": [
+            "expreport=expreport:cli"
+        ]
+    },
+)
