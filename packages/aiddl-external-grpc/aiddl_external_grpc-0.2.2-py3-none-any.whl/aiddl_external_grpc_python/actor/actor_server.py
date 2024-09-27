78c592ef453b4473d1c5aa6a77ee4766d2ca4181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 04:05:26 +0000

    Add package aiddl-external-grpc/0.2.2/aiddl_external_grpc-0.2.2-py3-none-any.whl

---
+++ packages/aiddl-external-grpc/aiddl_external_grpc-0.2.2-py3-none-any.whl/aiddl_external_grpc_python/actor/actor_server.py
@@ -0,0 +1,39 @@
+from concurrent import futures
+from abc import ABC, abstractmethod
+
+import grpc
+
+import aiddl_external_grpc_python.generated.actor_pb2_grpc as actor_pb2_grpc
+
+
+class ActorServer(ABC, actor_pb2_grpc.ActorServicer):
+    def __init__(self, port):
+        self.server = None
+        self.port = port
+        self.next_id = 0
+
+    @abstractmethod
+    def IsSupported(self, request, context):
+        pass
+
+    @abstractmethod
+    def Dispatch(self, request, context):
+        pass
+
+    @abstractmethod
+    def GetStatus(self, request, context):
+        pass
+
+    @abstractmethod
+    def Cancel(self, request, context):
+        pass
+
+    def start(self):
+        self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        actor_pb2_grpc.add_ActorServicer_to_server(
+            self, self.server)
+        self.server.add_insecure_port(f'0.0.0.0:{self.port}')
+        self.server.start()
+
+    def wait_for_termination(self):
+        self.server.wait_for_termination()
