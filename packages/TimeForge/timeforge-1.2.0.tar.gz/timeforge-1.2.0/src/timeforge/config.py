b4cadeb3155606aa3ddc122e50e3aef9bfead80c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 16:05:13 +0000

    Add package TimeForge/1.2.0/timeforge-1.2.0.tar.gz

---
+++ packages/TimeForge/timeforge-1.2.0.tar.gz/timeforge-1.2.0/src/timeforge/config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from typing import Final
+
+MILOG_FORM_URL: Final = r"https://www.pse.kit.edu/downloads/Formulare/KIT%20Arbeitszeitdokumentation%20MiLoG.pdf"
+FEDERAL_STATE: Final = "BW" # short notation for Baden-Württemberg
+
+# define the working time between 08:00 and 20:00
+START_WORKING: 8
+STOP_WORKING: 20
