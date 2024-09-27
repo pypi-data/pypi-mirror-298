7e727b314a0819d16d06fff58fec45c9d32aeaed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 07:04:20 +0000

    Add package lifebgp/0.1.0/lifebgp-0.1.0-py3-none-any.whl

---
+++ packages/lifebgp/lifebgp-0.1.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about a_game_of_life."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Wenjie Cheng"
+__email__ = "w.cheng-1@student.utwente.nl"
+__version__ = "0.1.0"
