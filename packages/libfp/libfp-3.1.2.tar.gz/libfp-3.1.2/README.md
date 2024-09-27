d45c31d93c6c9de37e8735c6eff6ed269cd46f75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 06:05:35 +0000

    Add package libfp/3.1.2/libfp-3.1.2.tar.gz

---
+++ packages/libfp/libfp-3.1.2.tar.gz/libfp-3.1.2/README.md
@@ -0,0 +1,51 @@
+# libfp
+
+libfp is a library for calculating crystalline fingerprints and measuring similarities of materials (e.g., crystals, clusters, and molecules). The library is written in C with a Python interface.
+
+
+## Installation
+
+### Prerequisites
+
+Before installing libfp, ensure you have the following:
+
+- Python header files (python-dev)
+- C compiler (e.g., gcc, clang)
+- NumPy
+- OpenBLAS, LAPACK, or MKL
+
+### Installation Steps
+
+1. Clone the repository:
+   ```
+   git clone https://github.com/Rutgers-ZRG/libfp.git
+   cd libfp
+   ```
+
+2. Install using pip:
+   ```
+   pip install .
+   ```
+
+## Usage
+
+To use libfp in your Python project:
+
+```python
+import libfp
+
+# Your code here
+```
+
+## Examples
+
+Example
+Examples are found in `examples` directory.
+
+## Algorithm
+
+The detailed algorithm of libfp is described in the following paper:
+
+- "[A fingerprint based metric for measuring similarities of crystalline structures](http://scitation.aip.org/content/aip/journal/jcp/144/3/10.1063/1.4940026)",
+  Li Zhu, Maximilian Amsler, Tobias Fuhrer, Bastian Schaefer, Somayeh Fareji, Alireza Ghasemi, Migle Grauzinyte, Chris Wolverton, and Stefan Goedecker
+  **J. Chem. Phys. 144**, 034203 (2016)
---
+++ packages/libfp/libfp-3.1.2.tar.gz/libfp-3.1.2/setup.py
@@ -0,0 +1,69 @@
+import os
+import sys
+import sysconfig
+from setuptools import setup, Extension, find_packages
+from setuptools.command.build_ext import build_ext
+import numpy
+from ctypes.util import find_library
+
+class CustomBuildExtCommand(build_ext):
+    def run(self):
+        self.include_dirs.append(numpy.get_include())
+        build_ext.run(self)
+
+def find_lapack():
+    lapack_libs = ['openblas', 'lapack', 'mkl_rt']
+    for lib in lapack_libs:
+        if find_library(lib):
+            return [lib]
+    
+    print("Warning: No LAPACK library found. Trying to build without explicit LAPACK linkage.")
+    return []
+
+# Workaround Python issue 21121
+config_var = sysconfig.get_config_var("CFLAGS")
+if (config_var is not None and
+    "-Werror=declaration-after-statement" in config_var):
+    os.environ['CFLAGS'] = config_var.replace(
+        "-Werror=declaration-after-statement", "")
+
+sources = ['libfp.c', 'fingerprint.c', 'rcov.c', 'apc.c']
+source_dir = "src" if os.path.exists('src') else "../src"
+include_dirs = [source_dir, numpy.get_include()]
+sources = [f"{source_dir}/{s}" for s in sources]
+
+# Detect LAPACK library
+lapack_lib = find_lapack()
+
+# Try to find LAPACK library directory
+lapack_dir = []
+if sys.platform == "darwin":
+    lapack_dir = ['/usr/local/opt/openblas/lib', '/usr/local/opt/lapack/lib']
+elif sys.platform.startswith("linux"):
+    lapack_dir = ['/usr/lib', '/usr/local/lib']
+
+extra_link_args = []
+if sys.platform == "darwin":
+    extra_link_args = ['-Wl,-rpath,/usr/local/opt/openblas/lib', '-Wl,-rpath,/usr/local/opt/lapack/lib']
+elif sys.platform.startswith("linux"):
+    extra_link_args = ['-Wl,-rpath,/usr/lib', '-Wl,-rpath,/usr/local/lib']
+
+extension = Extension('libfp._libfp',
+                      include_dirs=include_dirs,
+                      library_dirs=lapack_dir,
+                      libraries=lapack_lib,
+                      sources=['fppy/_libfp.c'] + sources,
+                      extra_compile_args=[],
+                      extra_link_args=extra_link_args,
+                      define_macros=[])
+
+setup(
+    packages=find_packages(where='fppy'),
+    package_dir={'': 'fppy'},
+    ext_modules=[extension],
+    cmdclass={'build_ext': CustomBuildExtCommand},
+)
+
+if not lapack_lib:
+    print("No LAPACK library was found. The installation might fail or the library might not work correctly.")
+    print("Please ensure you have LAPACK, OpenBLAS, or MKL installed on your system.")
