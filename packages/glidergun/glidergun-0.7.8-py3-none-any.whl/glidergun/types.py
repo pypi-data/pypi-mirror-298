e8aa242ea2592d45e3445e21ff2d6cf8dc3eeebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 15:05:01 +0000

    Add package glidergun/0.7.8/glidergun-0.7.8-py3-none-any.whl

---
+++ packages/glidergun/glidergun-0.7.8-py3-none-any.whl/glidergun/types.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+from glidergun._grid import Grid
+from glidergun._literals import BaseMap, ColorMap, DataType, ResamplingMethod
+from glidergun._mosaic import Mosaic
+from glidergun._stack import Stack
+from glidergun._types import CellSize, Extent, Point
