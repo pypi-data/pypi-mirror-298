43e21033d339e1e41d57f97c2ce1cfc116431e86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 03:09:30 +0000

    Add package extracTR/0.2.2/extracTR-0.2.2.tar.gz

---
+++ packages/extracTR/extracTR-0.2.2.tar.gz/extracTR-0.2.2/setup.py
@@ -0,0 +1,45 @@
+import re
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+version = "0.2.2"
+
+setup(
+    name="extracTR",
+    version=version,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    package_data={"": ["README.md"]},
+    python_requires=">=3.6",
+    include_package_data=True,
+    scripts=[],
+    license="BSD",
+    url="https://github.com/aglabx/extracTR",
+    author="Aleksey Komissarov",
+    author_email="ad3002@gmail.com",
+    description="Extract and analyze satellite DNA from raw sequences.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "argparse",
+        "networkx",
+        "tqdm",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    entry_points={
+        'console_scripts': [
+            'extracTR = extractr.extractr:run_it',
+        ],
+    },
+)
