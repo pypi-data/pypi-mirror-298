b3425825bd685b62c98e7a01ad5bf953fa8a8229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 03:09:15 +0000

    Add package confluent-kafka-helpers/1.0.0.dev0/confluent_kafka_helpers-1.0.0.dev0.tar.gz

---
+++ packages/confluent-kafka-helpers/confluent_kafka_helpers-1.0.0.dev0.tar.gz/confluent_kafka_helpers-1.0.0.dev0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="confluent-kafka-helpers",
+    version="1.0.0.dev0",
+    description="Helpers for Confluent's Kafka Python client",
+    url="https://github.com/fyndiq/confluent_kafka_helpers",
+    author="Fyndiq AB",
+    author_email="support@fyndiq.com",
+    license="MIT",
+    packages=find_packages(),
+    setup_requires=['wheel'],
+    install_requires=[
+        'structlog>=17.2.0',
+        'confluent-kafka[avro,schema-registry]==2.*',
+        'opentelemetry-api==1.27.0',
+        'opentelemetry-semantic-conventions==0.48b0',  # 1.27.0
+    ],
+    zip_safe=False,
+)
