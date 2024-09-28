2f3ab345862fcf99acdf4d6406a6b67887ff40cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 14:05:43 +0000

    Add package espn-hidden-api/0.0.5/espn_hidden_api-0.0.5.tar.gz

---
+++ packages/espn-hidden-api/espn_hidden_api-0.0.5.tar.gz/espn_hidden_api-0.0.5/README.md
@@ -0,0 +1,10 @@
+import with "import espn_hidden_api"
+
+current functions:
+
+tester1() #returns "tester1"
+tester2() #returns "tester2"
+
+Under construction, nt ready for use 
+
+Developed by Justin He
---
+++ packages/espn-hidden-api/espn_hidden_api-0.0.5.tar.gz/espn_hidden_api-0.0.5/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.5'
+DESCRIPTION = 'Retrieve ESPN hidden api endpoints in a cleaned format'
+LONG_DESCRIPTION = 'Retrieve ESPN hidden api endpoints in a cleaned format'
+
+# Setting up
+setup(
+    name="espn_hidden_api",
+    version=VERSION,
+    author="peepei",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=['espn_hidden_api'],
+    install_requires=['pandas','requests'],
+    keywords=['python', 'api','espn'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
