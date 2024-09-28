365608a062a4d1efcedeecf5ef07be250c4753b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 15:05:25 +0000

    Add package python-polaris/0.1.0/python_polaris-0.1.0.tar.gz

---
+++ packages/python-polaris/python_polaris-0.1.0.tar.gz/python_polaris-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="python-polaris",
+    version="0.1.0",
+    packages=find_packages(where="polaris"),
+    install_requires=[
+        # Add any dependencies here, e.g., requests
+    ],
+)
