90b018e0e396663975f46b224297ad04864c8e2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 04:05:41 +0000

    Add package mcnparse/0.4.1/mcnparse-0.4.1.tar.gz

---
+++ packages/mcnparse/mcnparse-0.4.1.tar.gz/mcnparse-0.4.1/mcnparse/__init__.py
@@ -0,0 +1,11 @@
+"""MCNParse
+
+Package for parsing MCNP output files and saving data to JSON format.
+
+Data can be either compressed or uncompressed. Currently unable to parse tallies
+that have more than one bin technique (or tallies that contain multiple
+surfaces, cells, or detector points) or tallies which are unbinned.
+
+"""
+from .core import mcnparse
+__all__=["mcnparse"]
