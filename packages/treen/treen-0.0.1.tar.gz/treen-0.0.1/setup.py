2d951691e629b15781b6c77b19f7cadf9f98012d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 04:05:31 +0000

    Add package treen/0.0.1/treen-0.0.1.tar.gz

---
+++ packages/treen/treen-0.0.1.tar.gz/treen-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="treen",
+    version="0.0.1",
+    author="rebase.energy",
+    description="A Python library for energy forecasting using decision trees.",
+    packages=find_packages(),
+)
