a02daee0e61c12c4cf6a16af6980c5169bc243eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 06:06:18 +0000

    Add package tapo/0.5.1/tapo-0.5.1-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/tapo/tapo-0.5.1-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/tapo/responses/__init__.pyi
@@ -0,0 +1,7 @@
+from .child_device_hub_result import *
+from .current_power_result import *
+from .device_info_result import *
+from .device_usage_energy_monitoring_result import *
+from .device_usage_result import *
+from .energy_data_result import *
+from .energy_usage_result import *
