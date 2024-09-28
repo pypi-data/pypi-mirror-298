ead6faa08130c946796150e9fb5814d9c42479fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 03:08:57 +0000

    Add package epygram/1.5.2/epygram-1.5.2-py3-none-any.whl

---
+++ packages/epygram/epygram-1.5.2-py3-none-any.whl/epygram/_plugins/with_cartopy/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) Météo France (2014-)
+# This software is governed by the CeCILL-C license under French law.
+# http://www.cecill.info
+"""
+Contains all classes needed to plot fields using cartopy.
+"""
+from __future__ import print_function, absolute_import, unicode_literals, division
+
+from . import (H2DField, H2DVectorField, AcademicGeometry, GaussGeometry,
+               Geometry, ProjectedGeometry, RectangularGeometry)
+
+
+def activate():
+    """Activate plugin."""
+    H2DField.activate()
+    H2DVectorField.activate()
+    AcademicGeometry.activate()
+    GaussGeometry.activate()
+    Geometry.activate()
+    ProjectedGeometry.activate()
+    RectangularGeometry.activate()
---
+++ packages/epygram/epygram-1.5.2-py3-none-any.whl/epygram/_plugins/with_vtk/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) Météo France (2014-)
+# This software is governed by the CeCILL-C license under French law.
+# http://www.cecill.info
+"""
+Contains all classes needed to plot 3D fields through vtk.
+"""
+from __future__ import print_function, absolute_import, unicode_literals, division
+
+from . import _D3CommonField, D3VectorField, Geometry
+
+
+def activate():
+    """Activate plugin."""
+    _D3CommonField.activate()
+    D3VectorField.activate()
+    Geometry.activate()
