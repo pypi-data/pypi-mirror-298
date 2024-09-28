43739ab01833e4ae9a6ab6bb69029738e7cc2bfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 04:06:04 +0000

    Add package external-dependencies/1.0.4/external_dependencies-1.0.4.tar.gz

---
+++ packages/external-dependencies/external_dependencies-1.0.4.tar.gz/external_dependencies-1.0.4/tests/test_external_dependencies.py
@@ -0,0 +1,10 @@
+"""Tests for `external_dependencies_py` package."""
+
+import unittest
+
+
+class Testexternaldependenciespy(unittest.TestCase):
+    """Tests for `external_dependencies_py` package."""
+
+    def empty_test(self):
+        pass
