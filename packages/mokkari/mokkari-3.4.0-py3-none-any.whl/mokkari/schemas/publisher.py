376ceedff6cbfb028b2e117767ad187d55daa284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 05:05:29 +0000

    Add package mokkari/3.4.0/mokkari-3.4.0-py3-none-any.whl

---
+++ packages/mokkari/mokkari-3.4.0-py3-none-any.whl/mokkari/schemas/publisher.py
@@ -0,0 +1,28 @@
+"""Publisher module.
+
+This module provides the following classes:
+
+- Publisher
+"""
+
+from pydantic import HttpUrl
+
+from mokkari.schemas.base import BaseResource
+
+
+class Publisher(BaseResource):
+    """A data model representing a publisher that extends BaseResource.
+
+    Attributes:
+        founded (int, optional): The year the publisher was founded.
+        desc (str): The description of the publisher.
+        image (HttpUrl, optional): The image URL of the publisher.
+        cv_id (int, optional): The Comic Vine ID of the publisher.
+        resource_url (HttpUrl): The URL of the publisher resource.
+    """
+
+    founded: int | None = None
+    desc: str
+    image: HttpUrl | None = None
+    cv_id: int | None = None
+    resource_url: HttpUrl
