03d7141ac327122a20d0267a9d5eeab3149471e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 03:09:38 +0000

    Add package aiopaypaython/1.8.8/aiopaypaython-1.8.8-py3-none-any.whl

---
+++ packages/aiopaypaython/aiopaypaython-1.8.8-py3-none-any.whl/aiopaypaython/__init__.py
@@ -0,0 +1,10 @@
+from .main import (
+    NetWorkError,
+    PayPay,
+    PayPayError,
+    PayPayLoginError,
+    PayPayNetWorkError,
+)
+
+__version__ = "1.8.8"
+__url__ = "https://github.com/nennneko5787/aiopaypaython"
