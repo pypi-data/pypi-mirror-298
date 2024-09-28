072f868d503f00825fbf67fe3df07a5e7e5cee1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 05:04:50 +0000

    Add package flagpypi/5.6.14/flagpypi-5.6.14.tar.gz

---
+++ packages/flagpypi/flagpypi-5.6.14.tar.gz/flagpypi-5.6.14/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '5.6.14' 
+DESCRIPTION = 'flag package'
+LONG_DESCRIPTION = 'You just have to look for the flag'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="flagpypi", 
+        version=VERSION,
+        author="John Doe",
+        author_email="john_doe@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+
+        keywords=['python package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
