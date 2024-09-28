db46e05a549cc59c32f3b9e7cf5a2f4160c73bb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 03:08:55 +0000

    Add package recordclass/0.22.1/recordclass-0.22.1-cp39-cp39-win_amd64.whl

---
+++ packages/recordclass/recordclass-0.22.1-cp39-cp39-win_amd64.whl/recordclass/test/__init__.py
@@ -0,0 +1,32 @@
+# {{LICENCE}}
+
+from recordclass.test.test_recordclass import *
+from recordclass.test.test_arrayclass import *
+from recordclass.test.test_dataobject import *
+from recordclass.test.test_litelist import *
+from recordclass.test.test_litetuple import *
+
+import sys
+_PY36 = sys.version_info[:2] >= (3, 6)
+_PY310 = sys.version_info[:2] >= (3, 10)
+
+if _PY36:
+    from recordclass.test.typing.test_recordclass import *
+    from recordclass.test.typing.test_dataobject import *
+    from recordclass.test.typing.test_datastruct import *
+    pass
+
+if _PY310:
+    from recordclass.test.match.test_dataobject_match import *
+
+try:
+    import sqlite3 as sql
+except:
+    sql = None
+    
+if sql is not None:
+    from recordclass.test.test_sqlite import *
+    
+def test_all():
+    import unittest
+    unittest.main(verbosity=3)
