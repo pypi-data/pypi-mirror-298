5134a348b2636487abe778f87992e28567984d57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 14:06:28 +0000

    Add package rosetta_finder/0.0.9rc89.post1/rosetta_finder-0.0.9rc89.post1.tar.gz

---
+++ packages/rosetta_finder/rosetta_finder-0.0.9rc89.post1.tar.gz/rosetta_finder-0.0.9rc89.post1/src/rosetta_finder/app/cart_ddg.py
@@ -0,0 +1,20 @@
+import os
+from typing import List, Optional
+from dataclasses import dataclass
+from rosetta_finder import Rosetta, RosettaScriptsVariableGroup, RosettaEnergyUnitAnalyser, MPI_node
+from rosetta_finder.utils import timing
+from rosetta_finder.app.utils import PDBProcessor
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+@dataclass
+class CartesianDDG:
+    pdb: str
+
+    def relax(self): ...
+
+    def cartesian_ddg(self): ...
+
+    @staticmethod
+    def mut2mutfile(): ...
