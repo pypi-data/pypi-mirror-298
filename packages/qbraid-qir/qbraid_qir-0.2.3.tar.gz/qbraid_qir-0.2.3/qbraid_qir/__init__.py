50d789cb8da0f00a3cf0778c0cbd088b1183df26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 04:05:13 +0000

    Add package qbraid-qir/0.2.3/qbraid_qir-0.2.3.tar.gz

---
+++ packages/qbraid-qir/qbraid_qir-0.2.3.tar.gz/qbraid_qir-0.2.3/qbraid_qir/__init__.py
@@ -0,0 +1,60 @@
+# Copyright (C) 2024 qBraid
+#
+# This file is part of the qBraid-SDK
+#
+# The qBraid-SDK is free software released under the GNU General Public License v3
+# or later. You can redistribute and/or modify it under the terms of the GPL v3.
+# See the LICENSE file in the project root or <https://www.gnu.org/licenses/gpl-3.0.html>.
+#
+# THERE IS NO WARRANTY for the qBraid-SDK, as per Section 15 of the GPL v3.
+
+"""
+This top level module contains the main qBraid QIR functionality.
+
+.. currentmodule:: qbraid_qir
+
+Functions
+-----------
+
+.. autosummary::
+   :toctree: ../stubs/
+
+   dumps
+
+
+Exceptions
+-----------
+
+.. autosummary::
+   :toctree: ../stubs/
+
+   QbraidQirError
+   QirConversionError
+
+"""
+from ._version import __version__
+from .exceptions import QbraidQirError, QirConversionError
+from .serialization import dumps
+
+__all__ = [
+    "__version__",
+    "QbraidQirError",
+    "QirConversionError",
+    "dumps",
+]
+
+_lazy_mods = ["cirq", "qasm3"]
+
+
+def __getattr__(name):
+    if name in _lazy_mods:
+        import importlib  # pylint: disable=import-outside-toplevel
+
+        module = importlib.import_module(f".{name}", __name__)
+        globals()[name] = module
+        return module
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+
+
+def __dir__():
+    return sorted(__all__ + _lazy_mods)
---
+++ packages/qbraid-qir/qbraid_qir-0.2.3.tar.gz/qbraid_qir-0.2.3/qbraid_qir/qasm3/__init__.py
@@ -0,0 +1,55 @@
+# Copyright (C) 2024 qBraid
+#
+# This file is part of the qBraid-SDK
+#
+# The qBraid-SDK is free software released under the GNU General Public License v3
+# or later. You can redistribute and/or modify it under the terms of the GPL v3.
+# See the LICENSE file in the project root or <https://www.gnu.org/licenses/gpl-3.0.html>.
+#
+# THERE IS NO WARRANTY for the qBraid-SDK, as per Section 15 of the GPL v3.
+
+"""
+Module containing OpenQASM 3 QIR functionality.
+
+.. currentmodule:: qbraid_qir.qasm3
+
+Functions
+-----------
+
+.. autosummary::
+   :toctree: ../stubs/
+
+   qasm3_to_qir
+   validate_qasm
+
+Classes
+---------
+
+.. autosummary::
+   :toctree: ../stubs/
+
+   Qasm3Module
+   BasicQasmVisitor
+
+Exceptions
+-----------
+
+.. autosummary::
+   :toctree: ../stubs/
+
+   Qasm3ConversionError
+
+"""
+from .checker import validate_qasm
+from .convert import qasm3_to_qir
+from .elements import Qasm3Module
+from .exceptions import Qasm3ConversionError
+from .visitor import BasicQasmVisitor
+
+__all__ = [
+    "validate_qasm",
+    "qasm3_to_qir",
+    "Qasm3Module",
+    "Qasm3ConversionError",
+    "BasicQasmVisitor",
+]
