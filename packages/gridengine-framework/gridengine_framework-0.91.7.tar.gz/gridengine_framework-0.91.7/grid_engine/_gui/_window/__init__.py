14e54759538fcd9ec00d36efea2866f08fd3500b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 04:06:00 +0000

    Add package gridengine-framework/0.91.7/gridengine_framework-0.91.7.tar.gz

---
+++ packages/gridengine-framework/gridengine_framework-0.91.7.tar.gz/gridengine_framework-0.91.7/grid_engine/_gui/_window/__init__.py
@@ -0,0 +1,20 @@
+import sys
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, 
+                             QPushButton, QLabel, QSlider, QGraphicsView, QGraphicsScene,
+                             QGraphicsRectItem, QGraphicsPixmapItem, QDialog, QTextBrowser,
+                             QLineEdit)
+from PyQt5.QtCore import Qt, QRectF, QPointF
+from PyQt5.QtGui import QImage, QPixmap, QWheelEvent, QMouseEvent, QPen, QColor, QPainter, QIntValidator
+
+
+from ._view import GEGraphicsView
+from ._dialog import CellDataDialog
+
+from ._window import GridEngineUI
+
+
+if 'ui' in sys.argv:
+    app = QApplication(sys.argv)
+    window = GridEngineUI()
+    window.show()
+    sys.exit(app.exec_())
