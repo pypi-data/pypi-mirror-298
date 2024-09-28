9b3210460d9961ce69f5a8e1c268dd390daec929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 15:05:28 +0000

    Add package python-polaris/0.1.1/python_polaris-0.1.1.tar.gz

---
+++ packages/python-polaris/python_polaris-0.1.1.tar.gz/python_polaris-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="python-polaris",
+    version="0.1.1",
+    packages=find_packages(where="polaris"),
+    install_requires=[
+        # Add any dependencies here, e.g., requests
+    ],
+)
---
+++ packages/python-polaris/python_polaris-0.1.1.tar.gz/python_polaris-0.1.1/tests/test_polaris.py
@@ -0,0 +1,4 @@
+from polaris import some_function
+
+def test_some_function():
+    assert some_function() == "Hello from Polaris!"
