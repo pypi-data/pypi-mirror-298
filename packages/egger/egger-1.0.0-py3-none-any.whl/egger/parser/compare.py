7a4f38d8deb5336683cb2f6cf0163ac97576aefe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 03:08:59 +0000

    Add package egger/1.0.0/egger-1.0.0-py3-none-any.whl

---
+++ packages/egger/egger-1.0.0-py3-none-any.whl/egger/parser/compare.py
@@ -0,0 +1,46 @@
+'''
+subparser for compare plot module of egger
+
+functions:
+xxxx
+'''
+def get_parser(subparsers):
+    '''
+    create the plot subparser
+        arguments:
+            subparsers: 
+                the subparsers object from argparse
+        returns:
+            parser: 
+                the parser for the compare module
+    '''
+    parser = subparsers.add_parser(
+        'compare', help='plot data from multiple genomes and compare between them'
+        )
+    parser.add_argument(
+        'annotations',
+        nargs='+',
+        default=None,
+        help='paths to the eggnog annotations files to compare (default: %(default)s)'
+    )
+    parser.add_argument(
+        '-b',
+        '--barchart',
+        type=str,
+        default=None,
+        help='prefix for the output barchart svg (default: %(default)s)'
+    )
+    parser.add_argument(
+        '-s',
+        '--spearmans',
+        type=str,
+        default=None,
+        help='prefix for the output the Spearmans rank data (default: %(default)s)'
+    )
+    parser.add_argument(
+        '-p',
+        '--pearsons',
+        type=str,
+        default=None,
+        help='prefix for the output the Pearsons rank data (default: %(default)s)'
+    )
