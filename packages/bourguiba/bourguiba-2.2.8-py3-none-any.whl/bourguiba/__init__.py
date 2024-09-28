584ed25049c74e478b6daff38553bc6b28fa829b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 15:05:22 +0000

    Add package bourguiba/2.2.8/bourguiba-2.2.8-py3-none-any.whl

---
+++ packages/bourguiba/bourguiba-2.2.8-py3-none-any.whl/bourguiba/__init__.py
@@ -0,0 +1,5 @@
+# This file can be left empty or include package-level docstring
+"""
+Bourguiba: A package for generating shell commands based on descriptions.
+"""
+from .bourguiba import main  # Import main function for command line execution
