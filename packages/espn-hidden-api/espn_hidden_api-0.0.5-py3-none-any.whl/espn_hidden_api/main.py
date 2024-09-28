d7e80e133d72602562e911f9b577f96946eb4139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 14:05:43 +0000

    Add package espn-hidden-api/0.0.5/espn_hidden_api-0.0.5-py3-none-any.whl

---
+++ packages/espn-hidden-api/espn_hidden_api-0.0.5-py3-none-any.whl/espn_hidden_api/main.py
@@ -0,0 +1,5 @@
+from tester import tester1
+
+
+print(tester1.test1())  # Output: "test1"
+print(tester1.test2())  # Output: "test2"
---
+++ packages/espn-hidden-api/espn_hidden_api-0.0.5-py3-none-any.whl/espn_hidden_api/tester.py
@@ -0,0 +1,8 @@
+class tester1:
+    @staticmethod
+    def test1():
+        return "test1"
+
+    @staticmethod
+    def test2():
+        return "test2"
