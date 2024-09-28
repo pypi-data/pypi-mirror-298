d1828f2124dcf637d35cce6ef2f1036f084463f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 03:08:54 +0000

    Add package ylz-utils/0.3.5/ylz_utils-0.3.5-py3-none-any.whl

---
+++ packages/ylz-utils/ylz_utils-0.3.5-py3-none-any.whl/ylz_utils/langchain/tools/python_repl.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from ylz_utils.langchain import LangchainLib
+
+from typing import Type
+from langchain_core.tools import Tool
+from langchain_experimental.utilities import PythonREPL
+from pydantic import BaseModel,Field
+class PythonREPLArgSchema(BaseModel):
+    ''' input str for python repl tool'''
+    command: str = Field(description="the command to execute use python repl with `print(...)` at last.")
+
+class PythonREPLTool():
+    def __init__(self,langchainLib:LangchainLib):
+        self.langchainLib = langchainLib
+        self.config = langchainLib.config
+    def get_tool(self,name=None):
+        # You can create the tool to pass to an agent
+        python_repl = PythonREPL()
+        name = name or "python_repl"
+        #So You are not skilled at dealing with computation issues,All question about calculate shoud use this tool when you get all argument value.
+        repl_tool = Tool(
+            name=name,
+            description="""
+The tool is a Python shell with print(...) as last. Use this to execute python commands and get result. Input should be a valid python command. """,
+            args_schema = PythonREPLArgSchema ,
+            func=python_repl.run
+        )
+        return repl_tool
+
