34ff5455f281b7f7aa056b07f0b0c10c8505987e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 06:06:01 +0000

    Add package robotcode-analyze/0.91.0/robotcode_analyze-0.91.0-py3-none-any.whl

---
+++ packages/robotcode-analyze/robotcode_analyze-0.91.0-py3-none-any.whl/robotcode/analyze/hooks.py
@@ -0,0 +1,18 @@
+from typing import Any, List, Tuple, Type
+
+import click
+
+from robotcode.plugin import hookimpl
+
+from .cli import analyze
+from .config import AnalyzerConfig
+
+
+@hookimpl
+def register_cli_commands() -> List[click.Command]:
+    return [analyze]
+
+
+@hookimpl
+def register_config_classes() -> List[Tuple[str, Type[Any]]]:
+    return [("tool.robotcode-analyze", AnalyzerConfig)]
