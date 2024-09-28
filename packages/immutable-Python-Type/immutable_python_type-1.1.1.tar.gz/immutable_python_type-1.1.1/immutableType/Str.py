5132f974ea882cb8d08cdbb30c25032986f6d7d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 06:06:11 +0000

    Add package immutable-Python-Type/1.1.1/immutable_python_type-1.1.1.tar.gz

---
+++ packages/immutable-Python-Type/immutable_python_type-1.1.1.tar.gz/immutable_python_type-1.1.1/immutableType/Str.py
@@ -0,0 +1,31 @@
+from ._error import StrError
+class Str_:
+    def __init__(self, string: str) -> None:
+
+        if not isinstance(string, str):
+            raise StrError(string)
+
+        self.__string = string
+
+    @property
+    def str_(self) -> str:
+        return self.__string
+
+    @str_.setter
+    def str_(self, new_value):
+        if not isinstance(new_value, str):
+            raise StrError(new_value)
+
+        self.__string = new_value
+
+    def __str__(self):
+        return self.__string
+
+    def __len__(self):
+        return len(self.__string)
+
+    def __bool__(self):
+        return True if self.__string else False
+
+    def __repr__(self):
+        return f"Str({self.__string!r})"
