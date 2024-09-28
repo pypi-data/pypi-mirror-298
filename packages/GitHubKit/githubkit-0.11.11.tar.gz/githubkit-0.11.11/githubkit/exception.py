d9ff55fa3b4ed3b39a00f9baef482f08021f2d8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 15:05:34 +0000

    Add package GitHubKit/0.11.11/githubkit-0.11.11.tar.gz

---
+++ packages/GitHubKit/githubkit-0.11.11.tar.gz/githubkit-0.11.11/githubkit/exception.py
@@ -0,0 +1,115 @@
+from datetime import timedelta
+from typing import TYPE_CHECKING
+
+import httpx
+
+if TYPE_CHECKING:
+    from .response import Response
+    from .graphql import GraphQLResponse
+
+
+class GitHubException(Exception): ...
+
+
+class AuthCredentialError(GitHubException):
+    """Auth Credential Error"""
+
+
+class AuthExpiredError(GitHubException):
+    """Auth Expired Error"""
+
+
+class RequestError(GitHubException):
+    """Simple API request failed with unknown error"""
+
+
+class RequestTimeout(GitHubException):
+    """Simple API request timeout"""
+
+    def __init__(self, request: httpx.Request):
+        self.request = request
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}(method={self.request.method}, "
+            f"url={self.request.url})"
+        )
+
+
+class RequestFailed(GitHubException):
+    """Simple API request failed with error status code"""
+
+    def __init__(self, response: "Response"):
+        self.request = response.raw_request
+        self.response = response
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}(method={self.request.method}, "
+            f"url={self.request.url}, status_code={self.response._status_reason})"
+        )
+
+
+class RateLimitExceeded(RequestFailed):
+    """API request failed with rate limit exceeded"""
+
+    def __init__(self, response: "Response", retry_after: timedelta):
+        super().__init__(response)
+        self.retry_after = retry_after
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}(method={self.request.method}, "
+            f"url={self.request.url}, status_code={self.response._status_reason}, "
+            f"retry_after={self.retry_after})"
+        )
+
+
+class PrimaryRateLimitExceeded(RateLimitExceeded):
+    """API request failed with primary rate limit exceeded"""
+
+
+class SecondaryRateLimitExceeded(RateLimitExceeded):
+    """API request failed with secondary rate limit exceeded"""
+
+
+class GraphQLError(GitHubException):
+    """Simple GraphQL request error"""
+
+
+class GraphQLFailed(GraphQLError):
+    """GraphQL request with errors in response"""
+
+    def __init__(self, response: "GraphQLResponse"):
+        self.response = response
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.response.errors!r})"
+
+
+class GraphQLPaginationError(GraphQLError):
+    """GraphQL paginate response error"""
+
+    def __init__(self, response: "GraphQLResponse"):
+        self.response = response
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.response})"
+
+
+class GraphQLMissingPageInfo(GraphQLPaginationError):
+    """GraphQL paginate response missing PageInfo object"""
+
+
+class GraphQLMissingCursorChange(GraphQLPaginationError):
+    """GraphQL paginate response missing cursor change"""
+
+
+class WebhookTypeNotFound(GitHubException):
+    """Webhook event type not found"""
+
+    def __init__(self, name: str):
+        self.name = name
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(name={self.name})"
