f48cbf2205994902f87ef4cf240f22442ffb170f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 03:09:30 +0000

    Add package linktools/0.8.10/linktools-0.8.10-py3-none-any.whl

---
+++ packages/linktools/linktools-0.8.10-py3-none-any.whl/linktools/__main__.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@author  : Hu Ji
+@file    : __main__.py 
+@time    : 2022/12/13
+@site    :  
+@software: PyCharm 
+
+              ,----------------,              ,---------,
+         ,-----------------------,          ,"        ,"|
+       ,"                      ,"|        ,"        ,"  |
+      +-----------------------+  |      ,"        ,"    |
+      |  .-----------------.  |  |     +---------+      |
+      |  |                 |  |  |     | -==----'|      |
+      |  | $ sudo rm -rf / |  |  |     |         |      |
+      |  |                 |  |  |/----|`---=    |      |
+      |  |                 |  |  |   ,/|==== ooo |      ;
+      |  |                 |  |  |  // |(((( [33]|    ,"
+      |  `-----------------'  |," .;'| |((((     |  ,"
+      +-----------------------+  ;;  | |         |,"
+         /_)______________(_/  //'   | +---------+
+    ___________________________/___  `,
+   /  oooooooooooooooo  .o.  oooo /,   `,"-----------
+  / ==ooooooooooooooo==.o.  ooo= //   ,``--{)B     ,"
+ /_==__==========__==_ooo__ooo=_/'   /___________,"
+"""
+from typing import Any
+
+from .cli import commands, BaseCommandGroup
+
+
+class Command(BaseCommandGroup):
+
+    def init_subcommands(self) -> Any:
+        return commands
+
+
+command = Command()
+if __name__ == '__main__':
+    command.main()
