268180e0a58b161eb3088c283e0323909cb363e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 06:06:01 +0000

    Add package robotcode-plugin/0.91.0/robotcode_plugin-0.91.0-py3-none-any.whl

---
+++ packages/robotcode-plugin/robotcode_plugin-0.91.0-py3-none-any.whl/robotcode/plugin/manager.py
@@ -0,0 +1,29 @@
+from typing import List, Tuple, Type, cast
+
+import click
+import pluggy
+
+from . import specs
+
+
+class PluginManager:
+    def __init__(self) -> None:
+        self._plugin_manager = pluggy.PluginManager("robotcode")
+        self._plugin_manager.add_hookspecs(specs)
+        self._plugin_manager.load_setuptools_entrypoints("robotcode")
+
+    @property
+    def cli_commands(self) -> List[List[click.Command]]:
+        return cast(
+            List[List[click.Command]],
+            self._plugin_manager.hook.register_cli_commands(),
+        )
+
+    @property
+    def config_classes(
+        self,
+    ) -> List[List[Tuple[str, Type[specs.TConfigClass]]]]:
+        return cast(
+            List[List[Tuple[str, Type[specs.TConfigClass]]]],
+            self._plugin_manager.hook.register_config_classes(),
+        )
