46b5b49fc7abc447dd395acaf945f2c5e0906daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 04:06:18 +0000

    Add package voxlab/0.2/voxlab-0.2.tar.gz

---
+++ packages/voxlab/voxlab-0.2.tar.gz/voxlab-0.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+# Read requirements from the file
+with open('voxlab/requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+
+
+setup(
+    name='voxlab',
+    version='0.2',
+    packages=find_packages(),
+    py_modules=['voxlab'],
+    install_requires=requirements,
+    package_data={
+        'voxlab': ['requirements.txt'],
+    },
+    entry_points={
+        'console_scripts': [
+            
+        ],
+    },
+    author='Rafaello Virgilli',
+    author_email='rvirgilli@gmail.com',
+    description='A toolbox for audio processing and voice deep learning models.',
+    url='https://github.com/rvirgilli/voxlab',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.11',
+)
