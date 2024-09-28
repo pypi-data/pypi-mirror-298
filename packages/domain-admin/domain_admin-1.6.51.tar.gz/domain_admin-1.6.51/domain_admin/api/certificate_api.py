37f6698ba63d5910d9a044382a09c9e0bb8aea27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 04:05:39 +0000

    Add package domain-admin/1.6.51/domain_admin-1.6.51.tar.gz

---
+++ packages/domain-admin/domain_admin-1.6.51.tar.gz/domain_admin-1.6.51/domain_admin/api/certificate_api.py
@@ -0,0 +1,208 @@
+# -*- coding: utf-8 -*-
+"""
+@File    : certificate_api.py
+@Date    : 2024-02-25
+"""
+from flask import g, request
+from peewee import SQL
+
+from domain_admin.enums.role_enum import RoleEnum
+from domain_admin.model.certificate_model import CertificateModel
+from domain_admin.service import certificate_service, auth_service
+from domain_admin.utils.flask_ext.app_exception import AppException, DataNotFoundAppException, ForbiddenAppException
+
+
+@auth_service.permission(role=RoleEnum.USER)
+def get_certificate_list():
+    """
+    获取列表
+    :return:
+    @since v1.6.12
+    """
+
+    current_user_id = g.user_id
+
+    page = request.json.get('page', 1)
+    size = request.json.get('size', 10)
+    keyword = request.json.get('keyword')
+    order_prop = request.json.get('order_prop') or 'create_time'
+    order_type = request.json.get('order_type') or 'desc'
+
+    if order_prop not in ['create_time']:
+        raise AppException('params error: order_prop')
+
+    if order_type not in ['desc', 'asc']:
+        raise AppException('params error: order_type')
+
+    query = CertificateModel.select().where(
+        CertificateModel.user_id == current_user_id,
+    )
+
+    if keyword:
+        query = query.where(CertificateModel.domain.contains(keyword))
+
+    total = query.count()
+    lst = []
+
+    if total > 0:
+        ordering = [
+            SQL(f"`{order_prop}` {order_type}"),
+            CertificateModel.id.desc()
+        ]
+
+        rows = query.order_by(*ordering).paginate(page, size)
+
+        lst = [row.to_dict() for row in rows]
+
+        # 查询部署数量
+        certificate_service.load_cert_deploy_count(lst)
+
+    return {
+        "list": lst,
+        "total": total
+    }
+
+
+@auth_service.permission(role=RoleEnum.USER)
+def add_certificate():
+    """
+    添加
+    :return:
+    @since v1.6.12
+    """
+
+    current_user_id = g.user_id
+
+    domain = request.json['domain']
+    ssl_certificate = request.json['ssl_certificate']
+    ssl_certificate_key = request.json['ssl_certificate_key']
+    start_time = request.json.get('start_time')
+    expire_time = request.json.get('expire_time')
+    comment = request.json.get('comment') or ''
+
+    data = {
+        'user_id': current_user_id,
+        'domain': domain,
+        'start_time': start_time,
+        'expire_time': expire_time,
+        'ssl_certificate': ssl_certificate,
+        'ssl_certificate_key': ssl_certificate_key,
+        'comment': comment,
+    }
+
+    CertificateModel.create(**data)
+
+
+@auth_service.permission(role=RoleEnum.USER)
+def update_certificate_by_id():
+    """
+    更新主机地址
+    :return:
+    @since v1.6.12
+    """
+
+    current_user_id = g.user_id
+
+    certificate_id = request.json['certificate_id']
+
+    domain = request.json['domain']
+    ssl_certificate = request.json['ssl_certificate']
+    ssl_certificate_key = request.json['ssl_certificate_key']
+    start_time = request.json.get('start_time')
+    expire_time = request.json.get('expire_time')
+    comment = request.json.get('comment') or ''
+
+    # check data
+    certificate_row = CertificateModel.select().where(
+        CertificateModel.id == certificate_id,
+        CertificateModel.user_id == current_user_id
+    ).first()
+
+    if not certificate_row:
+        raise DataNotFoundAppException()
+
+    data = {
+        'domain': domain,
+        'start_time': start_time,
+        'expire_time': expire_time,
+        'ssl_certificate': ssl_certificate,
+        'ssl_certificate_key': ssl_certificate_key,
+        'comment': comment,
+    }
+
+    CertificateModel.update(data).where(
+        CertificateModel.id == certificate_id
+    ).execute()
+
+
+@auth_service.permission(role=RoleEnum.USER)
+def delete_certificate_by_id():
+    """
+    删除
+    :return:
+    @since v1.6.12
+    """
+
+    current_user_id = g.user_id
+
+    certificate_id = request.json['certificate_id']
+
+    # check data
+    certificate_row = CertificateModel.select().where(
+        CertificateModel.id == certificate_id,
+        CertificateModel.user_id == current_user_id
+    ).first()
+
+    if not certificate_row:
+        raise DataNotFoundAppException()
+
+    # delete
+    CertificateModel.delete().where(
+        CertificateModel.id == certificate_row.id
+    ).execute()
+
+
+@auth_service.permission(role=RoleEnum.USER)
+def delete_certificate_by_ids():
+    """
+    批量删除
+    :return:
+    @since v1.6.12
+    """
+
+    current_user_id = g.user_id
+
+    certificate_ids = request.json['certificate_ids']
+
+    CertificateModel.delete().where(
+        CertificateModel.id.in_(certificate_ids),
+        CertificateModel.user_id == current_user_id
+    ).execute()
+
+
+@auth_service.permission(role=RoleEnum.USER)
+def get_certificate_by_id():
+    """
+    获取
+    :return:
+    @since v1.6.12
+    """
+
+    current_user_id = g.user_id
+
+    certificate_id = request.json['certificate_id']
+
+    # check data
+    certificate_row = CertificateModel.select().where(
+        CertificateModel.id == certificate_id,
+        CertificateModel.user_id == current_user_id
+    ).first()
+
+    if not certificate_row:
+        raise DataNotFoundAppException()
+
+    # 查询部署数量
+    certificate_dict = certificate_row.to_dict()
+    certificate_service.load_cert_deploy_count([certificate_dict])
+
+    return certificate_dict
