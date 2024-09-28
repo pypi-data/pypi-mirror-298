a296ff26bcf9e68e4fa61932637f4c1f11d48c5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 14:05:44 +0000

    Add package biliass/2.0.0b1/biliass-2.0.0b1-cp39-abi3-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl

---
+++ packages/biliass/biliass-2.0.0b1-cp39-abi3-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl/biliass/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from biliass._core import get_danmaku_meta_size as get_danmaku_meta_size
+
+from .biliass import (
+    Danmaku2ASS as Danmaku2ASS,
+    read_comments_bilibili_protobuf as read_comments_bilibili_protobuf,
+    read_comments_bilibili_xml as read_comments_bilibili_xml,
+)
