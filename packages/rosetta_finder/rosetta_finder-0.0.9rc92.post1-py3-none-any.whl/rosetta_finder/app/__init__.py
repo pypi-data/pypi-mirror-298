f3cd1a9aaffdad278a62db00c1324105f8a46727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 15:05:18 +0000

    Add package rosetta_finder/0.0.9rc92.post1/rosetta_finder-0.0.9rc92.post1-py3-none-any.whl

---
+++ packages/rosetta_finder/rosetta_finder-0.0.9rc92.post1-py3-none-any.whl/rosetta_finder/app/__init__.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+import os
+from abc import ABC
+
+
+@dataclass
+class RosettaApplication:
+
+    job_id: str
+
+    def prepare(self):
+        if hasattr(self, "pdb") and isinstance(self.pdb, str) and os.path.isfile(self.pdb):  # type:ignore
+            self.instance = os.path.basename(self.pdb)[:-4]  # type:ignore
+            self.pdb = os.path.abspath(self.pdb)  # type:ignore
+
+        if hasattr(self, "save_dir"):
+            os.makedirs(os.path.join(self.save_dir, self.job_id), exist_ok=True)  # type:ignore
+            self.save_dir = os.path.abspath(self.save_dir)  # type:ignore
+
+
+from .supercharge import supercharge
+from .pross import PROSS
+from .rosettaligand import RosettaLigand
+
+
+__all__ = ["RosettaApplication", "supercharge", "PROSS", "RosettaLigand"]
