c402371e11256254922c50568f7c2cfa9d7aaf6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 04:06:14 +0000

    Add package semantic-chunkers/0.0.10/semantic_chunkers-0.0.10-py3-none-any.whl

---
+++ packages/semantic-chunkers/semantic_chunkers-0.0.10-py3-none-any.whl/semantic_chunkers/__init__.py
@@ -0,0 +1,20 @@
+from semantic_chunkers.chunkers import (
+    BaseChunker,
+    ConsecutiveChunker,
+    CumulativeChunker,
+    RegexChunker,
+    StatisticalChunker,
+)
+from semantic_chunkers.splitters import BaseSplitter, RegexSplitter
+
+__all__ = [
+    "BaseChunker",
+    "ConsecutiveChunker",
+    "CumulativeChunker",
+    "StatisticalChunker",
+    "RegexSplitter",
+    "BaseSplitter",
+    "RegexChunker",
+]
+
+__version__ = "0.0.10"
