09dbe27eebb44872a813ff6950660852c8d020eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 03:08:53 +0000

    Add package evidently/0.4.38/evidently-0.4.38-py3-none-any.whl

---
+++ packages/evidently/evidently-0.4.38-py3-none-any.whl/evidently/features/text_contains_feature.py
@@ -0,0 +1,114 @@
+from typing import ClassVar
+from typing import List
+from typing import Optional
+
+import pandas as pd
+
+from evidently.base_metric import ColumnName
+from evidently.core import ColumnType
+from evidently.features.generated_features import GeneratedFeature
+from evidently.utils.data_preprocessing import DataDefinition
+
+
+class Contains(GeneratedFeature):
+    class Config:
+        type_alias = "evidently:feature:Contains"
+
+    __feature_type__: ClassVar = ColumnType.Categorical
+    column_name: str
+    items: List[str]
+    case_sensitive: bool
+    mode: str
+
+    def __init__(
+        self,
+        column_name: str,
+        items: List[str],
+        case_sensitive: bool = True,
+        mode: str = "any",
+        display_name: Optional[str] = None,
+    ):
+        self.column_name = column_name
+        self.display_name = display_name
+        self.case_sensitive = case_sensitive
+        if mode not in ["any", "all"]:
+            raise ValueError("mode must be either 'any' or 'all'")
+        self.mode = mode
+        self.items = items
+        super().__init__()
+
+    def _feature_column_name(self) -> str:
+        return f"{self.column_name}_" + "_".join(self.items) + "_" + str(self.case_sensitive) + "_" + self.mode
+
+    def generate_feature(self, data: pd.DataFrame, data_definition: DataDefinition) -> pd.DataFrame:
+        if self.mode == "any":
+            calculated = data[self.column_name].str.contains("|".join(self.items), case=self.case_sensitive)
+        elif self.mode == "all":
+            calculated = data[self.column_name].apply(lambda x: all(self.comparison(i, x) for i in self.items))
+        else:
+            raise ValueError("mode must be either 'any' or 'all'")
+        return pd.DataFrame({self._feature_column_name(): calculated})
+
+    def _as_column(self) -> ColumnName:
+        return self._create_column(
+            self._feature_column_name(),
+            default_display_name=f"Text Contains of {self.mode} [{', '.join(self.items)}] for {self.column_name}",
+        )
+
+    def comparison(self, item: str, string: str):
+        if self.case_sensitive:
+            return item in string
+        return item.casefold() in string.casefold()
+
+
+class DoesNotContain(GeneratedFeature):
+    class Config:
+        type_alias = "evidently:feature:DoesNotContain"
+
+    __feature_type__: ClassVar = ColumnType.Categorical
+    column_name: str
+    items: List[str]
+    case_sensitive: bool
+    mode: str
+
+    def __init__(
+        self,
+        column_name: str,
+        items: List[str],
+        case_sensitive: bool = True,
+        mode: str = "any",
+        display_name: Optional[str] = None,
+    ):
+        self.column_name = column_name
+        self.display_name = display_name
+        self.case_sensitive = case_sensitive
+        if mode not in ["any", "all"]:
+            raise ValueError("mode must be either 'any' or 'all'")
+        self.mode = mode
+        self.items = items
+        super().__init__()
+
+    def _feature_column_name(self) -> str:
+        return f"{self.column_name}_" + "_".join(self.items) + "_" + str(self.case_sensitive) + "_" + self.mode
+
+    def generate_feature(self, data: pd.DataFrame, data_definition: DataDefinition) -> pd.DataFrame:
+        if self.mode == "any":
+            calculated = ~data[self.column_name].str.contains("|".join(self.items), case=self.case_sensitive)
+        elif self.mode == "all":
+            calculated = ~data[self.column_name].apply(lambda x: all(self.comparison(i, x) for i in self.items))
+        else:
+            raise ValueError("mode must be either 'any' or 'all'")
+        return pd.DataFrame({self._feature_column_name(): calculated})
+
+    def _as_column(self) -> ColumnName:
+        return self._create_column(
+            self._feature_column_name(),
+            default_display_name=f"Text Contains of {self.mode} [{', '.join(self.items)}] for {self.column_name}",
+        )
+
+    def comparison(self, item: str, string: str):
+        if not isinstance(string, str):
+            return False
+        if self.case_sensitive:
+            return item in string
+        return item.casefold() in string.casefold()
---
+++ packages/evidently/evidently-0.4.38-py3-none-any.whl/evidently/features/word_count_feature.py
@@ -0,0 +1,29 @@
+import re
+from typing import Any
+from typing import ClassVar
+from typing import Optional
+
+import numpy as np
+
+from evidently import ColumnType
+from evidently.features.generated_features import ApplyColumnGeneratedFeature
+
+
+class WordCount(ApplyColumnGeneratedFeature):
+    class Config:
+        type_alias = "evidently:feature:WordCount"
+
+    __feature_type__: ClassVar = ColumnType.Numerical
+    _reg: ClassVar[re.Pattern] = re.compile(r"[^a-zA-Z ]+")
+    display_name_template: ClassVar = "Word Count for {column_name}"
+    column_name: str
+
+    def __init__(self, column_name: str, display_name: Optional[str] = None):
+        self.column_name = column_name
+        self.display_name = display_name
+        super().__init__()
+
+    def apply(self, value: Any):
+        if value is None or (isinstance(value, float) and np.isnan(value)):
+            return 0
+        return len(self._reg.sub("", value).split())
