0ee51f346612061141b00dda370913e2ca39df92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 06:06:08 +0000

    Add package firebase-cart/0.1.1/firebase_cart-0.1.1.tar.gz

---
+++ packages/firebase-cart/firebase_cart-0.1.1.tar.gz/firebase_cart-0.1.1/src/firebase_cart/__init__.py
@@ -0,0 +1,3 @@
+from .cart import CartHandler
+from .order import OrderHandler
+from .models import CartItem, Cart, FirebaseConfig, Order
---
+++ packages/firebase-cart/firebase_cart-0.1.1.tar.gz/firebase_cart-0.1.1/src/firebase_cart/models.py
@@ -0,0 +1,53 @@
+from pydantic import BaseModel
+from typing import List, Optional, Dict, Any
+
+class CartItem(BaseModel):
+    item_id: str
+    product_id: str
+    name: str
+    description: str | None
+    slug: str
+    image: str | None
+    quantity: int
+    price: float
+
+
+class Cart(BaseModel):
+    cart_id: str
+    customer_id: str | None
+    email: str | None
+    items: List[CartItem]
+    shipping_method: dict
+    shipping_address: dict
+    billing_address: dict
+    subtotal: float
+    tax_total: float
+    delivery_fee: float
+    total: float
+    payment_session: dict
+
+
+class FirebaseConfig(BaseModel):
+    credentials: Dict[str, Any]
+    database_url: Optional[str] = None
+    bucket: Optional[str] = None
+
+
+class Order(BaseModel):
+    order_id: str
+    status: str
+    fulfillment_status: Optional[str] = None
+    cart_id: str
+    customer_id: Optional[str] = ""
+    email: Optional[str] = ""
+    line_items: List[CartItem]
+    shipping_method: dict
+    shipping_address: dict
+    billing_address: dict
+    subtotal: float
+    tax_total: float
+    delivery_fee: float
+    total: float
+    payment_session: dict
+    fulfillments: Optional[list[dict]] = []
+    payment_status: Optional[str] = ""
