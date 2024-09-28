e33f37797af3d7ce19a4bba87d4a47ba8b48e944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 04:05:22 +0000

    Add package qoqo-qryd/0.19.0/qoqo_qryd-0.19.0.tar.gz

---
+++ packages/qoqo-qryd/qoqo_qryd-0.19.0.tar.gz/qoqo_qryd-0.19.0/qoqo-qryd/src/lib.rs
@@ -0,0 +1,138 @@
+// Copyright © 2021-2022 HQS Quantum Simulations GmbH. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+
+#![deny(missing_docs)]
+#![deny(missing_crate_level_docs)]
+#![deny(missing_debug_implementations)]
+#![allow(ambiguous_glob_reexports)]
+
+//! # qoqo-qryd
+//!
+//! Components for the qoqo/roqoqo quantum toolkit by [HQS Quantum Simulations](https://quantumsimulations.de) that support QRyd quantum computers.
+//!
+//! The qoqo-qryd/roqoqo-qryd packages provide three components
+//!
+//! * devices: python/rust representation of QRyd devices
+//! * operations: roqoqo Pragma operations specific to QRyd devices that can change the topology of QRyd devices
+//! * simulator (optional): A QuEST based simulator for QRyd devices that checks the availability of the quantum operations on a chosen device during simulation
+
+use pyo3::prelude::*;
+use pyo3::types::PyDict;
+use pyo3::wrap_pymodule;
+
+/// qoqo utilities for QRyd quantum computers.
+///
+/// Provides devices for the QRyd quantum hardware for the qoqo quantum toolkit.
+/// Also provides qoqo PRAGMA operations specific to those devices.
+/// Includes an optional QRydSimulator backend.
+/// Furthermore, provides a collection of all QRyd as well as Tweezer devices for the WebAPI.
+///
+/// .. autosummary::
+///     :toctree: generated/
+///
+///     qryd_devices
+///     api_devices
+///     pragma_operations
+///     SimulatorBackend
+///     APIBackend
+///     tweezer_devices
+///     emulator_devices
+///
+pub mod qryd_devices;
+pub use qryd_devices::*;
+
+pub mod pragma_operations;
+pub use pragma_operations::*;
+
+/// QRyd Tweezer Devices.
+///
+pub mod tweezer_devices;
+pub use tweezer_devices::*;
+
+/// QRyd Emulator Devices.
+///
+pub mod emulator_devices;
+pub use emulator_devices::*;
+
+#[cfg(feature = "simulator")]
+pub mod simulator_backend;
+#[cfg(feature = "simulator")]
+pub use simulator_backend::SimulatorBackendWrapper;
+
+/// QRyd WebAPI Backend.
+///
+/// The WebAPI Backend implements methods available in the QRyd Web API.
+///
+#[cfg(feature = "web-api")]
+pub mod api_backend;
+#[cfg(feature = "web-api")]
+pub use api_backend::APIBackendWrapper;
+
+/// Collection of all QRyd devices for WebAPI.
+///
+/// At the moment only contains a square and a triangular device.
+///
+pub mod api_devices;
+pub use api_devices::*;
+
+/// QRyd utilities for qoqo quantum computation toolkit.
+///
+/// qoqo is the HQS python package to represent quantum circuits.
+///
+/// .. autosummary::
+///     :toctree: generated/
+///
+///     api_devices
+///     Backend
+///     pragma_operations
+///     qryd_devices
+///     tweezer_devices
+///     emulator_devices
+///
+///
+#[pymodule]
+fn qoqo_qryd(_py: Python, module: &Bound<PyModule>) -> PyResult<()> {
+    #[cfg(feature = "simulator")]
+    module.add_class::<SimulatorBackendWrapper>()?;
+    #[cfg(feature = "web-api")]
+    module.add_class::<APIBackendWrapper>()?;
+    let wrapper = wrap_pymodule!(qryd_devices::qryd_devices);
+    module.add_wrapped(wrapper)?;
+    let wrapper = wrap_pymodule!(api_devices::api_devices);
+    module.add_wrapped(wrapper)?;
+    let wrapper = wrap_pymodule!(tweezer_devices::tweezer_devices);
+    module.add_wrapped(wrapper)?;
+    // Adding nice imports corresponding to maturin example
+    let wrapper = wrap_pymodule!(pragma_operations::pragma_operations);
+    module.add_wrapped(wrapper)?;
+    let wrapper = wrap_pymodule!(emulator_devices::emulator_devices);
+    module.add_wrapped(wrapper)?;
+    // Adding nice imports corresponding to maturin example
+    let system = PyModule::import_bound(_py, "sys")?;
+    let binding = system.getattr("modules")?;
+    let system_modules: &Bound<PyDict> = binding.downcast()?;
+    system_modules.set_item(
+        "qoqo_qryd.pragma_operations",
+        module.getattr("pragma_operations")?,
+    )?;
+    system_modules.set_item("qoqo_qryd.qryd_devices", module.getattr("qryd_devices")?)?;
+    system_modules.set_item("qoqo_qryd.api_devices", module.getattr("api_devices")?)?;
+    system_modules.set_item(
+        "qoqo_qryd.tweezer_devices",
+        module.getattr("tweezer_devices")?,
+    )?;
+    system_modules.set_item(
+        "qoqo_qryd.emulator_devices",
+        module.getattr("emulator_devices")?,
+    )?;
+    Ok(())
+}
---
+++ packages/qoqo-qryd/qoqo_qryd-0.19.0.tar.gz/qoqo_qryd-0.19.0/roqoqo-qryd/src/lib.rs
@@ -0,0 +1,104 @@
+// Copyright © 2021-2022 HQS Quantum Simulations GmbH. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#![deny(missing_docs)]
+#![warn(rustdoc::private_intra_doc_links)]
+#![warn(rustdoc::missing_crate_level_docs)]
+#![warn(rustdoc::missing_doc_code_examples)]
+#![warn(rustdoc::private_doc_tests)]
+#![deny(missing_debug_implementations)]
+
+//! # roqoqo-qryd
+//!
+//! The `roqoqo-qryd` rust crate implements [qoqo](https://github.com/HQSquantumsimulations/qoqo) support for quantum computers and quantum computer emulators of the [QRydDemo](https://thequantumlaend.de/qryddemo/) project.
+//!
+//! The QRydDemo project builds on Quantum computers using Rydberg atoms.
+//! qoqo is quantum computing toolkit by [HQS Quantum Simulations](https://quantumsimulations.de).
+//!
+//! The roqoqo-qryd package contains the following functionality:
+//!
+//! ### Interface to the current QRydDemo WebAPI
+//!
+//! At the moment QRydDemo WebAPI allows access to Quantum Hardware Emulators of different device topology. roqoqo-qryd supports interfacing with the corresponding [REST-API](https://api.qryddemo.itp3.uni-stuttgart.de/docs) with low level calls as well as a high-level backend to qoqo quantum programs. For this it provides the backend `APIBackend` to evaluate roqoqo quantum programs and the `api_devices` module to represent devices available on the emulators.
+//!
+//! ### QRydDemo specific hardware operations (prototype)
+//!
+//! Rydberg atom based quantum devices support, in principle, operations not commonly found in other quantum hardware. Changes in device topology are one of these operations. roqoqo-qryd adds support for changes in device topology to roqoqo via the operations in its `pragma_operations` module.
+//! Note that this is a preview prototype and does not represent a finalized set of operations on the QRydDemo hardware.
+//!
+//! ### Local simulator supporting specific hardware operations
+//!
+//! roqoqo-qryd includes a local [QuEST](https://github.com/QuEST-Kit/QuEST) based simulator for quantum devices supporting the Rydberg specific quantum operations. The simulator is intended to let users test the capabilities of quantum hardware with the additional operations.
+//! roqoqo-qryd provides the simulator via the `SimulatorBackend` backend the implements the roqoqo `Backend` trait.The backend uses the device prototypes in roqoqo-qryd's `qryd_devices` module.
+//! Note that the devices for the simulator do not represent a finalised design for QRydDemo.
+
+/// Devices representing QRyd quantum computer(s)
+pub mod qryd_devices;
+pub use qryd_devices::*;
+
+/// Tweezer devices representing QRyd quantum computer(s)
+pub mod tweezer_devices;
+pub use tweezer_devices::*;
+
+/// Devices representing QRyd quantum computer(s)
+pub mod api_devices;
+pub use api_devices::*;
+
+/// QRyd specific PragmaOperations that support changing the QRyd device during a circuit evaluation
+pub mod pragma_operations;
+pub use pragma_operations::*;
+
+/// Emulator device, TweezerDevice instance with all-to-all connectivity
+pub mod emulator_devices;
+pub use emulator_devices::*;
+
+/// Simulator backend for the QRyd quantum computer
+#[cfg(feature = "simulator")]
+mod simulator_backend;
+#[cfg(feature = "simulator")]
+pub use simulator_backend::*;
+
+/// WebAPI backend for the QRyd quantum computer(s)
+#[cfg(feature = "web-api")]
+pub mod api_backend;
+#[cfg(feature = "web-api")]
+pub use api_backend::*;
+
+/// Compute the angle according to the appropriate relation and phi/theta values.
+///
+/// # Arguments
+///
+/// `relation_name` - The name of the relation to refer to.
+/// `theta` - The theta angle to check.
+///
+/// # Returns
+///
+/// `Some<f64>` - The phi-theta relation.
+/// 'None' - The relation does not exist.
+///
+pub fn phi_theta_relation(relation_name: &str, mut theta: f64) -> Option<f64> {
+    while theta < 0.0 {
+        theta += 2.0 * std::f64::consts::PI;
+    }
+    while theta > 2.0 * std::f64::consts::PI {
+        theta -= 2.0 * std::f64::consts::PI
+    }
+    match relation_name {
+        "DefaultRelation" => Some(
+            5.11382
+                - 0.32933
+                    * f64::ln(1.63085 * theta * theta * f64::exp(2.0 * theta) + theta + 0.02889),
+        ),
+        _ => None,
+    }
+}
