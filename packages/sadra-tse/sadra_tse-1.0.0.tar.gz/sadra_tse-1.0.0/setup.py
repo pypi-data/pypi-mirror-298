1e4aeeb2c658d3ca27aa2ed57a4dc7b6f98c6427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 05:05:15 +0000

    Add package sadra-tse/1.0.0/sadra_tse-1.0.0.tar.gz

---
+++ packages/sadra-tse/sadra_tse-1.0.0.tar.gz/sadra_tse-1.0.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+# Reads the content of your README.md into a variable to be used in the setup below
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='sadra_tse',                           # should match the package folder
+    packages=['sadra_tse'],                     # should match the package folder
+    version='1.0.0',                            # important for updates
+    license='BSD (3-clause)',                                  # should match your chosen license
+    description='A Python Module to Access Tehran Stock Exchange Historical and Real-Time Data',
+    long_description=long_description,              # loads your README.md
+    long_description_content_type="text/markdown",  # README.md is of type 'markdown'
+    author='Dr.Amirsadra Akbari',
+    author_email='amirsadra.akbari@iau.ir',
+    install_requires=['requests','jdatetime','pandas','numpy','requests','bs4','asyncio','urllib3','aiohttp','unsync','IPython','persiantools','datetime','XlsxWriter','lxml'],                  # list all packages that your package uses
+    
+)
