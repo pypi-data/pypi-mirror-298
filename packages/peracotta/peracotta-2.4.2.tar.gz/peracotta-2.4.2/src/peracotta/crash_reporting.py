1b169e9fda12d7f5d3b78735879566a79882b65a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 04:06:05 +0000

    Add package peracotta/2.4.2/peracotta-2.4.2.tar.gz

---
+++ packages/peracotta/peracotta-2.4.2.tar.gz/peracotta-2.4.2/src/peracotta/crash_reporting.py
@@ -0,0 +1,19 @@
+from .config import CONFIG
+from .peralog import logdir
+from requests import Session
+
+url = CONFIG["REPORT_URL"]
+
+
+def send_crash_notification():
+    with Session() as s:
+        try:
+            s.get(url, timeout=2)
+        except TimeoutError:
+            return False
+
+        with open(logdir.joinpath("peracotta.log"), "r") as fs:
+            res = s.post(url, files={"file": fs})
+
+        if res.status_code == 200:
+            return True
