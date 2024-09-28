61e20aae711f78e3f5c94031ab1a3a5ce81b5538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 06:06:24 +0000

    Add package openpy-pkg/1.0.5/openpy_pkg-1.0.5.tar.gz

---
+++ packages/openpy-pkg/openpy_pkg-1.0.5.tar.gz/openpy_pkg-1.0.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+reqs=[]
+
+with open("./requirements.txt", 'r+') as f:
+    for line in f.readlines():
+        reqs.append(line.replace("\n", ""))
+
+setup(
+    name='openpy-pkg',
+    version='1.0.5',
+    packages=find_packages(),
+    install_requires=reqs,
+    author='Corbin Chandler',
+    author_email='corbinchandler777@icloud.com',
+    description='OpenPython is a python package that allows users to collaborate to make the best python package available!',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/thatcodingdonut/openpython',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
