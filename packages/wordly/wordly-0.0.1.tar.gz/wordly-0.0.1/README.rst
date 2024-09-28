2817b5f3c0a15a480b60427f1fdc97b415d14021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 14:06:10 +0000

    Add package wordly/0.0.1/wordly-0.0.1.tar.gz

---
+++ packages/wordly/wordly-0.0.1.tar.gz/wordly-0.0.1/README.rst
@@ -0,0 +1,6 @@
+======
+Wordly
+======
+
+A python client to communicate with servers implementing the 
+`Dictionary Server Protocol <https://datatracker.ietf.org/doc/html/rfc2229>`_
---
+++ packages/wordly/wordly-0.0.1.tar.gz/wordly-0.0.1/src/wordly/__init__.py
@@ -0,0 +1,11 @@
+"""Wordly
+
+"""
+from wordly.words import Word
+
+__app_name__ = "Wordly"
+__app_description__ = "A simple python client that requests dictionary definitions from a server that implements Dictionary Server Protocol"
+__epilog__ = "Stay in the know."
+__version__ = "0.0.1"
+
+__all__ = ["Word"]
