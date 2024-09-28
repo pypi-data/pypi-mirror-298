b79aa34f7915621ac5f6c27c2ca6b806b4979d2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 03:09:25 +0000

    Add package seq-mutator/0.2.8/seq_mutator-0.2.8.tar.gz

---
+++ packages/seq-mutator/seq_mutator-0.2.8.tar.gz/seq_mutator-0.2.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='seq_mutator',
+    version='0.2.8',
+    description='A toolbox for protein engineering and DNA shuffling',
+    author='iGEM Team Muenster 2024',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author_email='j_albr16@uni-muenster.de',
+    url='https://gitlab.igem.org/2024/software-tools/unimuenster',
+    packages=find_packages(),
+    install_requires=open('requirements.txt').read().splitlines(),
+    include_package_data=True,
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'seq_mutator = src.app:main'
+        ]
+    },
+)
+
