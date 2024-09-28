6af5858a02b3e6e0f45305b1d7721f3a573fdc01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 14:06:24 +0000

    Add package knw-Chromedriver-manager/0.2.1/knw_Chromedriver_manager-0.2.1.tar.gz

---
+++ packages/knw-Chromedriver-manager/knw_Chromedriver_manager-0.2.1.tar.gz/knw_Chromedriver_manager-0.2.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='knw_Chromedriver_manager',
+    version='0.2.1',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='archon.oh',
+    author_email='archon.oh@knworks.co.kr',
+    url='https://github.com/daumsong/knw_webdriver_manager',
+    install_requires=['requests',],
+    packages=find_packages(exclude=[]),
+    keywords=['knw','webdriver', 'manager', 'selenium'],
+    python_requires='>=3.6',
+    package_data={},
+    zip_safe=False,
+)
