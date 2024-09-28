5f2d06a653699f27f017c031e6d02ea3a44b334b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 04:06:18 +0000

    Add package litestar-granian/0.6.0/litestar_granian-0.6.0.tar.gz

---
+++ packages/litestar-granian/litestar_granian-0.6.0.tar.gz/litestar_granian-0.6.0/examples/basic.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+import os
+import signal
+
+import anyio
+from litestar import Controller, Litestar, get
+
+from litestar_granian import GranianPlugin
+
+
+async def dont_run_forever() -> None:
+    await anyio.sleep(5)
+    os.kill(os.getpid(), signal.SIGTERM)
+
+
+class SampleController(Controller):
+    @get(path="/sample")
+    async def sample_route(self) -> dict[str, str]:  # noqa: PLR6301
+        """Sample Route."""
+        return {"sample": "hello-world"}
+
+
+app = Litestar(plugins=[GranianPlugin()], route_handlers=[SampleController], on_startup=[dont_run_forever])
