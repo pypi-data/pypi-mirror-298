a58d0304b11865ef08092cd2000faa1cf1118eb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 05:05:32 +0000

    Add package ttlinks/0.1.18/ttlinks-0.1.18.tar.gz

---
+++ packages/ttlinks/ttlinks-0.1.18.tar.gz/ttlinks-0.1.18/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+# python setup.py sdist bdist_wheel
+# twine upload dist/*
+setup(
+    name='ttlinks',
+    version='0.1.18',
+    packages=find_packages(),
+    license='MIT',
+    description='TTLinks Network Service Packages',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Yantao Tao',
+    author_email='dorisjingjing531@gmail.com',
+    url='https://github.com/tyt063144/TTLinks.git',
+    install_requires=[
+        'inflect'
+    ],
+    keywords=['network', 'ttlinks', 'networking', 'automation', 'ip', 'mac', 'python', 'wildcard', 'subnet'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Telecommunications Industry",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: System :: Networking",
+    ]
+)
