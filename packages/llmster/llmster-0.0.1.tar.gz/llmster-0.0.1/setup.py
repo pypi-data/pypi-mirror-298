88509100785e5c4465260d909d321281f17aae5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 04:05:55 +0000

    Add package llmster/0.0.1/llmster-0.0.1.tar.gz

---
+++ packages/llmster/llmster-0.0.1.tar.gz/llmster-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='llmster',
+    version='0.0.1',
+    description='llmster',
+    author='LM Studio',
+    author_email='team@lmstudio.ai',
+    zip_safe=False
+)
