8f45ed14e5de0b138637192347359e6307d4075d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 14:06:11 +0000

    Add package enimation/0.1.1/enimation-0.1.1.tar.gz

---
+++ packages/enimation/enimation-0.1.1.tar.gz/enimation-0.1.1/setup.py
@@ -0,0 +1,20 @@
+# ani_mation/setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='enimation',
+    version='0.1.1',
+    packages=find_packages(),
+    description='A customizable loading tool',
+    long_description="README.md",
+    author='Hasanfq',
+    author_email='hasanfq818@gmail.com',
+    url='https://github.com/Kamanati/enimation',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
