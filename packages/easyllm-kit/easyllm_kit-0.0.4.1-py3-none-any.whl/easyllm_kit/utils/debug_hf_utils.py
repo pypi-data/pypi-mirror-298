7b40725911f781353e8d6c3af33d4ca41d06c0bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 03:09:04 +0000

    Add package easyllm-kit/0.0.4.1/easyllm_kit-0.0.4.1-py3-none-any.whl

---
+++ packages/easyllm-kit/easyllm_kit-0.0.4.1-py3-none-any.whl/easyllm_kit/utils/debug_hf_utils.py
@@ -0,0 +1,30 @@
+
+# Debugging: Print the evaluation metrics after training
+def print_evaluation_metrics(trainer):
+    eval_result = trainer.evaluate()
+    message = f"Evaluation Metrics: {eval_result}"
+    return message
+
+
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    message = f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param:.2f}"
+    return message
+
+
+def print_trainable_layers(model):
+    # print trainable parameters for inspection
+    message = "Trainable layers:\n"
+    for name, param in model.named_parameters():
+        if param.requires_grad:
+            message += f"\t{name}\n"
+    return message.strip()  # Remove trailing newline
+    
