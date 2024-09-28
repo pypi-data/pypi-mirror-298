278e4ce2f8deca71b7d1a4e19f37170fa93c7f5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 06:06:21 +0000

    Add package buzzerboy-saas-tenants/0.1.5/buzzerboy_saas_tenants-0.1.5-py3-none-any.whl

---
+++ packages/buzzerboy-saas-tenants/buzzerboy_saas_tenants-0.1.5-py3-none-any.whl/buzzerboy_saas_tenants/apps/invites/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class InvitesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'buzzerboy_saas_tenants.apps.invites'
+    label = 'buzzerboy_saas_tenants_apps_invites'
