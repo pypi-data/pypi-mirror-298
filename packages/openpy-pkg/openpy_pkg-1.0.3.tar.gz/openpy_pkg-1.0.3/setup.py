88f68799e2631abc006679a77910b18fff4502ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 06:06:04 +0000

    Add package openpy-pkg/1.0.3/openpy_pkg-1.0.3.tar.gz

---
+++ packages/openpy-pkg/openpy_pkg-1.0.3.tar.gz/openpy_pkg-1.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='openpy-pkg',
+    version='1.0.3',
+    packages=find_packages(),
+    install_requires=[
+        "colorama"
+    ],
+    author='Corbin Chandler',
+    author_email='corbinchandler777@icloud.com',
+    description='OpenPython is a python package that allows users to collaborate to make the best python package available!',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/thatcodingdonut/openpython',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
