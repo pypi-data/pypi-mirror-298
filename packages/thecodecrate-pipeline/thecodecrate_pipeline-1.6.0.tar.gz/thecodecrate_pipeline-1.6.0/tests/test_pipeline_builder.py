efcc9a888ca73850e961201e17e33092da1bce89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 14:05:49 +0000

    Add package thecodecrate-pipeline/1.6.0/thecodecrate_pipeline-1.6.0.tar.gz

---
+++ packages/thecodecrate-pipeline/thecodecrate_pipeline-1.6.0.tar.gz/thecodecrate_pipeline-1.6.0/tests/test_pipeline_builder.py
@@ -0,0 +1,44 @@
+import pytest
+
+from thecodecrate_pipeline import (
+    PipelineBuilder,
+)
+from .stubs.stub_int_stages import (
+    AddOneStage,
+    TimesThreeStage,
+    TimesTwoStage,
+)
+
+
+@pytest.mark.asyncio
+async def test_pipeline_builder():
+    pipeline_builder = (
+        (PipelineBuilder[int]())
+        .add(AddOneStage())
+        .add(TimesTwoStage())
+        .add(lambda payload: payload + 1)
+        .add(AddOneStage())
+    )
+
+    pipeline = pipeline_builder.build()
+
+    assert await pipeline.process(1) == 6
+
+
+@pytest.mark.asyncio
+async def test_immutability():
+    pipeline_builder = (
+        (PipelineBuilder[int]())
+        .add(AddOneStage())
+        .add(TimesTwoStage())
+        .add(AddOneStage())
+    )
+
+    pipeline = pipeline_builder.build()
+
+    assert await pipeline.process(1) == 5
+
+    new_builder = pipeline_builder.add(TimesThreeStage())
+
+    assert await new_builder.build().process(1) == 15
+    assert await pipeline.process(1) == 5
