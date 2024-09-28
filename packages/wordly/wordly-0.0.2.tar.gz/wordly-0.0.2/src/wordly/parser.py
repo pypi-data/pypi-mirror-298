d2c31e37aa8ce138a85416a3ccb99a7d6dd30ddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 14:05:55 +0000

    Add package wordly/0.0.2/wordly-0.0.2.tar.gz

---
+++ packages/wordly/wordly-0.0.2.tar.gz/wordly-0.0.2/src/wordly/parser.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+from wordly.status_codes import Status
+
+
+class DictParser:
+    def __init__(self, delimiter: bytes = b"\r\n"):
+        self.line = b""
+        self.mapping = defaultdict(str)
+        self.DELIMITER = delimiter
+
+    def _process_line(self, ending: str = ""):
+        code = self.line[:3]
+
+        if status := Status.by_value(code):
+            self.mapping[status.name] += self.line[4:].decode() + ending
+            self.part = status.name
+        else:
+            self.mapping[self.part] += self.line.decode() + ending
+
+    def feed(self, stream: bytes):
+        split = stream.split(self.DELIMITER, 1)
+        while len(split) > 1:
+            old, new = split
+            self.line += old
+            self._process_line("\n")
+            self.line = b""
+            split = new.split(self.DELIMITER, 1)
+
+        if line := split[0]:
+            self.line += line
+            self._process_line()
+            self.line = b""
