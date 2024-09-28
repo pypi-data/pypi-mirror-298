a510f3940a64dfbd16fcd2dbecb698e684275e43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 14:06:19 +0000

    Add package pixeltable/0.2.19/pixeltable-0.2.19.tar.gz

---
+++ packages/pixeltable/pixeltable-0.2.19.tar.gz/pixeltable-0.2.19/pixeltable/exceptions.py
@@ -0,0 +1,21 @@
+from typing import List, Any
+from types import TracebackType
+from dataclasses import dataclass
+
+
+class Error(Exception):
+    pass
+
+
+@dataclass
+class ExprEvalError(Exception):
+    expr: Any  # exprs.Expr, but we're not importing pixeltable.exprs to avoid circular imports
+    expr_msg: str
+    exc: Exception
+    exc_tb: TracebackType
+    input_vals: List[Any]
+    row_num: int
+
+
+class PixeltableWarning(Warning):
+    pass
