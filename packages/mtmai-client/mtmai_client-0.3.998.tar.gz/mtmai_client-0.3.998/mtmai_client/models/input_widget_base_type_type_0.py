65f1144fdc6e0f521145782b470831555b145d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-29 15:06:03 +0000

    Add package mtmai-client/0.3.998/mtmai_client-0.3.998.tar.gz

---
+++ packages/mtmai-client/mtmai_client-0.3.998.tar.gz/mtmai_client-0.3.998/mtmai_client/models/input_widget_base_type_type_0.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class InputWidgetBaseTypeType0(str, Enum):
+    ARRAY = "array"
+    BOOLEAN = "boolean"
+    NUMBER = "number"
+    OBJECT = "object"
+    SELECT = "select"
+    STRING = "string"
+    TAGS = "tags"
+
+    def __str__(self) -> str:
+        return str(self.value)
