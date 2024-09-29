bc08f08d81f6febf981db86aa06b3108cf823db8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-29 14:06:10 +0000

    Add package psgd-torch/0.6.1/psgd_torch-0.6.1-py3-none-any.whl

---
+++ packages/psgd-torch/psgd_torch-0.6.1-py3-none-any.whl/psgd_torch/__init__.py
@@ -0,0 +1,3 @@
+from .psgd import LRA, Affine, Newton,XMat, Kron  # Import LRA, Affine, and Newton from the psgd module
+
+__all__ = ['LRA', 'Affine', 'Newton', 'XMat', 'Kron']  # This makes LRA accessible as psgd.LRA when the package is imported
