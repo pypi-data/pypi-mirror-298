27e563ac4988acb3605842be6a411b39570ab6c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-29 03:14:35 +0000

    Add package knowing-machine/0.0.3/knowing_machine-0.0.3.tar.gz

---
+++ packages/knowing-machine/knowing_machine-0.0.3.tar.gz/knowing_machine-0.0.3/README.md
@@ -0,0 +1,10 @@
+# Knowing Machine
+The machine that knows the answers to all questions.
+<pre>
+  pip install knowing-machine
+</pre>
+Then:
+```Python
+  # Python
+  import knowing_machine
+```
---
+++ packages/knowing-machine/knowing_machine-0.0.3.tar.gz/knowing_machine-0.0.3/src/knowing_machine/__init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Python
+
+"""Copyright (c) Alexander Fedotov.
+This source code is licensed under the license found in the
+LICENSE file in the root directory of this source tree.
+"""
+import os
+from . import githf
+import yaml
+
+# Constants
+MACHINE_ORGANIZATION_NAME = 'knowing-machine'  # or other organization
+PRIVATE_REPO_WITH_TEXT = 'knowing_machine'
+
+# Location of the file that is running
+where_are_we = os.path.dirname(__file__)
+
+try:
+    gh = githf.connectto_repo(organization=MACHINE_ORGANIZATION_NAME,
+                              repository_name=PRIVATE_REPO_WITH_TEXT,
+                              private=True)
+    MACHINE_YAML = githf.read_file(repository=gh, file_path='machina.yaml')
+
+except Exception as e:
+    machina_path = os.path.join(where_are_we, 'machina.yaml')
+    with open(machina_path, 'r') as yaml_file:
+        MACHINE_YAML = yaml_file.read()
+
+NAME_OF_THE_MACHINE = yaml.load(MACHINE_YAML, Loader=yaml.FullLoader)
+
+# Save the file
+file_path = os.path.join(where_are_we, 'other.yaml')
+with open(file_path, 'w') as yaml_file:
+    yaml.dump(MACHINE_YAML, yaml_file)
