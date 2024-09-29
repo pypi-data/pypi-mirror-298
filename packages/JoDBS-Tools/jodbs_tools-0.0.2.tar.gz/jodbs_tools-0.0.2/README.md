f9d67b60a5cbfec14f05b71caf9f7a0eab59e42d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-29 04:05:57 +0000

    Add package JoDBS-Tools/0.0.2/jodbs_tools-0.0.2.tar.gz

---
+++ packages/JoDBS-Tools/jodbs_tools-0.0.2.tar.gz/jodbs_tools-0.0.2/README.md
@@ -0,0 +1,8 @@
+# JoDBS Tools
+
+A utility for cleaning up your code in Discord Bots, and connecting it to your own nodes; If configured correctly.
+
+## Installation
+
+```sh
+pip install jodbs_tools 
---
+++ packages/JoDBS-Tools/jodbs_tools-0.0.2.tar.gz/jodbs_tools-0.0.2/src/JoDBS_Tools/Database.py
@@ -0,0 +1,44 @@
+import requests
+from pymongo import MongoClient, errors
+from .utils import Get_ENV
+
+class Database:
+    def __init__(self, connection_string=None, collection=None):
+        self.connection_string = connection_string or Get_ENV("CONNECTION_STRING")
+        self.collection = collection
+        self.client = None
+        self.db = None
+
+    def connect(self):
+        try:
+            self.client = MongoClient(self.connection_string)
+            self.db = self.client[self.collection]
+            print("MongoDB Connection: Successful ✔️")
+        except errors.ServerSelectionTimeoutError as err:
+            print(f"MongoDB Connection: Failed ❌ - {err}")
+            raise Exception("MongoDB Connection: Failed ❌")
+
+    def get_database(self):
+        if not self.db:
+            self.connect()
+        return self.db
+    
+class BotNetworkConnection:
+    def __init__(self, api_url=None, token=None):
+        self.api_url = api_url or Get_ENV("API_URL")
+        self.token = token or Get_ENV("API_TOKEN")
+        self.headers = {
+            'Authorization': f'Bearer {self.token}',
+            'Content-Type': 'application/json'
+        }
+
+    def get_data(self, endpoint):
+        url = f"{self.api_url}/{endpoint}"
+        try:
+            response = requests.get(url, headers=self.headers)
+            response.raise_for_status()
+            print("API Connection: Successful ✔️", self.api_url, endpoint, response.status_code)
+            return response.json()
+        except requests.exceptions.HTTPError as err:
+            print(f"API Connection: Failed ❌ - {err}")
+            raise Exception("API Connection: Failed ❌")
