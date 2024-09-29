08639f39747072f5db42020fead1cb13e9995b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-29 03:13:55 +0000

    Add package cvxpy-gurobi/1.0.0/cvxpy_gurobi-1.0.0.tar.gz

---
+++ packages/cvxpy-gurobi/cvxpy_gurobi-1.0.0.tar.gz/cvxpy_gurobi-1.0.0/tests/test_atoms.py
@@ -0,0 +1,24 @@
+import cvxpy as cp
+import gurobipy as gp
+import pytest
+
+import cvxpy_gurobi
+import test_problems
+from cvxpy_gurobi.translation import Translater
+
+
+@pytest.mark.xfail(reason="TODO: implement all atoms")
+def test_no_missing_atoms() -> None:
+    missing = [
+        atom
+        for atom in cp.EXP_ATOMS + cp.PSD_ATOMS + cp.SOC_ATOMS + cp.NONPOS_ATOMS
+        if getattr(Translater, f"visit_{atom.__name__}", None) is None  # type: ignore[attr-defined]
+    ]
+    assert missing == []
+
+
+@pytest.mark.parametrize("case", test_problems.invalid_expressions())
+def test_failing_atoms(case: test_problems.ProblemTestCase) -> None:
+    translater = Translater(gp.Model())
+    with pytest.raises(cvxpy_gurobi.UnsupportedExpressionError):
+        translater.visit(case.problem.objective.expr)
