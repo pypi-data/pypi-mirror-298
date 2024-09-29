8af21ad7ce09fffbe859ae28a9c0d7a9b419cbff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-29 14:06:10 +0000

    Add package dj-multihash/0.1/dj_multihash-0.1.tar.gz

---
+++ packages/dj-multihash/dj_multihash-0.1.tar.gz/dj_multihash-0.1/setup.py
@@ -0,0 +1,21 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="dj_multihash",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=["hashlib"],  # Ajoute ici les dépendances
+    author="DJSTUDIO",
+    description="library that combiens multiple technics of hash in only one function",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="",  # Lien vers le dépôt
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
