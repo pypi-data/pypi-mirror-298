82dbfb890b7192a7099d3e682b36ff7b00379760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-29 14:06:28 +0000

    Add package beat/2.0.3/beat-2.0.3.tar.gz

---
+++ packages/beat/beat-2.0.3.tar.gz/beat-2.0.3/beat/info.py
@@ -0,0 +1,7 @@
+# This module is automatically created from setup.py
+project_root = '/home/runner/work/beat/beat'
+git_sha1 = '8a90e49c32bbda6702db35e18631d3b6017e529c'
+local_modifications = False
+version = '2.0.3'
+long_version = 'beat-2.0.3-8a90e49c32bbda6702db35e18631d3b6017e529c-2024-09-28_22:27:05'  # noqa
+installed_date = '2024-09-28_22:27:05'
