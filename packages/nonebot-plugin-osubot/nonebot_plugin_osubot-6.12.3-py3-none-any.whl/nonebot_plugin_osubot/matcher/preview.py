49a03d503b9e5e5dd41a238e27bd92c8c0dfe4e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-29 15:05:50 +0000

    Add package nonebot-plugin-osubot/6.12.3/nonebot_plugin_osubot-6.12.3-py3-none-any.whl

---
+++ packages/nonebot-plugin-osubot/nonebot_plugin_osubot-6.12.3-py3-none-any.whl/nonebot_plugin_osubot/matcher/preview.py
@@ -0,0 +1,37 @@
+from nonebot import on_command
+from nonebot.typing import T_State
+from nonebot.params import CommandArg
+from nonebot.internal.adapter import Message
+from nonebot_plugin_alconna import UniMessage
+
+from ..api import osu_api
+from ..file import download_tmp_osu
+from ..mania import generate_preview_pic
+from ..draw.catch_preview import draw_cath_preview
+
+generate_preview = on_command("预览", aliases={"preview", "完整预览"}, priority=11, block=True)
+
+
+@generate_preview.handle()
+async def _(
+    state: T_State,
+    args: Message = CommandArg(),
+):
+    osu_id = args.extract_plain_text().strip()
+    if not osu_id or not osu_id.isdigit():
+        await UniMessage.text("请输入正确的地图mapID").finish(reply_to=True)
+    data = await osu_api("map", map_id=int(osu_id))
+    if not data:
+        await UniMessage.text("未查询到该地图").finish(reply_to=True)
+    if isinstance(data, str):
+        await UniMessage.text(data).finish(reply_to=True)
+    if data["mode"] == "mania":
+        osu = await download_tmp_osu(osu_id)
+        if state["_prefix"]["command"][0] == "完整预览":
+            pic = await generate_preview_pic(osu, True)
+        else:
+            pic = await generate_preview_pic(osu)
+        await UniMessage.image(raw=pic).finish(reply_to=True)
+    elif data["mode"] == "fruits" or data["mode"] == "osu":
+        pic = await draw_cath_preview(int(osu_id), data["beatmapset_id"])
+        await UniMessage.image(raw=pic).finish(reply_to=True)
