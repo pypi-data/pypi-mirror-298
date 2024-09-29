b5be79c9429d9fbb2323ae76e27abbe8660264f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-29 03:18:27 +0000

    Add package cbr-athena/0.56.15/cbr_athena-0.56.15.tar.gz

---
+++ packages/cbr-athena/cbr_athena-0.56.15.tar.gz/cbr_athena-0.56.15/cbr_athena/llms/chats/LLM__Platform_Engine__Open_Router.py
@@ -0,0 +1,23 @@
+from cbr_athena.llms.chats.LLM__Platform_Engine import LLM__Platform_Engine
+from cbr_athena.llms.providers.open_router.LLM__Open_Router import LLM__Open_Router
+from cbr_shared.schemas.base_models.chat_threads.LLMs__Chat_Completion import LLMs__Chat_Completion
+
+
+class LLM__Platform_Engine__Open_Router(LLM__Platform_Engine):
+    llm_platform       : str
+    llm_provider       : str
+    llm_model          : str
+    llm_chat_completion: LLMs__Chat_Completion
+    llm_open_router    : LLM__Open_Router
+
+    def execute_request(self):
+        with self.llm_open_router as _:
+            _.add_messages__system(self.llm_chat_completion.system_prompts)
+            _.add_histories       (self.llm_chat_completion.histories     )
+            _.add_message__user   (self.llm_chat_completion.user_prompt   )
+            _.set_model           (self.llm_model                         )
+            _.set_stream          (self.llm_chat_completion.stream        )
+            return _.chat_completion()
+
+
+
