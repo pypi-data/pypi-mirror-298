58825194b760313e8aa3fa64e28c7b883cbb9ef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-29 14:06:32 +0000

    Add package mtmai/0.3.990/mtmai-0.3.990-py3-none-any.whl

---
+++ packages/mtmai/mtmai-0.3.990-py3-none-any.whl/mtmai/agents/nodes/chat_post_gen.py
@@ -0,0 +1,33 @@
+"""
+GraphIterator Module
+"""
+
+import mtmai.chainlit as cl
+
+from .base_node import BaseNode
+
+DEFAULT_BATCHSIZE = 16
+
+
+class ChatPostGenNode(BaseNode):
+    """ """
+
+    def __init__(
+        self,
+        # input: str,
+        # output: List[str],
+        # node_config: Optional[dict] = None,
+        # node_name: str = "GraphIterator",
+    ):
+        # super().__init__(codename, "node", input, output, 2, node_config)
+
+        # self.verbose = (
+        #     False if node_config is None else node_config.get("verbose", False)
+        # )
+        pass
+
+    async def __call__(self, state: dict, batchsize: int) -> dict:
+        """"""
+        await cl.Message(content="欢迎 post gen开始吧").send()
+
+        return {}
