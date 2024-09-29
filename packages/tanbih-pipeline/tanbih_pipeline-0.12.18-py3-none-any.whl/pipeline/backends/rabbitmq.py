eb0c38e79ee864bc30d771729c62ee3ec542891b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-29 03:14:42 +0000

    Add package tanbih-pipeline/0.12.18/tanbih_pipeline-0.12.18-py3-none-any.whl

---
+++ packages/tanbih-pipeline/tanbih_pipeline-0.12.18-py3-none-any.whl/pipeline/backends/rabbitmq.py
@@ -0,0 +1,174 @@
+import time
+import logging
+from logging import Logger
+from typing import ClassVar, Iterator, Annotated
+
+import pika
+from pydantic.networks import Url, UrlConstraints
+from pydantic import Field
+
+from ..tap import SourceTap, SourceSettings, DestinationTap, DestinationSettings
+from ..message import MessageBase
+from ..helpers import namespaced_topic
+
+
+pipelineLogger = logging.getLogger("pipeline")
+
+
+RabbitMQDsn = Annotated[Url, UrlConstraints(allowed_schemes=["amqp"])]
+
+
+class RabbitMQSourceSettings(SourceSettings):
+    rabbitmq: RabbitMQDsn = Field("amqp://localhost", title="RabbitMQ host")
+
+
+class RabbitMQSource(SourceTap):
+    """RabbitMQSource reads from RabbitMQ
+
+    RabbitMQ options:
+        --rabbitmq (env: RABBITMQ): RabbitMQ host
+
+    Source options:
+        --in-topic (env: INTOPIC): queue to read from
+        --timeout (env: TIMEOUT): seconds to exit if no new messages
+
+    >>> import logging
+    >>> from unittest.mock import patch
+    >>> settings = RabbitMQSourceSettings()
+    >>> with patch('pika.ConnectionParameters') as c1:
+    ...     with patch('pika.BlockingConnection') as c2:
+    ...         RabbitMQSource(settings=settings, logger=logging)
+    RabbitMQSource(queue="in-topic")
+    """
+
+    settings: RabbitMQSourceSettings
+
+    kind: ClassVar[str] = "RABBITMQ"
+
+    def __init__(
+        self, settings: RabbitMQSourceSettings, logger: Logger = pipelineLogger
+    ) -> None:
+        super().__init__(settings, logger)
+        self.settings = settings
+        self.topic = settings.topic
+        self.name = namespaced_topic(settings.topic, settings.namespace)
+        self.timeout = settings.timeout
+        parameters = pika.ConnectionParameters(settings.rabbitmq)
+        self.rabbit = pika.BlockingConnection(parameters)
+        self.channel = self.rabbit.channel()
+        self.channel.queue_declare(queue=self.name)
+        self.delivery_tag = None
+        self.msg = None
+        self.logger.info("RabbitMQSource initialized.")
+
+    def __repr__(self) -> str:
+        return f'RabbitMQSource(queue="{self.name}")'
+
+    def __len__(self) -> int:
+        return -1
+
+    def read(self) -> Iterator[MessageBase]:
+        timedOut = False
+        lastMessageBaseTime = time.time()
+
+        while not timedOut:
+            try:
+                method, header, body = self.channel.basic_get(self.name)
+            except pika.exceptions.AMQPConnectionError:
+                self.logger.warning("Trying to restore connection to RabbitMQ...")
+                parameters = pika.ConnectionParameters(self.settings.rabbitmq)
+                self.rabbit = pika.BlockingConnection(parameters)
+                self.channel = self.rabbit.channel()
+                self.logger.warning("Connection to RabbitMQ restored.")
+                method, header, body = self.channel.basic_get(self.name)
+            except Exception as ex:
+                self.logger.error(ex)
+                break
+
+            if method:
+                self.delivery_tag = method.delivery_tag
+                self.logger.info("Read message %s", self.delivery_tag)
+                yield MessageBase.deserialize(body)
+                lastMessageBaseTime = time.time()
+            time.sleep(0.01)
+            if self.timeout > 0 and time.time() - lastMessageBaseTime > self.timeout:
+                self.logger.info("RabbitMQSource timed out.")
+                timedOut = True
+
+    def acknowledge(self) -> None:
+        self.logger.info("acknowledged message %s", self.delivery_tag)
+        self.channel.basic_ack(self.delivery_tag)
+
+    def close(self) -> None:
+        self.logger.info("RabbitMQSource closed.")
+        self.channel.close()
+        self.rabbit.close()
+
+
+class RabbitMQDestinationSettings(DestinationSettings):
+    rabbitmq: RabbitMQDsn = Field("amqp://localhost", title="RabbitMQ host")
+
+
+class RabbitMQDestination(DestinationTap):
+    """RabbitMQDestination writes to RabbitMQ
+
+    options:
+        --rabbitmq (env: RABBITMQ): RabbitMQ host
+
+    standard options:
+        --out-topic (env: OUTTOPIC): queue to write to
+
+    >>> import logging
+    >>> from unittest.mock import patch
+    >>> settings = RabbitMQDestinationSettings()
+    >>> with patch('pika.ConnectionParameters') as c1:
+    ...     with patch('pika.BlockingConnection') as c2:
+    ...        RabbitMQDestination(settings=settings, logger=logging)
+    RabbitMQDestination(queue="out-topic")
+    """
+
+    settings: RabbitMQDestinationSettings
+
+    kind: ClassVar[str] = "RABBITMQ"
+
+    def __init__(self, settings: RabbitMQDestinationSettings, logger: Logger) -> None:
+        super().__init__(settings, logger)
+        self.settings = settings
+        self.topic = settings.topic
+        self.name = namespaced_topic(settings.topic, settings.namespace)
+        parameters = pika.ConnectionParameters(settings.rabbitmq, heartbeat=5)
+        self.rabbit = pika.BlockingConnection(parameters)
+        self.channel = self.rabbit.channel()
+        self.channel.queue_declare(queue=self.name)
+        self.logger.info("RabbitMQDestination initialized.")
+
+    def __repr__(self) -> str:
+        return f'RabbitMQDestination(queue="{self.name}")'
+
+    def __len__(self) -> int:
+        return -1
+
+    def write(self, message: MessageBase) -> int:
+        serialized = message.serialize()
+        try:
+            self.channel.basic_publish(
+                exchange="", routing_key=self.name, body=serialized
+            )
+        except pika.exceptions.StreamLostError:
+            self.logger.warning("Trying to restore connection to RabbitMQ...")
+            self.rabbit = pika.BlockingConnection(
+                pika.ConnectionParameters(self.settings.rabbitmq)
+            )
+            self.channel = self.rabbit.channel()
+            self.logger.warning("Connection to RabbitMQ restored.")
+            self.channel.basic_publish(
+                exchange="",
+                routing_key=self.name,
+                body=message.serialize(compress=self.settings.compress),
+            )
+        return len(serialized)
+
+    def close(self) -> None:
+        self.logger.info("RabbitMQDestination closed.")
+        self.channel.close()
+        self.rabbit.close()
---
+++ packages/tanbih-pipeline/tanbih_pipeline-0.12.18-py3-none-any.whl/pipeline/helpers.py
@@ -0,0 +1,259 @@
+import sys
+import time
+from enum import Enum
+from argparse import ArgumentParser, Action
+from typing import List, Dict, Any, Optional, Union
+from os import PathLike
+from logging import Logger
+
+from pydantic_settings import BaseSettings
+
+
+def namespaced_topic(topic: str, namespace: str = None) -> str:
+    if namespace:
+        return "{}/{}".format(namespace, topic)
+    else:
+        return topic
+
+
+def parse_mappings(mappings: str) -> Dict[str, str]:
+    d = {}
+    if mappings:
+        for pair in mappings.split(","):
+            map_from, map_to = (n.strip() for n in pair.split(":"))
+            d[map_from] = map_to
+    return d
+
+
+StrPath = Union[str, PathLike]
+env_file_sentinel = str(object())
+
+
+class Settings(BaseSettings):
+    """Settings can read from environment variable and parse
+    command line arguments as well.
+
+    Usage:
+    >>> class ASettings(Settings):
+    ...     name:str
+    ...     class Config:
+    ...         env_prefix = 'in_'
+    >>> settings = ASettings(name='name')
+    >>> settings.name
+    'name'
+    >>> options, _ = settings.parse_args(args="--in-name pipeline".split())
+    >>> settings.name
+    'pipeline'
+    >>> settings = ASettings(_args="--in-name yifan".split())
+    >>> settings.name
+    'yifan'
+    """
+
+    def __init__(
+        __pipeline_self,
+        _env_prefix: Optional[str] = None,
+        _env_file: Optional[StrPath] = env_file_sentinel,
+        _env_file_encoding: Optional[str] = None,
+        _secrets_dir: Optional[StrPath] = None,
+        _args: List[str] = sys.argv,
+        _parser: Optional[ArgumentParser] = None,
+        **values: Any,
+    ) -> None:
+        init_kwargs = __pipeline_self._build_values_args(
+            values,
+            args=_args,
+            parser=_parser,
+        )
+        super(Settings, __pipeline_self).__init__(
+            _env_file=_env_file,
+            _env_file_encoding=_env_file_encoding,
+            _secrets_dir=_secrets_dir,
+            **init_kwargs,
+        )
+
+    def _build_values_args(
+        self,
+        values: Dict[str, Any],
+        args: List[str],
+        parser: Optional[ArgumentParser] = None,
+    ) -> Dict[str, Any]:
+        options, unknown = self.parse_args(args, parser=parser, update=False)
+        args = vars(options)
+
+        for name, field in self.__fields__.items():
+            fullname = self.model_config.get("env_prefix", "") + name
+            value = args.get(fullname, None)
+            if value is not None:
+                values[name] = value
+
+        return values
+
+    def parse_args(self, args: List[str], parser=None, update=True) -> ArgumentParser:
+        """
+        :param args:
+        :param parser:
+        :param update: True if update Settings in place
+        """
+        settings_ref: Settings = self
+        env_prefix = self.model_config.get("env_prefix", "")
+
+        class BooleanOptionalAction(Action):
+            settings = settings_ref
+
+            def __init__(  # type: ignore
+                self,
+                option_strings,
+                dest,
+                default=None,
+                type=None,
+                choices=None,
+                required=False,
+                help=None,
+                metavar=None,
+            ):
+
+                _option_strings = []
+                for option_string in option_strings:
+                    _option_strings.append(option_string)
+
+                    if option_string.startswith("--"):
+                        option_string = "--no-" + option_string[2:]
+                        _option_strings.append(option_string)
+
+                if help is not None and default is not None:
+                    help += f" (default: {default})"
+
+                super().__init__(
+                    option_strings=_option_strings,
+                    dest=dest,
+                    nargs=0,
+                    default=default,
+                    type=type,
+                    choices=choices,
+                    required=required,
+                    help=help,
+                    metavar=metavar,
+                )
+
+            def __call__(self, parser, namespace, values, option_string=None):  # type: ignore
+                dest = self.dest
+                if env_prefix:
+                    base_dest = self.dest[len(env_prefix) :]  # NOQA
+                else:
+                    base_dest = self.dest
+                if option_string in self.option_strings:
+                    if update:
+                        setattr(
+                            self.settings,
+                            base_dest,
+                            not option_string.startswith("--no-"),
+                        )
+                    else:
+                        setattr(namespace, dest, not option_string.startswith("--no-"))
+
+            def format_usage(self):  # type: ignore
+                return " | ".join(self.option_strings)
+
+        class SetSettingsAction(Action):
+            settings = settings_ref
+
+            def __call__(self, parser, namespace, values, option_string=None):  # type: ignore
+                dest = self.dest
+                if env_prefix:
+                    base_dest = self.dest[len(env_prefix) :]  # NOQA
+                else:
+                    base_dest = self.dest
+                if update and hasattr(self.settings, base_dest):
+                    setattr(self.settings, base_dest, values)
+                else:
+                    setattr(namespace, dest, values)
+
+        class SetEnumSettingsAction(Action):
+            settings = settings_ref
+
+            def __call__(self, parser, namespace, value, option_string=None):  # type: ignore
+                if value in tuple(val.name for val in self.annotation):
+                    value = self.annotation[value]
+                dest = self.dest
+                if env_prefix:
+                    base_dest = self.dest[len(env_prefix) :]  # NOQA
+                else:
+                    base_dest = self.dest
+                if update and hasattr(self.settings, base_dest):
+                    setattr(self.settings, base_dest, value)
+                else:
+                    setattr(namespace, dest, value)
+
+        if parser is None:
+            parser = ArgumentParser(add_help=False)
+
+        for name, field in self.__fields__.items():
+            name = (env_prefix + name).replace("_", "-")
+            if field.annotation == bool:
+                action = BooleanOptionalAction
+            elif isinstance(field.annotation, type) and issubclass(
+                field.annotation, Enum
+            ):
+                class_attrs = {
+                    "annotation": field.annotation,
+                }
+                action = type(
+                    "SetEnumSettingsAction" + (env_prefix + name),
+                    (SetEnumSettingsAction,),
+                    class_attrs,
+                )
+            else:
+                action = SetSettingsAction
+            parser.add_argument(
+                f"--{name}",
+                type=field.annotation,
+                action=action,
+                help=field.title,
+            )
+
+        options, unknown = parser.parse_known_args(args)
+        return options, unknown
+
+
+class Timer:
+    def __init__(self) -> None:
+        self.startTime = time.perf_counter()
+        self.startProcessTime = time.process_time()
+        self.totalTime = 0.0
+        self.totalProcessTime = 0.0
+        self.timeCount = 0
+        self.processTimeCount = 0
+
+    def elapsed_time(self) -> float:
+        t = time.perf_counter() - self.startTime
+        self.totalTime += t
+        self.timeCount += 1
+        return t
+
+    def average_time(self) -> float:
+        return self.totalTime / self.timeCount
+
+    def process_time(self) -> float:
+        t = time.process_time() - self.startProcessTime
+        self.totalProcessTime += t
+        self.processTimeCount += 1
+        return t
+
+    def average_process_time(self) -> float:
+        return self.totalProcessTime / self.processTimeCount
+
+    def start(self) -> None:
+        self.startTime = time.perf_counter()
+        self.startProcessTime = time.process_time()
+
+    def log(self, logger: Logger) -> None:
+        elapsedTime = self.elapsed_time()
+        averageTime = self.average_time()
+        logger.info("Elapsed Time: %.2f, Average Time: %.2f", elapsedTime, averageTime)
+        processTime = self.process_time()
+        averageProcessTime = self.average_process_time()
+        logger.info(
+            "Process Time: %.2f, Average Process Time: %.2f",
+            processTime,
+            averageProcessTime,
+        )
