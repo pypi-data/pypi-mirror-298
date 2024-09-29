93073cd479d75a71b9a39359e603699350ef40c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-29 15:05:55 +0000

    Add package mtmai/0.3.997/mtmai-0.3.997-py3-none-any.whl

---
+++ packages/mtmai/mtmai-0.3.997-py3-none-any.whl/mtmai/agents/chat_profiles/chat_post_gen.py
@@ -0,0 +1,50 @@
+"""
+GraphIterator Module
+"""
+
+import mtmai.chainlit as cl
+from mtmai.chainlit.chat_settings import ThreadForm
+from mtmai.chainlit.thread_uistate import ThreadUIState
+from mtmai.core.logging import get_logger
+from mtmai.mtlibs.inputs.input_widget import TextInput
+
+from ..nodes.base_node import BaseNode
+
+logger = get_logger()
+
+
+class ChatPostGenNode(BaseNode):
+    """ """
+
+    def __init__(
+        self,
+    ):
+        pass
+
+    async def __call__(self, state: dict, batchsize: int) -> dict:
+        """"""
+        # TODO: langgraph 调用入口
+
+        return {}
+
+    async def chat_start(self):
+        await cl.Message(content="post gen 博客文章生成开始").send()
+        await ThreadUIState(
+            title="博客文章生成器",
+            description="博客文章生成器",
+            threadForm=ThreadForm(
+                open=True,
+                inputs=[
+                    TextInput(
+                        name="test1",
+                        label="prompt",
+                        placeholder="请输入prompt",
+                        description="附加的提示语",
+                    ),
+                ],
+            ),
+        ).send()
+
+    async def on_message(self, message: cl.Message):
+        logger.info("TODO: on_message (ChatPostGenNode)")
+        pass
---
+++ packages/mtmai/mtmai-0.3.997-py3-none-any.whl/mtmai/chainlit/thread_uistate.py
@@ -0,0 +1,25 @@
+from typing import Any
+
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+
+from mtmai.chainlit.chat_settings import ThreadForm
+from mtmai.chainlit.context import context
+
+
+class ThreadUIState(BaseModel):
+    """ThreadView 的UI 状态"""
+
+    enableChat: bool | None = False
+    enableScrollToBottom: bool = True
+    title: str | None = None
+    description: str | None = None
+    icons: str | None = None
+    layout: str | None = None
+    theme: str | None = None
+    threadForm: ThreadForm | None = None
+
+    async def send(self) -> dict[str, Any]:
+        await context.emitter.emit("thread_ui_state", jsonable_encoder(self))
+
+        return self
