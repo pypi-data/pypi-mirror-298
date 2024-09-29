bb777de445f082da0c9bb5ed9c2e04c427354827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-29 03:13:45 +0000

    Add package rf-quick-dashboard/0.1.4/rf_quick_dashboard-0.1.4.tar.gz

---
+++ packages/rf-quick-dashboard/rf_quick_dashboard-0.1.4.tar.gz/rf_quick_dashboard-0.1.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rf_quick_dashboard",
+    version="0.1.4",
+    packages=find_packages(),
+    py_modules=["quick_report"],
+    include_package_data=True,
+    install_requires=[
+        "pandas",
+        "plotly",
+        "lxml",
+    ],
+    entry_points={
+        "console_scripts": [
+            "generate_dashboard=quick_report:main",
+        ],
+    },
+    author="Chandan Mishra",
+    author_email="testautomasi@gmail.com",
+    description="A package to generate a simple html dashboard report for Robot Framework using junit xml output",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
