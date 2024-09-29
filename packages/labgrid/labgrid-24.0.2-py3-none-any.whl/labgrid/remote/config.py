336ba76f7e2bafd975ef690e708f588c86562a86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-29 04:06:38 +0000

    Add package labgrid/24.0.2/labgrid-24.0.2-py3-none-any.whl

---
+++ packages/labgrid/labgrid-24.0.2-py3-none-any.whl/labgrid/remote/config.py
@@ -0,0 +1,31 @@
+from pprint import pprint
+
+import os
+
+import attr
+import jinja2
+
+from ..util.yaml import load
+from ..exceptions import NoConfigFoundError
+
+
+@attr.s(eq=False)
+class ResourceConfig:
+    filename = attr.ib(validator=attr.validators.instance_of(str))
+    template_env = attr.ib(default=attr.Factory(dict), validator=attr.validators.instance_of(dict))
+
+    def __attrs_post_init__(self):
+        env = jinja2.Environment(
+            loader=jinja2.FileSystemLoader(os.path.dirname(self.filename)),
+            line_statement_prefix="#",
+            line_comment_prefix="##",
+        )
+        try:
+            with open(self.filename) as file:
+                template = env.from_string(file.read())
+        except FileNotFoundError:
+            raise NoConfigFoundError(f"{self.filename} could not be found")
+        rendered = template.render(self.template_env)
+        pprint(("rendered", rendered))
+        self.data = load(rendered)
+        pprint(("loaded", self.data))
