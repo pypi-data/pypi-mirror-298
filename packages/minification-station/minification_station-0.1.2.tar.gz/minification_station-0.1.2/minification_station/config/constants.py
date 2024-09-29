23c03f809b031b04f2c95f52272deb2329f5a13d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-29 03:17:53 +0000

    Add package minification-station/0.1.2/minification_station-0.1.2.tar.gz

---
+++ packages/minification-station/minification_station-0.1.2.tar.gz/minification_station-0.1.2/minification_station/config/constants.py
@@ -0,0 +1,5 @@
+# Constants
+DEFAULT_FILE_EXTENSION = ".py"
+DEFAULT_IGNORE_FILES: list[str] = [".python-version"]
+DEFAULT_IGNORE_FOLDERS: list[str] = [".git", ".vscode", "venv", ".github", "__pycache__", "logs"]
+DEFAULT_FILE_SIZE_LIMIT = 10 * 1024 * 1024  # 10 MB
