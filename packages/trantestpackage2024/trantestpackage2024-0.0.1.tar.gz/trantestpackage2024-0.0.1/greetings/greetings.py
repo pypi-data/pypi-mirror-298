dadbf062c6aa0a02febfc52a783090f4c3bc9268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-29 14:06:22 +0000

    Add package trantestpackage2024/0.0.1/trantestpackage2024-0.0.1.tar.gz

---
+++ packages/trantestpackage2024/trantestpackage2024-0.0.1.tar.gz/trantestpackage2024-0.0.1/greetings/greetings.py
@@ -0,0 +1,6 @@
+def hi(name: str) -> None:
+    print(f"hi {name}")
+
+
+def hello(name: str) -> None:
+    print(f"hello {name}")
---
+++ packages/trantestpackage2024/trantestpackage2024-0.0.1.tar.gz/trantestpackage2024-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 11',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='trantestpackage2024',
+    version='0.0.1',
+    description='Test Python Package',
+    long_description='Test Python Package Long Description',
+    url='',
+    author='Tran',
+    author_email='tuanlearnoauth2@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='greetings',
+    packages=find_packages(),
+    install_requires=['']
+)
