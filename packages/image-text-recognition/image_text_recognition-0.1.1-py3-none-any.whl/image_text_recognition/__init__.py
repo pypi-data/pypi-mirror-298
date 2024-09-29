161d24f50cb04160cd1676cdc5c58b61f6201561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-29 03:13:42 +0000

    Add package image-text-recognition/0.1.1/image_text_recognition-0.1.1-py3-none-any.whl

---
+++ packages/image-text-recognition/image_text_recognition-0.1.1-py3-none-any.whl/image_text_recognition/__init__.py
@@ -0,0 +1,5 @@
+from .image_text_recognition import ImageTextRecognizer
+from .evaluate import evaluate_model
+from .generate import generate_test_images
+
+__all__ = ['ImageTextRecognizer', 'evaluate_model', 'generate_test_images']
