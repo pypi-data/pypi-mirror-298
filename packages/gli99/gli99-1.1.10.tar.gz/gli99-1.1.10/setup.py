ac4e22a6950fd539e17f70207826aa1eb71b9483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-29 03:14:29 +0000

    Add package gli99/1.1.10/gli99-1.1.10.tar.gz

---
+++ packages/gli99/gli99-1.1.10.tar.gz/gli99-1.1.10/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gli99",
+    version='1.1.10',
+    description="Web scraper for gifcities.org",
+    package_dir={'':'src'},
+    packages=setuptools.find_packages(where="src"),
+    include_package_data=True,
+    python_requires=">3.10.0",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'selenium',
+        'requests',
+        'webdriver-manager',
+    ],
+    url=f"https://github.com/TomTkacz/gli99",
+    author="Tom Tkacz",
+    author_email="thomasatkacz@gmail.com",
+)
