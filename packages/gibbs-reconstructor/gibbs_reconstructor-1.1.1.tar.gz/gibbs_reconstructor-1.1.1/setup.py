930181ad7ea6eec0704858e64dffec21fc27f0b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-29 14:06:11 +0000

    Add package gibbs-reconstructor/1.1.1/gibbs_reconstructor-1.1.1.tar.gz

---
+++ packages/gibbs-reconstructor/gibbs_reconstructor-1.1.1.tar.gz/gibbs_reconstructor-1.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gibbs-reconstructor",
+    version="1.1.1",
+    author="Félix Laplante",
+    author_email="flheight0@gmail.com",
+    description="Gibbs Reconstruction for linear data reconstruction",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/flheight/gibbs-reconstructor-pypi/",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "tqdm",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.8',
+)
