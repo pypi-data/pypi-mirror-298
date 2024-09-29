589413746900a66cc36190ad4fcae3333cf7eba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-29 14:06:05 +0000

    Add package automata-building-management/1.0.1/automata_building_management-1.0.1.tar.gz

---
+++ packages/automata-building-management/automata_building_management-1.0.1.tar.gz/automata_building_management-1.0.1/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+# Read the contents of your README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="automata-building-management",
+    version="1.0.1",
+    author="A. Jewell Sr",
+    author_email="your.email@example.com",
+    description="Automata Building Management & HVAC Controller - Advanced IoT-driven solution for commercial and industrial applications",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AutomataControls/AutomataBuildingManagment-HvacController",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "pillow",
+        # Add other dependencies here if needed
+    ],
+    include_package_data=True,  # Include data files as specified in MANIFEST.in
+    package_data={
+        # Define the directories and file types to be included in the package
+        'automata': [
+            'data/*',              # Include all files in 'data' directory
+            'NodeRedLogic/*.json', # Include all JSON files in 'NodeRedLogic' directory
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            # If you want to define custom commands that can be run from the console,
+            # specify them here. Uncomment and modify if applicable.
+            # 'automata-start=automata.some_module:start_function',
+        ],
+    },
+)
