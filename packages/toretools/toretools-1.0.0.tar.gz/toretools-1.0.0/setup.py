460b1c12050d3929272fc23744f3730eb9a3c5de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-29 14:06:03 +0000

    Add package toretools/1.0.0/toretools-1.0.0.tar.gz

---
+++ packages/toretools/toretools-1.0.0.tar.gz/toretools-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="toretools",
+    version="1.0.0",
+    author="Torrez Tsoi",
+    author_email="that1.stinkyarmpits@gmail.com",
+    description="Useful functions and classes for creating mathematical or scientific programs!",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+    install_requires=["echowave", "yt_dlp"],
+    include_package_data=True,
+)
---
+++ packages/toretools/toretools-1.0.0.tar.gz/toretools-1.0.0/toretools/__version__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+#
+# ToreTools - The useful tools for creating mathematical programs and more.
+#
+# Torrez Tsoi
+# that1.stinkyarmpits@gmail.com
+#
+# License: MIT
+#
+
+__title__ = "toretools"
+__license__ = "MIT"
+__version__ = "1.0.0"
+__author__ = "Torrez Tsoi"
+__contact__ = "that1.stinkyarmpits@gmail.com"
+__description__ = "Use various tools to create mathematical or scientific programs!"
