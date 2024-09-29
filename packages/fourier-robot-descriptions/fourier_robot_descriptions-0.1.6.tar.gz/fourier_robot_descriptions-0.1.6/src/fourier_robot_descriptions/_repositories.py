56be20eff6c2c095dc1a1f6f42725fcd934dc076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-29 15:05:58 +0000

    Add package fourier-robot-descriptions/0.1.6/fourier_robot_descriptions-0.1.6.tar.gz

---
+++ packages/fourier-robot-descriptions/fourier_robot_descriptions-0.1.6.tar.gz/fourier_robot_descriptions-0.1.6/src/fourier_robot_descriptions/_repositories.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2022 Stéphane Caron
+
+"""Git utility functions to clone model repositories."""
+
+from dataclasses import dataclass
+from typing import Dict
+
+
+@dataclass
+class Repository:
+    """Remote git repository.
+
+    Attributes:
+        cache_path: Path to clone the repository to in the local cache.
+        commit: Commit ID or tag to checkout after cloning.
+        url: URL to the remote git repository.
+    """
+
+    cache_path: str
+    commit: str
+    url: str
+
+
+REPOSITORIES: Dict[str, Repository] = {
+    "fourier_grx_descriptions": Repository(
+        url="https://gitee.com/FourierIntelligence/wiki-grx-models.git",
+        commit="9fa0d3430776ee242d06860ac268710c1c7c2153",
+        cache_path="fourier_grx_descriptions",
+    ),
+    "fourier_grx_descriptions_private": Repository(
+        url="https://gitee.com/FourierIntelligence/Fourier_Models.git",
+        commit="e6a96ce13932cfa48144fd03156345a98fcf56ff",
+        cache_path="fourier_grx_descriptions_private",
+    ),
+}
