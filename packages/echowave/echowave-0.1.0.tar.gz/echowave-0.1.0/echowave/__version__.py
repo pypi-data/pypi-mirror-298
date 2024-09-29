27582e0ecdf44956ce3c53aca28183c9cc0cc423
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-29 04:05:54 +0000

    Add package echowave/0.1.0/echowave-0.1.0.tar.gz

---
+++ packages/echowave/echowave-0.1.0.tar.gz/echowave-0.1.0/echowave/__version__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# License: MIT
+#
+
+__title__ = "echowave"
+__license__ = "MIT"
+__version__ = "0.1.0"
+__author__ = "Torrez Tsoi"
+__contact__ = "that1.stinkyarmpits@gmail.com"
+__description__ = (
+    "Never use print() to debug again; inspect variables, expressions, and "
+    "program execution with a single, simple function call."
+)
---
+++ packages/echowave/echowave-0.1.0.tar.gz/echowave-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="echowave",
+    version="0.1.0",
+    author="Torrez Tsoi",
+    author_email="that1.stinkyarmpits@gmail.com",
+    description="A cloned version of the IceCream package that replaces print() with ic() when debugging.",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+    install_requires=[
+        # Add your dependencies here, for example:
+        # "requests",
+    ],
+    include_package_data=True,
+)
