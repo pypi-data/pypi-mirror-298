43a54e428c00146bd2a4a7593abf4202012770cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-29 15:06:06 +0000

    Add package that-depends/1.19.6/that_depends-1.19.6-py3-none-any.whl

---
+++ packages/that-depends/that_depends-1.19.6-py3-none-any.whl/that_depends/providers/resources.py
@@ -0,0 +1,45 @@
+import typing
+import warnings
+
+from that_depends.providers.base import AbstractResource, ResourceContext
+
+
+T = typing.TypeVar("T")
+P = typing.ParamSpec("P")
+
+
+class Resource(AbstractResource[T]):
+    __slots__ = (
+        "_is_async",
+        "_creator",
+        "_args",
+        "_kwargs",
+        "_override",
+        "_context",
+    )
+
+    def __init__(
+        self,
+        creator: typing.Callable[P, typing.Iterator[T] | typing.AsyncIterator[T]],
+        *args: P.args,
+        **kwargs: P.kwargs,
+    ) -> None:
+        super().__init__(creator, *args, **kwargs)
+        self._context: typing.Final[ResourceContext[T]] = ResourceContext(is_async=self._is_async)
+
+    def _fetch_context(self) -> ResourceContext[T]:
+        return self._context
+
+    async def tear_down(self) -> None:
+        await self._fetch_context().tear_down()
+
+
+class AsyncResource(Resource[T]):
+    def __init__(
+        self,
+        creator: typing.Callable[P, typing.AsyncIterator[T]],
+        *args: P.args,
+        **kwargs: P.kwargs,
+    ) -> None:
+        warnings.warn("AsyncResource is deprecated, use Resource instead", RuntimeWarning, stacklevel=1)
+        super().__init__(creator, *args, **kwargs)
