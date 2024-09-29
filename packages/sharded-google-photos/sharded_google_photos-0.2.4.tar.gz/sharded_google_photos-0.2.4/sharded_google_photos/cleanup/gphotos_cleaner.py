f95d2976036852173a1f347b96d4c9a6821b40b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-29 14:06:05 +0000

    Add package sharded-google-photos/0.2.4/sharded_google_photos-0.2.4.tar.gz

---
+++ packages/sharded-google-photos/sharded_google_photos-0.2.4.tar.gz/sharded_google_photos-0.2.4/sharded_google_photos/cleanup/gphotos_cleaner.py
@@ -0,0 +1,86 @@
+import logging
+from event_bus import EventBus
+
+from . import events
+from sharded_google_photos.shared.gphotos_client import GPhotosClient
+
+
+logger = logging.getLogger(__name__)
+
+
+class GPhotosCleaner:
+    """
+    A class that is responsible for cleaning up and deleting photos that are no longer
+    in albums anymore.
+    """
+
+    def __init__(self, gphoto_client: GPhotosClient, event_bus: EventBus = None):
+        self.gphoto_client = gphoto_client
+        self.event_bus = event_bus if event_bus is not None else EventBus()
+
+    def mark_unalbumed_photos_to_trash(self) -> None:
+        """
+        Finds all of the photos that are not in an album, and puts it in a
+        dedicated "Trash" album to be deleted by the user manually.
+        """
+
+        # Find the trash album
+        logger.debug("Step 1: Find the trash album, and if not, create one")
+        trash_album = None
+        for album in self.gphoto_client.albums().list_albums():
+            if album["title"] == "Trash":
+                trash_album = album
+
+        if trash_album is None:
+            logger.debug("No trash album found. Creating new trash album")
+            trash_album = self.gphoto_client.albums().create_album("Trash")
+            self.event_bus.emit(events.CREATED_TRASH_ALBUM, trash_album["id"])
+        else:
+            self.event_bus.emit(events.FOUND_TRASH_ALBUM, trash_album)
+
+        logger.debug(f"Trash album: {trash_album['id']}")
+
+        # Find all of the media item ids in all shared albums
+        logger.debug("Step 2: Find all media item ids in shared albums")
+        media_item_ids_in_albums = set()
+        for shared_album in self.gphoto_client.albums().list_shared_albums():
+            shared_album_id = shared_album["id"]
+            for media_items in self.gphoto_client.media_items().search_for_media_items(
+                shared_album_id
+            ):
+                media_item_ids_in_albums.add(media_items["id"])
+
+        logger.debug(f"Media item ids in albums: {media_item_ids_in_albums}")
+        self.event_bus.emit(
+            events.FOUND_MEDIA_ITEMS_IN_ALBUMS, media_item_ids_in_albums
+        )
+
+        # Go through all of the media items, and if they are not in albums, move it to trash
+        logger.debug(
+            "Step 3: Find all media item ids not in a shared album, and trash them"
+        )
+        media_item_ids_to_trash = []
+        for media_item in self.gphoto_client.media_items().search_for_media_items():
+            media_item_id = media_item["id"]
+            if media_item_id not in media_item_ids_in_albums:
+                media_item_ids_to_trash.append(media_item_id)
+
+        if len(media_item_ids_to_trash) > 0:
+            self.__add_photos_to_album_safely(
+                trash_album["id"], media_item_ids_to_trash
+            )
+
+        logger.debug(f"Media item ids moved to trash: {len(media_item_ids_to_trash)}")
+        self.event_bus.emit(events.ADDED_MEDIA_ITEMS_TO_TRASH, media_item_ids_to_trash)
+
+    def __add_photos_to_album_safely(self, album_id: str, media_item_ids: list[str]):
+        MAX_MEDIA_ITEMS_LENGTH_PER_CALL = 50
+
+        for i in range(0, len(media_item_ids), MAX_MEDIA_ITEMS_LENGTH_PER_CALL):
+            chunked_media_item_ids = media_item_ids[
+                i : i + MAX_MEDIA_ITEMS_LENGTH_PER_CALL
+            ]
+
+            self.gphoto_client.albums().add_photos_to_album(
+                album_id, chunked_media_item_ids
+            )
---
+++ packages/sharded-google-photos/sharded_google_photos-0.2.4.tar.gz/sharded_google_photos-0.2.4/sharded_google_photos/shared/gphotos_client.py
@@ -0,0 +1,119 @@
+import json
+import logging
+import backoff
+from requests.exceptions import RequestException
+
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import AuthorizedSession
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+from sharded_google_photos.shared.gphotos_album_client import GPhotosAlbumClient
+from sharded_google_photos.shared.gphotos_mediaitem_client import GPhotosMediaItemClient
+
+logger = logging.getLogger(__name__)
+
+DEFAULT_CLIENT_SECRETS_FILE = "client_secret.json"
+DEFAULT_SCOPES = [
+    "https://www.googleapis.com/auth/photoslibrary.readonly",
+    "https://www.googleapis.com/auth/photoslibrary.appendonly",
+    "https://www.googleapis.com/auth/photoslibrary.sharing",
+    "https://www.googleapis.com/auth/photoslibrary.edit.appcreateddata",
+    "https://www.googleapis.com/auth/photoslibrary",
+    "https://www.googleapis.com/auth/drive.photos.readonly",
+]
+
+
+class GPhotosClient:
+    def __init__(
+        self,
+        name,
+        creds_file,
+        client_secret=DEFAULT_CLIENT_SECRETS_FILE,
+        scopes=DEFAULT_SCOPES,
+    ):
+        self.name = name
+        self.creds_file = creds_file
+        self.client_secret = client_secret
+        self.scopes = scopes
+
+        self.session: AuthorizedSession = None
+        self._albums_client: GPhotosAlbumClient = None
+        self._media_items_client: GPhotosMediaItemClient = None
+
+    def authenticate(self):
+        credentials = None
+        try:
+            credentials = self.__get_saved_credentials__()
+        except Exception:
+            logger.debug("Failed to get saved credentials")
+            logger.debug("Fetching credentials")
+            credentials = self.__get_credentials_via_oauth__()
+
+        self.__save_credentials__(credentials)
+        self.session = AuthorizedSession(credentials)
+
+        self._albums_client = GPhotosAlbumClient(self.session)
+        self._media_items_client = GPhotosMediaItemClient(self.session)
+
+    def __get_saved_credentials__(self):
+        """Read in any saved OAuth data/tokens"""
+        fileData = {}
+        with open(self.creds_file, "r") as file:
+            fileData = json.load(file)
+
+        if fileData is None:
+            raise Exception(f"Creds file {self.creds_file} is empty")
+
+        if "refresh_token" not in fileData:
+            raise Exception(f"Creds file {self.creds_file} has no refresh token")
+
+        if "client_id" not in fileData:
+            raise Exception(f"Creds file {self.creds_file} has no client id")
+
+        if "client_secret" not in fileData:
+            raise Exception(f"Creds file {self.creds_file} has no client secret")
+
+        logger.debug(f"Obtained saved credentials from {self.creds_file}")
+        return Credentials(**fileData)
+
+    def __save_credentials__(self, credentials: Credentials):
+        fileData = {
+            "refresh_token": credentials.refresh_token,
+            "token": credentials.token,
+            "client_id": credentials.client_id,
+            "client_secret": credentials.client_secret,
+            "token_uri": credentials.token_uri,
+        }
+        with open(self.creds_file, "w") as file:
+            json.dump(fileData, file)
+
+        logger.debug(f"Credentials serialized to {self.creds_file}")
+
+    def __get_credentials_via_oauth__(self):
+        """Use data in the given filename to get oauth data"""
+        iaflow: InstalledAppFlow = InstalledAppFlow.from_client_secrets_file(
+            self.client_secret, self.scopes
+        )
+        logger.debug("Obtained saved credentials from oauth2 flow")
+        iaflow.run_local_server(
+            authorization_prompt_message=f"For {self.name}, please visit this URL: {{url}}",
+            success_message="The auth flow is complete; you may close this window.",
+            open_browser=False,
+        )
+
+        return iaflow.credentials
+
+    @backoff.on_exception(backoff.expo, (RequestException), max_time=60)
+    def get_storage_quota(self):
+        params = {"fields": "storageQuota"}
+        uri = "https://www.googleapis.com/drive/v3/about"
+        res = self.session.get(uri, params=params)
+        res.raise_for_status()
+
+        return res.json()["storageQuota"]
+
+    def albums(self):
+        return self._albums_client
+
+    def media_items(self):
+        return self._media_items_client
