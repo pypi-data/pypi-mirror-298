2e7251dfb7be5436db3781fcbbf9d58736da50e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-29 15:06:05 +0000

    Add package tphan86-distributions/0.1.0/tphan86_distributions-0.1.0.tar.gz

---
+++ packages/tphan86-distributions/tphan86_distributions-0.1.0.tar.gz/tphan86_distributions-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='tphan86_distributions',
+      version='0.1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['tphan86_distributions'],
+      author = 'Tuan Phan',
+      author_email = 'tuanpa141186@gmail.com',
+      zip_safe=False)
