de5c19e3eeadc0f4eedc26196e32fef4ba71d18e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 04:06:01 +0000

    Add package PyPDFForm/1.4.36/PyPDFForm-1.4.36-py3-none-any.whl

---
+++ packages/PyPDFForm/PyPDFForm-1.4.36-py3-none-any.whl/PyPDFForm/middleware/base.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""Contains widget middleware."""
+
+from typing import Any
+
+
+class Widget:
+    """Base class for all PDF form widgets."""
+
+    def __init__(
+        self,
+        name: str,
+        value: Any = None,
+    ) -> None:
+        """Constructs basic attributes for the object."""
+
+        super().__init__()
+        self._name = name
+        self.value = value
+
+    @property
+    def name(self) -> str:
+        """Name of the widget."""
+
+        return self._name
+
+    @property
+    def schema_definition(self) -> dict:
+        """Json schema definition of the widget."""
+
+        raise NotImplementedError
+
+    @property
+    def sample_value(self) -> Any:
+        """Sample value of the widget."""
+
+        raise NotImplementedError
---
+++ packages/PyPDFForm/PyPDFForm-1.4.36-py3-none-any.whl/PyPDFForm/middleware/text.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""Contains text middleware."""
+
+from .base import Widget
+
+
+class Text(Widget):
+    """A class to represent a text field widget."""
+
+    def __init__(
+        self,
+        name: str,
+        value: str = None,
+    ) -> None:
+        """Constructs all attributes for the text field."""
+
+        super().__init__(name, value)
+
+        self.font = None
+        self.font_size = None
+        self.font_color = None
+        self.text_wrap_length = None
+        self.max_length = None
+        self.comb = None
+        self.character_paddings = []
+        self.text_lines = None
+        self.text_line_x_coordinates = None
+        self.preview = False
+
+    @property
+    def schema_definition(self) -> dict:
+        """Json schema definition of the text field."""
+
+        result = {"type": "string"}
+
+        if self.max_length is not None:
+            result["maxLength"] = self.max_length
+
+        return result
+
+    @property
+    def sample_value(self) -> str:
+        """Sample value of the text field."""
+
+        return (
+            self.name[: self.max_length] if self.max_length is not None else self.name
+        )
