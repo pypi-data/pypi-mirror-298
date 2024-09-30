ca187b488e73bfc562b8c102d59719ef6e53632f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 04:05:54 +0000

    Add package streamlit-marshal-helloworld/0.0.6/streamlit-marshal-helloworld-0.0.6.tar.gz

---
+++ packages/streamlit-marshal-helloworld/streamlit-marshal-helloworld-0.0.6.tar.gz/streamlit-marshal-helloworld-0.0.6/streamlit_marshal_helloworld/__init__.py
@@ -0,0 +1,25 @@
+import os
+import streamlit.components.v1 as components
+
+# _RELEASE = False
+_RELEASE = True
+
+if not _RELEASE:
+  _component_func = components.declare_component(
+    "my_component",
+    url="http://localhost:5173", # vite dev server port
+  )
+else:
+  parent_dir = os.path.dirname(os.path.abspath(__file__))
+  build_dir = os.path.join(parent_dir, "frontend/dist")
+  _component_func = components.declare_component("my_component", path=build_dir)
+
+def my_component(key=None):
+  component_value = _component_func(key=key, default=0)
+  return component_value
+
+if not _RELEASE:
+  import streamlit as st
+  st.subheader("组件测试")
+  num_clicks = my_component()
+  st.markdown(f"点击了 {num_clicks} 次")
