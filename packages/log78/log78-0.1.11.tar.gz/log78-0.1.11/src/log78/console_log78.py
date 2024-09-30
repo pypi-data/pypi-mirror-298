e3b7f1455fba7919a865d3d818e24c6b469d31ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 04:05:32 +0000

    Add package log78/0.1.11/log78-0.1.11.tar.gz

---
+++ packages/log78/log78-0.1.11.tar.gz/log78-0.1.11/src/log78/console_log78.py
@@ -0,0 +1,20 @@
+import logging
+import sys
+from .log_entry import LogEntry
+
+class ConsoleLog78:
+    def __init__(self):
+        self._logger = logging.getLogger(__name__)
+        self._logger.setLevel(logging.INFO)
+        handler = logging.StreamHandler(sys.stdout)  # 使用 sys.stdout
+        formatter = logging.Formatter('%(message)s')
+        handler.setFormatter(formatter)
+        self._logger.addHandler(handler)
+
+    def write_line(self, log_entry: LogEntry):
+        self._logger.info(log_entry.to_json())
+
+    def __del__(self):
+        for handler in self._logger.handlers[:]:
+            self._logger.removeHandler(handler)
+            handler.close()
