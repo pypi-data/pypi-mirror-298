f30396cca2c7e0b36b8b846b171dcc16d6b77447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 03:13:54 +0000

    Add package logaid/1.2.0/logaid-1.2.0-py3-none-any.whl

---
+++ packages/logaid/logaid-1.2.0-py3-none-any.whl/logaid/__init__.py
@@ -0,0 +1,4 @@
+from .log import debug, info, warning, error,fatal,critical
+from . import log as logger
+from . import log as Logger
+__all__ = ['debug', 'info', 'warning', 'error','fatal','critical','logger','Logger']
