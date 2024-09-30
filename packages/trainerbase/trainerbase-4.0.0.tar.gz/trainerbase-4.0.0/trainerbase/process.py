2abc9ab9d1492d8debebb79ec0f6b0d7baddec9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 03:15:47 +0000

    Add package trainerbase/4.0.0/trainerbase-4.0.0.tar.gz

---
+++ packages/trainerbase/trainerbase-4.0.0.tar.gz/trainerbase-4.0.0/trainerbase/process.py
@@ -0,0 +1,48 @@
+from contextlib import suppress
+from itertools import cycle
+from os import _exit as force_exit
+from typing import cast
+
+from psutil import Process, pid_exists
+from pymem import Pymem
+from pymem.exception import ProcessNotFound
+
+from trainerbase.config import pymem_config
+
+
+def attach_to_process() -> Pymem:
+    process_names = pymem_config["process_names"]
+
+    if not process_names:
+        raise ValueError("Empty process name list")
+
+    print("Waiting for process in:", process_names)
+
+    with suppress(KeyboardInterrupt):
+        for process_name in cycle(process_names):
+            try:
+                pm = Pymem(
+                    process_name,
+                    exact_match=pymem_config["exact_match"],
+                    ignore_case=pymem_config["ignore_case"],
+                )
+            except ProcessNotFound:
+                continue
+
+            print(f"Found {process_name}, pid: {pm.process_id}")
+
+            return pm
+
+    force_exit(0)
+
+
+def shutdown_if_process_exited():
+    if not pid_exists(cast(int, _pm.process_id)):
+        force_exit(0)
+
+
+def kill_game_process():
+    Process(_pm.process_id).kill()
+
+
+_pm = attach_to_process()
