092fe65472eee4d73356d0b7af4fa8d41ccb5ef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 04:05:19 +0000

    Add package sheet2db-sdk/1.0.2/sheet2db-sdk-1.0.2.tar.gz

---
+++ packages/sheet2db-sdk/sheet2db-sdk-1.0.2.tar.gz/sheet2db-sdk-1.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sheet2db-sdk',
+    version='1.0.2',
+    description='A python library to interact with Google Sheets using sheet2db api',
+    author='Sheet2DB',
+    packages=find_packages(),
+    url="https://sheet2db.com",
+    author_email="support@sheet2db.com",
+    keywords="sheet2db, google sheets, python, sdk",
+    project_urls={  # Optional
+        "Documentation": "https://sheet2db.com/docs/sdks/python",
+    },
+    install_requires=[
+        'requests'
+    ]
+)
