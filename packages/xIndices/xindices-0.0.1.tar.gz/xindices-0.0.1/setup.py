3300b9ad332670acd4643b4003ba3a4b418c905a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 03:17:02 +0000

    Add package xIndices/0.0.1/xindices-0.0.1.tar.gz

---
+++ packages/xIndices/xindices-0.0.1.tar.gz/xindices-0.0.1/setup.py
@@ -0,0 +1,29 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='xIndices',
+    version='0.0.1',
+    description='A xarray based module for computing SST trends and SST variability modes and other atmospheric variability modes',
+    author='Jivesh Dixit',
+    author_email='jiveshdixit@cas.iitd.ac.in',
+    packages=find_packages(),
+    classifiers=[
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Operating System :: OS Independent',
+    'Topic :: Communications :: Email'],
+    install_requires=[
+        'xarray',
+        'numpy',
+        'xesmf',
+        'eofs',
+        'dask',
+        'xesmf',
+        'matplotlib',
+        'cartopy',
+        'xeofs'
+    ],
+    python_requires='>=3.8',
+)
