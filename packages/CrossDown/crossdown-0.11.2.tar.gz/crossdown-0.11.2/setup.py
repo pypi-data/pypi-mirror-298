65283c12c68fe633bcd227ca7e7d3bb15221d7fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 03:16:33 +0000

    Add package CrossDown/0.11.2/crossdown-0.11.2.tar.gz

---
+++ packages/CrossDown/crossdown-0.11.2.tar.gz/crossdown-0.11.2/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="CrossDown",
+    version="0.11.2",
+    author="CrossDark",
+    author_email="liuhanbo333@icloud.com",
+    description="CrossDark's MarkDown",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CrossDark/CrossDown",
+    py_modules=[
+        'CrossDown',
+        'CrossMore',
+    ],
+    install_requires=[
+        'markdown',
+        'matplotlib',
+        'numpy',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'License :: OSI Approved :: MIT License',
+        "Operating System :: OS Independent"
+    ],
+)
