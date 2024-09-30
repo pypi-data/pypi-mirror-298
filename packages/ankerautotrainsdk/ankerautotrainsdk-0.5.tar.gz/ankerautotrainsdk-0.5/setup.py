8e1ff104328f2c5fb917b72fcc732abf2012882e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 03:13:55 +0000

    Add package ankerautotrainsdk/0.5/ankerautotrainsdk-0.5.tar.gz

---
+++ packages/ankerautotrainsdk/ankerautotrainsdk-0.5.tar.gz/ankerautotrainsdk-0.5/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(name='ankerautotrainsdk',
+                 version="0.5",
+                 description='Python Package Boilerplate',
+                 long_description=open('README.md').read().strip(),
+                 author='taco',
+                 author_email='taco.wang@anker-in.com',
+                 url='',
+                 py_modules=['ankerautotrainsdk'],
+                 install_requires=["requests"],
+                 license='MIT License',
+                 zip_safe=False,
+                 keywords='',
+)
