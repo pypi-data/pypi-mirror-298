943011c8a535c55b64f4826498e1683456cc775b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 03:13:53 +0000

    Add package aberoth-ephemeris/1.0.0/aberoth-ephemeris-1.0.0.tar.gz

---
+++ packages/aberoth-ephemeris/aberoth-ephemeris-1.0.0.tar.gz/aberoth-ephemeris-1.0.0/README.md
@@ -0,0 +1,1 @@
+# aberoth-ephemeris
---
+++ packages/aberoth-ephemeris/aberoth-ephemeris-1.0.0.tar.gz/aberoth-ephemeris-1.0.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from os import path
+
+working_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(working_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="aberoth-ephemeris",
+    version="1.0.0",
+    author="Jeremiah Vandagrift",
+    url="https://github.com/jvandag/aberoth-ephemeris",
+    author_email="",
+    description="Provides prediction for scroll and lunar events in the MMO Aberoth",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=["aberoth", "solar system", "orbs", "ephemeris", "predictions", "MMO"],
+    packages=find_packages(),
+    package_data={
+        "aberoth_ephemeris.ephemeris": [".variables.json", "sampleData/glows/*.json", "sampleData/dims/*.json"],
+        "aberoth_ephemeris.UpdateWebServer": ["*.json", "*.html"]
+    },
+    install_requires=[
+    "numpy==1.26.4",
+    "python-dotenv==1.0.1",
+    "Flask==3.0.3",
+    "Flask-Cors==4.0.1",
+    "waitress==3.0.0"
+    ],
+    include_package_data=True,
+)
