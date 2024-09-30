33526371f6fa2bcfc6e6f512108d4b1bc669e3ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 03:15:22 +0000

    Add package rkNseClient/0.0.19/rkNseClient-0.0.19-py3-none-any.whl

---
+++ packages/rkNseClient/rkNseClient-0.0.19-py3-none-any.whl/rkNseClient/schema.py
@@ -0,0 +1,76 @@
+from dataclasses import dataclass, asdict
+import xml.etree.ElementTree as ET
+import json
+
+@dataclass
+class BaseModel:
+    def to_json(self) -> str:
+        """Convert the instance to a JSON string."""
+        return json.dumps(asdict(self), indent=4)
+    
+    def to_xml(self) -> str:
+        """Convert the instance to an XML string."""
+        root = ET.Element(self.__class__.__name__)
+        for field, value in asdict(self).items():
+            child = ET.SubElement(root, field)
+            child.text = str(value)
+        return ET.tostring(root, encoding='unicode', method='xml')
+    
+    def to_dict(self) -> dict:
+        """Convert the instance to a dictionary."""
+        return asdict(self)
+
+@dataclass
+class EquityInfo(BaseModel):
+    symbol: str
+    nameOfCompany: str
+    series: str
+    dateOfListing: str
+    isinNumber: str
+    faceValue: int
+    marketLot: int
+    paidUpValue: int
+
+@dataclass
+class IndexInfo(BaseModel):
+    key: str
+    index: str
+    indexSymbol: str
+    last: float
+    variation: float
+    percentChange: float
+    open: float
+    high: float
+    low: float
+    previousClose: float
+    yearHigh: float
+    yearLow: float
+    indicativeClose: int
+    pe: str
+    pb: str
+    dy: str
+    declines: str
+    advances: str
+    unchanged: str
+    perChange365d: float
+    date365dAgo: str
+    chart365dPath: str
+    date30dAgo: str
+    perChange30d: float
+    chart30dPath: str
+    chartTodayPath: str
+    previousDay: float
+    oneWeekAgo: float
+    oneMonthAgo: float
+    oneYearAgo: float
+
+@dataclass
+class IndexStockWeightageInfo(BaseModel):
+    symbol: str
+    weight: float
+
+@dataclass
+class IndexSectorWeightageInfo(BaseModel):
+    sector: str
+    weight: float
+    stocks: list[IndexStockWeightageInfo]
