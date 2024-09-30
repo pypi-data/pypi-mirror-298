b9211284488a6f10d46c588c5882d83a52fa8dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 04:05:59 +0000

    Add package fastlifeweb/0.15.0/fastlifeweb-0.15.0-py3-none-any.whl

---
+++ packages/fastlifeweb/fastlifeweb-0.15.0-py3-none-any.whl/fastlife/__init__.py
@@ -0,0 +1,33 @@
+from fastapi import Response
+
+from .config import (
+    Configurator,
+    Registry,
+    Settings,
+    configure,
+    resource,
+    resource_view,
+    view_config,
+)
+from .request import Request
+
+# from .request.form_data import model
+from .templates import Template, template
+
+__all__ = [
+    # Config
+    "configure",
+    "Configurator",
+    "template",
+    "Template",
+    "Registry",
+    "Settings",
+    "view_config",
+    "resource",
+    "resource_view",
+    # Model
+    # "model",
+    # Fast API reexport
+    "Request",
+    "Response",
+]
