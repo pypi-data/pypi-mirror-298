713005d52296f33fcac23fe4d519694c0e51e8b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 04:06:01 +0000

    Add package saraApi/1.1.8/saraapi-1.1.8.tar.gz

---
+++ packages/saraApi/saraapi-1.1.8.tar.gz/saraapi-1.1.8/sara/sfw.py
@@ -0,0 +1,70 @@
+from .req import get
+
+def neko():
+    return get("neko")
+
+def foxgirl():
+    return get("sfwfoxes")
+
+def wallpapers():
+    return get('wallpapers')
+
+def mobileWallpapers():
+    return get("mobileWallpapers")
+
+def sad():
+    return get("sad")
+
+def angry():
+    return get("angry")
+
+def work():
+    return get("work")
+
+def yandere():
+    return get("yandere")
+
+def wink():
+    return get("wink")
+
+def vomit():
+    return get("vomit")
+
+def think():
+    return get("think")
+
+def teehee():
+    return get("teehee")
+
+def pout():
+    return get("pout")
+
+def like():
+    return get("like")
+
+def laugh():
+    return get("laugh")
+
+def dance():
+    return get("dance")
+
+def cry():
+    return get("cry")
+
+def bored():
+    return get("bored")
+
+def blush():
+    return get("blush")
+
+def run():
+    return get("run")
+
+def sleep():
+    return get("sleep")
+
+def travel():
+    return get("travel")
+
+def stared():
+    return get("stared")
