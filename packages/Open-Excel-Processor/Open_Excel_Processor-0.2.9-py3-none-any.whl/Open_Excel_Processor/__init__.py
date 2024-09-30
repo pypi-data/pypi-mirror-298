c65bdadfbacd263c22012255d693136fd1622266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 03:17:01 +0000

    Add package Open-Excel-Processor/0.2.9/Open_Excel_Processor-0.2.9-py3-none-any.whl

---
+++ packages/Open-Excel-Processor/Open_Excel_Processor-0.2.9-py3-none-any.whl/Open_Excel_Processor/__init__.py
@@ -0,0 +1,7 @@
+def get_long_excel():
+    from Open_Excel_Processor.main import process_long_excel
+    return process_long_excel
+
+def get_short_excel():
+    from Open_Excel_Processor.main import process_short_excel
+    return process_short_excel
