c6f9fab509f78318f9b522ab865a1a550ec5ad18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 03:14:46 +0000

    Add package oban-classifier/0.1.5/oban_classifier-0.1.5.tar.gz

---
+++ packages/oban-classifier/oban_classifier-0.1.5.tar.gz/oban_classifier-0.1.5/oban_classifier/__init__.py
@@ -0,0 +1,2 @@
+from .classifier import oban_classifier
+
---
+++ packages/oban-classifier/oban_classifier-0.1.5.tar.gz/oban_classifier-0.1.5/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import pathlib
+
+# Get the long description from the README file
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='oban_classifier',  # Your package name
+    version='0.1.5',
+    description='Oban Classifier: A Skorch-based flexible neural network for binary and multiclass classification',
+    long_description=README,  # This will use the content from README.md
+    long_description_content_type="text/markdown",  # Ensure it renders markdown properly on PyPI
+    author='Dr. Volkan OBAN',
+    author_email='volkanobn@gmail.com',
+    #url='https://github.com/yourusername/oban_classifier',  # Your project URL
+    packages=find_packages(),  # Automatically find all packages
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scikit-learn',
+        'skorch',
+        'torch',
+    ],
+     classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+],
+    python_requires='>=3.8',  # Minimum Python version
+)
