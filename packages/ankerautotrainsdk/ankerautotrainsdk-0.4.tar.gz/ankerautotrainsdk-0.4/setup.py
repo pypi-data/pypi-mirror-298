b445bb6b1758a57595d3057d8ec0329db21920ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 03:13:54 +0000

    Add package ankerautotrainsdk/0.4/ankerautotrainsdk-0.4.tar.gz

---
+++ packages/ankerautotrainsdk/ankerautotrainsdk-0.4.tar.gz/ankerautotrainsdk-0.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ankerautotrainsdk",
+    version="0.4",
+    packages=find_packages(),
+    install_requires=[
+        "requests"
+    ],
+    author="taco",
+    author_email="taco.wang@example.com",
+    description="auto train for anker",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_dirs=["."]
+)
